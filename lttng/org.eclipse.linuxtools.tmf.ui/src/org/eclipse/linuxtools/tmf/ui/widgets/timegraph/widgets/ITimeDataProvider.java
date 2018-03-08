6f9e6101ec2627ca094fd0a21dab2b3456b6e88a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:53:37 +0000

    First commited as before_9501_rev4

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/timegraph/widgets/ITimeDataProvider.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/timegraph/widgets/ITimeDataProvider.java
@@ -137,12 +137,6 @@ public interface ITimeDataProvider {
     int getTimeSpace();
 
     /**
-     * @return If the time format is calendar (true) or relative (false)
-     * @deprecated replaced by {@link #getTimeFormat()}
-     */
-    boolean isCalendarFormat();
-
-    /**
      * @return the time format, one of:
      * <ul>
      *   <li>{@link TimeFormat#CALENDAR} absolute time, displayed as year/month/day/hours/minutes/seconds/ms/us/ns
