369da4f874082f4523447aec45b981ac630322d6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:04:50 +0000

    First commited as before_16602_rev1

--- lttng/org.eclipse.linuxtools.tmf.core.tests/shared/org/eclipse/linuxtools/tmf/core/tests/shared/CtfTmfTestTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.core.tests/shared/org/eclipse/linuxtools/tmf/core/tests/shared/CtfTmfTestTrace.java
@@ -32,8 +32,16 @@ public enum CtfTmfTestTrace {
     TRACE2,
     /** Kernel trace with event contexts */
     KERNEL_VM,
+    /** Trace synchronization: source trace */
+    SYNC_SRC,
+    /** Trace synchronization: destination trace */
+    SYNC_DEST,
     /** UST trace with lots of lost events */
-    HELLO_LOST;
+    HELLO_LOST,
+    /** UST trace with lttng-ust-cyg-profile events (aka -finstrument-functions) */
+    CYG_PROFILE,
+    /** UST trace with lttng-ust-cyg-profile-fast events (no address in func_exit) */
+    CYG_PROFILE_FAST;
 
 
     private final String fPath;
