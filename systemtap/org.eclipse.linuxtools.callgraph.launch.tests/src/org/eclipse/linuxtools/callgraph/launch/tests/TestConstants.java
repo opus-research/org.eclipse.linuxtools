71c8b5564302e64e78bacf5e0050a4d999e5d9c8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:20:48 +0000

    First commited as before_12401_rev2

--- systemtap/org.eclipse.linuxtools.callgraph.launch.tests/src/org/eclipse/linuxtools/callgraph/launch/tests/TestConstants.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch.tests/src/org/eclipse/linuxtools/callgraph/launch/tests/TestConstants.java
@@ -4,19 +4,12 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *    Kent Sebastian <ksebasti@redhat.com> - initial API and implementation
+ *     Red Hat - initial API and implementation
  *******************************************************************************/
+package org.eclipse.linuxtools.callgraph.launch.tests;
 
-package org.eclipse.linuxtools.oprofile.launch.tests;
-
-import junit.framework.TestCase;
-
-// Dummy test just so that the launch tests have something
-// running when oprofile does not
-public class TestDummy extends TestCase {
-	public void testPass() {
-		assertTrue(true);
-	}
+public class TestConstants {
+	public static boolean canRunStap = false;
 }
