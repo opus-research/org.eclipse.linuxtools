7a68ae826a617c9d669efbd60e822b83ba492398
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:50:38 +0000

    First commited as after_11392_rev1

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/AbstractDataManipulator.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/AbstractDataManipulator.java
@@ -13,8 +13,16 @@ package org.eclipse.linuxtools.internal.perf;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.PrintStream;
 
+import org.eclipse.debug.core.DebugPlugin;
+import org.eclipse.debug.core.ILaunch;
 import org.eclipse.linuxtools.tools.launch.core.factory.RuntimeProcessFactory;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.IOConsole;
 
 /**
  * This class represents the general flow of a perf command being
@@ -24,6 +32,7 @@ public abstract class AbstractDataManipulator {
 
 	private String text;
 	private String title;
+	private ILaunch launch;
 
 	AbstractDataManipulator (String title) {
 		this.title = title;
@@ -37,11 +46,16 @@ public abstract class AbstractDataManipulator {
 		return title;
 	}
 
+	public void setLaunch (ILaunch launch) {
+		this.launch = launch;
+	}
+
 	public void performCommand(String[] cmd, int fd) {
 		BufferedReader buff = null;
 		BufferedReader bufftmp = null;
 
 		try {
+
 			Process proc = RuntimeProcessFactory.getFactory().exec(cmd, null);
 
 			switch (fd) {
@@ -59,10 +73,13 @@ public abstract class AbstractDataManipulator {
 			}
 
 			StringBuffer strBuff = new StringBuffer();
+			StringBuffer strBuffTmp = new StringBuffer();
 			String line = ""; //$NON-NLS-1$
 
 			// If the buffer is not being consumed, the other one may block.
-			while (bufftmp.readLine() != null) {
+			while ((line = bufftmp.readLine()) != null) {
+				strBuffTmp.append(line);
+				strBuffTmp.append("\n"); //$NON-NLS-1$
 			}
 
 			while ((line = buff.readLine()) != null) {
@@ -70,6 +87,34 @@ public abstract class AbstractDataManipulator {
 				strBuff.append("\n"); //$NON-NLS-1$
 			}
 			text = strBuff.toString();
+
+			if (launch != null) {
+				String configName = launch.getLaunchConfiguration().getName();
+				// Console will try to read from stream so create afterwards
+				// Console will have the configuration name as a substring
+				DebugPlugin.newProcess(launch, proc, ""); //$NON-NLS-1$
+
+				ConsolePlugin plugin = ConsolePlugin.getDefault();
+				IConsoleManager conMan = plugin.getConsoleManager();
+				IConsole[] existing = conMan.getConsoles();
+				IOConsole binaryOutCons = null;
+				PrintStream print;
+
+				// Find the console
+				for (IConsole x : existing) {
+					if (x.getName().contains(configName)) { //$NON-NLS-1$
+						binaryOutCons = (IOConsole) x;
+					}
+				}
+
+				// Get the printstream via the outputstream.
+				// Get ouput stream
+				if (binaryOutCons != null) {
+					OutputStream outputTo = binaryOutCons.newOutputStream();
+					print = new PrintStream(outputTo);
+					print.println(strBuffTmp.toString());
+				}
+			}
 		} catch (IOException e) {
 			text = ""; //$NON-NLS-1$
 		} finally {
