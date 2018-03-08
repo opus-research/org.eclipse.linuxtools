3871de5981ee2ec5426f7260a0f2a849f95fd544
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:04:29 +0000

    First commited as before_11741_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.dashboard/src/org/eclipse/linuxtools/systemtap/ui/dashboard/views/ActiveModuleBrowserView.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.dashboard/src/org/eclipse/linuxtools/systemtap/ui/dashboard/views/ActiveModuleBrowserView.java
@@ -15,7 +15,6 @@ import org.eclipse.jface.action.MenuManager;
 import org.eclipse.jface.action.Separator;
 import org.eclipse.jface.viewers.LabelProvider;
 import org.eclipse.linuxtools.internal.systemtap.ui.dashboard.actions.ActivateGraphAction;
-import org.eclipse.linuxtools.systemtap.graphingapi.core.filters.IDataSetFilter;
 import org.eclipse.linuxtools.systemtap.structures.TreeNode;
 import org.eclipse.linuxtools.systemtap.ui.dashboard.internal.DashboardPlugin;
 import org.eclipse.linuxtools.systemtap.ui.dashboard.structures.ActiveModuleData;
@@ -23,6 +22,7 @@ import org.eclipse.linuxtools.systemtap.ui.dashboard.structures.ActiveModuleTree
 import org.eclipse.linuxtools.systemtap.ui.dashboard.structures.DashboardGraphData;
 import org.eclipse.linuxtools.systemtap.ui.dashboard.structures.DashboardModule;
 import org.eclipse.linuxtools.systemtap.ui.dashboard.structures.GraphTreeNode;
+import org.eclipse.linuxtools.systemtap.ui.graphingapi.nonui.filters.IDataSetFilter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
@@ -40,6 +40,9 @@ import org.eclipse.ui.PlatformUI;
  * @author Ryan Morse
  */
 public class ActiveModuleBrowserView extends ModuleView {
+	public ActiveModuleBrowserView() {
+		super();
+	}
 
 	@Override
 	protected void generateModuleTree() {
