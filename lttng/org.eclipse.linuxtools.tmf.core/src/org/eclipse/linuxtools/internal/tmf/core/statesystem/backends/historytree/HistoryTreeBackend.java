7e8de6ed196e30202be24409e2c21db9a2798577
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:30:49 +0000

    First commited as before_10569_rev1

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/backends/historytree/HistoryTreeBackend.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/backends/historytree/HistoryTreeBackend.java
@@ -45,7 +45,7 @@ public class HistoryTreeBackend implements IStateHistoryBackend {
     protected boolean isFinishedBuilding = false;
 
     /**
-     * Constructor for new history files. Use this when creating a new history
+     * Construtor for new history files. Use this when creating a new history
      * from scratch.
      *
      * @param newStateFile
@@ -56,43 +56,33 @@ public class HistoryTreeBackend implements IStateHistoryBackend {
      *            multiple of 4096.
      * @param maxChildren
      *            The maximum number of children each core node can have
-     * @param providerVersion
-     *            Version of of the state provider. We will only try to reopen
-     *            existing files if this version matches the one in the
-     *            framework.
      * @param startTime
      *            The earliest time stamp that will be stored in the history
      * @throws IOException
      *             Thrown if we can't create the file for some reason
      */
     public HistoryTreeBackend(File newStateFile, int blockSize,
-            int maxChildren, int providerVersion, long startTime) throws IOException {
-        final HTConfig conf = new HTConfig(newStateFile, blockSize, maxChildren,
-                providerVersion, startTime);
-        sht = new HistoryTree(conf);
+            int maxChildren, long startTime) throws IOException {
+        sht = new HistoryTree(newStateFile, blockSize, maxChildren, startTime);
         treeIO = sht.getTreeIO();
     }
 
     /**
-     * Constructor for new history files. Use this when creating a new history
+     * Construtor for new history files. Use this when creating a new history
      * from scratch. This version supplies sane defaults for the configuration
      * parameters.
      *
      * @param newStateFile
      *            The filename/location where to store the state history (Should
      *            end in .ht)
-     * @param providerVersion
-     *            Version of of the state provider. We will only try to reopen
-     *            existing files if this version matches the one in the
-     *            framework.
      * @param startTime
      *            The earliest time stamp that will be stored in the history
      * @throws IOException
      *             Thrown if we can't create the file for some reason
      */
-    public HistoryTreeBackend(File newStateFile, int providerVersion, long startTime)
+    public HistoryTreeBackend(File newStateFile, long startTime)
             throws IOException {
-        this(newStateFile, 64 * 1024, 50, providerVersion, startTime);
+        this(newStateFile, 64 * 1024, 50, startTime);
     }
 
     /**
@@ -100,16 +90,12 @@ public class HistoryTreeBackend implements IStateHistoryBackend {
      *
      * @param existingStateFile
      *            Filename/location of the history we want to load
-     * @param providerVersion
-     *            Expected version of of the state provider plugin.
      * @throws IOException
-     *             If we can't read the file, if it doesn't exist, is not
-     *             recognized, or if the version of the file does not match the
-     *             expected providerVersion.
+     *             If we can't read the file, if it doesn't exist or is not
+     *             recognized
      */
-    public HistoryTreeBackend(File existingStateFile, int providerVersion)
-            throws IOException {
-        sht = new HistoryTree(existingStateFile, providerVersion);
+    public HistoryTreeBackend(File existingStateFile) throws IOException {
+        sht = new HistoryTree(existingStateFile);
         treeIO = sht.getTreeIO();
         isFinishedBuilding = true;
     }
--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/TmfExperiment.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/TmfExperiment.java
@@ -22,6 +22,9 @@ import org.eclipse.linuxtools.internal.tmf.core.trace.TmfExperimentContext;
 import org.eclipse.linuxtools.internal.tmf.core.trace.TmfExperimentLocation;
 import org.eclipse.linuxtools.internal.tmf.core.trace.TmfLocationArray;
 import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.ITmfTimestamp;
+import org.eclipse.linuxtools.tmf.core.event.TmfTimeRange;
+import org.eclipse.linuxtools.tmf.core.event.TmfTimestamp;
 import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
 import org.eclipse.linuxtools.tmf.core.request.ITmfDataRequest;
 import org.eclipse.linuxtools.tmf.core.request.ITmfEventRequest;
@@ -29,9 +32,6 @@ import org.eclipse.linuxtools.tmf.core.signal.TmfClearExperimentSignal;
 import org.eclipse.linuxtools.tmf.core.signal.TmfSignalHandler;
 import org.eclipse.linuxtools.tmf.core.signal.TmfTraceOpenedSignal;
 import org.eclipse.linuxtools.tmf.core.signal.TmfTraceRangeUpdatedSignal;
-import org.eclipse.linuxtools.tmf.core.timestamp.ITmfTimestamp;
-import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimeRange;
-import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimestamp;
 
 /**
  * TmfExperiment presents a time-ordered, unified view of a set of ITmfTrace:s
@@ -80,25 +80,8 @@ public class TmfExperiment extends TmfTrace implements ITmfEventParser {
      * @param traces the experiment set of traces
      */
     public TmfExperiment(final Class<? extends ITmfEvent> type, final String id, final ITmfTrace[] traces) {
-        this(type, id, traces, DEFAULT_INDEX_PAGE_SIZE, null);
+        this(type, id, traces, DEFAULT_INDEX_PAGE_SIZE);
     }
-
-    /**
-     * Constructor of experiment taking type, path, traces and resource
-     *
-     * @param type
-     *            the event type
-     * @param id
-     *            the experiment id
-     * @param traces
-     *            the experiment set of traces
-     * @param resource
-     *            the resource associated to the experiment
-     */
-    public TmfExperiment(final Class<? extends ITmfEvent> type, final String id, final ITmfTrace[] traces, IResource resource) {
-        this(type, id, traces, DEFAULT_INDEX_PAGE_SIZE, resource);
-    }
-
 
     /**
      * @param type the event type
@@ -107,31 +90,12 @@ public class TmfExperiment extends TmfTrace implements ITmfEventParser {
      * @param indexPageSize the experiment index page size
      */
     public TmfExperiment(final Class<? extends ITmfEvent> type, final String path, final ITmfTrace[] traces, final int indexPageSize) {
-        this(type, path, traces, indexPageSize, null);
-    }
-
-    /**
-     * Full constructor of an experiment, taking the type, path, traces,
-     * indexPageSize and resource
-     *
-     * @param type
-     *            the event type
-     * @param path
-     *            the experiment path
-     * @param traces
-     *            the experiment set of traces
-     * @param indexPageSize
-     *            the experiment index page size
-     * @param resource
-     *            the resource associated to the experiment
-     */
-    public TmfExperiment(final Class<? extends ITmfEvent> type, final String path, final ITmfTrace[] traces, final int indexPageSize, IResource resource) {
         setCacheSize(indexPageSize);
         setStreamingInterval(0);
         setIndexer(new TmfCheckpointIndexer(this, indexPageSize));
         setParser(this);
         try {
-            super.initialize(resource, path, type);
+            super.initialize(null, path, type);
         } catch (TmfTraceException e) {
             e.printStackTrace();
         }
@@ -206,7 +170,6 @@ public class TmfExperiment extends TmfTrace implements ITmfEventParser {
      *
      * @param index the event index (rank)
      * @return the corresponding event timestamp
-     * @since 2.0
      */
     public ITmfTimestamp getTimestamp(final int index) {
         final ITmfContext context = seekEvent(index);
