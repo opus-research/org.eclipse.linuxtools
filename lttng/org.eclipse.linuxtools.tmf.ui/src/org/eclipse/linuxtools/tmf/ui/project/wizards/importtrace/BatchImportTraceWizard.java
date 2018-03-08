66153988ef3e6da4e52279a8d10397d112eeefee
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:34:56 +0000

    First commited as after_12899_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/BatchImportTraceWizard.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/BatchImportTraceWizard.java
@@ -85,7 +85,7 @@ public class BatchImportTraceWizard extends ImportTraceWizard {
     private final List<String> fTraceTypesToScan = new ArrayList<String>();
     private final Set<String> fParentFilesToScan = new HashSet<String>();
 
-    private ImportTraceContentProvider fScannedTraces = new ImportTraceContentProvider();
+    private ImportTraceContentProvider fScannedTraces = new ImportTraceContentProvider(fTraceTypesToScan, fParentFilesToScan);
 
     private final Map<TraceValidationHelper, Boolean> fResults = new HashMap<TraceValidationHelper, Boolean>();
     private boolean fOverwrite = true;
@@ -550,6 +550,7 @@ public class BatchImportTraceWizard extends ImportTraceWizard {
             updateScanQueue(pm, filesToScan, added);
         } catch (InterruptedException e) {
         }
+
     }
 
     /*
@@ -616,12 +617,14 @@ public class BatchImportTraceWizard extends ImportTraceWizard {
                         fTracesToScan.put(tv);
                         monitor.subTask(tv.getTraceToScan());
                         if (monitor.isCanceled()) {
+                            ((ImportTraceWizardScanPage)fScanPage).refresh();
                             return CANCEL_STATUS;
                         }
                     }
                 }
             }
         }
+        ((ImportTraceWizardScanPage)fScanPage).refresh();
         return Status.OK_STATUS;
     }
 
