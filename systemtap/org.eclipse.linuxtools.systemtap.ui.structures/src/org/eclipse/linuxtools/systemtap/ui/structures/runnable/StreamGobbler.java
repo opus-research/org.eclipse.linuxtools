63352c295e7337ccd83d8e6facf3653e11989ace
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:21:28 +0000

    First commited as before_8702_rev3

--- systemtap/org.eclipse.linuxtools.systemtap.ui.structures/src/org/eclipse/linuxtools/systemtap/ui/structures/runnable/StreamGobbler.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.structures/src/org/eclipse/linuxtools/systemtap/ui/structures/runnable/StreamGobbler.java
@@ -17,6 +17,8 @@ import java.util.ArrayList;
 
 import org.eclipse.linuxtools.systemtap.ui.structures.listeners.IGobblerListener;
 
+
+
 /**
  * A separate thread to listen to an InputStream and pull all the data
  * out of it. When data is found a new event is fired share the data with
@@ -24,13 +26,13 @@ import org.eclipse.linuxtools.systemtap.ui.structures.listeners.IGobblerListener
  * @author Ryan Morse
  */
 public class StreamGobbler implements Runnable {
-
 	public StreamGobbler(InputStream is) {
 		if(null != is) {
 			this.is = is;
 			line = new StringBuilder();
 			listeners = new ArrayList<IGobblerListener>();
 		}
+		locked = false;
 	}
 	
 	/**
@@ -41,62 +43,65 @@ public class StreamGobbler implements Runnable {
 	 */
 	//Make sure to call this method to start the StreamGobbler
 	public void start() {
-		reader = new Thread(this, "StreamGobbler"); //$NON-NLS-1$
-		reader.start();
+		t = new Thread(this, "StreamGobbler");
+		t.start();
 	}
-
+	
 	/**
 	 * Checks to see if the gobbler is still running.
 	 * @return boolean representing whether or not it is sill running
 	 */
 	public boolean isRunning() {
-		return (null != reader);
+		return (null != t);
 	}
 
 	/**
 	 * The main method of this class. It monitors the provided thread to see
 	 * when new data is available and then appends it to its current list of
 	 * data.  When a new line is read it will fire a DataEvent for listeners
-	 * to get a hold of the data.
+	 * to get ahold of the data.
 	 */
 	public void run() {
-		if (reader != Thread.currentThread())
-			return;
-
+		Thread thisThread = Thread.currentThread();
 		try {
-			int val = is.read();
-			while(val != -1) {
-				line.append((char)val);
+			int val=-1;
 
-				if ('\n' == val)
-					this.fireNewDataEvent();
-
-				val = is.read();
+			while(t == thisThread) {
+				while(0 < is.available()) {
+					if(-1 == (val = is.read()))
+						this.stop();
+					else
+						line.append((char)val);
+					if ('\n' == val)
+						this.fireNewDataEvent();
+					
+				}
+				try {
+					Thread.sleep(10);
+				} catch(InterruptedException ie) {}
 			}
-		} catch (IOException ioe) {}	// If stream closed before thread shuts down
+		} catch (IOException ioe) {}	//If stream closed before thread shuts down
 	}
 
 	/**
-	 * Stops the gobbler from monitoring the stream, and fires one last data event
+	 * Stops the gobbler from monitering the stream, and fires one last data event
 	 * to make sure that listeners have the entire contents of what was read in
 	 * from the stream.
 	 */
 	public synchronized void stop() {
-		try {
-			// Interrupt the thread just in case it is blocked on a read.
-			reader.interrupt();
-			// Wait for the reader thread to finish.
-			reader.join();
-		} catch (InterruptedException e) {
-			// The thread was interrupted; nothing to do; finish stopping.
-		}
-		reader = null;
+		try {	//Make sure we don't stop while there is still data in the stream
+			while(0 != is.available()) {
+				Thread.sleep(10);
+			}
+		} catch(Exception e) {}
+		t = null;
 		notify();
-		// Fire one last time to ensure listeners have gotten everything.
+		//Fire one last time to ensure listeners have gotten everything.
 		this.fireNewDataEvent();
 	}
 	
-	/**	 * Method for getting the most recently read line from the stream.
+	/**
+	 * Method for getting the most recently read line from the stream.
 	 * @return String representing the current line being read from the 
 	 * <code>InputStream</code>
 	 */
@@ -111,61 +116,86 @@ public class StreamGobbler implements Runnable {
 		if(isRunning())
 			stop();
 		line = null;
-		reader = null;
+		t = null;
 		is = null;
 	}
 	
 	/**
-	 * Fires new events to everything that is monitoring this stream. Then clears
+	 * Fires new events to everything that is monitering this stream. Then clears
 	 * the current line of data.
 	 */
 	private void fireNewDataEvent() {
-		this.fireNewDataEvent(line.toString());
+		//Implement our own lock since using synchronized causes a deadlock here
+	/*	while(locked) {
+			try {
+				wait(10);
+			} catch(Exception e) {}
+		}
+		locked = true;*/
+		for(int i = 0; i < listeners.size(); i++)
+		{
+		
+			listeners.get(i).handleDataEvent(line.toString());
+		}
 		line.delete(0, line.length());
+		locked = false;
 	}
 	
 	public void fireNewDataEvent(String l) {
-		synchronized (listeners) {
-			for(int i = 0; i < listeners.size(); i++){
-				listeners.get(i).handleDataEvent(l);
-			}
+		//Implement our own lock since using synchronized causes a deadlock here
+	/*	while(locked) {
+			try {
+				wait(10);
+			} catch(Exception e) {}
 		}
+		locked = true;*/
+		for(int i = 0; i < listeners.size(); i++)
+		{
+			listeners.get(i).handleDataEvent(l);
+		}
+//		line.delete(0, line.length());
+		locked = false;
 	}
 	
 	/**
 	 * Registers the provided listener to get data events.
-	 * @param l A listener that needs to monitor the stream.
+	 * @param l A listener that needs to moniter the stream.
 	 */
 	public void addDataListener(IGobblerListener l) {
-		synchronized (listeners) {
-			if(l != null && !listeners.contains(l)){
-				listeners.add(l);
-			}
+		
+		if(l != null && !listeners.contains(l))
+		{
+			
+			listeners.add(l);
+			
 		}
+		
 	}
 
 	/**
-	 * Unregisters the provided listener from getting new data events.
-	 * @param l A listener that is monitoring the stream and should be removed
+	 * Unregisters the provied listener from getting new data events.
+	 * @param l A listener that is monitering the stream and should be removed
 	 */
 	public void removeDataListener(IGobblerListener l) {
-		synchronized (listeners) {
-			if(listeners.contains(l))
-				listeners.remove(l);
-		}
+		
+		if(listeners.contains(l))
+			listeners.remove(l);
 	}
 
 	/**
 	 * Returns a list of all of the <code>IGobblerListeners</code> that
-	 * are listening for data events.
+	 * are lstening for data events.
 	 * @return ArrayList of all of the listeners registered.
 	 */
 	public ArrayList<IGobblerListener> getDataListeners() {
 		return listeners;
 	}
-
+	
 	private ArrayList<IGobblerListener> listeners;
 	private StringBuilder line;
-	private Thread reader;
+	private Thread t;
 	private InputStream is;
+	
+	@SuppressWarnings("unused")
+	private boolean locked;
 }
