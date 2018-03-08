95134ad6e86d6ab65f1f726cab112a0f246bed32
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:51:21 +0000

    First commited as before_9449_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.dashboardextension/src/org/eclipse/linuxtools/internal/systemtap/ui/dashboardextension/actions/ExportScriptAction.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.dashboardextension/src/org/eclipse/linuxtools/internal/systemtap/ui/dashboardextension/actions/ExportScriptAction.java
@@ -213,8 +213,8 @@ public class ExportScriptAction extends RunScriptAction {
 			return;
 
 		File f;
-		for(String fileName: files) {
-			f = new File(fileName);
+		for(int i=0; i<files.length; i++) {
+			f = new File(files[i]);
 			if(f.exists())
 				f.delete();
 		}
@@ -241,4 +241,11 @@ public class ExportScriptAction extends RunScriptAction {
 		} catch(WorkbenchException we) {}
 	}
 
+	/**
+	 * Removes all internal references to objects.  No other method should be called after this.
+	 */
+	@Override
+	public void dispose() {
+		super.dispose();
+	}
 }
