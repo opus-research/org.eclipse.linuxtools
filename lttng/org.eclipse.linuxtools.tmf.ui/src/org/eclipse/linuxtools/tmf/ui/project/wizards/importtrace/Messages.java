ee152f4ef75ba20d77dcf865624dacde28083408
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:43:11 +0000

    First commited as after_11125_rev3

---
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/Messages.java
@@ -0,0 +1,30 @@
+package org.eclipse.linuxtools.tmf.ui.project.wizards.importtrace;
+
+import org.eclipse.osgi.util.NLS;
+
+public class Messages extends NLS {
+    private static final String BUNDLE_NAME = "org.eclipse.linuxtools.tmf.ui.project.wizards.importtrace.messages"; //$NON-NLS-1$
+    public static String ImportTraceWizardPageOptions_0;
+    public static String ImportTraceWizardPageOptions_1;
+    public static String ImportTraceWizardPageOptions_2;
+    public static String ImportTraceWizardPageScan_0;
+    public static String ImportTraceWizardPageScan_1;
+    public static String ImportTraceWizardPageScan_2;
+    public static String ImportTraceWizardPageScan_3;
+    public static String ImportTraceWizardPageScan_4;
+    public static String ImportTraceWizardPageSelectDirectories_0;
+    public static String ImportTraceWizardPageSelectDirectories_1;
+    public static String ImportTraceWizardPageSelectDirectories_2;
+    public static String ImportTraceWizardPageSelectDirectories_3;
+    public static String ImportTraceWizardPageSelectDirectories_4;
+    public static String ImportTraceWizardPageSelectTraceType_0;
+    public static String ImportTraceWizardPageSelectTraceType_1;
+    public static String ImportTraceWizardPageSelectTraceType_2;
+    static {
+        // initialize resource bundle
+        NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+    }
+
+    private Messages() {
+    }
+}
