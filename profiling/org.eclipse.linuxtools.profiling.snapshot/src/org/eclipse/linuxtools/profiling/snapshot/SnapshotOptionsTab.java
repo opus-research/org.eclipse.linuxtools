2870d7b2c2a180e14d5296d6ecd9f4567a4c70f6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:26:46 +0000

    First commited as before_6657_rev1

--- profiling/org.eclipse.linuxtools.profiling.snapshot/src/org/eclipse/linuxtools/profiling/snapshot/SnapshotOptionsTab.java
+++ profiling/org.eclipse.linuxtools.profiling.snapshot/src/org/eclipse/linuxtools/profiling/snapshot/SnapshotOptionsTab.java
@@ -67,7 +67,6 @@ public class SnapshotOptionsTab extends ProfileLaunchConfigurationTab {
 
 				// initialize all tab widgets based on the configuration
 				initializeFrom(initial);
-				top.layout();
 			}
 		});
 
