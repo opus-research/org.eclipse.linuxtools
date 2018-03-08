fa154425c7ccda5a3295fea36e19963bd656d8b0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:08:42 +0000

    First commited as after_16943_rev1

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfLaunchConfigDelegate.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfLaunchConfigDelegate.java
@@ -202,7 +202,7 @@ public class PerfLaunchConfigDelegate extends ProfileLaunchConfigurationDelegate
 		}
 
 		Object[] titleArgs = new Object[]{exePath.toOSString(), args.toString(), String.valueOf(runCount)};
-		String title = MessageFormat.format(Messages.PerfLaunchConfigDelegate_stat_title, titleArgs);
+		String title = renderProcessLabel(MessageFormat.format(Messages.PerfLaunchConfigDelegate_stat_title, titleArgs));
 
 		@SuppressWarnings("unchecked")
 		List<String> configEvents = config.getAttribute(PerfPlugin.ATTR_SelectedEvents,
