44ec0b264d3196db1db94022c54bee3976ca7278
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:58:22 +0000

    First commited as after_13780_rev28

---
+++ lttng/org.eclipse.linuxtools.tmf.ui.tests/src/org/eclipse/linuxtools/tmf/ui/tests/smoketest/conditions/TreeNodeAvailable.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Matthew Khouzam - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.ui.tests.smoketest.conditions;
+
+import org.eclipse.swtbot.swt.finder.SWTBot;
+import org.eclipse.swtbot.swt.finder.waits.ICondition;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTree;
+import org.eclipse.swtbot.swt.finder.widgets.SWTBotTreeItem;
+
+class TreeNodeAvailable implements ICondition {
+
+    private final SWTBotTree fTree;
+    private final String fName;
+
+    /**
+     * Is a tree node available
+     *
+     * @param name
+     *            The name of the node
+     * @param tree
+     *            the swtbotTree
+     */
+    public TreeNodeAvailable(String name, SWTBotTree tree) {
+        fTree = tree;
+        fName = name;
+    }
+
+    @Override
+    public boolean test() throws Exception {
+        try {
+            final SWTBotTreeItem[] treeItems = fTree.getAllItems();
+            for( SWTBotTreeItem ti : treeItems){
+                final String text = ti.getText();
+                if( text.equals(fName)) {
+                    return true;
+                }
+            }
+        } catch (Exception e) {
+        }
+        return false;
+    }
+
+    @Override
+    public void init(SWTBot bot) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String getFailureMessage() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
