fc5c4eb5480416d55d57df9eb12366e59f2cdaf5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:31:26 +0000

    First commited as before_10593_rev6

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/PerfPlugin.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/PerfPlugin.java
@@ -16,7 +16,6 @@ package org.eclipse.linuxtools.internal.perf;
 
 import java.util.List;
 
-import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.linuxtools.internal.perf.model.TreeParent;
 import org.eclipse.linuxtools.internal.perf.ui.PerfProfileView;
@@ -104,14 +103,8 @@ public class PerfPlugin extends AbstractUIPlugin {
 	// Source Disassembly Data
 	private SourceDisassemblyData sourceDisassemblyData;
 
-	// Profile view
+	//Profile view
 	private PerfProfileView _ProfileView = null;
-
-	// Current profile data
-	private IPath curProfileData;
-
-	// Current working directory
-	private IPath curWorkingDir;
 
 	public TreeParent getModelRoot() {
 		return _modelRoot;
@@ -119,14 +112,6 @@ public class PerfPlugin extends AbstractUIPlugin {
 
 	public SourceDisassemblyData getSourceDisassemblyData () {
 		return sourceDisassemblyData;
-	}
-
-	public IPath getPerfProfileData() {
-		return curProfileData;
-	}
-
-	public IPath getWorkingDir(){
-		return curWorkingDir;
 	}
 
 	/**
@@ -148,14 +133,6 @@ public class PerfPlugin extends AbstractUIPlugin {
 
 	public void setSourceDisassemblyData (SourceDisassemblyData sourceDisassemblyData) {
 		this.sourceDisassemblyData = sourceDisassemblyData;
-	}
-
-	public void setPerfProfileData(IPath perfProfileData) {
-		this.curProfileData = perfProfileData;
-	}
-
-	public void setWorkingDir(IPath workingDir){
-		curWorkingDir = workingDir;
 	}
 
 	public PerfProfileView getProfileView() {
