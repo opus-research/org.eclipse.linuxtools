9e1be1d5be451a03534e012c2c8da51f1e19e892
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:20:43 +0000

    First commited as before_17974_rev2

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/SystemTapScriptLaunchShortcut.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/SystemTapScriptLaunchShortcut.java
@@ -14,8 +14,8 @@ import java.util.ArrayList;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.NullProgressMonitor;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.debug.core.DebugPlugin;
 import org.eclipse.debug.core.ILaunchConfiguration;
 import org.eclipse.debug.core.ILaunchConfigurationType;
@@ -33,28 +33,23 @@ public class SystemTapScriptLaunchShortcut extends ProfileLaunchShortcut {
 
 	@Override
 	public void launch(IEditorPart editor, String mode) {
-		String path;
-		String project = null;
+		String path = ""; //$NON-NLS-1$
 		if(editor.getEditorInput() instanceof PathEditorInput){
 			path = ((PathEditorInput)editor.getEditorInput()).getPath().toString();
 		} else {
-			IFile file = ResourceUtil.getFile(editor.getEditorInput());
-			path = file.getLocation().toString();
-			project = file.getProject().getName();
+			path = ResourceUtil.getFile(editor.getEditorInput()).getLocation().toString();
 		}
-		this.searchAndLaunch(path, project);
+		this.searchAndLaunch(path);
 	}
 
 	@Override
 	public void launch(ISelection selection, String mode) {
-		IFile file = (IFile)((TreeSelection)selection).getFirstElement();
-		String path = file.getLocation().toOSString();
-		String project = file.getProject().getName();
-		this.searchAndLaunch(path, project);
+		IPath path = ((IFile)((TreeSelection)selection).getFirstElement()).getLocation();
+		this.searchAndLaunch(path.toOSString());
 	}
 
-	private void searchAndLaunch(String path, String project){
-		ILaunchConfiguration configuration = findLaunchConfiguration(path, project);
+	private void searchAndLaunch(String path){
+		ILaunchConfiguration configuration = findLaunchConfiguration(path);
 		if (configuration == null){
 			return;
 		}
@@ -66,9 +61,9 @@ public class SystemTapScriptLaunchShortcut extends ProfileLaunchShortcut {
 
 	}
 
-	protected ILaunchConfiguration findLaunchConfiguration(String scriptPath, String scriptProject) {
+	protected ILaunchConfiguration findLaunchConfiguration(String scriptPath) {
 		ILaunchConfiguration configuration = null;
-		ArrayList<ILaunchConfiguration> candidateConfigurations = new ArrayList<ILaunchConfiguration>();
+		ArrayList<ILaunchConfiguration> candidateConfiguraions = new ArrayList<ILaunchConfiguration>();
 		try {
 			ILaunchConfiguration[] configs = DebugPlugin.getDefault()
 					.getLaunchManager()
@@ -76,22 +71,21 @@ public class SystemTapScriptLaunchShortcut extends ProfileLaunchShortcut {
 
 			for (ILaunchConfiguration config: configs){
 				if (config.getAttribute(SystemTapScriptLaunchConfigurationTab.SCRIPT_PATH_ATTR, "").equals(scriptPath)){ //$NON-NLS-1$
-					candidateConfigurations.add(config);
+					candidateConfiguraions.add(config);
 				}
 			}
 
-			int candidateCount = candidateConfigurations.size();
+			int candidateCount = candidateConfiguraions.size();
 			if (candidateCount == 0) {
 				ILaunchConfigurationType type = getLaunchConfigType();
-				String wcName = (scriptProject == null ? "" : scriptProject + " ") //$NON-NLS-1$ //$NON-NLS-2$
-						+ Path.fromOSString(scriptPath).lastSegment();
-				ILaunchConfigurationWorkingCopy wc = type.newInstance(null, wcName);
+				configuration = type.newInstance(null, "Default"); //$NON-NLS-1$
+				ILaunchConfigurationWorkingCopy wc = configuration.getWorkingCopy();
 				wc.setAttribute(SystemTapScriptLaunchConfigurationTab.SCRIPT_PATH_ATTR, scriptPath);
 				configuration = wc.doSave();
 			} else if (candidateCount == 1) {
-				configuration = candidateConfigurations.get(0);
+				configuration = candidateConfiguraions.get(0);
 			} else {
-				configuration = chooseConfiguration(candidateConfigurations,
+				configuration = chooseConfiguration(candidateConfiguraions,
 						ILaunchManager.RUN_MODE);
 			}
 		} catch (CoreException e) {
