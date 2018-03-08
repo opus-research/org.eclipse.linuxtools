894c1191b1bee7cd63037abbc4c6b0ccefd83c96
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:29:32 +0000

    First commited as before_12728_rev4

--- perf/org.eclipse.linuxtools.perf.tests/resources/fibTest/fib.cpp
+++ perf/org.eclipse.linuxtools.perf.tests/resources/fibTest/fib.cpp
@@ -24,12 +24,14 @@ long rightfib(unsigned long n) {
 }
 
 int main(int argc, char *argv[]) {
+	//vector<int> first;
 	if (argc != 2) {
 		cout << "Not right args" << endl;
 		return 1;
 	}
 	cout << atoi(argv[1]) << endl;
 	cout << leftfib(atoi(argv[1])) << endl;
-
+	//cout << first.front() << endl;
+	//cout << "Hello" << endl;
 	return 0;
 }
