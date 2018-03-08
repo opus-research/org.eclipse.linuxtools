b3bee0b8cb827cc499b4a1a36485d6d239434bda
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:14:01 +0000

    First commited as before_14278_rev2

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/handlers/PerfSaveSessionHandler.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/handlers/PerfSaveSessionHandler.java
@@ -14,9 +14,6 @@ import java.io.File;
 import java.io.IOException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.linuxtools.internal.perf.PerfPlugin;
-import org.eclipse.linuxtools.internal.perf.ui.PerfProfileView;
-import org.eclipse.ui.PartInitException;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * Handler for saving a perf profile session.
@@ -41,15 +38,8 @@ public class PerfSaveSessionHandler extends AbstractSaveDataHandler {
 				newDataFile.createNewFile();
 				copyFile(defaultDataFile, newDataFile);
 				PerfPlugin.getDefault().setPerfProfileData(newDataLoc);
-				PerfProfileView view;
-				try {
-					view = (PerfProfileView) PlatformUI.getWorkbench()
-							.getActiveWorkbenchWindow().getActivePage()
-							.showView(PerfPlugin.VIEW_ID);
-					view.setContentDescription(newDataLoc.toOSString());
-				} catch (PartInitException e) {
-					// fail silently
-				}
+				PerfPlugin.getDefault().getProfileView()
+						.setContentDescription(newDataLoc.toOSString());
 
 				return newDataFile;
 			} catch (IOException e) {
