257230f82caa4a91df99ae14e540337b166fb14b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:35:49 +0000

    First commited as before_12949_rev1

--- lttng/org.eclipse.linuxtools.lttng2.core/src/org/eclipse/linuxtools/internal/lttng2/core/control/model/IUstProviderInfo.java
+++ lttng/org.eclipse.linuxtools.lttng2.core/src/org/eclipse/linuxtools/internal/lttng2/core/control/model/IUstProviderInfo.java
@@ -1,12 +1,12 @@
 /**********************************************************************
- * Copyright (c) 2012, 2013 Ericsson
- *
+ * Copyright (c) 2012 Ericsson
+ * 
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
- * Contributors:
+ * 
+ * Contributors: 
  *   Bernd Hufmann - Initial API and implementation
  **********************************************************************/
 package org.eclipse.linuxtools.internal.lttng2.core.control.model;
@@ -17,36 +17,36 @@ import java.util.List;
  * <p>
  * Interface for retrieval of UST provider information.
  * </p>
- *
+ * 
  * @author Bernd Hufmann
  */
 public interface IUstProviderInfo extends ITraceInfo {
-
+   
     /**
      * @return the process ID of the UST provider.
      */
-    int getPid();
+    public int getPid();
 
     /**
      * Sets the process ID of the UST provider to the given value.
      * @param pid - process ID to set
      */
-    void setPid(int pid);
-
+    public void setPid(int pid);
+    
     /**
      * @return all event information as array.
      */
-    IBaseEventInfo[] getEvents();
-
+    public IBaseEventInfo[] getEvents();
+    
     /**
      * Sets the event information specified by given list.
      * @param events - all event information to set.
      */
-    void setEvents(List<IBaseEventInfo> events);
-
+    public void setEvents(List<IBaseEventInfo> events);
+    
     /**
      * Adds a single event information.
      * @param event - event information to add.
      */
-    void addEvent(IBaseEventInfo event);
+    public void addEvent(IBaseEventInfo event);
 }
