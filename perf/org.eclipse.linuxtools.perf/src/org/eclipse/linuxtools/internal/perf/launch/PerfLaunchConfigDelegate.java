3ec8fa0e6a05a44b7fdec12ee00b5f7b82b5c226
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:57:55 +0000

    First commited as before_7837_rev2

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfLaunchConfigDelegate.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfLaunchConfigDelegate.java
@@ -119,9 +119,11 @@ public class PerfLaunchConfigDelegate extends ProfileLaunchConfigurationDelegate
 				OutputStream outputTo;
 				if (binaryOutCons != null) {
 					outputTo = binaryOutCons.newOutputStream();
-					//Get the printstream for that console
-					print = new PrintStream(outputTo);
+				} else { //This probably shouldn't ever happen...
+					outputTo = System.out;
 				}				
+				//Get the printstream for that console
+				print = new PrintStream(outputTo);
 				for (int i = 0; i < commandArray.length; i++) {
 					print.print(commandArray[i] + " ");
 				}
