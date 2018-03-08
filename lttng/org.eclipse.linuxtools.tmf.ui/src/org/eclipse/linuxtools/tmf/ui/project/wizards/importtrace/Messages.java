4c0ef07166b69dc96683da5d678b41a44ef5c448
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:34:37 +0000

    First commited as after_12898_rev3

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/Messages.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/Messages.java
@@ -44,6 +44,8 @@ public class Messages extends NLS {
     public static String BatchImportTraceWizard_remove;
     public static String BatchImportTraceWizard_add;
 
+    public static String BatchImportTraceWizard_errorImportingTraceResource;
+
     static {
         // initialize resource bundle
         NLS.initializeMessages(BUNDLE_NAME, Messages.class);
