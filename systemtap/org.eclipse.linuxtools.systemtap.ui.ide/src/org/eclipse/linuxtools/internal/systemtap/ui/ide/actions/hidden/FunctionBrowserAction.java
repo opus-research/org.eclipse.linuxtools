dd213360371f3394086ec777501baefb625434e6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:12:31 +0000

    First commited as before_10009_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/actions/hidden/FunctionBrowserAction.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/actions/hidden/FunctionBrowserAction.java
@@ -20,8 +20,8 @@ import org.eclipse.linuxtools.internal.systemtap.ui.ide.Localization;
 import org.eclipse.linuxtools.internal.systemtap.ui.ide.editors.stp.STPEditor;
 import org.eclipse.linuxtools.internal.systemtap.ui.ide.views.FunctionBrowserView;
 import org.eclipse.linuxtools.systemtap.ui.editor.actions.file.NewFileAction;
+import org.eclipse.linuxtools.systemtap.ui.logging.LogManager;
 import org.eclipse.linuxtools.systemtap.ui.structures.TreeNode;
-import org.eclipse.linuxtools.systemtap.ui.structures.ui.ExceptionErrorDialog;
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.ISelectionListener;
@@ -59,6 +59,7 @@ public class FunctionBrowserAction extends Action implements ISelectionListener,
 	 * @param browser	browser that fires this action
 	 */
 	public FunctionBrowserAction(IWorkbenchWindow window, FunctionBrowserView browser) {
+		LogManager.logInfo("initialized", this); //$NON-NLS-1$
 		this.window = window;
 		setId(ID);
 		setActionDefinitionId(ID);
@@ -75,6 +76,7 @@ public class FunctionBrowserAction extends Action implements ISelectionListener,
 		selection = null;
 		expandAction.dispose();
 		expandAction = null;
+		LogManager.logInfo("disposed", this); //$NON-NLS-1$
 	}
 
 	/**
@@ -111,6 +113,7 @@ public class FunctionBrowserAction extends Action implements ISelectionListener,
 				IEditorPart ed = page.getActiveEditor();
 				if(ed == null) {
 					NewFileAction action = new NewFileAction();
+					//action.init(page.getWorkbenchWindow());
 					action.run();
 					if (action.isSuccessful())
 						ed = page.getWorkbenchWindow().getActivePage().getActiveEditor();
@@ -130,7 +133,7 @@ public class FunctionBrowserAction extends Action implements ISelectionListener,
 
 					}
 				} catch (PartInitException e) {
-					ExceptionErrorDialog.openError(Localization.getString("FunctionBrowserAction.UnableToInsertFunction"), e); //$NON-NLS-1$
+					LogManager.logCritical("PartInitException run: " + e.getMessage(), this); //$NON-NLS-1$
 				}
 			} else {
 				expandAction.run();
