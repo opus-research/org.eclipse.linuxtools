92f3a30539efd90e01c885e9f46f7fa2acdb0134
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:51:34 +0000

    First commited as before_7607_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/environment/TmfEnvironmentView.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/environment/TmfEnvironmentView.java
@@ -14,6 +14,8 @@ package org.eclipse.linuxtools.tmf.ui.views.environment;
 import java.util.ArrayList;
 
 import org.eclipse.linuxtools.tmf.core.ctfadaptor.CtfTmfTrace;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.event.TmfEvent;
 import org.eclipse.linuxtools.tmf.core.signal.TmfExperimentSelectedSignal;
 import org.eclipse.linuxtools.tmf.core.signal.TmfSignalHandler;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
@@ -36,7 +38,7 @@ public class TmfEnvironmentView extends TmfView {
     /** The Environment View's ID */
     public static final String ID = "org.eclipse.linuxtools.tmf.ui.views.environment"; //$NON-NLS-1$
 
-    private TmfExperiment fExperiment;
+    private TmfExperiment<?> fExperiment;
     private Table fTable;
 //    final private String fTitlePrefix;
     private Composite fParent;
@@ -62,11 +64,13 @@ public class TmfEnvironmentView extends TmfView {
     }
 
     @Override
+    @SuppressWarnings({ "unchecked", "rawtypes" })
     public void createPartControl(Composite parent) {
         fParent = parent;
         TableItem ti[];
         // If an experiment is already selected, update the table
-        TmfExperiment experiment = TmfExperiment.getCurrentExperiment();
+        TmfExperiment<ITmfEvent> experiment = (TmfExperiment<ITmfEvent>) TmfExperiment
+                .getCurrentExperiment();
         if (experiment == null) {
             return;
         }
@@ -131,10 +135,11 @@ public class TmfEnvironmentView extends TmfView {
      * @param signal
      *            The incoming signal
      */
+    @SuppressWarnings("unchecked")
     @TmfSignalHandler
-    public void experimentSelected(TmfExperimentSelectedSignal signal) {
+    public void experimentSelected(TmfExperimentSelectedSignal<TmfEvent> signal) {
         // Update the trace reference
-        TmfExperiment exp = signal.getExperiment();
+        TmfExperiment<TmfEvent> exp = (TmfExperiment<TmfEvent>) signal.getExperiment();
         if (!exp.equals(fExperiment)) {
             fExperiment = exp;
             if (fTable != null) {
