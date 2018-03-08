934bda811991cdc5dccf5657814a50c4cd4b1db0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:10:11 +0000

    First commited as before_12078_rev2

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/ITmfTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/ITmfTrace.java
@@ -18,6 +18,7 @@ import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.linuxtools.tmf.core.component.ITmfDataProvider;
 import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
 import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
@@ -149,8 +150,9 @@ public interface ITmfTrace extends ITmfDataProvider {
      * @param path the trace path
      *
      * @return true if trace is valid
+     * @since 2.0
      */
-    public boolean validate(IProject project, String path);
+    public IStatus validate(IProject project, String path);
 
     // ------------------------------------------------------------------------
     // Basic getters
@@ -221,17 +223,6 @@ public interface ITmfTrace extends ITmfDataProvider {
      * @since 2.0
      */
     public void registerStateSystem(String id, ITmfStateSystem ss);
-
-    /**
-     * Index the trace. Depending on the trace type, this could be done at the
-     * constructor or initTrace phase too, so this could be implemented as a
-     * no-op.
-     *
-     * @param waitForCompletion
-     *            Should we block the caller until indexing is finished, or not.
-     * @since 2.0
-     */
-    public void indexTrace(boolean waitForCompletion);
 
     // ------------------------------------------------------------------------
     // Trace characteristics getters
