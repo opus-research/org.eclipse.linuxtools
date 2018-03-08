0b806aceef10b660be430f4d7a18b88dce4b4341
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:53:31 +0000

    First commited as after_9501_rev2

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/HistoryBuilder.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/HistoryBuilder.java
@@ -43,6 +43,7 @@ import org.eclipse.linuxtools.tmf.core.trace.TmfExperiment;
  * @author alexmont
  *
  */
+@SuppressWarnings("deprecation")
 public class HistoryBuilder extends TmfComponent {
 
     private final IStateChangeInput sci;
@@ -249,6 +250,7 @@ class StateSystemBuildRequest extends TmfEventRequest {
     private final IStateChangeInput sci;
     private final ITmfTrace trace;
 
+    @SuppressWarnings("deprecation")
     StateSystemBuildRequest(HistoryBuilder builder) {
         super(builder.getInputPlugin().getExpectedEventType(),
                 TmfTimeRange.ETERNITY,
@@ -261,6 +263,7 @@ class StateSystemBuildRequest extends TmfEventRequest {
     }
 
     @Override
+    @SuppressWarnings("deprecation")
     public void handleData(final ITmfEvent event) {
         super.handleData(event);
         if (event != null) {
--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/component/ITmfEventProvider.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/component/ITmfEventProvider.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2010 Ericsson
+ * Copyright (c) 2009, 2010, 2012 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -17,16 +17,16 @@ import org.eclipse.linuxtools.tmf.core.request.ITmfRequest;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfContext;
 
 /**
- * This is the interface of the data providers in TMF. Data providers have the
- * capability of handling data requests.
+ * This is the interface of the event providers in TMF. Event providers have the
+ * capability of handling event requests.
  *
- * @version 1.0
  * @author Francois Chouinard
+ * @version 1.0
  *
  * @see TmfDataProvider
  * @see TmfEventProvider
  */
-public interface ITmfDataProvider extends ITmfComponent {
+public interface ITmfEventProvider extends ITmfComponent {
 
     /**
      * Queue the request for processing.
@@ -59,4 +59,5 @@ public interface ITmfDataProvider extends ITmfComponent {
      * @return the event referred to by context
      */
     public ITmfEvent getNext(ITmfContext context);
+
 }
