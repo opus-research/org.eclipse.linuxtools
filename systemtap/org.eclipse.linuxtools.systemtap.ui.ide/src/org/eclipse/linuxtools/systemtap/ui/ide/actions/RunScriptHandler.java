28907e6bb0c20e962764f5bcaf4fcbf4ed0c86eb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:49:36 +0000

    First commited as before_11350_rev3

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/actions/RunScriptHandler.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/actions/RunScriptHandler.java
@@ -340,22 +340,18 @@ public class RunScriptHandler extends AbstractHandler {
 		}
 
 		// Make sure script name only contains underscores and/or alphanumeric characters.
-		Pattern validModName = Pattern.compile("^[a-z0-9_A-Z]+$"); //$NON-NLS-1$
+		Pattern validModName = Pattern.compile("^[a-z0-9_]+$"); //$NON-NLS-1$
 		Matcher modNameMatch = validModName.matcher(modname);
 		if (!modNameMatch.matches()) {
 			continueRun = false;
-        	Display.getDefault().asyncExec(new Runnable() {
-        		@Override
-				public void run() {
 
 			Shell parent = PlatformUI.getWorkbench().getDisplay()
 					.getActiveShell();
 			MessageDialog.openError(parent,
 					Messages.ScriptRunAction_InvalidScriptTitle,
 					Messages.ScriptRunAction_InvalidScriptTMessage);
-        		}
-        	});
-        	return new String[0];
+
+			return new String[0];
 		}
 
 		script[script.length-2]=modname;
