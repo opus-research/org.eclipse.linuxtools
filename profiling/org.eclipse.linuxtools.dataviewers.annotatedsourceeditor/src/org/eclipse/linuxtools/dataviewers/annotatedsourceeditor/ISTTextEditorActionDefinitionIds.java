d7a13c27b6254354cb7c3b5eec6cf066a6b38cd5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:08:06 +0000

    First commited as before_9858_rev3

--- profiling/org.eclipse.linuxtools.dataviewers.annotatedsourceeditor/src/org/eclipse/linuxtools/dataviewers/annotatedsourceeditor/ISTTextEditorActionDefinitionIds.java
+++ profiling/org.eclipse.linuxtools.dataviewers.annotatedsourceeditor/src/org/eclipse/linuxtools/dataviewers/annotatedsourceeditor/ISTTextEditorActionDefinitionIds.java
@@ -1,16 +1,17 @@
 /*******************************************************************************
- * Copyright (c) 2006 IBM Corporation.
+ * Copyright (c) 2009 STMicroelectronics.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     IBM Corporation - Jeff Briggs, Henry Hughes, Ryan Morse
+ *    Marzia Maugeri <marzia.maugeri@st.com> - initial API and implementation
  *******************************************************************************/
+package org.eclipse.linuxtools.dataviewers.annotatedsourceeditor;
 
-package org.eclipse.linuxtools.systemtap.ui.structures;
+public interface ISTTextEditorActionDefinitionIds {
 
-public interface IPasswordPrompt {
-	public String getPassword();
+    String ST_TOGGLE = "org.eclipse.linuxtools.dataviewers.annotatedsourceeditor.STColumnToggle"; //$NON-NLS-1$
+
 }
