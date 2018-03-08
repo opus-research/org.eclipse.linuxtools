267f3a9d9cf601a86feb93f6c80803ef191b62dd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:15:50 +0000

    First commited as before_10086_rev1

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfTmfTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfTmfTrace.java
@@ -154,7 +154,11 @@ public class CtfTmfTrace extends TmfTrace implements ITmfEventParser {
         final CtfTmfLightweightContext context = new CtfTmfLightweightContext(this);
         context.setLocation(curLocation);
         context.seek(curLocation.getLocationInfo());
-        return getIterator(this, context).getApproxPosition();
+        final CtfLocationData currentTime = ((CtfLocationData)context.getLocation().getLocationInfo());
+        final long startTime = getIterator(this, context).getStartTime();
+        final long endTime = getIterator(this, context).getEndTime();
+        return ((double) currentTime.getTimestamp() - startTime)
+                / (endTime - startTime);
     }
 
     /**
