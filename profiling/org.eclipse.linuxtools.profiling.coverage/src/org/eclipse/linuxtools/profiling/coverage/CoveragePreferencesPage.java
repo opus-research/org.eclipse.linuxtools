0aae8c98e9fe2ba79e306a10c82ef0001ae94fb2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:59:13 +0000

    First commited as after_7906_rev2

---
+++ profiling/org.eclipse.linuxtools.profiling.coverage/src/org/eclipse/linuxtools/profiling/coverage/CoveragePreferencesPage.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.profiling.coverage;
+
+import org.eclipse.linuxtools.internal.profiling.provider.AbstractProviderPreferencesPage;
+
+public class CoveragePreferencesPage extends AbstractProviderPreferencesPage {
+
+	@Override
+	public String getProfilingType() {
+		return CoverageProfileConstants.PROFILING_TYPE;
+	}
+}
