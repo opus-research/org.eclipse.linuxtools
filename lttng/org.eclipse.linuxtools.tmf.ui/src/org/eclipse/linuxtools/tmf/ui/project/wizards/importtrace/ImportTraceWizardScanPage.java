89d11b5a41adda15c5066c4212e73db0b27d19dd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:44:00 +0000

    First commited as before_13294_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/ImportTraceWizardScanPage.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/ImportTraceWizardScanPage.java
@@ -13,14 +13,13 @@
 package org.eclipse.linuxtools.tmf.ui.project.wizards.importtrace;
 
 import java.io.File;
+import java.lang.reflect.InvocationTargetException;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.SubMonitor;
-import org.eclipse.core.runtime.jobs.Job;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.CheckStateChangedEvent;
 import org.eclipse.jface.viewers.CheckboxTreeViewer;
@@ -61,12 +60,11 @@ import org.eclipse.ui.IWorkbench;
  */
 public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
 
-    private static final int COL_WIDTH = 200;
     private static final int MAX_TRACES = 65536;
     private CheckboxTreeViewer traceTypeViewer;
 
     // private int position = 0;
-    final ScanRunnable fRunnable = new ScanRunnable("Scan job"); //$NON-NLS-1$
+    final ScanRunnable fRunnable = new ScanRunnable();
     final private BlockingQueue<TraceValidationHelper> fTracesToScan = new ArrayBlockingQueue<TraceValidationHelper>(MAX_TRACES);
     private volatile boolean fCanRun = true;
 
@@ -101,7 +99,6 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
     @Override
     public void dispose() {
         fCanRun = false;
-        fRunnable.done(Status.OK_STATUS);
         super.dispose();
     }
 
@@ -132,7 +129,7 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
         // Column 1
         // --------------------
         TreeViewerColumn column = new TreeViewerColumn(traceTypeViewer, SWT.NONE);
-        column.getColumn().setWidth(COL_WIDTH);
+        column.getColumn().setWidth(200);
         column.getColumn().setText(Messages.ImportTraceWizardImportCaption);
         column.setLabelProvider(new FirstColumnLabelProvider());
         column.setEditingSupport(new ColumnEditorSupport(traceTypeViewer, textCellEditor));
@@ -142,7 +139,7 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
         // --------------------
 
         column = new TreeViewerColumn(traceTypeViewer, SWT.NONE);
-        column.getColumn().setWidth(COL_WIDTH);
+        column.getColumn().setWidth(200);
         column.getColumn().setText(Messages.ImportTraceWizardTraceDisplayName);
         column.setLabelProvider(new ColumnLabelProvider() {
             @Override
@@ -158,8 +155,8 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
         init();
         getBatchWizard().setTracesToScan(fTracesToScan);
         getBatchWizard().setTraceFolder(fTargetFolder);
-        fRunnable.schedule();
-        setErrorMessage(Messages.ImportTraceWizardScanPageSelectAtleastOne);
+        getBatchWizard().getNMContainer().backgroundRun(this, fRunnable);
+        setErrorMessage(Messages.ImportTraceWizardScanPage_SelectAtleastOne);
     }
 
     private void init() {
@@ -219,6 +216,7 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
 
         @Override
         public void widgetDefaultSelected(SelectionEvent e) {
+            // TODO Auto-generated method stub
 
         }
     }
@@ -328,9 +326,9 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
             }
             getBatchWizard().updateConflicts();
             if (getBatchWizard().hasConflicts()) {
-                setErrorMessage(Messages.ImportTraceWizardScanPageRenameError);
+                setErrorMessage(Messages.ImportTraceWizardScanPage_renameError);
             } else if (!getBatchWizard().hasTracesToImport()) {
-                setErrorMessage(Messages.ImportTraceWizardScanPageSelectAtleastOne);
+                setErrorMessage(Messages.ImportTraceWizardScanPage_SelectAtleastOne);
             } else {
                 setErrorMessage(null);
             }
@@ -352,12 +350,7 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
         }
     }
 
-    private final class ScanRunnable extends Job {
-
-        public ScanRunnable(String name) {
-            super(name);
-        }
-
+    private final class ScanRunnable implements IRunnableWithProgress {
         private IProgressMonitor fMonitor;
 
         private synchronized IProgressMonitor getMonitor() {
@@ -365,14 +358,14 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
         }
 
         @Override
-        public IStatus run(IProgressMonitor monitor) {
+        public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
             fMonitor = monitor;
             final Control control = traceTypeViewer.getControl();
             control.getDisplay().syncExec(new Runnable() {
                 @Override
                 public void run() {
                     fMonitor = SubMonitor.convert(getMonitor());
-                    getMonitor().setTaskName(Messages.ImportTraceWizardPageScanScanning + ' ');
+                    getMonitor().setTaskName(Messages.ImportTraceWizardPageScan_scanning + " "); //$NON-NLS-1$
                     ((SubMonitor) getMonitor()).setWorkRemaining(IProgressMonitor.UNKNOWN);
                 }
             });
@@ -386,63 +379,60 @@ public class ImportTraceWizardScanPage extends AbstractImportTraceWizardPage {
                         @Override
                         public void run() {
                             if (!control.isDisposed()) {
-                                getMonitor().setTaskName(Messages.ImportTraceWizardPageScanScanning + ' ');
-                                getMonitor().subTask(Messages.ImportTraceWizardPageScanDone);
+                                getMonitor().setTaskName(Messages.ImportTraceWizardPageScan_scanning + " "); //$NON-NLS-1$
+                                getMonitor().subTask(Messages.ImportTraceWizardPageScan_done);
                             }
                         }
                     });
                 }
-                try {
-                    final TraceValidationHelper traceToScan = fTracesToScan.take();
+                final TraceValidationHelper traceToScan = fTracesToScan.take();
 
-                    if (!getBatchWizard().hasScanned(traceToScan)) {
-                        getBatchWizard().addResult(traceToScan, TmfTraceType.getInstance().validate(traceToScan));
+                if (!getBatchWizard().hasScanned(traceToScan)) {
+                    getBatchWizard().addResult(traceToScan, TmfTraceType.getInstance().validate(traceToScan));
+                }
+                validCombo = getBatchWizard().getResult(traceToScan);
+                if (validCombo) {
+                    // Synched on it's parent
+
+                    getBatchWizard().getScannedTraces().addCandidate(traceToScan.getTraceType(), new File(traceToScan.getTraceToScan()));
+                    updated = true;
+                }
+                // position++;
+
+                if (updated) {
+                    if (!control.isDisposed()) {
+                        control.getDisplay().asyncExec(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (!control.isDisposed()) {
+                                    getMonitor().setTaskName(Messages.ImportTraceWizardPageScan_scanning + " "); //$NON-NLS-1$
+                                    getMonitor().subTask(traceToScan.getTraceToScan());
+                                    getMonitor().worked(1);
+                                }
+                            }
+                        }
+                                );
                     }
-                    validCombo = getBatchWizard().getResult(traceToScan);
-                    if (validCombo) {
-                        // Synched on it's parent
+                }
 
-                        getBatchWizard().getScannedTraces().addCandidate(traceToScan.getTraceType(), new File(traceToScan.getTraceToScan()));
-                        updated = true;
-                    }
+                final boolean editing = traceTypeViewer.isCellEditorActive();
+                if (updated && !editing)
+                {
+                    if (!control.isDisposed()) {
+                        control.getDisplay().asyncExec(new Runnable() {
 
-                    if (updated) {
-                        if (!control.isDisposed()) {
-                            control.getDisplay().asyncExec(new Runnable() {
-                                @Override
-                                public void run() {
-                                    if (!control.isDisposed()) {
-                                        getMonitor().setTaskName(Messages.ImportTraceWizardPageScanScanning + ' ');
-                                        getMonitor().subTask(traceToScan.getTraceToScan());
-                                        getMonitor().worked(1);
+                            @Override
+                            public void run() {
+                                if (!control.isDisposed()) {
+                                    if (!traceTypeViewer.isCellEditorActive()) {
+                                        traceTypeViewer.refresh();
                                     }
                                 }
                             }
-                                    );
-                        }
+                        });
                     }
-
-                    final boolean editing = traceTypeViewer.isCellEditorActive();
-                    if (updated && !editing) {
-                        if (!control.isDisposed()) {
-                            control.getDisplay().asyncExec(new Runnable() {
-
-                                @Override
-                                public void run() {
-                                    if (!control.isDisposed()) {
-                                        if (!traceTypeViewer.isCellEditorActive()) {
-                                            traceTypeViewer.refresh();
-                                        }
-                                    }
-                                }
-                            });
-                        }
-                    }
-                } catch (InterruptedException e) {
-                    return new Status(IStatus.CANCEL, Activator.PLUGIN_ID, new String());
                 }
             }
-            return Status.OK_STATUS;
         }
     }
 }
