eb0f03853009bd5cb22dd4ae1bcf9b198fb99ce0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:42:07 +0000

    First commited as before_11112_rev1

--- lttng/org.eclipse.linuxtools.gdbtrace.ui/src/org/eclipse/linuxtools/internal/gdbtrace/ui/views/project/handlers/SelectTraceExecutableHandler.java
+++ lttng/org.eclipse.linuxtools.gdbtrace.ui/src/org/eclipse/linuxtools/internal/gdbtrace/ui/views/project/handlers/SelectTraceExecutableHandler.java
@@ -20,30 +20,68 @@ import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.ISelectionProvider;
+import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.window.Window;
 import org.eclipse.linuxtools.internal.gdbtrace.trace.GdbTrace;
 import org.eclipse.linuxtools.internal.gdbtrace.ui.views.project.dialogs.SelectTraceExecutableDialog;
 import org.eclipse.linuxtools.tmf.ui.project.model.TmfTraceElement;
 import org.eclipse.swt.widgets.MessageBox;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
- * Handler for the Select Trace Executable command
+ * Handler for the Select Trace Executable command.
  * @author Patrick Tasse
  */
 public class SelectTraceExecutableHandler extends AbstractHandler {
 
+    /** The trace element reference. */
+    private TmfTraceElement fTraceElement = null;
+
+    @Override
+    public boolean isEnabled() {
+
+        // Check if we are closing down
+        final IWorkbenchWindow window = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+        if (window == null) {
+            return false;
+        }
+
+        // Get the selection
+        final IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        final IWorkbenchPart part = page.getActivePart();
+        if (part == null) {
+            return false;
+        }
+        final ISelectionProvider selectionProvider = part.getSite().getSelectionProvider();
+        if (selectionProvider == null) {
+            return false;
+        }
+        final ISelection selection = selectionProvider.getSelection();
+
+        // Make sure there is only one selection and that it is a trace
+        fTraceElement = null;
+        if (selection instanceof TreeSelection) {
+            final TreeSelection sel = (TreeSelection) selection;
+            // There should be only one item selected as per the plugin.xml
+            final Object element = sel.getFirstElement();
+            if (element instanceof TmfTraceElement) {
+                fTraceElement = (TmfTraceElement) element;
+            }
+        }
+
+        // We only enable opening from the Traces folder for now
+        return (fTraceElement != null);
+    }
+
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
         Shell shell = HandlerUtil.getActiveShell(event);
-
-        // Get the selection before opening the dialog because otherwise the
-        // getCurrentSelection() call will always return null
-        ISelection selection = HandlerUtil.getCurrentSelection(event);
-
         SelectTraceExecutableDialog dialog = new SelectTraceExecutableDialog(shell);
         dialog.open();
         if (dialog.getReturnCode() != Window.OK) {
@@ -51,21 +89,15 @@ public class SelectTraceExecutableHandler extends AbstractHandler {
         }
         IPath tracedExecutable = dialog.getExecutablePath();
 
-        if (selection instanceof IStructuredSelection) {
-            for (Object o : ((IStructuredSelection) selection).toList()) {
-                TmfTraceElement traceElement = (TmfTraceElement) o;
-                IResource resource = traceElement.getResource();
-                try {
-                    resource.setPersistentProperty(GdbTrace.EXEC_KEY, tracedExecutable.toString());
-                } catch (CoreException e) {
-                    final MessageBox mb = new MessageBox(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
-                    mb.setText(e.getClass().getName());
-                    mb.setMessage(e.getMessage());
-                    mb.open();
-                }
-            }
+        IResource resource = fTraceElement.getResource();
+        try {
+            resource.setPersistentProperty(GdbTrace.EXEC_KEY, tracedExecutable.toString());
+        } catch (CoreException e) {
+            final MessageBox mb = new MessageBox(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell());
+            mb.setText(e.getClass().getName());
+            mb.setMessage(e.getMessage());
+            mb.open();
         }
         return null;
     }
-
 }
