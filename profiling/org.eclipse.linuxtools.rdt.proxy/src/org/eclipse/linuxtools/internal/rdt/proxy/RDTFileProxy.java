d08e1fabec668230440afa5a2696f09fef98e50b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:51:42 +0000

    First commited as before_9454_rev2

--- profiling/org.eclipse.linuxtools.rdt.proxy/src/org/eclipse/linuxtools/internal/rdt/proxy/RDTFileProxy.java
+++ profiling/org.eclipse.linuxtools.rdt.proxy/src/org/eclipse/linuxtools/internal/rdt/proxy/RDTFileProxy.java
@@ -14,9 +14,6 @@ import java.net.URI;
 
 import org.eclipse.core.filesystem.IFileStore;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IResource;
-import org.eclipse.core.resources.IWorkspaceRoot;
-import org.eclipse.core.resources.ResourcesPlugin;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
@@ -25,16 +22,12 @@ import org.eclipse.linuxtools.profiling.launch.IRemoteFileProxy;
 import org.eclipse.linuxtools.rdt.proxy.Activator;
 import org.eclipse.ptp.remote.core.IRemoteConnection;
 import org.eclipse.ptp.remote.core.IRemoteFileManager;
-import org.eclipse.ptp.remote.core.IRemoteResource;
 import org.eclipse.ptp.remote.core.IRemoteServices;
 import org.eclipse.ptp.remote.core.PTPRemoteCorePlugin;
 
 public class RDTFileProxy implements IRemoteFileProxy {
 
-	private final static String SYNC_NATURE = "org.eclipse.ptp.rdt.sync.core.remoteSyncNature"; //$NON-NLS-1$
-	private IProject project;
 	private IRemoteFileManager manager;
-	private IRemoteResource remoteRes;
 
 	private void initialize(URI uri) throws CoreException {
 		IRemoteServices services = PTPRemoteCorePlugin.getDefault().getRemoteServices(uri);
@@ -52,16 +45,7 @@ public class RDTFileProxy implements IRemoteFileProxy {
 	}
 
 	public RDTFileProxy(IProject project) throws CoreException {
-		this.project = project;
 		URI uri = project.getLocationURI();
-		IWorkspaceRoot workspaceRoot = ResourcesPlugin.getWorkspace().getRoot();
-		IResource resource = workspaceRoot.findMember(project.getName());
-		if (resource != null) {
-			remoteRes = (IRemoteResource)resource.getAdapter(IRemoteResource.class);
-			if (project.hasNature(SYNC_NATURE)) {
-				uri = remoteRes.getActiveLocationURI();
-			} 
-		} 
 		initialize(uri);
 	}
 
@@ -72,12 +56,6 @@ public class RDTFileProxy implements IRemoteFileProxy {
 
 	@Override
 	public URI toURI(String path) {
-		try {
-			if (project.hasNature(SYNC_NATURE))
-			return remoteRes.getActiveLocationURI();
-		} catch (CoreException e) {
-			e.printStackTrace();
-		}
 		return manager.toURI(path);
 	}
 
