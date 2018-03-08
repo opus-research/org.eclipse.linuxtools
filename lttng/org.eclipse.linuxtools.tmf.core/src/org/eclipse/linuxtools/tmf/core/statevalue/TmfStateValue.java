0f7bcd729ead04878fe189511d5eea15dbfc0b42
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:01:18 +0000

    First commited as after_16396_rev4

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/statevalue/TmfStateValue.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/statevalue/TmfStateValue.java
@@ -104,9 +104,6 @@ public abstract class TmfStateValue implements ITmfStateValue {
      * @return The newly-created TmfStateValue object
      */
     public static TmfStateValue newValueInt(int intValue) {
-        if (intValue == -1) {
-            return nullValue();
-        }
         return new IntegerStateValue(intValue);
     }
 
@@ -119,9 +116,6 @@ public abstract class TmfStateValue implements ITmfStateValue {
      * @since 2.0
      */
     public static TmfStateValue newValueLong(long longValue) {
-        if (longValue == -1) {
-            return nullValue();
-        }
         return new LongStateValue(longValue);
     }
 
@@ -133,9 +127,6 @@ public abstract class TmfStateValue implements ITmfStateValue {
      * @return The newly-created TmfStateValue object
      */
     public static TmfStateValue newValueDouble(double value) {
-        if (value == Double.NaN) {
-            return nullValue();
-        }
         return new DoubleStateValue(value);
     }
 
