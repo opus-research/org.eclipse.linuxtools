4951c987a5e924fd16c620de395af7a1e7c4e7f4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:25:15 +0000

    First commited as after_12653_rev5

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/ImportTraceWizardPage.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/wizards/importtrace/ImportTraceWizardPage.java
@@ -731,7 +731,29 @@ public class ImportTraceWizardPage extends WizardResourceImportPage {
                     }
                 }
             } else {
-                IConfigurationElement ce = TmfTraceType.getInstance().getTraceAttributes(traceType);
+                String temp[] = traceType.split(":"); //$NON-NLS-1$
+                if (temp.length < 2) {
+                    Activator.getDefault().logError("Error with trace type " + traceType); //$NON-NLS-1$
+                    return false;
+                }
+                if (temp.length > 2) {
+                    for (int i = 2; i < temp.length; i++) {
+                        temp[1] += temp[i];
+                    }
+                }
+                final String traceId = TmfTraceType.getInstance().getTraceTypeId(temp[0], temp[1]);
+                if (traceId != null) {
+                    if (!TmfTraceType.getInstance().validateTrace(traceId, getSelectedResources())) {
+                        setMessage(null);
+                        setErrorMessage(Messages.ImportTraceWizard_TraceValidationFailed);
+                        return false;
+                    }
+                } else {
+                    setMessage(null);
+                    setErrorMessage(Messages.ImportTraceWizard_TraceValidationFailed);
+                    return false;
+                }
+                IConfigurationElement ce = TmfTraceType.getInstance().getTraceAttributes(traceId);
                 if (ce != null) {
                     traceTypeOK = true;
                     traceBundle = ce.getContributor().getName();
