c4aa39d8fcc320691e8895522624d1adf49ed6e6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:51:42 +0000

    First commited as after_7613_rev2

---
+++ profiling/org.eclipse.linuxtools.profiling.time/src/org/eclipse/linuxtools/profiling/time/launch/TimeLaunchShortcut.java
@@ -0,0 +1,23 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.profiling.time.launch;
+
+import org.eclipse.linuxtools.internal.profiling.provider.launch.ProviderLaunchShortcut;
+import org.eclipse.linuxtools.profiling.time.TimeProviderPlugin;
+
+public class TimeLaunchShortcut extends ProviderLaunchShortcut {
+
+	@Override
+	protected String getProfilingType() {
+		return TimeProviderPlugin.PROFILING_TYPE;
+	}
+
+}
