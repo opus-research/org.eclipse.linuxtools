1c45d044cea2722823eafecd609ddbc9dd66f694
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:02:14 +0000

    First commited as before_11670_rev2

--- systemtap/org.eclipse.linuxtools.systemtap.ui.structures.tests/src/org/eclipse/linuxtools/systemtap/ui/structures/AllTests.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.structures.tests/src/org/eclipse/linuxtools/systemtap/ui/structures/AllTests.java
@@ -13,6 +13,8 @@ package org.eclipse.linuxtools.systemtap.ui.structures;
 
 import org.eclipse.linuxtools.systemtap.ui.structures.runnable.CommandTest;
 import org.eclipse.linuxtools.systemtap.ui.structures.runnable.StreamGobblerTest;
+import org.eclipse.linuxtools.systemtap.ui.structures.validators.ConditionalExpressionValidatorTest;
+import org.eclipse.linuxtools.systemtap.ui.structures.validators.DirectoryValidatorTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
@@ -22,6 +24,9 @@ import org.junit.runners.Suite;
 		LoggingStreamDaemonTest.class, SortTest.class, StreamGobblerTest.class,
 		StringFormatterTest.class, TreeDefinitionNodeTest.class,
 		TreeNodeTest.class,
-		ZipArchiveTest.class})
+		ZipArchiveTest.class,
+
+		// structures.validators
+		ConditionalExpressionValidatorTest.class, DirectoryValidatorTest.class })
 public class AllTests {
 }
