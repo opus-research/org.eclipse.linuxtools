860d042289a3553de877bc8f060c76da19ee2e76
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:23:36 +0000

    First commited as before_18108_rev4

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/handlers/Messages.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/handlers/Messages.java
@@ -16,6 +16,7 @@ public class Messages extends NLS {
 	private static final String BUNDLE_NAME = "org.eclipse.linuxtools.internal.perf.handlers.messages"; //$NON-NLS-1$
 	public static String PerfSaveSession_title;
 	public static String PerfSaveSession_msg;
+	public static String PerfSaveSession_invalid_filename_title;
 	public static String PerfSaveSession_invalid_filename_msg;
 	public static String PerfSaveSession_file_exists_title;
 	public static String PerfSaveSession_file_exists_msg;
@@ -23,17 +24,33 @@ public class Messages extends NLS {
 	public static String PerfSaveStat_error_msg;
 	public static String PerfSaveSession_failure_title;
 	public static String PerfSaveSession_failure_msg;
+	public static String PerfStatDiffMenuAction_diff_text;
+	public static String PerfStatDiffMenuAction_new_tooltip;
+	public static String PerfStatDiffMenuAction_new_text;
+	public static String PerfStatDiffMenuAction_old_tooltip;
+	public static String PerfStatDiffMenuAction_old_text;
+	public static String PerfStatDiffMenuAction_stats_tooltip;
 	public static String PerfResourceLeak_title;
+	public static String PerfResourceLeak_msg;
 
 	public static String MsgProxyError;
+	public static String MsgClearSelection;
+	public static String MsgSelectionDiff;
+	public static String MsgSelectFiles;
 	public static String MsgError;
+	public static String MsgError_0;
+	public static String MsgError_1;
+	public static String MsgError_2;
+	public static String MsgWarning_0;
+	public static String MsgWarning_1;
+	public static String MsgConfirm_title;
+	public static String MsgConfirm_msg;
 	public static String ContentDescription_0;
 
 	public static String PerfEditorLauncher_stat_title;
 	public static String PerfEditorLauncher_file_dne_error;
 	public static String PerfEditorLauncher_file_read_error;
-	public static String ReportComparisonView_label;
-	public static String StatComparisonView_label;
+	public static String StatComparisonData_temp_files_error;
 
 	static {
 		// initialize resource bundle
