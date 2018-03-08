e1614bfb2ffbbaafe05016de928ecb1bad9a9194
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:21:18 +0000

    First commited as before_17980_rev1

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/ITmfTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/ITmfTrace.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2013 Ericsson, École Polytechnique de Montréal
+ * Copyright (c) 2009, 2013 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -9,30 +9,23 @@
  * Contributors:
  *   Francois Chouinard - Initial API and implementation
  *   Francois Chouinard - Updated as per TMF Trace Model 1.0
- *   Geneviève Bastien  - Added timestamp transforms and timestamp
- *                        creation functions
  *******************************************************************************/
 
 package org.eclipse.linuxtools.tmf.core.trace;
 
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.IStatus;
-import org.eclipse.linuxtools.tmf.core.analysis.IAnalysisModule;
 import org.eclipse.linuxtools.tmf.core.component.ITmfDataProvider;
 import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
 import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
 import org.eclipse.linuxtools.tmf.core.statesystem.ITmfStateSystem;
 import org.eclipse.linuxtools.tmf.core.statistics.ITmfStatistics;
-import org.eclipse.linuxtools.tmf.core.synchronization.ITmfTimestampTransform;
 import org.eclipse.linuxtools.tmf.core.timestamp.ITmfTimestamp;
 import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimeRange;
-import org.eclipse.linuxtools.tmf.core.trace.indexer.ITmfTraceIndexer;
-import org.eclipse.linuxtools.tmf.core.trace.location.ITmfLocation;
 
 /**
  * The event stream structure in TMF. In its basic form, a trace has:
@@ -44,9 +37,9 @@ import org.eclipse.linuxtools.tmf.core.trace.location.ITmfLocation;
  * <li> the time range (span) of the events it contains
  * </ul>
  * Concrete ITmfTrace classes have to provide a parameter-less constructor and
- * an initialization method (<i>initTrace</i>) if they are to be opened from the
- * Project View. Also, a validation method (<i>validate</i>) has to be provided
- * to ensure that the trace is of the correct type.
+ * an initialization method (<i>initTrace</i>) if they are to be opened from
+ * the Project View. Also, a validation method (<i>validate</i>) has to be
+ * provided to ensure that the trace is of the correct type.
  * <p>
  * A trace can be accessed simultaneously from multiple threads by various
  * application components. To avoid obvious multi-threading issues, the trace
@@ -87,22 +80,19 @@ import org.eclipse.linuxtools.tmf.core.trace.location.ITmfLocation;
  *     event = trace.getNext(context);
  * }
  * </pre>
- *
  * A trace is also an event provider so it can process event requests
  * asynchronously (and coalesce compatible, concurrent requests).
  * <p>
- *
- * <b>Example 4</b>: Process a whole trace (see ITmfEventRequest for
- * variants)
+ * </pre>
+ * <b>Example 4</b>: Process a whole trace (see ITmfEventRequest for variants)
  * <pre>
  * ITmfRequest request = new TmfEventRequest&lt;MyEventType&gt;(MyEventType.class) {
- *     &#064;Override
+ *     &#64;Override
  *     public void handleData(MyEventType event) {
  *         super.handleData(event);
  *         processEvent(event);
  *     }
- *
- *     &#064;Override
+ *     &#64;Override
  *     public void handleCompleted() {
  *         finish();
  *         super.handleCompleted();
@@ -143,29 +133,23 @@ public interface ITmfTrace extends ITmfDataProvider {
      * properly parameterize an ITmfTrace instantiated with its parameterless
      * constructor.
      * <p>
-     * Typically, the parameterless constructor will provide the block size and
-     * its associated parser and indexer.
+     * Typically, the parameterless constructor will provide the block size
+     * and its associated parser and indexer.
      *
-     * @param resource
-     *            the trace resource
-     * @param path
-     *            the trace path
-     * @param type
-     *            the trace event type
-     * @throws TmfTraceException
-     *             If we couldn't open the trace
+     * @param resource the trace resource
+     * @param path the trace path
+     * @param type the trace event type
+     * @throws TmfTraceException If we couldn't open the trace
      */
     void initTrace(IResource resource, String path, Class<? extends ITmfEvent> type) throws TmfTraceException;
 
     /**
      * Validate that the trace is of the correct type.
      *
-     * @param project
-     *            the eclipse project
-     * @param path
-     *            the trace path
-     * @return an IStatus object with validation result. Use severity OK to
-     *         indicate success.
+     * @param project the eclipse project
+     * @param path the trace path
+     *
+     * @return an IStatus object with validation result. Use severity OK to indicate success.
      * @since 2.0
      */
     IStatus validate(IProject project, String path);
@@ -237,35 +221,6 @@ public interface ITmfTrace extends ITmfDataProvider {
      */
     void indexTrace(boolean waitForCompletion);
 
-    /**
-     * Returns an analysis module with the given id
-     *
-     * @param analysisId
-     *            The analysis module id
-     * @return The {@link IAnalysisModule} object
-     */
-    IAnalysisModule getAnalysisModule(String analysisId);
-
-    /**
-     * Return a list of analysis modules that are of a given class
-     *
-     * @param moduleclass
-     *            Class returned module must extend
-     * @return List of {@link IAnalysisModule} of class moduleclass
-     */
-    List<IAnalysisModule> getAnalysisModules(Class<? extends IAnalysisModule> moduleclass);
-
-    /**
-     * Returns a map of analysis modules applicable to this trace. The key is
-     * the analysis id.
-     *
-     * This view should be read-only (implementations should use
-     * {@link Collections#unmodifiableMap}).
-     *
-     * @return The map of analysis modules
-     */
-    Map<String, IAnalysisModule> getAnalysisModules();
-
     // ------------------------------------------------------------------------
     // Trace characteristics getters
     // ------------------------------------------------------------------------
@@ -304,17 +259,14 @@ public interface ITmfTrace extends ITmfDataProvider {
 
     /**
      * @return the current trace location
-     * @since 3.0
      */
     ITmfLocation getCurrentLocation();
 
     /**
      * Returns the ratio (proportion) corresponding to the specified location.
      *
-     * @param location
-     *            a trace specific location
+     * @param location a trace specific location
      * @return a floating-point number between 0.0 (beginning) and 1.0 (end)
-     * @since 3.0
      */
     double getLocationRatio(ITmfLocation location);
 
@@ -331,32 +283,29 @@ public interface ITmfTrace extends ITmfDataProvider {
      * If not null, the location requested must be valid otherwise the returned
      * context is undefined (up to the implementation to recover if possible).
      * <p>
-     *
-     * @param location
-     *            the trace specific location
+     * @param location the trace specific location
      * @return a context which can later be used to read the corresponding event
-     * @since 3.0
      */
     ITmfContext seekEvent(ITmfLocation location);
 
     /**
      * Position the trace at the 'rank'th event in the trace.
      * <p>
-     * A rank <= 0 is interpreted as seeking for the first event of the trace.
+     * A rank <= 0 is interpreted as seeking for the first event of the
+     * trace.
      * <p>
      * If the requested rank is beyond the last trace event, the context
      * returned will yield a null event if used in a subsequent read.
      *
-     * @param rank
-     *            the event rank
+     * @param rank the event rank
      * @return a context which can later be used to read the corresponding event
      */
     ITmfContext seekEvent(long rank);
 
     /**
      * Position the trace at the first event with the specified timestamp. If
-     * there is no event with the requested timestamp, a context pointing to the
-     * next chronological event is returned.
+     * there is no event with the requested timestamp, a context pointing to
+     * the next chronological event is returned.
      * <p>
      * A null timestamp is interpreted as seeking for the first event of the
      * trace.
@@ -364,8 +313,7 @@ public interface ITmfTrace extends ITmfDataProvider {
      * If the requested timestamp is beyond the last trace event, the context
      * returned will yield a null event if used in a subsequent read.
      *
-     * @param timestamp
-     *            the timestamp of desired event
+     * @param timestamp the timestamp of desired event
      * @return a context which can later be used to read the corresponding event
      * @since 2.0
      */
@@ -379,8 +327,7 @@ public interface ITmfTrace extends ITmfDataProvider {
      * voluntarily vague. Typically, it would refer to the event proportional
      * rank (arguably more intuitive) or timestamp in the trace file.
      *
-     * @param ratio
-     *            the proportional 'rank' in the trace
+     * @param ratio the proportional 'rank' in the trace
      * @return a context which can later be used to read the corresponding event
      */
     ITmfContext seekEvent(double ratio);
@@ -392,49 +339,4 @@ public interface ITmfTrace extends ITmfDataProvider {
      * @since 2.0
      */
     ITmfTimestamp getInitialRangeOffset();
-
-    /**
-     * Returns the ID of the host this trace is from. The host ID is not
-     * necessarily the hostname, but should be a unique identifier for the
-     * machine on which the trace was taken. It can be used to determine if two
-     * traces were taken on the exact same machine (timestamp are already
-     * synchronized, resources with same id are the same if taken at the same
-     * time, etc).
-     *
-     * @return The host id of this trace
-     * @since 3.0
-     */
-    String getHostId();
-
-    // ------------------------------------------------------------------------
-    // Timestamp transformation functions
-    // ------------------------------------------------------------------------
-
-    /**
-     * Returns the timestamp transformation for this trace
-     *
-     * @return the timestamp transform
-     * @since 3.0
-     */
-    ITmfTimestampTransform getTimestampTransform();
-
-    /**
-     * Sets the trace's timestamp transform
-     *
-     * @param tt
-     *            The timestamp transform for all timestamps of this trace
-     * @since 3.0
-     */
-    void setTimestampTransform(final ITmfTimestampTransform tt);
-
-    /**
-     * Creates a timestamp for this trace, using the transformation formula
-     *
-     * @param ts
-     *            The time in long with which to create the timestamp
-     * @return The new timestamp
-     * @since 3.0
-     */
-    ITmfTimestamp createTimestamp(long ts);
-
 }
