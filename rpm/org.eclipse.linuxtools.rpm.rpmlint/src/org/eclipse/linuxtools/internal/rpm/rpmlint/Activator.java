dfb396b6441f2c54539a3b362af1227f7ff6622b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:50:59 +0000

    First commited as before_9441_rev1

--- rpm/org.eclipse.linuxtools.rpm.rpmlint/src/org/eclipse/linuxtools/internal/rpm/rpmlint/Activator.java
+++ rpm/org.eclipse.linuxtools.rpm.rpmlint/src/org/eclipse/linuxtools/internal/rpm/rpmlint/Activator.java
@@ -11,10 +11,13 @@
 
 package org.eclipse.linuxtools.internal.rpm.rpmlint;
 
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator {
+public class Activator extends AbstractUIPlugin {
 
 	/**
 	 * The id of this plugin.
@@ -30,4 +33,33 @@ public class Activator {
 	 * RPM extension constant, i.e. .rpm files.
 	 */
 	public static final String RPMFILE_EXTENSION = "rpm"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+		plugin = this;
+	}
+
+	/**
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	@Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
 }
