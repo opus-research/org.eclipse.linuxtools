406d221a14a78d266035daa0fdbede52da15af82
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:47:23 +0000

    First commited as before_9273_rev2

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/histogram/HistogramView.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/histogram/HistogramView.java
@@ -178,7 +178,7 @@ public class HistogramView extends TmfView {
         gridData.verticalAlignment = SWT.CENTER;
         fCurrentEventTimeControl = new HistogramCurrentTimeControl(this, controlsComposite, currentEventLabel, 0L);
         fCurrentEventTimeControl.setLayoutData(gridData);
-        fCurrentEventTimeControl.setValue(Long.MIN_VALUE);
+        fCurrentEventTimeControl.setValue(0L);
 
         // Window span time control
         gridData = new GridData();
@@ -186,7 +186,7 @@ public class HistogramView extends TmfView {
         gridData.verticalAlignment = SWT.CENTER;
         fTimeSpanControl = new HistogramTimeRangeControl(this, controlsComposite, windowSpanLabel, 0L);
         fTimeSpanControl.setLayoutData(gridData);
-        fTimeSpanControl.setValue(Long.MIN_VALUE);
+        fTimeSpanControl.setValue(0L);
 
         // --------------------------------------------------------------------
         // Time range histogram
@@ -426,9 +426,9 @@ public class HistogramView extends TmfView {
         // Clear the UI widgets
         fFullTraceHistogram.clear();
         fTimeRangeHistogram.clear();
-        fCurrentEventTimeControl.setValue(Long.MIN_VALUE);
+        fCurrentEventTimeControl.setValue(0L);
 
-        fTimeSpanControl.setValue(Long.MIN_VALUE);
+        fTimeSpanControl.setValue(0);
     }
 
     /**
