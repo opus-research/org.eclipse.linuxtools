d1efe674230347d6095ffb10ca3dacb0348a6c58
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:50:33 +0000

    First commited as after_13454_rev2

---
+++ lttng/org.eclipse.linuxtools.tracing.examples/src/org/eclipse/linuxtools/tracing/examples/test/headless/MakeMeANexus.java
@@ -0,0 +1,66 @@
+/*******************************************************************************
+ * Copyright (c) 2012, 2014 Ericsson
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Matthew Khouzam - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tracing.examples.test.headless;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Random;
+
+/**
+ * Make a nexus file
+ *
+ * @author Matthew Khouzam
+ */
+public class MakeMeANexus {
+  private static final byte[] intToByteArray(int value) {
+    return new byte[]{(byte) (value >>> 24), (byte) (value >>> 16),
+        (byte) (value >>> 8), (byte) value};
+  }
+
+  /**
+   * Main
+   *
+   * @param args
+   *          arguments
+   */
+  public static void main(String[] args) {
+    Random rnd = new Random();
+    String[] eventTypes = new String[64];
+    try (FileOutputStream fos = new FileOutputStream("/home/ematkho/nexusTrace")) {
+      for (int i = 0; i < 64; i++) {
+        eventTypes[i] = new String("Event " + i);
+      }
+      for (int i = 0; i < 64; i++) {
+        fos.write(eventTypes[i].getBytes());
+        if (i != 63) {
+          fos.write(',');
+        } else {
+          fos.write('\n');
+        }
+      }
+      int timestamp = 500 + rnd.nextInt(500);
+      for (int i = 0; i < 400000; i++) {
+        timestamp = timestamp + rnd.nextInt(50) + 1;
+        int data = rnd.nextInt();
+        fos.write(intToByteArray(timestamp));
+        fos.write(intToByteArray(data));
+        if (i % 10000 == 0 && i != 0) {
+          System.out.print('.');
+        }
+      }
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
+  }
+}
