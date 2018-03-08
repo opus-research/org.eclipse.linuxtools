4b04242ae400c6ff50a442cfe45fb146b0a5a6d1
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:39:59 +0000

    First commited as after_10903_rev5

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfOptionsTab.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/launch/PerfOptionsTab.java
@@ -150,11 +150,7 @@ public class PerfOptionsTab extends AbstractLaunchConfigurationTab {
 		chkShowStat.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent se) {
-				if (chkShowStat.getSelection()) {
-					statRunCount.setEnabled(true);
-				}else{
-					statRunCount.setEnabled(false);
-				}
+				handleShowStatSelection();
 			}
 		});
 		statRunCount = new Spinner(showStatComp, SWT.BORDER);
@@ -223,6 +219,16 @@ public class PerfOptionsTab extends AbstractLaunchConfigurationTab {
 		updateLaunchConfigurationDialog();
 	}
 
+	private void handleShowStatSelection() {
+		if (chkShowStat.getSelection()) {
+			statRunCount.setEnabled(true);
+			toggleButtonsEnablement(false);
+		} else {
+			statRunCount.setEnabled(false);
+			toggleButtonsEnablement(true);
+		}
+	}
+
 	// Displays a file dialog to allow the user to select the kernel image file
 	private void showFileDialog(Shell shell) {
 		FileDialog fDialog = new FileDialog(shell, SWT.OPEN);
@@ -253,6 +259,22 @@ public class PerfOptionsTab extends AbstractLaunchConfigurationTab {
 		}
 	}
 
+	/**
+	 * Toggle enablement of all buttons, excluding the stat button.
+	 * @param enable enablement of buttons
+	 */
+	public void toggleButtonsEnablement(boolean enable) {
+		txtKernelLocation.setEnabled(enable);
+		chkRecordRealtime.setEnabled(enable);
+		chkRecordVerbose.setEnabled(enable);
+		chkSourceLineNumbers.setEnabled(enable);
+		chkKernelSourceLineNumbers.setEnabled(enable);
+		chkMultiplexEvents.setEnabled(enable);
+		chkModuleSymbols.setEnabled(enable);
+		chkHideUnresolvedSymbols.setEnabled(enable);
+		chkShowSourceDisassembly.setEnabled(enable);
+	}
+
 	@Override
 	public String getName() {
 		return "Perf Options";
@@ -280,6 +302,7 @@ public class PerfOptionsTab extends AbstractLaunchConfigurationTab {
 			} else {
 				statRunCount.setEnabled(true);
 			}
+			handleShowStatSelection();
 		} catch (CoreException e) {
 			// do nothing
 		}		
