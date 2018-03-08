d05ed85aedddd6c8c44e130730ab87bfc2300eb3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:44:36 +0000

    First commited as before_7376_rev3

--- systemtap/org.eclipse.linuxtools.systemtap.ui.editor/src/org/eclipse/linuxtools/systemtap/ui/editor/SimpleDocumentProvider.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.editor/src/org/eclipse/linuxtools/systemtap/ui/editor/SimpleDocumentProvider.java
@@ -18,23 +18,23 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.Writer;
-import java.net.MalformedURLException;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
+
 import org.eclipse.jface.operation.IRunnableContext;
+
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.IDocument;
 import org.eclipse.jface.text.source.IAnnotationModel;
+
 import org.eclipse.ui.IEditorInput;
 import org.eclipse.ui.IPathEditorInput;
-import org.eclipse.ui.ide.FileStoreEditorInput;
 import org.eclipse.ui.texteditor.AbstractDocumentProvider;
 
 public class SimpleDocumentProvider extends AbstractDocumentProvider {
@@ -65,22 +65,16 @@ public class SimpleDocumentProvider extends AbstractDocumentProvider {
 	 * @throws CoreException if reading the file fails
 	 */
 	private boolean setDocumentContent(IDocument document, IEditorInput input) throws CoreException {
-		Reader reader = null;
+		Reader reader;
 		try {
-			if (input instanceof FileStoreEditorInput){
-				reader = new InputStreamReader(((FileStoreEditorInput)input).getURI().toURL().openStream());
-			} else if (input instanceof IPathEditorInput){
+			if (input instanceof IPathEditorInput)
 				reader= new FileReader(((IPathEditorInput)input).getPath().toFile());
-			} else {
+			else {
 				return false;
 			}
 		} catch (FileNotFoundException e) {
 			// return empty document and save later
 			return true;
-		} catch (MalformedURLException e) {
-			throw new CoreException(new Status(IStatus.ERROR, "org.eclipse.linuxtools.systemtap.ui.editor", "Incorrect URL", e)); //$NON-NLS-1$ //$NON-NLS-2$
-		} catch (IOException e) {
-			throw new CoreException(new Status(IStatus.ERROR, "org.eclipse.linuxtools.systemtap.ui.editor", "Error reading file", e)); //$NON-NLS-1$ //$NON-NLS-2$
 		}
 		
 		try {
