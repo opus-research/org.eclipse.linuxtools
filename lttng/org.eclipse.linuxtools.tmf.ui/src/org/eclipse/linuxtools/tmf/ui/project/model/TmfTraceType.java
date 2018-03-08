476b0c630ac5695dd1a7f2f1c5848bafa6944917
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:32:59 +0000

    First commited as after_12884_rev2

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/model/TmfTraceType.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/model/TmfTraceType.java
@@ -456,7 +456,14 @@ public final class TmfTraceType {
      * @since 2.0
      */
     public boolean validate(TraceValidationHelper traceToValidate) {
-        return validate(traceToValidate.getTraceType(), traceToValidate.getTraceToScan());
+        boolean valid = false;
+        try {
+            valid = validate(traceToValidate.getTraceType(), traceToValidate.getTraceToScan());
+        } catch (Exception e) {
+            Activator.getDefault().logError(
+                    Messages.TmfTraceType_errorValidatingTrace + " " + traceToValidate.getTraceToScan() + " " + Messages.TmfTraceType_withTraceType + " " + traceToValidate.getTraceType() + " " + Messages.TmfTraceType_exception + ":" + e.toString());   //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+        }
+        return valid;
     }
 
     /**
