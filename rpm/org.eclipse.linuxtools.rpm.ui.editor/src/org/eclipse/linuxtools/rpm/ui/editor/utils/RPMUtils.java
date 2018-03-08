66a1561a44a7e247dd6a21cbd7de2bd7e3262fd8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:06:09 +0000

    First commited as after_13989_rev2

---
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/rpm/ui/editor/utils/RPMUtils.java
@@ -0,0 +1,107 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.rpm.ui.editor.utils;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.linuxtools.internal.rpm.ui.editor.Activator;
+import org.eclipse.linuxtools.rpm.ui.editor.parser.Specfile;
+import org.eclipse.linuxtools.rpm.ui.editor.parser.SpecfileParser;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.console.ConsolePlugin;
+import org.eclipse.ui.console.IConsole;
+import org.eclipse.ui.console.IConsoleManager;
+import org.eclipse.ui.console.MessageConsole;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+
+/**
+ * Utility class for RPM UI Editor related things.
+ * 
+ */
+public class RPMUtils {
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.fedoraproject.eclipse.packager.IProjectRoot#getSpecfileModel()
+	 */
+	public static Specfile parseToSpecfile(IFile file) {
+		SpecfileParser parser = new SpecfileParser();
+		StringBuilder sb = new StringBuilder();
+		String line = null;
+		try {
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					file.getContents()));
+			while ((line = reader.readLine()) != null) {
+				sb.append(line + "\n"); //$NON-NLS-1$
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (CoreException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Specfile specfile = parser.parse(sb.toString());
+		return specfile;
+	}
+
+	/**
+	 * Show an error dialog.
+	 *
+	 * @param shell A valid shell
+	 * @param title The error dialog title
+	 * @param message The message to be displayed.
+	 */
+	public static void showErrorDialog(final Shell shell,
+			final String title, final String message) {
+		PlatformUI.getWorkbench().getDisplay().asyncExec(new Runnable() {
+			public void run() {
+				MessageDialog.openError(shell, title, message);
+			}
+		});
+	}
+
+	private static final String PROJECT_HOLDER = "%projectName"; //$NON-NLS-1$
+	private static final String CONSOLE_NAME = "Packager Console (%projectName)"; //$NON-NLS-1$
+
+	/**
+	 * @param packageName The name of the package(RPM) this console will be for.
+	 * @return A console instance.
+	 */
+	public static MessageConsole getConsole(String packageName) {
+		ConsolePlugin plugin = ConsolePlugin.getDefault();
+		IConsoleManager conMan = plugin.getConsoleManager();
+		String projectConsoleName = CONSOLE_NAME.replace(PROJECT_HOLDER, packageName);
+		MessageConsole ret = null;
+		for (IConsole cons : ConsolePlugin.getDefault().getConsoleManager()
+				.getConsoles()) {
+			if (cons.getName().equals(projectConsoleName)) {
+				ret = (MessageConsole) cons;
+			}
+		}
+		// no existing console, create new one
+		if (ret == null) {
+			ret = new MessageConsole(projectConsoleName,
+					AbstractUIPlugin.imageDescriptorFromPlugin(Activator.PLUGIN_ID, "icons/rpm.gif")); //$NON-NLS-1$
+		}
+		conMan.addConsoles(new IConsole[] { ret });
+		ret.clearConsole();
+		ret.activate();
+		return ret;
+	}
+
+}
