f70e1f45966d97166d9b8588a375c1aea7502031
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:52:35 +0000

    First commited as after_7633_rev1

---
+++ profiling/org.eclipse.linuxtools.profiling.memory/src/org/eclipse/linuxtools/profiling/memory/Activator.java
@@ -0,0 +1,69 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.profiling.memory;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.eclipse.linuxtools.profiling.memory"; //$NON-NLS-1$
+
+	// Type of profiling this plug-in supports
+	public static final String PROFILING_TYPE = "memory"; //$NON-NLS-1$
+
+	// Plug-in id of snapshot launch configuration type
+	public static final String PLUGIN_CONFIG_ID = "org.eclipse.linuxtools.profiling.memory.launchConfigurationType"; //$NON-NLS-1$
+	
+	// Plug-in name
+	public static final String PLUGIN_NAME = "Memory"; //$NON-NLS-1$
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+
+}
