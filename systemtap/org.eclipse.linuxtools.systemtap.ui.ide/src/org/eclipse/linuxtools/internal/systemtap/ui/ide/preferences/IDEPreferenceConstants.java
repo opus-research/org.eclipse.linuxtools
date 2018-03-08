7015009268fa564614a1a249a2923b6c5c5c6109
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:33:13 +0000

    First commited as after_10710_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/preferences/IDEPreferenceConstants.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/preferences/IDEPreferenceConstants.java
@@ -15,7 +15,7 @@ public class IDEPreferenceConstants {
 	//ide
 	public static final String P_STORED_TREE = "UseStoredTreePreference"; //$NON-NLS-1$
 	public static final String P_REMOTE_PROBES = "RemoteProbes"; //$NON-NLS-1$
-	
+
 	//ide.path
 	public static final String P_KERNEL_SOURCE = "KernelSourcePreference"; //$NON-NLS-1$
 	public static final String P_EXCLUDED_KERNEL_SOURCE = "ExcludedKernelSource"; //$NON-NLS-1$
@@ -27,7 +27,7 @@ public class IDEPreferenceConstants {
 	//ide.editor
 	public static final String P_EDITOR_BACKGROUND = "EditorBackgroundPreference"; //$NON-NLS-1$
 	public static final String P_SHOW_LINE_NUMBERS = "ShowLineNumbers"; //$NON-NLS-1$
-	
+
 	//ide.editor.codeassist
 	public static final String P_USE_CODE_ASSIST = "UseCodeAssistPreference"; //$NON-NLS-1$
 	public static final String P_COMPLETION = "CompletionPreference"; //$NON-NLS-1$
@@ -35,40 +35,33 @@ public class IDEPreferenceConstants {
 	public static final String P_COMPLETION_OVERWRITE = "CompletionOverwritePreference"; //$NON-NLS-1$
 	public static final String P_ACTIVATION_DELAY = "ActivationDelayPreference"; //$NON-NLS-1$
 	public static final String P_ACTIVATION_TRIGGER = "ActivationTriggerPreference"; //$NON-NLS-1$
-	
+
 	//ide.editor.preferenceconstants
 	public static final String P_CONDITIONAL_FILTERS = "ConditionalFilters"; //$NON-NLS-1$
-	
-	//ide.stap.stapoptions
-	public static final String[][] P_STAP = new String[][] {
-		{"-k", "\tkeep temporary directory", "kStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-u", "\tunoptimized translation", "uStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-b", "\tbulk (relayfs) mode", "bStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-t", "\tbenchmarking timing information", "tStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-v", "\t\t\tincrease verbosity [0]", "vStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-p NUM", "\t\tstop after pass NUM 1-5", "pStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-s NUM", "\t\tbuffer size in megabytes", "sStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-D NM=VAL", "\temit macro definition into C code", "DStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-R DIR", "\t\tlook in DIR for runtime", "RStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-r RELEASE", "\tuse kernel RELEASE", "rStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-m MODULE", "\tset probe module name", "mStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-o FILE", "\t\tsend output to file", "oStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-c CMD", "\t\tstart the probes, run CMD, and exit when it finishes", "cStapPreference"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-		{"-x PID", "\t\tsets target() to PID", "xStapPreference"}}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
-	public static final String[] P_STAP_OPTS = new String[] {
-		"vStapOptPreference", //$NON-NLS-1$
-		"pStapOptPreference", //$NON-NLS-1$
-		"sStapOptPreference", //$NON-NLS-1$
-		"DStapOptPreference", //$NON-NLS-1$
-		"RStapOptPreference", //$NON-NLS-1$
-		"rStapOptPreference", //$NON-NLS-1$
-		"mStapOptPreference", //$NON-NLS-1$
-		"oStapOptPreference", //$NON-NLS-1$
-		"cStapOptPreference", //$NON-NLS-1$
-		"xStapOptPreference" //$NON-NLS-1$
-	};
-	
+	public static final int FLAG = 0;
+	public static final int LABEL = 1;
+	public static final int KEY = 2;
+	public static final int TOOLTIP = 3;
+
+	public static final String[][] STAP_BOOLEAN_OPTIONS  = new String[][] {
+		{"-k", "keep temporary directory", "kStapPreference", "keep temporary directory"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-u", "unoptimized translation", "uStapPreference", "unoptimized translation"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-b", "bulk (relayfs) mode", "bStapPreference", "bulk (relayfs) mode"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-t", "benchmarking timing information", "tStapPreference", "benchmarking timing information"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-v", "increase verbosity [0]", "vStapPreference", "increase verbosity [0]"}}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+
+	public static final String[][] STAP_STRING_OPTIONS = new String[][] {
+		{"-p", "NUM stop after pass NUM 1-5", "pStapPreference", "stop after pass NUM 1-5"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-s", "NUM buffer size in megabytes", "sStapPreference", "buffer size in megabytes"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-D", "NM=VAL emit macro definition into C code", "DStapPreference", "emit macro definition into C code"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-R", "DIR look in DIR for runtime", "RStapPreference", "look in DIR for runtime"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-r", "RELEASE use kernel RELEASE", "rStapPreference", "use kernel RELEASE"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-m", "MODULE set probe module name", "mStapPreference", "set probe module name"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-o", "FILE send output to file", "oStapPreference", "send output to file"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-c", "CMD run CMD under systemtap", "cStapPreference", "start the probes, run CMD, and exit when it finishes"},  //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+		{"-x", "PID sets target() to PID", "xStapPreference", "sets target() to PID"}}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+
 	//ide.editor.syntaxcoloring
 	public static final String P_STP_DEFAULT_COLOR = "stpDefaultColorPreference"; //$NON-NLS-1$
 	public static final String P_STP_KEYWORD_COLOR = "stpKeywordColorPreference"; //$NON-NLS-1$
@@ -83,5 +76,5 @@ public class IDEPreferenceConstants {
 	public static final String P_C_PREPROCESSOR_COLOR = "cPreprocessorColorPreference"; //$NON-NLS-1$
 	public static final String P_C_TYPE_COLOR = "cTypeColorPreference"; //$NON-NLS-1$
 	public static final String P_C_STRING_COLOR = "cStringColorPreference"; //$NON-NLS-1$
-	
+
 }
