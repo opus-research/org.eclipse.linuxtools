4ea6889e45951e47a31199cf6e80b3239efdf5bf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:32:54 +0000

    First commited as before_8939_rev2

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide.tests/src/org/eclipse/linuxtools/systemtap/ui/ide/test/editors/stp/STPCompletionProcessorTest.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide.tests/src/org/eclipse/linuxtools/systemtap/ui/ide/test/editors/stp/STPCompletionProcessorTest.java
@@ -17,7 +17,7 @@ import org.eclipse.linuxtools.tools.launch.core.factory.RuntimeProcessFactory;
 import org.junit.Test;
 
 public class STPCompletionProcessorTest {
-	
+
 	private static String TEST_STP_SCRIPT = ""+
 			"\n"+
 			"\n//marker1"+
@@ -76,7 +76,7 @@ public class STPCompletionProcessorTest {
 		ICompletionProposal[] proposals = completionProcessor
 				.computeCompletionProposals(testDocument,
 						offset);
-		
+
 		assertTrue(proposalsContain(proposals, "probe "));
 		assertTrue(proposalsContain(proposals, "global "));
 		assertTrue(proposalsContain(proposals, "function "));
@@ -84,39 +84,33 @@ public class STPCompletionProcessorTest {
 
 	@Test
 	public void testGlobalPartialCompletion() throws BadLocationException {
-		Document testDocument = new Document(TEST_STP_SCRIPT);
-		int offset = TEST_STP_SCRIPT.indexOf("//marker1");
 		String prefix = "prob";
-		testDocument.replace(offset, 0, prefix);
-		offset += prefix.length();
-
-		STPCompletionProcessor completionProcessor = new STPCompletionProcessor();
-		ICompletionProposal[] proposals = completionProcessor
-				.computeCompletionProposals(testDocument,
-						offset);
-		
+		ICompletionProposal[] proposals = getCompletionsForPrefix(prefix);
 		assertTrue(proposalsContain(proposals, "probe "));
 		assertTrue(!proposalsContain(proposals, "global "));
 		assertTrue(!proposalsContain(proposals, "function "));
 	}
-	
+
 	@Test
 	public void testProbeCompletion() throws BadLocationException {
 		assumeTrue(stapInstalled());
-
-		Document testDocument = new Document(TEST_STP_SCRIPT);
-		int offset = TEST_STP_SCRIPT.indexOf("//marker1");
 		String prefix = "probe ";
-		testDocument.replace(offset, 0, prefix);
-		offset += prefix.length();
-
-		STPCompletionProcessor completionProcessor = new STPCompletionProcessor();
-		ICompletionProposal[] proposals = completionProcessor
-				.computeCompletionProposals(testDocument,
-						offset);
-
+		ICompletionProposal[] proposals = getCompletionsForPrefix(prefix);
 		assertTrue(proposalsContain(proposals, "syscall"));
 		assertTrue(!proposalsContain(proposals, "syscall.write"));
+	}
+
+	@Test
+	public void testGlobalInvalidCompletion() throws BadLocationException {
+		ICompletionProposal[] proposals = getCompletionsForPrefix("probe fake.fake");
+		assertTrue(proposalsContain(proposals, "No completion data found."));
+	}
+
+	@Test
+	public void testStaticProbeCompletion() throws BadLocationException{
+		ICompletionProposal[] proposals = getCompletionsForPrefix("probe kernel.");
+		assertTrue(proposalsContain(proposals, "kernel.function(\"PATTERN\")"));
+		assertTrue(proposalsContain(proposals, "kernel.mark(\"MARK\")"));
 	}
 
 	@Test
@@ -137,12 +131,42 @@ public class STPCompletionProcessorTest {
 				.computeCompletionProposals(testDocument,
 						offset);
 
-		assertTrue(proposalsContain(proposals, "fd:long "));
+		assertTrue(proposalsContain(proposals, "fd:long"));
 		assertTrue(proposalsContain(proposals, "name:string"));
-		assertTrue(proposalsContain(proposals, "buf_uaddr:long "));
+		assertTrue(proposalsContain(proposals, "buf_uaddr:long"));
 	}
 
-	
+	@Test
+	public void testStaticProbeNormalizationCompletion() throws BadLocationException{
+		ICompletionProposal[] proposals = getCompletionsForPrefix("probe kernel.function(\"PATTERNASDF\").");
+		assertTrue(proposalsContain(proposals, "kernel.function(\"PATTERN\").return"));
+
+        proposals = getCompletionsForPrefix("probe probe process(\"PAT/H/\").");
+		assertTrue(proposalsContain(proposals, "process(\"PATH\").begin"));
+		assertTrue(proposalsContain(proposals, "process(\"PATH\").end"));
+
+        proposals = getCompletionsForPrefix("probe  process(123).");
+		assertTrue(proposalsContain(proposals, "process(PID).begin"));
+		assertTrue(proposalsContain(proposals, "process(PID).end"));
+
+        proposals = getCompletionsForPrefix("probe module(\"MPATTERasdfN\").");
+		assertTrue(proposalsContain(proposals, "module(\"MPATTERN\").function(\"PATTERN\")"));
+		assertTrue(proposalsContain(proposals, "module(\"MPATTERN\").statement(\"PATTERN\")"));
+	}
+
+	private ICompletionProposal[] getCompletionsForPrefix(String prefix) throws BadLocationException{
+		Document testDocument = new Document(TEST_STP_SCRIPT);
+		int offset = TEST_STP_SCRIPT.indexOf("//marker1");
+		testDocument.replace(offset, 0, prefix);
+		offset += prefix.length();
+
+		STPCompletionProcessor completionProcessor = new STPCompletionProcessor();
+		ICompletionProposal[] proposals = completionProcessor
+				.computeCompletionProposals(testDocument,
+						offset);
+		return proposals;
+	}
+
 	private boolean stapInstalled(){
 		try {
 			Process process = RuntimeProcessFactory.getFactory().exec(new String[]{"stap", "-V"}, null);
@@ -153,48 +177,9 @@ public class STPCompletionProcessorTest {
 		return false;
 	}
 
-	@Test
-	public void testGlobalInvalidCompletion() throws BadLocationException {
-		Document testDocument = new Document(TEST_STP_SCRIPT);
-		int offset = TEST_STP_SCRIPT.indexOf("//marker1");
-		String prefix = "probe fake.fake";
-		testDocument.replace(offset, 0, prefix);
-		offset += prefix.length();
-
-		STPCompletionProcessor completionProcessor = new STPCompletionProcessor();
-		ICompletionProposal[] proposals = completionProcessor
-				.computeCompletionProposals(testDocument,
-						offset);
-		
-		assertTrue(proposalsContain(proposals, "No completion data found."));
-	}
-
-	@Test
-	public void testFunctionCompletion() throws BadLocationException {
-		assumeTrue(stapInstalled());
-
-		Document testDocument = new Document(TEST_STP_SCRIPT);
-		@SuppressWarnings("unused")
-		MockSTPEditor editor = new MockSTPEditor(testDocument);
-
-		int offset = TEST_STP_SCRIPT.indexOf("//marker1");
-		String prefix = "probe syscall.write{user_}";
-		testDocument.replace(offset, 0, prefix);
-		offset += prefix.length() - 1;
-
-		STPCompletionProcessor completionProcessor = new STPCompletionProcessor();
-		ICompletionProposal[] proposals = completionProcessor
-				.computeCompletionProposals(testDocument,
-						offset);
-
-		assertTrue(proposalsContain(proposals, "user_int16"));
-		assertTrue(proposalsContain(proposals, "user_int32"));
-		assertTrue(proposalsContain(proposals, "user_int64"));
-	}
-
 	private boolean proposalsContain(ICompletionProposal[] proposals, String proposal){
 		for (ICompletionProposal p : proposals) {
-			if (p.getDisplayString().contains(proposal))
+			if (p.getDisplayString().equals(proposal))
 				return true;
 		}
 		return false;
