89c0e7c43a273e09253a121a84534d9a2d223d44
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:05:44 +0000

    First commited as before_9758_rev1

--- systemtap/org.eclipse.linuxtools.callgraph.tests/src/org/eclipse/linuxtools/callgraph/tests/SystemTapGraphViewTest.java
+++ systemtap/org.eclipse.linuxtools.callgraph.tests/src/org/eclipse/linuxtools/callgraph/tests/SystemTapGraphViewTest.java
@@ -4,33 +4,31 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     Red Hat - initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.linuxtools.callgraph.tests;
 
-import static org.junit.Assert.assertEquals;
+import junit.framework.TestCase;
 
 import org.eclipse.linuxtools.internal.callgraph.core.SystemTapTextView;
 import org.eclipse.linuxtools.internal.callgraph.core.ViewFactory;
-import org.junit.Test;
 
-public class SystemTapGraphViewTest {
+public class SystemTapGraphViewTest extends TestCase {
 	private SystemTapTextView stapView = new SystemTapTextView();
 	private String testText = "blah";
-
+	
 	//TODO: write some better tests here
-	@Test
 	public void test() {
 		stapView = (SystemTapTextView)  ViewFactory.createView("org.eclipse.linuxtools.callgraph.core.staptextview");
-
+		
 		stapView.println(testText);
 		assertEquals(stapView.getText(), testText);
-
+		
 		stapView.clearAll();
 		assertEquals(stapView.getText(), "");
 	}
-
+	
 }
