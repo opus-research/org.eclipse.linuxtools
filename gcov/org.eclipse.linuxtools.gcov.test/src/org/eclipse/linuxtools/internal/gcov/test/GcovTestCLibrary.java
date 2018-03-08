8381938510277d4ecff345c3794299d300da728b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:44:56 +0000

    First commited as after_11163_rev1

--- gcov/org.eclipse.linuxtools.gcov.test/src/org/eclipse/linuxtools/internal/gcov/test/GcovTestCLibrary.java
+++ gcov/org.eclipse.linuxtools.gcov.test/src/org/eclipse/linuxtools/internal/gcov/test/GcovTestCLibrary.java
@@ -9,7 +9,10 @@ import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.junit.FixMethodOrder;
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 @RunWith(SWTBotJunit4ClassRunner.class)
 public class GcovTestCLibrary {
 
