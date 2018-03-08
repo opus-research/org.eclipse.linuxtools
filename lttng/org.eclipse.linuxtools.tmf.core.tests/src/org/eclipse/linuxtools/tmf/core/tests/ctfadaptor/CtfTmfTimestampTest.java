013b422cfb8ee74717c851f963ab806fd7c4763a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:56:04 +0000

    First commited as after_9545_rev1

--- lttng/org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/ctfadaptor/CtfTmfTimestampTest.java
+++ lttng/org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/ctfadaptor/CtfTmfTimestampTest.java
@@ -47,7 +47,7 @@ public class CtfTmfTimestampTest {
         CtfTmfTimestamp result = new CtfTmfTimestamp(timestamp);
 
         assertNotNull(result);
-        assertEquals("00:00:00.000 000 001", result.toString()); //$NON-NLS-1$
+        assertEquals("19:00:00.000 000 001", result.toString()); //$NON-NLS-1$
         assertEquals(0, result.getPrecision());
         assertEquals(-9, result.getScale());
         assertEquals(1L, result.getValue());
