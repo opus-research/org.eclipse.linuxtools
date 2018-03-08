a5512d3c163f98fb016a84711197fdefa9a10d84
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:33:37 +0000

    First commited as after_12894_rev1

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/filter/ITmfFilter.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/filter/ITmfFilter.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2010, 2012 Ericsson
- * 
+ *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *   Patrick Tasse - Initial API and implementation
  *******************************************************************************/
@@ -16,7 +16,7 @@ import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
 
 /**
  * The TMF filter interface.
- * 
+ *
  * @version 1.0
  * @author Patrick Tasse
  */
@@ -24,10 +24,10 @@ public interface ITmfFilter {
 
 	/**
 	 * Verify the filter conditions on an event
-	 * 
+	 *
 	 * @param event The event to verify.
 	 * @return True if the event matches the filter conditions.
 	 */
-	public boolean matches(ITmfEvent event);
-	
+	boolean matches(ITmfEvent event);
+
 }
