ec186d40ed3c2508c6438eae5ea796d20742623b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:49:29 +0000

    First commited as before_7506_rev1

--- rpm/org.eclipse.linuxtools.rpm.core/src/org/eclipse/linuxtools/rpm/core/utils/Utils.java
+++ rpm/org.eclipse.linuxtools.rpm.core/src/org/eclipse/linuxtools/rpm/core/utils/Utils.java
@@ -60,7 +60,6 @@ public class Utils {
 	 * @throws IOException If an IOException occurs.
 	 * @deprecated use {@link Utils#runCommand(OutputStream, IProject, String...)} instead.
 	 */
-	@Deprecated
 	public static IStatus runCommand(final OutputStream outStream,
 			String... command) throws IOException {
 		return runCommand(null, null, command);
