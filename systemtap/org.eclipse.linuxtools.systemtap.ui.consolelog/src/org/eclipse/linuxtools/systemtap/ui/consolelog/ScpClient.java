88e046569c5fa1474065aa18f6a89886ea3c4190
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:14:55 +0000

    First commited as before_8419_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.consolelog/src/org/eclipse/linuxtools/systemtap/ui/consolelog/ScpClient.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.consolelog/src/org/eclipse/linuxtools/systemtap/ui/consolelog/ScpClient.java
@@ -1,38 +1,21 @@
 package org.eclipse.linuxtools.systemtap.ui.consolelog;
 
-import java.awt.Container;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
-import javax.swing.JPasswordField;
-import javax.swing.JTextField;
+import com.jcraft.jsch.*;
+import java.awt.*;
+import javax.swing.*;
 
 import org.eclipse.linuxtools.systemtap.ui.consolelog.dialogs.ErrorMessage;
 import org.eclipse.linuxtools.systemtap.ui.consolelog.internal.ConsoleLogPlugin;
 import org.eclipse.linuxtools.systemtap.ui.consolelog.preferences.ConsoleLogPreferenceConstants;
 
-import com.jcraft.jsch.Channel;
-import com.jcraft.jsch.ChannelExec;
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.Session;
-import com.jcraft.jsch.UIKeyboardInteractive;
-import com.jcraft.jsch.UserInfo;
+
+import java.io.*;
 
 public class ScpClient {
 	
 	private Session session;
   
-	public ScpClient() throws JSchException {
+	public ScpClient() throws Exception {
 	//public static void main(String[] arg){
      
   
@@ -55,7 +38,7 @@ public class ScpClient {
                       config.put("StrictHostKeyChecking", "no");
                       session.setConfig(config); 
       session.connect();
-      }catch(JSchException e)
+      }catch(Exception e)
       {
     	  e.printStackTrace(System.err);
     	  new ErrorMessage("Error in connection", "File Transfer failed.\n See stderr for more details").open();
@@ -63,7 +46,7 @@ public class ScpClient {
       }
     }
 
-    public void transfer(String fromFile, String toFile) throws IOException, JSchException{
+    public void transfer(String fromFile, String toFile) throws Exception {
       // exec 'scp -t rfile' remotely
     	FileInputStream fis=null;	
       String rfile=toFile;
@@ -124,7 +107,7 @@ public class ScpClient {
       session.disconnect();
       
     }
-    catch(IOException e){
+    catch(Exception e){
       try{if(fis!=null)fis.close();}catch(Exception ee){}
       throw e;
     }
