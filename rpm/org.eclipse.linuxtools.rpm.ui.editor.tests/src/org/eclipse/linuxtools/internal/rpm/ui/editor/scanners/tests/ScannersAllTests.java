5dca8f9ddfe014074e64342bf89edb591a7e8668
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:59:48 +0000

    First commited as before_16380_rev7

--- rpm/org.eclipse.linuxtools.rpm.ui.editor.tests/src/org/eclipse/linuxtools/internal/rpm/ui/editor/scanners/tests/ScannersAllTests.java
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor.tests/src/org/eclipse/linuxtools/internal/rpm/ui/editor/scanners/tests/ScannersAllTests.java
@@ -1,25 +1,20 @@
 /*******************************************************************************
- * Copyright (c) 2013 Red Hat, Inc.
+ * Copyright (c) 2009 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *    Red Hat - initial API and implementation
+ *     Red Hat - initial API and implementation
  *******************************************************************************/
-package org.eclipse.linuxtools.internal.rpm.ui.handlers;
+package org.eclipse.linuxtools.internal.rpm.ui.editor.scanners.tests;
 
-import org.eclipse.linuxtools.internal.rpm.ui.BuildType;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
 
-/**
- * Build Binary RPM.
- *
- * @since 1.0.0
- */
-public class SpecfileEditorBuildRPMSDelegate extends AbstractSpecfileEditorBuildDelegate {
-	@Override
-	protected BuildType getBuildType() {
-		return BuildType.BINARY;
-	}
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ SpecfileChangelogScannerTest.class,
+		SpecfilePackagesScannerTest.class, SpecfileScannerTest.class })
+public class ScannersAllTests {
 }
