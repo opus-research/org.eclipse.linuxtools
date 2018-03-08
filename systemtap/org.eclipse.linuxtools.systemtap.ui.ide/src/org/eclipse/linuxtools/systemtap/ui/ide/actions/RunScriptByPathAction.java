916641e9da0db6b7610415244895094b514a78ef
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:09:49 +0000

    First commited as after_8293_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/actions/RunScriptByPathAction.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/systemtap/ui/ide/actions/RunScriptByPathAction.java
@@ -11,6 +11,7 @@
 package org.eclipse.linuxtools.systemtap.ui.ide.actions;
 
 import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.linuxtools.systemtap.ui.ide.launcher.SystemtapScriptTester;
 import org.eclipse.ui.IWorkbenchWindow;
 
@@ -29,6 +30,10 @@ public class RunScriptByPathAction extends RunScriptBaseAction {
 	public void init(IWorkbenchWindow window, IPath path) {
 		super.init(window);
 		this.path = path;
+	}
+
+	public void init(IWorkbenchWindow window, String path) {
+		init(window, new Path(path));
 	}
 
 	@Override
