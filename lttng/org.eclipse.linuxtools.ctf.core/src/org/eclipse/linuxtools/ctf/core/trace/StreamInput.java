91a24ef87c8151f6d522b0684fb82162a0fc1f3e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:36:00 +0000

    First commited as after_18541_rev8

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/trace/StreamInput.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/trace/StreamInput.java
@@ -125,7 +125,7 @@ public class StreamInput implements IDefinitionScope {
 
     /**
      * Gets the filechannel of the streamInput. This is a limited Java
-     * ressource.
+     * resource.
      *
      * @return the filechannel
      */
@@ -143,7 +143,7 @@ public class StreamInput implements IDefinitionScope {
     }
 
     /**
-     * gets the last read timestamp of a stream. (this is not necessarily the
+     * Gets the last read timestamp of a stream. (this is not necessarily the
      * last time in the stream.)
      *
      * @return the last read timestamp
@@ -352,7 +352,6 @@ public class StreamInput implements IDefinitionScope {
                 throw new CTFReaderException(
                         "CTF magic mismatch " + Integer.toHexString(magic) + " vs " + Integer.toHexString(Utils.CTF_MAGIC)); //$NON-NLS-1$//$NON-NLS-2$
             }
-
         }
 
         /*
@@ -457,7 +456,7 @@ public class StreamInput implements IDefinitionScope {
 
         /* Read the end timestamp */
         if (tsEnd != null) {
-            if( tsEnd == -1 ) {
+            if (tsEnd == -1) {
                 tsEnd = Long.MAX_VALUE;
             }
             packetIndex.setTimestampEnd(tsEnd.longValue());
