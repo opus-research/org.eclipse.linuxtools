acf608b16c02f5622bebcefd26bd0fc20972e581
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:22:44 +0000

    First commited as after_12457_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/SystemTapScriptGraphOptionsTab.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/SystemTapScriptGraphOptionsTab.java
@@ -12,6 +12,7 @@
 package org.eclipse.linuxtools.internal.systemtap.ui.ide.launcher;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.debug.core.ILaunchConfiguration;
@@ -38,15 +39,15 @@ import org.eclipse.swt.events.SelectionListener;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
-import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Spinner;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.swt.widgets.Tree;
 import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.PlatformUI;
@@ -58,6 +59,14 @@ public class SystemTapScriptGraphOptionsTab extends
 	static final String RUN_WITH_CHART = "runWithChart"; //$NON-NLS-1$
 	static final String NUMBER_OF_COLUMNS = "numberOfColumns"; //$NON-NLS-1$
 	static final String REGEX_BOX = "regexBox_"; //$NON-NLS-1$
+
+	private static final String NUMBER_OF_GRAPHS = "numberOfGraphs"; //$NON-NLS-1$
+	private static final String GRAPH_TITLE = "graphTitle"; //$NON-NLS-1$
+	private static final String GRAPH_KEY = "graphKey"; //$NON-NLS-1$
+	private static final String GRAPH_X_SERIES = "graphXSeries"; //$NON-NLS-1$
+	private static final String GRAPH_ID = "graphID"; //$NON-NLS-1$
+	private static final String GRAPH_Y_SERIES_LENGTH = "graphYSeriesLength"; //$NON-NLS-1$
+	private static final String GRAPH_Y_SERIES = "graphYSeries"; //$NON-NLS-1$
 
 	private ModifyListener regExListener = new ModifyListener() {
 		@Override
@@ -79,7 +88,7 @@ public class SystemTapScriptGraphOptionsTab extends
 	private Group outputParsingGroup;
 	private Button runWithChartCheckButton;
 
-	private Tree treeGraphs;
+	private Table graphsTable;
 	private Button addGraphButton, removeGraphButton;
 	private TreeItem selectedTreeItem;
 	private Group graphsGroup;
@@ -126,6 +135,30 @@ public class SystemTapScriptGraphOptionsTab extends
 		return null;
 	}
 
+	public static LinkedList<GraphData> createGraphsFromConfiguration (ILaunchConfiguration configuration) throws CoreException {
+		LinkedList<GraphData> graphs = new LinkedList<GraphData>();
+		int n = configuration.getAttribute(NUMBER_OF_GRAPHS, 0);
+		for (int i = 0; i < n; i++) {
+			GraphData graphData = new GraphData();
+			graphData.title = configuration.getAttribute (GRAPH_TITLE + i, ""); //$NON-NLS-1$
+
+			graphData.key = configuration.getAttribute(GRAPH_KEY + i, ""); //$NON-NLS-1$
+			graphData.xSeries = configuration.getAttribute(GRAPH_X_SERIES + i, 0);
+			graphData.graphID = configuration.getAttribute(GRAPH_ID + i, ""); //$NON-NLS-1$
+
+			int ySeriesLength = configuration.getAttribute(GRAPH_Y_SERIES_LENGTH, 0);
+			int[] ySeries = new int[ySeriesLength];
+			for (int j = 0; j < ySeriesLength; j++) {
+				ySeries[j] = configuration.getAttribute(GRAPH_Y_SERIES + i + "_" + j, 0); //$NON-NLS-1$
+			}
+			graphData.ySeries = ySeries;
+
+			graphs.add(graphData);
+		}
+
+		return graphs;
+	}
+
 	@Override
 	public void createControl(Composite parent) {
 		GridLayout layout = new GridLayout();
@@ -155,13 +188,13 @@ public class SystemTapScriptGraphOptionsTab extends
 		outputParsingGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 		this.createColumnSelector(outputParsingGroup);
 
-		setGraphingEnabled(false);
-		runWithChartCheckButton.setSelection(false);
-
 		this.graphsGroup = new Group(top, SWT.SHADOW_ETCHED_IN);
-		graphsGroup.setText("Graphs:");
+		graphsGroup.setText(Messages.SystemTapScriptGraphOptionsTab_graphsTitle);
 		graphsGroup.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		createGraphCreateArea(graphsGroup);
+
+		setGraphingEnabled(false);
+		runWithChartCheckButton.setSelection(false);
 	}
 
 	protected void createColumnSelector(Composite parent) {
@@ -187,6 +220,7 @@ public class SystemTapScriptGraphOptionsTab extends
 			public void modifyText(ModifyEvent e) {
 				refreshRegexRows();
 				refreshRegEx();
+				updateLaunchConfigurationDialog();
 			}
 		});
 
@@ -233,21 +267,6 @@ public class SystemTapScriptGraphOptionsTab extends
 		refreshRegexRows();
 	}
 
-	private IDataSetParser getDatasetParser() {
-		int n;
-		n = this.numberOfColumnsSpinner.getSelection();
-		ArrayList<String> regEx = new ArrayList<String>(n * (COLUMNS - 1));
-		Control[] regExTexts = textFieldsComposite.getChildren();
-		for (int i = 0; i < (n * COLUMNS); i++) {
-			if (i % COLUMNS != 0) {
-				String text = ((Text)regExTexts[i]).getText();
-				regEx.add(text);
-			}
-		}
-
-		return new RowParser(regEx.toArray(new String[] {}));
-	}
-
 	private IDataSet getDataset() {
 		int n = this.numberOfColumnsSpinner.getSelection();
 		ArrayList<String> labels = new ArrayList<String>(n);
@@ -259,7 +278,6 @@ public class SystemTapScriptGraphOptionsTab extends
 				labels.add(text);
 			}
 		}
-
 		return DataSetFactory.createDataSet(RowDataSet.ID, labels.toArray(new String[] {}));
 	}
 
@@ -268,41 +286,33 @@ public class SystemTapScriptGraphOptionsTab extends
 		comp.setLayout(twoColumnsLayout);
 		twoColumnsLayout.numColumns = 2;
 
-		Composite treeComposite = new Composite(comp, SWT.NONE);
-		GridData layoutData = new GridData(GridData.FILL_BOTH);
-		layoutData.horizontalSpan = 2;
-		treeComposite.setLayoutData(layoutData);
-		GridLayout treeLayout = new GridLayout();
-		treeLayout.numColumns = 2;
-		treeLayout.makeColumnsEqualWidth = false;
-		treeComposite.setLayout(treeLayout);
-
-		treeGraphs = new Tree(treeComposite, SWT.SINGLE | SWT.BORDER);
-		layoutData = new GridData();
-		layoutData.verticalSpan = 3;
-		layoutData.grabExcessHorizontalSpace = true;
-		layoutData.horizontalAlignment = SWT.FILL;
-		treeGraphs.setLayoutData(layoutData);
+		graphsTable = new Table(comp, SWT.SINGLE | SWT.BORDER);
+		GridData layoutData = new GridData(SWT.FILL, SWT.FILL, true, true);
+		graphsTable.setLayoutData(layoutData);
 
 		// Button to add another graph
-		Composite buttonComposite = new Composite(treeComposite, SWT.None);
-		RowLayout buttonLayout = new RowLayout(SWT.VERTICAL);
-		buttonLayout.pack = false;
-		buttonLayout.marginHeight = 5;
-		buttonComposite.setLayout(buttonLayout);
+		Composite buttonComposite = new Composite(comp, SWT.NONE);
+		buttonComposite.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
+
+		GridLayout gridLayout = new GridLayout();
+		gridLayout.numColumns = 1;
+
+		buttonComposite.setLayout(gridLayout);
 		addGraphButton = new Button(buttonComposite, SWT.PUSH);
-		addGraphButton.setText("Add...");
-		addGraphButton.setToolTipText("Add a new graph");
+		addGraphButton.setText(Messages.SystemTapScriptGraphOptionsTab_AddGraphButton);
+		addGraphButton.setToolTipText(Messages.SystemTapScriptGraphOptionsTab_AddGraphButtonToolTip);
+		addGraphButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 
 		// Button to remove the selected graph/filter
 		removeGraphButton = new Button(buttonComposite, SWT.PUSH);
 		removeGraphButton.setText("Remove"); //$NON-NLS-1$
-		removeGraphButton.setToolTipText("Remove the selected graph");
+		removeGraphButton.setToolTipText(Messages.SystemTapScriptGraphOptionsTab_RemoveGraphButtonLabel);
 		removeGraphButton.setEnabled(false);
+		removeGraphButton.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
 
 		// Action to notify the buttons when to enable/disable themselves based
 		// on list selection
-		treeGraphs.addSelectionListener(new SelectionAdapter() {
+		graphsTable.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				selectedTreeItem = (TreeItem) e.item;
@@ -324,11 +334,13 @@ public class SystemTapScriptGraphOptionsTab extends
 				dialog.open();
 
 				GraphData gd = wizard.getGraphData();
+
 				if (null != gd) {
-					TreeItem item = new TreeItem(treeGraphs, SWT.NONE);
+					TableItem item = new TableItem(graphsTable, SWT.NONE);
 					item.setText(GraphFactory.getGraphName(gd.graphID) + ":" //$NON-NLS-1$
 							+ gd.title);
 					item.setData(gd);
+					updateLaunchConfigurationDialog();
 				}
 			}
 		});
@@ -339,6 +351,7 @@ public class SystemTapScriptGraphOptionsTab extends
 			public void widgetSelected(SelectionEvent e) {
 				selectedTreeItem.dispose();
 				removeGraphButton.setEnabled(false);
+				updateLaunchConfigurationDialog();
 			}
 		});
 	}
@@ -411,6 +424,7 @@ public class SystemTapScriptGraphOptionsTab extends
 	public void setDefaults(ILaunchConfigurationWorkingCopy configuration) {
 		configuration.setAttribute(RUN_WITH_CHART, false);
 		configuration.setAttribute(NUMBER_OF_COLUMNS, 3);
+		configuration.setAttribute(NUMBER_OF_GRAPHS, 0);
 	}
 
 	@Override
@@ -429,6 +443,17 @@ public class SystemTapScriptGraphOptionsTab extends
 					((Text)textBoxes[i]).setText(text);
 				}
 			}
+
+			// Add graphs
+			graphsTable.removeAll();
+			LinkedList<GraphData> graphs = createGraphsFromConfiguration(configuration);
+			for (GraphData graphData : graphs) {
+				TableItem item = new TableItem(graphsTable, SWT.NONE);
+				item.setText(GraphFactory.getGraphName(graphData.graphID) + ":" //$NON-NLS-1$
+						+ graphData.title);
+				item.setData(graphData);
+			}
+
 		} catch (CoreException e) {
 			ExceptionErrorDialog.openError(Messages.SystemTapScriptGraphOptionsTab_5, e);
 		}
@@ -444,6 +469,23 @@ public class SystemTapScriptGraphOptionsTab extends
 		for (int i = 0; i < textBoxes.length; i++) {
 			String text = ((Text)textBoxes[i]).getText();
 			configuration.setAttribute(REGEX_BOX+i, text);
+		}
+
+		// Save graphs.
+		TableItem[] list = this.graphsTable.getItems();
+		configuration.setAttribute(NUMBER_OF_GRAPHS, list.length);
+		for (int i = 0; i < list.length; i++) {
+			GraphData graphData = (GraphData)list[i].getData();
+			configuration.setAttribute(GRAPH_TITLE + i, graphData.title);
+
+			configuration.setAttribute(GRAPH_KEY + i, graphData.key);
+			configuration.setAttribute(GRAPH_X_SERIES + i, graphData.xSeries);
+			configuration.setAttribute(GRAPH_ID + i, graphData.graphID);
+
+			configuration.setAttribute(GRAPH_Y_SERIES_LENGTH, graphData.ySeries.length);
+			for (int j = 0; j < graphData.ySeries.length; j++) {
+				configuration.setAttribute(GRAPH_Y_SERIES + i + "_" + j, graphData.ySeries[j]); //$NON-NLS-1$
+			}
 		}
 	}
 
@@ -482,6 +524,7 @@ public class SystemTapScriptGraphOptionsTab extends
 
 	private void setGraphingEnabled(boolean enabled){
 		this.setControlEnabled(outputParsingGroup, enabled);
+		this.setControlEnabled(graphsGroup, enabled);
 		updateLaunchConfigurationDialog();
 	}
 
