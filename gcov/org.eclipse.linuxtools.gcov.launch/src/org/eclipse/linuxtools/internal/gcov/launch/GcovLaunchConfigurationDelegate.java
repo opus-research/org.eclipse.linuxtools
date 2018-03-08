e1cd7d42913da2a08bd6b7805a989d7a1d85eb1e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:31:40 +0000

    First commited as before_15138_rev2

--- gcov/org.eclipse.linuxtools.gcov.launch/src/org/eclipse/linuxtools/internal/gcov/launch/GcovLaunchConfigurationDelegate.java
+++ gcov/org.eclipse.linuxtools.gcov.launch/src/org/eclipse/linuxtools/internal/gcov/launch/GcovLaunchConfigurationDelegate.java
@@ -15,7 +15,6 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.gcov.launch;
 
-import java.io.File;
 import java.net.URI;
 import java.util.List;
 
@@ -55,17 +54,15 @@ public class GcovLaunchConfigurationDelegate extends ProfileLaunchConfigurationD
 		//set up and launch the local c/c++ program
 		IRemoteCommandLauncher launcher = RemoteProxyManager.getInstance().getLauncher(getProject());
 
-		File workDir = getWorkingDirectory(config);
-		if (workDir == null) {
-			workDir = new File(System.getProperty("user.home", ".")); //$NON-NLS-1$ //$NON-NLS-2$
-		}
+		URI workingDirURI = getProject().getLocationURI();
+		IPath workingDirPath = new Path(workingDirURI.getPath());
 		String arguments[] = getProgramArgumentsArray( config );
 
 		//add a listener for termination of the launch
 		ILaunchManager lmgr = DebugPlugin.getDefault().getLaunchManager();
 		lmgr.addLaunchListener(new LaunchTerminationWatcher(launch, exePath));
 
-		Process process = launcher.execute(exePath, arguments, getEnvironment(config), new Path(workDir.getAbsolutePath()), monitor);
+		Process process = launcher.execute(exePath, arguments, getEnvironment(config), workingDirPath, monitor);
 
 		DebugPlugin.newProcess( launch, process, renderProcessLabel( exePath.toOSString() ) );
 
