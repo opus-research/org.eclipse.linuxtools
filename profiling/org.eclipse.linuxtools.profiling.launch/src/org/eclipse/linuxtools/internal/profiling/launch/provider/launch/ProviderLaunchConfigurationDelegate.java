a5d26f34d95d4ce12d5a54dac35e358eb8a6d86e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:19:22 +0000

    First commited as before_8601_rev2

--- profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/internal/profiling/launch/provider/launch/ProviderLaunchConfigurationDelegate.java
+++ profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/internal/profiling/launch/provider/launch/ProviderLaunchConfigurationDelegate.java
@@ -28,22 +28,26 @@ public class ProviderLaunchConfigurationDelegate extends
 
 	@Override
 	public void launch(ILaunchConfiguration config, String mode,
-			ILaunch launch, IProgressMonitor monitor) throws CoreException {
+			ILaunch launch, IProgressMonitor monitor) {
+		try {
 
-		if (config != null) {
-			// get provider id from configuration.
-			String providerId = config.getAttribute(
-					ProviderProfileConstants.PROVIDER_CONFIG_ATT, "");
+			if (config != null) {
+				// get provider id from configuration.
+				String providerId = config.getAttribute(
+						ProviderProfileConstants.PROVIDER_CONFIG_ATT, "");
 
-			// get delegate associated with provider id.
-			ProfileLaunchConfigurationDelegate delegate = ProviderFramework
-					.getConfigurationDelegateFromId(providerId);
+				// get delegate associated with provider id.
+				ProfileLaunchConfigurationDelegate delegate = ProviderFramework.getConfigurationDelegateFromId(providerId);
 
-			// launch delegate
-			if (delegate != null) {
-				delegate.launch(config, mode, launch, monitor);
+				// launch delegate
+				if (delegate != null) {
+					delegate.launch(config, mode, launch, monitor);
+				}
 			}
+		} catch (CoreException e) {
+			e.printStackTrace();
 		}
+		return;
 	}
 
 	/**
