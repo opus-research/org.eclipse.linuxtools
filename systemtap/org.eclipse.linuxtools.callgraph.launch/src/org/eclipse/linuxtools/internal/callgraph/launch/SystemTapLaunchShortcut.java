98f283c38f9388928c1ae70fd76009f53afc832f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:54:49 +0000

    First commited as before_13655_rev1

--- systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapLaunchShortcut.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapLaunchShortcut.java
@@ -130,6 +130,7 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 	private Button OKButton;
 	private boolean testMode = false;
 	protected String secondaryID = ""; //$NON-NLS-1$
+	private final String [] escapableChars = new String []  {"(", ")", " "}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 
 	/**
 	 * Initialize variables. Highly recommend calling this function within the
@@ -232,6 +233,7 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 				wc.setAttribute(LaunchConfigurationConstants.BINARY_PATH,binaryPath);
 			}
 
+			outputPath = escapeSpecialCharacters(outputPath);
 			wc.setAttribute(LaunchConfigurationConstants.OUTPUT_PATH,outputPath);
 			wc.setAttribute(LaunchConfigurationConstants.ARGUMENTS, arguments);
 			wc.setAttribute(LaunchConfigurationConstants.GENERATED_SCRIPT,generatedScript);
@@ -635,7 +637,7 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 							if (!(validElement(c))) {
 								continue;
 							}
-							if (c.getElementName().contains("main") && !output.contains(c)) { //$NON-NLS-1$
+							if (c.getElementName().contains("main") && !output.contains(c)) {
 								output.add(c);
 							}
 						}
@@ -794,7 +796,7 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 	}
 
 	/**
-	 * Function for generating scripts. Should be overridden by interested
+	 * Function for generating scripts. Should be overriden by interested
 	 * classes
 	 *
 	 * @throws IOException
@@ -933,6 +935,7 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 
 			ILaunchConfigurationWorkingCopy wc = createConfiguration(bin, name);
 			binaryPath = bin.getResource().getLocation().toString();
+			binaryPath = escapeSpecialCharacters(binaryPath);
 			arguments = binaryPath;
 			outputPath = PluginConstants.getDefaultIOPath();
 			finishLaunch(name, mode, wc);
@@ -968,4 +971,18 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 		return ""; //$NON-NLS-1$
 	}
 
+	/**
+	 * Escapes special characters in the target string
+	 *
+	 * @param script the script to be executed by the shell.
+	 * @return the formatted string that will be executed.
+	 */
+	protected String escapeSpecialCharacters(String str) {
+		// Modify script to catch escapable characters.
+		String res = str;
+		for (int i = 0; i < escapableChars.length; i++) {
+			res = res.replace(escapableChars[i], "\\" + escapableChars[i]); //$NON-NLS-1$
+		}
+		return res;
+	}
 }
