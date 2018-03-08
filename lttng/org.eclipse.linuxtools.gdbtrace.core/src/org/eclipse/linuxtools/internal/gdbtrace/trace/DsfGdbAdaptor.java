204d977ef8b2ab9b50487dfda345c53e1e9a5be6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:42:08 +0000

    First commited as before_11112_rev2

--- lttng/org.eclipse.linuxtools.gdbtrace.core/src/org/eclipse/linuxtools/internal/gdbtrace/trace/DsfGdbAdaptor.java
+++ lttng/org.eclipse.linuxtools.gdbtrace.core/src/org/eclipse/linuxtools/internal/gdbtrace/trace/DsfGdbAdaptor.java
@@ -33,6 +33,7 @@ import org.eclipse.cdt.dsf.debug.service.IRunControl.IContainerDMContext;
 import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService;
 import org.eclipse.cdt.dsf.debug.service.command.ICommandControlService.ICommandControlDMContext;
 import org.eclipse.cdt.dsf.gdb.internal.GdbPlugin;
+import org.eclipse.cdt.dsf.gdb.internal.ui.GdbUIPlugin;
 import org.eclipse.cdt.dsf.gdb.launching.GdbLaunch;
 import org.eclipse.cdt.dsf.gdb.service.GDBTraceControl_7_2.TraceRecordSelectedChangedEvent;
 import org.eclipse.cdt.dsf.gdb.service.IGDBTraceControl;
@@ -317,12 +318,17 @@ public class DsfGdbAdaptor {
     public void dispose() {
         if (fLaunch != null && fLaunch.canTerminate() && !isTerminating) {
             isTerminating = true;
-            try {
-                fLaunch.terminate();
-            } catch (DebugException e) {
-                e.printStackTrace();
-            }
-            fLaunch = null;
+            GdbUIPlugin.getShell().getDisplay().asyncExec(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        fLaunch.terminate();
+                    } catch (DebugException e) {
+                        e.printStackTrace();
+                    }
+                    fLaunch = null;
+                }
+            });
         }
     }
 
