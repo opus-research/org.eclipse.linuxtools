dd18ff4a57036488038bf729ba25b31173aa0bf9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:57:34 +0000

    First commited as before_16304_rev1

--- rpm/org.eclipse.linuxtools.rpm.ui/src/org/eclipse/linuxtools/internal/rpm/ui/Activator.java
+++ rpm/org.eclipse.linuxtools.rpm.ui/src/org/eclipse/linuxtools/internal/rpm/ui/Activator.java
@@ -1,37 +1,28 @@
 /*******************************************************************************
- * Copyright (c) 2009 Red Hat, Inc.
+ * Copyright (c) 2013 Red Hat Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *     Red Hat - initial API and implementation
+ *     Red Hat Inc. - initial API and implementation
  *******************************************************************************/
-package org.eclipse.linuxtools.internal.callgraph.launch;
+package org.eclipse.linuxtools.internal.rpm.ui;
 
-
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.linuxtools.internal.callgraph.core.LaunchConfigurationConstants;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
- *
- * This is more or less the default Eclipse plugin-wizard Activator.
  */
-public class CallgraphLaunchPlugin extends AbstractUIPlugin {
+public class Activator extends AbstractUIPlugin {
+
+	/** The symbolic ID of the plugin */
+	public static final String PLUGIN_ID = "org.eclipse.linuxtools.rpm.ui"; //$NON-NLS-1$
 
 	// The shared instance
-	private static CallgraphLaunchPlugin plugin;
-
-	/**
-	 * The constructor
-	 */
-	public CallgraphLaunchPlugin() {
-	}
+	private static Activator plugin;
 
 	/*
 	 * (non-Javadoc)
@@ -41,9 +32,7 @@ public class CallgraphLaunchPlugin extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
-
 	}
-
 
 	/*
 	 * (non-Javadoc)
@@ -55,30 +44,12 @@ public class CallgraphLaunchPlugin extends AbstractUIPlugin {
 		super.stop(context);
 	}
 
-
-
 	/**
 	 * Returns the shared instance
 	 *
 	 * @return the shared instance
 	 */
-	public static CallgraphLaunchPlugin getDefault() {
+	public static Activator getDefault() {
 		return plugin;
 	}
-
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(LaunchConfigurationConstants.PLUGIN_ID, path);
-	}
-
-	public static Shell getActiveWorkbenchShell() {
-		return getDefault().getWorkbench().getActiveWorkbenchWindow().getShell();
-	}
-
 }
