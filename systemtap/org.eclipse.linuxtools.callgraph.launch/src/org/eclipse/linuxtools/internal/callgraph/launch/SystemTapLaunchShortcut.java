1b03d6a1cef10847876436cbbaf7ac9b429675cf
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:54:39 +0000

    First commited as before_11509_rev1

--- systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapLaunchShortcut.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapLaunchShortcut.java
@@ -129,7 +129,17 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 	private Button OKButton;
 	private boolean testMode = false;
 	protected String secondaryID = ""; //$NON-NLS-1$
-	private final String [] escapableChars = new String []  {"(", ")", " "}; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+	private final String [] escapableChars = new String []  {"(", ")", " "}; //$NON-NLS-1$
+
+	/**
+	 * Provides access to the Profiling Frameworks' launch method
+	 *
+	 * @param editor
+	 * @param mode
+	 */
+	public void reLaunch(IEditorPart editor, String mode) {
+		launch(editor, mode);
+	}
 
 	/**
 	 * Initialize variables. Highly recommend calling this function within the
@@ -693,7 +703,7 @@ public abstract class SystemTapLaunchShortcut extends ProfileLaunchShortcut {
 						output += numberOfValidFiles(((ICContainer) ele)
 								.getChildren());
 					}
-					if (validElement(ele)) {
+					if ((ele instanceof ICElement) && validElement(ele)) {
 						output++;
 					}
 				}
