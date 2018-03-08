a2bf307314f6cc9825062c15d2884c4d73fd32d4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:48:18 +0000

    First commited as before_15928_rev3

--- valgrind/org.eclipse.linuxtools.valgrind.launch/src/org/eclipse/linuxtools/internal/valgrind/launch/ClearMarkersHandler.java
+++ valgrind/org.eclipse.linuxtools.valgrind.launch/src/org/eclipse/linuxtools/internal/valgrind/launch/ClearMarkersHandler.java
@@ -7,11 +7,12 @@
  *
  * Contributors:
  *    Elliott Baron <ebaron@redhat.com> - initial API and implementation
- *******************************************************************************/
+ *******************************************************************************/ 
 package org.eclipse.linuxtools.internal.valgrind.launch;
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -21,8 +22,7 @@ import org.eclipse.swt.widgets.Display;
 
 public class ClearMarkersHandler extends AbstractHandler {
 
-	@Override
-	public Object execute(ExecutionEvent event) {
+	public Object execute(ExecutionEvent event) throws ExecutionException {
 		IWorkspaceRoot root = ResourcesPlugin.getWorkspace().getRoot();
 		try {
 			root.deleteMarkers(ValgrindLaunchPlugin.MARKER_TYPE, true, IResource.DEPTH_INFINITE);
@@ -31,12 +31,11 @@ public class ClearMarkersHandler extends AbstractHandler {
 		}
 		// Clear Valgrind view
 		Display.getDefault().syncExec(new Runnable() {
-			@Override
 			public void run() {
 				ValgrindUIPlugin.getDefault().resetView();
-			}
+			}							
 		});
 		return null;
-	}
+	}	
 
 }
