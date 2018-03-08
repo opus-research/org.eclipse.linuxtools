02990a621b84677e04f88921cb20053a6f3f64b3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:08:55 +0000

    First commited as after_8288_rev5

---
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/Messages.java
@@ -0,0 +1,25 @@
+package org.eclipse.linuxtools.internal.systemtap.ui.ide.launcher;
+
+import org.eclipse.osgi.util.NLS;
+
+
+public class Messages extends NLS {
+	private static final String BUNDLE_NAME = "org.eclipse.linuxtools.internal.systemtap.ui.ide.launcher.messages"; //$NON-NLS-1$
+	public static String SystemTapScriptLaunchConfigurationTab_0;
+	public static String SystemTapScriptLaunchConfigurationTab_1;
+	public static String SystemTapScriptLaunchConfigurationTab_2;
+	public static String SystemTapScriptLaunchConfigurationTab_3;
+	public static String SystemTapScriptLaunchConfigurationTab_4;
+	public static String SystemTapScriptLaunchConfigurationTab_5;
+	public static String SystemTapScriptLaunchConfigurationTab_6;
+	public static String SystemTapScriptLaunchConfigurationTab_7;
+	public static String SystemTapScriptLaunchConfigurationTab_8;
+	public static String SystemTapScriptLaunchConfigurationTab_9;
+	static {
+		// initialize resource bundle
+		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
+	}
+
+	private Messages() {
+	}
+}
