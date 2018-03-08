6378b196ae50636a49efa2c7da6e79bca3df525d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:39:24 +0000

    First commited as before_18695_rev6

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/BatchImportTraceHandler.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/project/handlers/BatchImportTraceHandler.java
@@ -15,6 +15,7 @@ package org.eclipse.linuxtools.internal.tmf.ui.project.handlers;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.ISelectionProvider;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -22,12 +23,12 @@ import org.eclipse.jface.viewers.TreeSelection;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.linuxtools.tmf.ui.project.model.TmfTraceFolder;
 import org.eclipse.linuxtools.tmf.ui.project.wizards.importtrace.BatchImportTraceWizard;
-import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 /**
  * Batch import handler, spawn a wizard
@@ -39,26 +40,32 @@ public class BatchImportTraceHandler extends AbstractHandler {
 
     @Override
     public Object execute(ExecutionEvent event) throws ExecutionException {
-        TmfTraceFolder traceFolder = getTraceFolder();
-        if (traceFolder == null) {
+        // Fire the Import Trace Wizard
+        IWorkbench workbench = PlatformUI.getWorkbench();
+        if (workbench == null) {
             return null;
         }
 
-        // Fire the Import Trace Wizard
-        IWorkbench workbench = PlatformUI.getWorkbench();
-        if (workbench != null) {
-            final IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
-            if (activeWorkbenchWindow != null) {
-                Shell shell = activeWorkbenchWindow.getShell();
-
-                BatchImportTraceWizard wizard = new BatchImportTraceWizard();
-                wizard.init(PlatformUI.getWorkbench(), new StructuredSelection(traceFolder));
-                WizardDialog dialog = new WizardDialog(shell, wizard);
-                dialog.open();
-
-                traceFolder.refresh();
-            }
+        final IWorkbenchWindow activeWorkbenchWindow = workbench.getActiveWorkbenchWindow();
+        if (activeWorkbenchWindow == null) {
+            return null;
         }
+
+        IStructuredSelection selection = new StructuredSelection(HandlerUtil.getCurrentSelection(event));
+        TmfTraceFolder traceFolder = getTraceFolder();
+        if (traceFolder != null) {
+            selection = new StructuredSelection(traceFolder);
+        }
+
+        BatchImportTraceWizard wizard = new BatchImportTraceWizard();
+        wizard.init(PlatformUI.getWorkbench(), selection);
+        WizardDialog dialog = new WizardDialog(activeWorkbenchWindow.getShell(), wizard);
+        dialog.open();
+
+        if (traceFolder != null) {
+            traceFolder.refresh();
+        }
+
         return null;
     }
 
