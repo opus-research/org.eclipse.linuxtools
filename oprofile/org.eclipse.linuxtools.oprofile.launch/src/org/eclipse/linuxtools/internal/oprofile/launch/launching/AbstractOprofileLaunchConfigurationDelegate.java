8b00022f26729108a764b2f5b50c1b2a50ef0986
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:03:05 +0000

    First commited as before_8006_rev3

--- oprofile/org.eclipse.linuxtools.oprofile.launch/src/org/eclipse/linuxtools/internal/oprofile/launch/launching/AbstractOprofileLaunchConfigurationDelegate.java
+++ oprofile/org.eclipse.linuxtools.oprofile.launch/src/org/eclipse/linuxtools/internal/oprofile/launch/launching/AbstractOprofileLaunchConfigurationDelegate.java
@@ -84,17 +84,11 @@ public abstract class AbstractOprofileLaunchConfigurationDelegate extends Profil
 		IPath workingDirPath = new Path(workingDirURI.getPath());
 
 		String arguments[] = getProgramArgumentsArray( config );
-		Process process = null;
-		for(int i = 0; i < options.getExecutionsNumber(); i++){
-			process = launcher.execute(exePath, arguments, getEnvironment(config), workingDirPath, monitor);
-			DebugPlugin.newProcess( launch, process, renderProcessLabel( exePath.toOSString() ) );
-			try {
-				process.waitFor();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		postExec(options, daemonEvents, process);
+		Process process = launcher.execute(exePath, arguments, getEnvironment(config), workingDirPath, monitor);
+
+		DebugPlugin.newProcess( launch, process, renderProcessLabel( exePath.toOSString() ) );
+
+			postExec(options, daemonEvents, process);
 	}
 	
 	protected abstract boolean preExec(LaunchOptions options, OprofileDaemonEvent[] daemonEvents, ILaunch launch);
