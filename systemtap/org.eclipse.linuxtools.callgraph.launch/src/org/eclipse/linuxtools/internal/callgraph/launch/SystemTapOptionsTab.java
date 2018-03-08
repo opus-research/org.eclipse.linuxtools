0d4830e7a11757ff5f764c61d2d0c1bc2218bc74
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:49:30 +0000

    First commited as before_7507_rev1

--- systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapOptionsTab.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch/src/org/eclipse/linuxtools/internal/callgraph/launch/SystemTapOptionsTab.java
@@ -894,7 +894,7 @@ public class SystemTapOptionsTab extends CLaunchConfigurationTab{
 			ICProject project =  CDebugUtils.verifyCProject(config);
 			IBinary[] binaries = project.getBinaryContainer().getBinaries();
 			if (binaries != null && binaries.length > 0) {
-				if (binaries.length == 1 && binaries[0] != null) {
+				if (binaries.length == 1 && binaries[0] instanceof IBinary) {
 					return binaries[0];
 				} else
 					return chooseBinary(binaries);
