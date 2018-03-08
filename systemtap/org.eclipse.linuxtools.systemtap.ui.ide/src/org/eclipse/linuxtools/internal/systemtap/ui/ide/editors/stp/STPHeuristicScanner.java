af6bcb101e69dd3721b51217356fb044a0c2b164
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:15:04 +0000

    First commited as before_14295_rev10

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/editors/stp/STPHeuristicScanner.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/editors/stp/STPHeuristicScanner.java
@@ -363,7 +363,7 @@ public final class STPHeuristicScanner implements STPSymbols {
 					++fPos;
 					if (peekNextChar() == LANGLE) {
 						++fPos;
-						return TokenAggregate;
+						return TokenAGGREGATE;
 					}
 					return TokenSHIFTLEFT;
 				case EQUAL:
@@ -940,7 +940,6 @@ public final class STPHeuristicScanner implements STPSymbols {
 	public boolean isDefaultPartition(int position) {
 		String type = getPartition(position).getType();
 		return fPartition.equals(type);
-//				|| STPPartitionScanner.STP_PROBE.equals(type);
 	}
 
 	/**
