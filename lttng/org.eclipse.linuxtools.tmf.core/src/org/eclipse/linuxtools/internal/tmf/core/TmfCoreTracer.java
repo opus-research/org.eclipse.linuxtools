72c09baf731bb873b9335a51e390c1f2e0f78260
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:00:53 +0000

    First commited as before_9651_rev1

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/TmfCoreTracer.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/TmfCoreTracer.java
@@ -69,7 +69,7 @@ public class TmfCoreTracer {
     static Boolean EVENT_CLASS_ENABLED     = Boolean.FALSE;
 
     // Trace log file
-    private static volatile BufferedWriter fTraceFile;
+    private static BufferedWriter fTraceFile;
 
     // ------------------------------------------------------------------------
     // Start/stop tracing - controlled by the plug-in
