e13851dc3812c1ed39b1c671953f04ccb28006a7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:13:17 +0000

    First commited as after_12160_rev1

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/trace/Metadata.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/trace/Metadata.java
@@ -1,13 +1,14 @@
 /*******************************************************************************
- * Copyright (c) 2011-2012 Ericsson, Ecole Polytechnique de Montreal and others
+ * Copyright (c) 2011, 2013 Ericsson, Ecole Polytechnique de Montreal and others
  *
  * All rights reserved. This program and the accompanying materials are made
  * available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors: Matthew Khouzam - Initial API and implementation
- * Contributors: Simon Marchi - Initial API and implementation
+ * Contributors:
+ *     Matthew Khouzam - Initial API and implementation
+ *     Simon Marchi - Initial API and implementation
  *******************************************************************************/
 
 package org.eclipse.linuxtools.ctf.core.trace;
@@ -343,6 +344,9 @@ public class Metadata {
 
         /* Extract the text from the packet */
         int payloadSize = ((header.contentSize / 8) - METADATA_PACKET_HEADER_SIZE);
+        if (payloadSize < 0) {
+            throw new CTFReaderException("Invalid metadata packet payload size."); //$NON-NLS-1$
+        }
         int skipSize = (header.packetSize - header.contentSize) / 8;
 
         /* Read the payload + the padding in a ByteBuffer */
