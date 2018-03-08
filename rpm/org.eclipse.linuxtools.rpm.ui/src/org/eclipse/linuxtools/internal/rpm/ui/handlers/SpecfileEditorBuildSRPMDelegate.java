775ec3422c798e7d6e2311f590271847f75df728
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:59:41 +0000

    First commited as after_16380_rev4

---
+++ rpm/org.eclipse.linuxtools.rpm.ui/src/org/eclipse/linuxtools/internal/rpm/ui/handlers/SpecfileEditorBuildSRPMDelegate.java
@@ -0,0 +1,25 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Red Hat, Inc.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Red Hat - initial API and implementation
+ *******************************************************************************/
+package org.eclipse.linuxtools.internal.rpm.ui.handlers;
+
+import org.eclipse.linuxtools.internal.rpm.ui.BuildType;
+
+/**
+ * Build Source RPM.
+ *
+ * @since 1.0.0
+ */
+public class SpecfileEditorBuildSRPMDelegate extends AbstractSpecfileEditorBuildDelegate {
+	@Override
+	protected BuildType getBuildType() {
+		return BuildType.SOURCE;
+	}
+}
