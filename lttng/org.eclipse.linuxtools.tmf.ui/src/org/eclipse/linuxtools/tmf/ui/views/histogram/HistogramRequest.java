c5ebb56e119943ca800f396197c6548764b2b9ce
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:24:18 +0000

    First commited as before_18139_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/histogram/HistogramRequest.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/histogram/HistogramRequest.java
@@ -22,7 +22,6 @@ import org.eclipse.linuxtools.tmf.core.request.ITmfDataRequest;
 import org.eclipse.linuxtools.tmf.core.request.TmfEventRequest;
 import org.eclipse.linuxtools.tmf.core.timestamp.ITmfTimestamp;
 import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimeRange;
-import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
 
 /**
  * Class to request events for given time range from a trace to fill a
@@ -71,9 +70,7 @@ public class HistogramRequest extends TmfEventRequest {
     public HistogramRequest(HistogramDataModel histogram, TmfTimeRange range,
             int rank, int nbEvents, int blockSize,
             ITmfDataRequest.ExecutionType execType) {
-        super(ITmfEvent.class, range, rank, nbEvents,
-                (blockSize > 0) ? blockSize : ITmfTrace.DEFAULT_TRACE_CACHE_SIZE,
-                execType);
+        super(ITmfEvent.class, range, rank, nbEvents, execType);
         fHistogram = histogram;
         if (execType == ExecutionType.FOREGROUND) {
             fFullRange = false;
@@ -105,9 +102,7 @@ public class HistogramRequest extends TmfEventRequest {
     public HistogramRequest(HistogramDataModel histogram, TmfTimeRange range,
             int rank, int nbEvents, int blockSize,
             ITmfDataRequest.ExecutionType execType, boolean fullRange) {
-        super(ITmfEvent.class, range, rank, nbEvents,
-                (blockSize > 0) ? blockSize : ITmfTrace.DEFAULT_TRACE_CACHE_SIZE,
-                execType);
+        super(ITmfEvent.class, range, rank, nbEvents, execType);
         fHistogram = histogram;
         fFullRange = fullRange;
     }
