6414bf5a8dddeb5474a5784c360fc441f6b742f8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:02:12 +0000

    First commited as before_13789_rev4

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/HistoryBuilder.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/internal/tmf/core/statesystem/HistoryBuilder.java
@@ -29,7 +29,7 @@ import org.eclipse.linuxtools.tmf.core.statesystem.ITmfStateSystem;
 import org.eclipse.linuxtools.tmf.core.statesystem.ITmfStateSystemBuilder;
 import org.eclipse.linuxtools.tmf.core.timestamp.TmfTimeRange;
 import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
-import org.eclipse.linuxtools.tmf.core.trace.TmfTraceManager;
+import org.eclipse.linuxtools.tmf.core.trace.TmfExperiment;
 
 /**
  * This is the high-level wrapper around the State History and its provider and
@@ -192,10 +192,16 @@ public class HistoryBuilder extends TmfComponent {
      * this trace.
      */
     private boolean signalIsForUs(ITmfTrace sender) {
-        for (ITmfTrace childtrace : TmfTraceManager.getTraceSet(sender)) {
-            if (sp.getTrace() == childtrace) {
-                return true;
+        if (sender instanceof TmfExperiment) {
+            /* Yeah doing a lazy instanceof check here, but it's a special case! */
+            TmfExperiment exp = (TmfExperiment) sender;
+            for (ITmfTrace childTrace : exp.getTraces()) {
+                if (childTrace == sp.getTrace()) {
+                    return true;
+                }
             }
+        } else if (sender == sp.getTrace()) {
+            return true;
         }
         return false;
     }
@@ -241,12 +247,8 @@ class StateSystemBuildRequest extends TmfEventRequest {
     @Override
     public void handleData(final ITmfEvent event) {
         super.handleData(event);
-        if (event != null) {
-            for (ITmfTrace subTrace : TmfTraceManager.getTraceSet(trace)) {
-                if (event.getTrace() == subTrace) {
-                    sci.processEvent(event);
-                }
-            }
+        if (event != null && event.getTrace() == trace) {
+            sci.processEvent(event);
         }
     }
 
