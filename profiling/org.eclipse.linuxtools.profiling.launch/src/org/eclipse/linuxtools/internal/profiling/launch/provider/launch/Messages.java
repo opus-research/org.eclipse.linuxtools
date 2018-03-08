1ec6ca30c3d535693823b9193190e807fefbe270
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:16:24 +0000

    First commited as before_8469_rev2

--- profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/internal/profiling/launch/provider/launch/Messages.java
+++ profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/internal/profiling/launch/provider/launch/Messages.java
@@ -21,9 +21,7 @@ public class Messages extends NLS {
 	public static String UseProjectSetting_0;
 	public static String ProjectSettings_0;
 	public static String PreferenceLink_0;
-	public static String ProviderLaunchConfigurationPrompt_0;
-	public static String ProviderLaunchConfigurationPrompt_1;
-
+	
 	static {
 		// initialize resource bundle
 		NLS.initializeMessages(BUNDLE_NAME, Messages.class);
