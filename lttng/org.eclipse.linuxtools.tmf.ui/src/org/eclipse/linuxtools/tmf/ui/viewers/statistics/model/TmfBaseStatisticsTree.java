c2ad6d8210be6d5669a77b7983064f7c14846d8f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:43:17 +0000

    First commited as before_7333_rev6

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/viewers/statistics/model/TmfBaseStatisticsTree.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/viewers/statistics/model/TmfBaseStatisticsTree.java
@@ -217,6 +217,8 @@ public class TmfBaseStatisticsTree extends AbsTmfStatisticsTree {
     @SuppressWarnings({ "rawtypes", "unchecked" })
     protected TmfFixedArray<String>[] getTypePaths(ITmfEvent event, ITmfExtraEventInfo extraInfo) {
         String trace = extraInfo.getTraceName();
+        // String type = event.getType().getTypeId(); // Add too much
+        // informations
         String type = event.getType().toString();
 
         TmfFixedArray[] paths = { new TmfFixedArray<String>(trace, HEADER_EVENT_TYPES, type) };
