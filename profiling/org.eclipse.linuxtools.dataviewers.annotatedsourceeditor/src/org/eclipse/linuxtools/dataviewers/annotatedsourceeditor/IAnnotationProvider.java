2b1f23741039b083d1d15ba63c822fbf54431ea9
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:47:44 +0000

    First commited as before_13406_rev3

--- profiling/org.eclipse.linuxtools.dataviewers.annotatedsourceeditor/src/org/eclipse/linuxtools/dataviewers/annotatedsourceeditor/IAnnotationProvider.java
+++ profiling/org.eclipse.linuxtools.dataviewers.annotatedsourceeditor/src/org/eclipse/linuxtools/dataviewers/annotatedsourceeditor/IAnnotationProvider.java
@@ -12,22 +12,19 @@ package org.eclipse.linuxtools.dataviewers.annotatedsourceeditor;
 
 import org.eclipse.swt.graphics.Color;
 
-/**
- * @since 5.0
- */
 public interface IAnnotationProvider {
     /**
      * gets the background column of a editor line
-     * 
+     *
      * @param line
-     * @return
+     * @return The color for the given line.
      */
     Color getColor(int line);
 
     /**
-     * gets the ISTAnnotationColumn objects list
-     * 
-     * @return
+     * Gets the ISTAnnotationColumn objects list.
+     *
+     * @return The annotation column.
      */
     ISTAnnotationColumn getColumn();
 
