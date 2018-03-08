c14b8386deef3d4c0cdbda33e9c932cf808b6463
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:23:48 +0000

    First commited as before_12591_rev1

--- systemtap/org.eclipse.linuxtools.callgraph.launch.tests/src/org/eclipse/linuxtools/callgraph/launch/tests/AllTests.java
+++ systemtap/org.eclipse.linuxtools.callgraph.launch.tests/src/org/eclipse/linuxtools/callgraph/launch/tests/AllTests.java
@@ -4,18 +4,34 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     Red Hat - initial API and implementation
  *******************************************************************************/
 package org.eclipse.linuxtools.callgraph.launch.tests;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
+import org.eclipse.linuxtools.internal.callgraph.core.SystemTapUIErrorMessages;
 
-@RunWith(Suite.class)
-@Suite.SuiteClasses({ SystemTapCommandTest.class, ConfigurationTest.class,
-		SystemTapCommandGeneratorTest.class, SystemTapTabTest.class,
-		LaunchShortcutsTest.class, SystemTapErrorHandlerTest.class })
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
 public class AllTests {
+
+	public static Test suite() {
+		
+		SystemTapUIErrorMessages.setActive(false);
+		
+		TestSuite suite = new TestSuite(
+				"Test for org.eclipse.linuxtools.callgraph.launch.tests");
+		//$JUnit-BEGIN$
+		suite.addTestSuite(SystemTapCommandTest.class);
+		suite.addTestSuite(ConfigurationTest.class);
+		suite.addTestSuite(SystemTapCommandGeneratorTest.class);
+		suite.addTestSuite(SystemTapTabTest.class);
+		suite.addTestSuite(LaunchShortcutsTest.class);
+		suite.addTestSuite(SystemTapErrorHandlerTest.class);
+		//$JUnit-END$
+		return suite;
+	}
+
 }
