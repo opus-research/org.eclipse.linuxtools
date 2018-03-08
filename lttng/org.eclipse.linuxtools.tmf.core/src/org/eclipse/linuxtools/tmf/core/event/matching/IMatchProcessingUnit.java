a4763f89c146413a86f39adcb2cf6dd187b2ab86
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:39:20 +0000

    First commited as after_10869_rev3

---
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/matching/IMatchProcessingUnit.java
@@ -0,0 +1,55 @@
+/*******************************************************************************
+ * Copyright (c) 2013 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Geneviève Bastien - Initial implementation and API
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.core.event.matching;
+
+import org.eclipse.linuxtools.tmf.core.trace.ITmfTrace;
+
+/**
+ * This class represent an action to be done when event matches are found. This
+ * interface needs to be implemented by all classes that want to be warned when
+ * new event matches are found. They need to register to an instance of
+ * TmfEventMatches class in order to be informed of matches.
+ *
+ * @author gbastien
+ * @since 2.0
+ */
+public interface IMatchProcessingUnit {
+
+    /**
+     * Function called when a match is found
+     *
+     * @param match
+     *            The event match
+     */
+    public void addMatch(TmfEventDependency match);
+
+    /**
+     * Function called after all matching has been done, to do any post-match
+     * treatment
+     */
+    public void matchingEnded();
+
+    /**
+     * Counts the matches
+     *
+     * @return the number of matches
+     */
+    public int countMatches();
+
+    /**
+     * Once the traces are known, hook function to initialize some things
+     *
+     * @param fTraces the set of traces that will be synchronized
+     */
+    public void init(ITmfTrace[] fTraces);
+}
