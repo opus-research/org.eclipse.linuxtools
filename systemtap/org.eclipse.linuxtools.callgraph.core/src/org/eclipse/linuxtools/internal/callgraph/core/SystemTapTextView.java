47fb334ff0001ae8b123cd5ded5ce3fcb7b96eb6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:03:18 +0000

    First commited as before_9699_rev1

--- systemtap/org.eclipse.linuxtools.callgraph.core/src/org/eclipse/linuxtools/internal/callgraph/core/SystemTapTextView.java
+++ systemtap/org.eclipse.linuxtools.callgraph.core/src/org/eclipse/linuxtools/internal/callgraph/core/SystemTapTextView.java
@@ -82,9 +82,8 @@ public class SystemTapTextView extends SystemTapView {
 			inLineOffset = 0;
 			for (int k = 0; k < split_txt.length; k++) {
 				// Skip blank substrings
-				if (split_txt[k].length() == 0) {
+				if (split_txt[k].length() == 0)
 					continue;
-				}
 
 				// Split for the number codes
 				String[] coloursAndText = split_txt[k].split("\\)~"); //$NON-NLS-1$
@@ -102,9 +101,8 @@ public class SystemTapTextView extends SystemTapView {
 
 				// The first element in the array should contain the colours
 				String[] colours = coloursAndText[0].split(","); //$NON-NLS-1$
-				if (colours.length < 3) {
+				if (colours.length < 3)
 					continue;
-				}
 
 				// The second element in the array should contain the text
 				viewer.append(coloursAndText[1]);
@@ -209,9 +207,8 @@ public class SystemTapTextView extends SystemTapView {
 	public void updateMethod() {
 		if (getParser().getData() instanceof String) {
 			String data = (String) getParser().getData();
-			if (data.length() > 0) {
+			if (data.length() > 0)
 				prettyPrintln((String) getParser().getData());
-			}
 		}
 	}
 
@@ -229,4 +226,6 @@ public class SystemTapTextView extends SystemTapView {
 	protected boolean createOpenDefaultAction() {
 		return false;
 	}
+
+
 }
