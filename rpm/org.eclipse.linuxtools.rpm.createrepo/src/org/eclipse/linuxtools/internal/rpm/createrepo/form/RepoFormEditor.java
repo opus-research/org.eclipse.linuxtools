319a732ebda7ff3fd2238c42d450b206e1385e91
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:31:27 +0000

    First commited as before_18444_rev3

--- rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/form/RepoFormEditor.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/form/RepoFormEditor.java
@@ -109,7 +109,7 @@ public class RepoFormEditor extends FormEditor {
 	 */
 	void createPage1() {
 		try {
-			FormPage composite = new MetadataPage(this);
+			FormPage composite = new MainOverviewPage(this);
 			addPage(composite);
 		} catch (PartInitException e) {
 			Activator.logError(Messages.RepoFormEditor_errorInitializingForm, e);
