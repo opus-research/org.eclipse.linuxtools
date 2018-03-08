956e2a7957ca97ece32611597620a479a525e902
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:10:30 +0000

    First commited as before_9928_rev1

--- libhover/org.eclipse.linuxtools.cdt.libhover.devhelp/src/org/eclipse/linuxtools/internal/cdt/libhover/devhelp/DevHelpTopic.java
+++ libhover/org.eclipse.linuxtools.cdt.libhover.devhelp/src/org/eclipse/linuxtools/internal/cdt/libhover/devhelp/DevHelpTopic.java
@@ -41,7 +41,6 @@ public class DevHelpTopic implements ITopic {
 	private String name;
 	private XPath xpath;
 	private String label;
-	private String link;
 	private List<ITopic> subTopics;
 
 	DevHelpTopic(String name) {
@@ -84,7 +83,6 @@ public class DevHelpTopic implements ITopic {
 				if (label.equals("")) {
 					label = name;
 				}
-				link = xpathEval("/book/@link", docroot);
 
 				// set subtopics
 				NodeList nodes = xpathEvalNodes("/book/chapters/sub", docroot);
@@ -135,7 +133,7 @@ public class DevHelpTopic implements ITopic {
 
 	@Override
 	public String getHref() {
-		return "/" + DevHelpPlugin.PLUGIN_ID + "/" + name + "/"+link; // $NON-NLS-1$ //$NON-NLS-2$" //$NON-NLS-3$
+		return "/" + DevHelpPlugin.PLUGIN_ID + "/" + name + "/index.html"; // $NON-NLS-1$ //$NON-NLS-2$" //$NON-NLS-3$
 
 	}
 
