7dfe59170cbde4847aff2b8198802b5888c8d1b0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:51:38 +0000

    First commited as before_7609_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide.tests/src/org/eclipse/linuxtools/systemtap/ui/ide/test/IDESessionSettingsTest.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide.tests/src/org/eclipse/linuxtools/systemtap/ui/ide/test/IDESessionSettingsTest.java
@@ -11,23 +11,8 @@
 
 package org.eclipse.linuxtools.systemtap.ui.ide.test;
 
-import org.eclipse.linuxtools.systemtap.ui.ide.test.structures.StapErrorParserTest;
-import org.eclipse.linuxtools.systemtap.ui.ide.test.structures.TreeSettingsTest;
+import junit.framework.TestCase;
 
-import junit.framework.Test;
-import junit.framework.TestSuite;
+public class IDESessionSettingsTest extends TestCase {
 
-public class AllTests {
-	public static Test suite() {
-		TestSuite suite = new TestSuite("Test for org.eclipse.linuxtools.systemtap.ui.ide.test");
-
-		//Root
-		suite.addTestSuite(IDESessionSettingsTest.class);
-		
-		//Structures
-		suite.addTestSuite(StapErrorParserTest.class);
-		suite.addTestSuite(TreeSettingsTest.class);
-
-		return suite;
-	}
 }
