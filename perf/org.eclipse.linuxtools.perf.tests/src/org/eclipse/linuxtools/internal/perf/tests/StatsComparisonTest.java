0855ecd9b9385d0c4f0bfbec021d0e5964af6367
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:03:57 +0000

    First commited as before_16528_rev9

--- perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/StatsComparisonTest.java
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/StatsComparisonTest.java
@@ -18,12 +18,11 @@ import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 
-import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.linuxtools.internal.perf.BaseDataManipulator;
 import org.eclipse.linuxtools.internal.perf.PerfPlugin;
 import org.eclipse.linuxtools.internal.perf.StatComparisonData;
@@ -109,7 +108,7 @@ public class StatsComparisonTest {
 
 	@Test
 	public void testStatDataCollection() {
-		IPath statData = Path.fromOSString(STAT_RES + "perf_simple.stat");
+		File statData = new File(STAT_RES + "perf_simple.stat");
 
 		//set up expected result
 		ArrayList<PMStatEntry> expectedStatList = new ArrayList<PMStatEntry>();
@@ -137,8 +136,8 @@ public class StatsComparisonTest {
 
 	@Test
 	public void testStatDataComparisonFieldGetters() {
-		IPath oldStatData = Path.fromOSString(STAT_RES + "perf_old.stat");
-		IPath newStatData = Path.fromOSString(STAT_RES + "perf_new.stat");
+		File oldStatData = new File(STAT_RES + "perf_old.stat");
+		File newStatData = new File(STAT_RES + "perf_new.stat");
 		String dataTitle = "title";
 		StatComparisonData diffData = new StatComparisonData(dataTitle,
 				oldStatData, newStatData);
@@ -146,16 +145,16 @@ public class StatsComparisonTest {
 		assertEquals(dataTitle, diffData.getTitle());
 		assertEquals("", diffData.getPerfData());
 		assertNotNull(diffData.getDataID());
-		assertEquals(oldStatData.toOSString(), diffData.getOldDataPath());
-		assertEquals(newStatData.toOSString(), diffData.getNewDataPath());
-		assertEquals(oldStatData.toOSString() + diffData.getDataID(),diffData.getOldDataID());
-		assertEquals(newStatData.toOSString() + diffData.getDataID(),diffData.getNewDataID());
+		assertEquals(oldStatData.getPath(), diffData.getOldDataPath());
+		assertEquals(newStatData.getPath(), diffData.getNewDataPath());
+		assertEquals(oldStatData.getPath() + diffData.getDataID(),diffData.getOldDataID());
+		assertEquals(newStatData.getPath() + diffData.getDataID(),diffData.getNewDataID());
 	}
 
 	@Test
 	public void testStatDataComparisonCaching() {
-		IPath oldStatData = Path.fromOSString(STAT_RES + "perf_old.stat");
-		IPath newStatData = Path.fromOSString(STAT_RES + "perf_new.stat");
+		File oldStatData = new File(STAT_RES + "perf_old.stat");
+		File newStatData = new File(STAT_RES + "perf_new.stat");
 		StatComparisonData diffData = new StatComparisonData("title",
 				oldStatData, newStatData);
 		diffData.cacheData();
@@ -164,9 +163,9 @@ public class StatsComparisonTest {
 		BaseDataManipulator dataMan = new BaseDataManipulator();
 
 		// check data was cached
-		assertEquals(dataMan.fileToString(oldStatData.toFile()),
+		assertEquals(dataMan.fileToString(oldStatData),
 				plugin.getCachedData(diffData.getOldDataID()));
-		assertEquals(dataMan.fileToString(newStatData.toFile()),
+		assertEquals(dataMan.fileToString(newStatData),
 				plugin.getCachedData(diffData.getNewDataID()));
 
 		diffData.clearCachedData();
@@ -178,8 +177,8 @@ public class StatsComparisonTest {
 
 	@Test
 	public void testStatDataComparison() {
-		IPath oldStatData = Path.fromOSString(STAT_RES + "perf_old.stat");
-		IPath newStatData = Path.fromOSString(STAT_RES + "perf_new.stat");
+		File oldStatData = new File(STAT_RES + "perf_old.stat");
+		File newStatData = new File(STAT_RES + "perf_new.stat");
 		StatComparisonData diffData = new StatComparisonData("title",
 				oldStatData, newStatData);
 
@@ -211,12 +210,12 @@ public class StatsComparisonTest {
 
 	@Test
 	public void testStatComparisonResult() throws IOException {
-		IPath oldStatData = Path.fromOSString(STAT_RES + "perf_old.stat");
-		IPath newStatData = Path.fromOSString(STAT_RES + "perf_new.stat");
-		IPath diffStatData = Path.fromOSString(STAT_RES + "perf_diff.stat");
+		File oldStatData = new File(STAT_RES + "perf_old.stat");
+		File newStatData = new File(STAT_RES + "perf_new.stat");
+		File diffStatData = new File(STAT_RES + "perf_diff.stat");
 
 		BufferedReader diffDataReader = new BufferedReader(new FileReader(
-				diffStatData.toFile()));
+				diffStatData));
 		StatComparisonData diffData = new StatComparisonData("title",
 				oldStatData, newStatData);
 
