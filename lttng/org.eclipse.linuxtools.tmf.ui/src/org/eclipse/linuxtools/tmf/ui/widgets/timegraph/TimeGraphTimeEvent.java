5b6c79e9f5e671a94f39b538df60663d06cbf421
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:14:29 +0000

    First commited as before_14292_rev4

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/timegraph/TimeGraphTimeEvent.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/timegraph/TimeGraphTimeEvent.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2012, 2013 Ericsson
+ * Copyright (c) 2012 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -15,7 +15,7 @@ package org.eclipse.linuxtools.tmf.ui.widgets.timegraph;
 import java.util.EventObject;
 
 /**
- * Time selection event
+ * Event for the time graph view
  *
  * @version 1.0
  * @author Patrick Tasse
@@ -29,14 +29,9 @@ public class TimeGraphTimeEvent extends EventObject {
     private static final long serialVersionUID = 1L;
 
     /**
-     * The selection begin time.
+     * The selected time.
      */
-    private final long fBeginTime;
-
-    /**
-     * The selection end time.
-     */
-    private final long fEndTime;
+    private final long fTime;
 
     /**
      * Standard constructor
@@ -45,55 +40,17 @@ public class TimeGraphTimeEvent extends EventObject {
      *            The source of this event
      * @param time
      *            The time that was requested
-     * @deprecated As of 2.1, use {@link #TimeGraphTimeEvent(Object, long, long)}
      */
-    @Deprecated
     public TimeGraphTimeEvent(Object source, long time) {
         super(source);
-        fBeginTime = time;
-        fEndTime = time;
-    }
-
-    /**
-     * Standard constructor
-     *
-     * @param source
-     *            The source of this event
-     * @param beginTime
-     *            The selection begin time
-     * @param endTime
-     *            The selection end time
-     * @since 2.1
-     */
-    public TimeGraphTimeEvent(Object source, long beginTime, long endTime) {
-        super(source);
-        fBeginTime = beginTime;
-        fEndTime = endTime;
+        fTime = time;
     }
 
     /**
      * @return the selected time
-     * @deprecated As of 2.1, use {@link #getBeginTime()} and {@link #getEndTime()}
      */
-    @Deprecated
     public long getTime() {
-        return fBeginTime;
-    }
-
-    /**
-     * @return the selection begin time
-     * @since 2.1
-     */
-    public long getBeginTime() {
-        return fBeginTime;
-    }
-
-    /**
-     * @return the selection end time
-     * @since 2.1
-     */
-    public long getEndTime() {
-        return fEndTime;
+        return fTime;
     }
 
 }
