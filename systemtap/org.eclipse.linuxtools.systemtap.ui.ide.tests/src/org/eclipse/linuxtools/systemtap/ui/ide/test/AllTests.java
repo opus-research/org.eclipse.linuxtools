c8085a1d0d2d8b404c15a9151b01a817027b37f3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:19:53 +0000

    First commited as before_14553_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide.tests/src/org/eclipse/linuxtools/systemtap/ui/ide/test/AllTests.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide.tests/src/org/eclipse/linuxtools/systemtap/ui/ide/test/AllTests.java
@@ -12,8 +12,6 @@
 package org.eclipse.linuxtools.systemtap.ui.ide.test;
 
 import org.eclipse.linuxtools.systemtap.ui.ide.test.editors.stp.STPCompletionProcessorTest;
-import org.eclipse.linuxtools.systemtap.ui.ide.test.editors.stp.STPFormattingTest;
-import org.eclipse.linuxtools.systemtap.ui.ide.test.editors.stp.STPToggleCommentTest;
 import org.eclipse.linuxtools.systemtap.ui.ide.test.structures.StapErrorParserTest;
 import org.eclipse.linuxtools.systemtap.ui.ide.test.structures.TreeSettingsTest;
 import org.eclipse.linuxtools.systemtap.ui.ide.test.swtbot.TestCreateSystemtapScript;
@@ -23,8 +21,7 @@ import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
 @SuiteClasses({ StapErrorParserTest.class, TreeSettingsTest.class,
-		STPCompletionProcessorTest.class, STPToggleCommentTest.class,
-		TestCreateSystemtapScript.class, ConditionalExpressionValidatorTest.class,
-		DirectoryValidatorTest.class, STPFormattingTest.class })
+		STPCompletionProcessorTest.class, TestCreateSystemtapScript.class,
+		ConditionalExpressionValidatorTest.class, DirectoryValidatorTest.class })
 public class AllTests {
 }
