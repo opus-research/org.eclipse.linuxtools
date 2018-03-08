7b0cd46046db5ebc889bc564b8f8d2d4018e8e4c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:36:42 +0000

    First commited as after_18583_rev1

---
+++ lttng/org.eclipse.linuxtools.tmf.analysis.xml.tests/src/org/eclipse/linuxtools/tmf/analysis/xml/tests/XmlAnalysisPluginTest.java
@@ -0,0 +1,51 @@
+/*******************************************************************************
+ * Copyright (c) 2013 École Polytechnique de Montréal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Geneviève Bastien - Initial API and implementation
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.analysis.xml.tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.eclipse.linuxtools.internal.tmf.analysis.xml.Activator;
+import org.junit.Test;
+
+/**
+ * Test the Xml Analysis plug-in activator
+ */
+public class XmlAnalysisPluginTest {
+    // ------------------------------------------------------------------------
+    // Attributes
+    // ------------------------------------------------------------------------
+
+    // Plug-in instantiation
+    static final Activator fPlugin = new Activator();
+
+    // ------------------------------------------------------------------------
+    // Test cases
+    // ------------------------------------------------------------------------
+
+    /**
+     * Test the plugin ID.
+     */
+    @Test
+    public void testTmfCorePluginId() {
+        assertEquals("Plugin ID", "org.eclipse.linuxtools.tmf.analysis.xml", Activator.PLUGIN_ID);
+    }
+
+    /**
+     * Test the getDefault() static method.
+     */
+    @Test
+    public void testGetDefault() {
+        Activator plugin = Activator.getDefault();
+        assertEquals("getDefault()", plugin, fPlugin);
+    }
+}
