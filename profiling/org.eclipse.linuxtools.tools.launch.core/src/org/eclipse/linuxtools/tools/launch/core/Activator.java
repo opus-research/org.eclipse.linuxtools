bfabb2bb3f46083d39d7cb8d8e7f63a4cbff4850
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:08:48 +0000

    First commited as before_9878_rev3

--- profiling/org.eclipse.linuxtools.tools.launch.core/src/org/eclipse/linuxtools/tools/launch/core/Activator.java
+++ profiling/org.eclipse.linuxtools.tools.launch.core/src/org/eclipse/linuxtools/tools/launch/core/Activator.java
@@ -1,17 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2009 STMicroelectronics.
+ * Copyright (c) 2011 IBM Corporation
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Marzia Maugeri <marzia.maugeri@st.com> - initial API and implementation
+ *    Otavio Busatto Pontes <obusatto@br.ibm.com> - initial API and implementation
  *******************************************************************************/
-package org.eclipse.linuxtools.dataviewers.annotatedsourceeditor;
 
-public interface ISTTextEditorActionDefinitionIds {
+package org.eclipse.linuxtools.tools.launch.core;
 
-    String ST_TOGGLE = "org.eclipse.linuxtools.dataviewers.annotatedsourceeditor.STColumnToggle"; //$NON-NLS-1$
+public class Activator {
+	public static final String PLUGIN_ID = "org.eclipse.linuxtools.tools.launch.core"; //$NON-NLS-1$
+	public static final String UI_PLUGIN_ID = "org.eclipse.linuxtools.tools.launch.ui"; //$NON-NLS-1$
 
 }
