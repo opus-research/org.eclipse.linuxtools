073393e305a59034fb1cde99f2eba71e8ccf04e2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:15:27 +0000

    First commited as before_10076_rev2

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/actions/hidden/TreeExpandCollapseAction.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/actions/hidden/TreeExpandCollapseAction.java
@@ -15,7 +15,12 @@ import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.linuxtools.internal.systemtap.ui.ide.views.BrowserView;
+import org.eclipse.linuxtools.internal.systemtap.ui.ide.views.FunctionBrowserView;
+import org.eclipse.linuxtools.internal.systemtap.ui.ide.views.KernelBrowserView;
+import org.eclipse.linuxtools.internal.systemtap.ui.ide.views.ProbeAliasBrowserView;
 import org.eclipse.ui.ISelectionListener;
+import org.eclipse.ui.IViewPart;
+import org.eclipse.ui.IViewReference;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
@@ -31,25 +36,24 @@ public class TreeExpandCollapseAction extends Action implements
 		ISelectionListener {
 	private final IWorkbenchWindow fWindow;
 	private IStructuredSelection selection;
-	private final BrowserView viewer;
+	private final Class<?> cl;
 
 	/**
 	 * The default constructor. Takes a <code>Class</code> representing the viewer that it is to expand
 	 * or collapse, as there is only one in the workbench at a time.
 	 * @param cls	<code>Class</code> of the viewer to expand/collapse
 	 */
-	public TreeExpandCollapseAction(BrowserView view) {
+	public TreeExpandCollapseAction(Class<?> cls) {
 		super();
 		fWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
 		fWindow.getSelectionService().addSelectionListener(this);
-		this.viewer = view;
+		cl = cls;
 	}
 
 	/**
 	 * Updates <code>selection</code> with the current selection whenever the user changes
 	 * the current selection.
 	 */
-	@Override
 	public void selectionChanged(IWorkbenchPart part, ISelection incoming) {
 		if (incoming instanceof IStructuredSelection) {
 			selection = (IStructuredSelection) incoming;
@@ -70,6 +74,28 @@ public class TreeExpandCollapseAction extends Action implements
 	 */
 	@Override
 	public void run() {
+		if(!(cl.equals(FunctionBrowserView.class) || cl.equals(ProbeAliasBrowserView.class) || cl.equals(KernelBrowserView.class))) {
+			return;
+		}
+		IViewReference[] references = fWindow.getActivePage().getViewReferences();
+		IViewPart part = null;
+		boolean found = false;
+		for(int i = 0; i < references.length; i++) {
+			part = references[i].getView(false);
+			if(part == null)
+				continue;
+			if(part.getClass().equals(cl)) {
+				found = true;
+				break;
+			}
+		}
+		if(!found) {
+			return;
+		}
+		if(part == null) {
+			return;
+		}
+		BrowserView viewer = (BrowserView) part;
 		ISelection incoming = viewer.getViewer().getSelection();
 		IStructuredSelection selection = (IStructuredSelection)incoming;
 		Object o = selection.getFirstElement();
