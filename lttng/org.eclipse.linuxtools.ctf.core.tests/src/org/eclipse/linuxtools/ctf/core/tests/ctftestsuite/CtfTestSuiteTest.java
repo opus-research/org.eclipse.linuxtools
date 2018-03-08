65a5b8da5e3a65cb603dff21ccbfcb3d706bde5f
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:33:57 +0000

    First commited as after_18479_rev1

--- lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/ctftestsuite/CtfTestSuiteTest.java
+++ lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/ctftestsuite/CtfTestSuiteTest.java
@@ -7,6 +7,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
+ *   Matthew Khouzam - Initial API and implementation
  *   Alexandre Montplaisir - Moved out of CTFTestTrace
  *******************************************************************************/
 
@@ -29,37 +30,136 @@ import org.junit.Test;
  */
 public class CtfTestSuiteTest {
 
+    // ------------------------------------------------------------------------
+    // Attributes
+    // ------------------------------------------------------------------------
+
     private static final String TRACES_DIRECTORY = "../org.eclipse.linuxtools.ctf.core.tests/traces";
     private static final String METADATA_FILENAME = "metadata";
 
-    private static final String CTF_VERSION_NUMBER = "1.8";
-    private static final String CTF_SUITE_TEST_DIRECTORY = "ctf-testsuite/tests/" + CTF_VERSION_NUMBER;
+    private static final String CTF_SUITE_TEST_DIRECTORY = "ctf-testsuite/tests/1.8";
+
+    // ------------------------------------------------------------------------
+    // Test methods - Expected failures
+    // ------------------------------------------------------------------------
 
     /**
-     * Open traces in specified directories and expect them to fail
-     *
-     * @throws CTFReaderException not expected
+     * Run tests in fuzzing/metadata/fail
      */
     @Test
-    public void testFailedParse() throws CTFReaderException {
-        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/fail"), true);
+    public void testFuzzingMetadataFail() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/fuzzing/metadata/fail"), true);
     }
 
     /**
-     * Open traces in specified directories and expect them to succeed
-     *
-     * @throws CTFReaderException not expected
+     * Run tests in fuzzing/stream/fail
      */
     @Test
-    public void testSuccessfulParse() throws CTFReaderException {
+    public void testFuzzingStreamFail() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/fuzzing/stream/fail"), true);
+    }
+
+    /**
+     * Run tests in regression/metadata/fail
+     */
+    @Test
+    public void testRegressionMetadataFail() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/regression/metadata/fail"), true);
+    }
+
+    /**
+     * Run tests in regression/stream/fail
+     */
+    @Test
+    public void testRegressionStreamFail() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/regression/stream/fail"), true);
+    }
+
+    /**
+     * Run tests in stress/metadata/fail
+     */
+    @Test
+    public void testStressMetadataFail() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/stress/metadata/fail"), true);
+    }
+
+    /**
+     * Run tests in stress/stream/fail
+     */
+    @Test
+    public void testStressStreamFail() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/stress/stream/fail"), true);
+    }
+
+    // ------------------------------------------------------------------------
+    // Test methods - Expected successes
+    // ------------------------------------------------------------------------
+
+    /**
+     * Run tests on the known 'kernel' and 'trace2' traces. They should be
+     * working!
+     */
+    @Test
+    public void testKnownTracesPass() {
         parseTracesInDirectory(getTestTracesSubDirectory("kernel"), false);
         parseTracesInDirectory(getTestTracesSubDirectory("trace2"), false);
-        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/pass"), false);
     }
 
+    /**
+     * Run tests in fuzzing/metadata/pass
+     */
+    @Test
+    public void testFuzzingMetadataPass() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/fuzzing/metadata/pass"), false);
+    }
 
     /**
-     * Get the File object for the subDir in the traces directory. If the sub directory doesn't exist, the test is skipped.
+     * Run tests in fuzzing/stream/pass
+     */
+    @Test
+    public void testFuzzingStreamPass() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/fuzzing/stream/pass"), false);
+    }
+
+    /**
+     * Run tests in regression/metadata/pass
+     */
+    @Test
+    public void testRegressionMetadataPass() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/regression/metadata/pass"), false);
+    }
+
+    /**
+     * Run tests in regression/stream/pass
+     */
+    @Test
+    public void testRegressionStreamPass() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/regression/stream/pass"), false);
+    }
+
+    /**
+     * Run tests in stress/metadata/pass
+     */
+    @Test
+    public void testStressMetadataPass() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/stress/metadata/pass"), false);
+    }
+
+    /**
+     * Run tests in stress/stream/pass
+     */
+    @Test
+    public void testStressStreamPass() {
+        parseTracesInDirectory(getTestTracesSubDirectory(CTF_SUITE_TEST_DIRECTORY + "/stress/stream/pass"), false);
+    }
+
+    // ------------------------------------------------------------------------
+    // Helper methods
+    // ------------------------------------------------------------------------
+
+    /**
+     * Get the File object for the subDir in the traces directory. If the sub
+     * directory doesn't exist, the test is skipped.
      */
     private static File getTestTracesSubDirectory(String subDir) {
         File file = new File(TRACES_DIRECTORY + "/" + subDir);
@@ -70,11 +170,14 @@ public class CtfTestSuiteTest {
     /**
      * Parse the traces in given directory recursively
      *
-     * @param directory The directory to search in
-     * @param expectException Whether or not traces in this directory are expected to throw an exception when parsed
+     * @param directory
+     *            The directory to search in
+     * @param expectException
+     *            Whether or not traces in this directory are expected to throw
+     *            an exception when parsed
      * @throws CTFReaderException
      */
-    void parseTracesInDirectory(File directory, boolean expectException) throws CTFReaderException {
+    private void parseTracesInDirectory(File directory, boolean expectException) {
         for (File file : directory.listFiles()) {
             if (file.getName().equals(METADATA_FILENAME)) {
                 try {
@@ -84,11 +187,13 @@ public class CtfTestSuiteTest {
                     }
                 } catch (RuntimeException e) {
                     if (!expectException) {
-                        throw new CTFReaderException("Failed parsing " + directory, e);
+                        e.printStackTrace();
+                        fail("Failed parsing " + directory);
                     }
                 } catch (CTFReaderException e) {
                     if (!expectException) {
-                        throw new CTFReaderException("Failed parsing " + directory, e);
+                        e.printStackTrace();
+                        fail("Failed parsing " + directory);
                     }
                 }
                 return;
