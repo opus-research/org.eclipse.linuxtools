ef827faf71e6e8bb49eb3579fe496763d1664e64
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:04:54 +0000

    First commited as before_11756_rev2

--- systemtap/org.eclipse.linuxtools.systemtap.ui.dashboard/src/org/eclipse/linuxtools/internal/systemtap/ui/dashboard/DashboardComposite.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.dashboard/src/org/eclipse/linuxtools/internal/systemtap/ui/dashboard/DashboardComposite.java
@@ -9,10 +9,11 @@
  *     IBM Corporation - Jeff Briggs, Henry Hughes, Ryan Morse, Anithra P J
  *******************************************************************************/
 
-package org.eclipse.linuxtools.systemtap.ui.dashboard;
+package org.eclipse.linuxtools.internal.systemtap.ui.dashboard;
 
 import java.util.ArrayList;
 
+import org.eclipse.linuxtools.internal.systemtap.ui.dashboard.DashboardAdapter;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.dnd.DND;
 import org.eclipse.swt.dnd.DragSourceAdapter;
@@ -72,10 +73,6 @@ public class DashboardComposite {
 	 * rendering queue, it is also reparented to the dead composite.
 	 */
 	public final Composite deadComposite;
-
-	public void layout() {	//TODO: Remove this.  It is a temporary solution to another bug
-		surface.layout(true, true);
-	}
 
 	/**
 	 * Default constructor for the DashboardComposite. Creates all of the internal composites, sets up
