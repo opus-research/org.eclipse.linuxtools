2bb27bf67ee93246498776589038059dfa8a8204
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:47:11 +0000

    First commited as before_7456_rev2

--- gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/parser/GcnoRecordsParser.java
+++ gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/parser/GcnoRecordsParser.java
@@ -18,6 +18,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.linuxtools.gcov.Activator;
 import org.eclipse.linuxtools.internal.gcov.utils.BEDataInputStream;
@@ -81,7 +82,7 @@ public class GcnoRecordsParser {
 				stream = new LEDataInputStream((DataInputStream) stream);
 			}else{
 				String message = magic + " :desn't correspond to a correct note file header\n";
-				Status status = new Status(Status.ERROR, Activator.PLUGIN_ID, message);
+				Status status = new Status(IStatus.ERROR, Activator.PLUGIN_ID, message);
 				throw new CoreException(status);
 			}
 		}
@@ -103,7 +104,7 @@ public class GcnoRecordsParser {
 				int length=stream.readInt();
 
 				// parse gcno data
-				if ((int)tag ==  GCOV_TAG_FUNCTION){
+				if (tag ==  GCOV_TAG_FUNCTION){
 					// before parse new function, add current function to functions list
 					if (parseFirstFnctn == true) fnctns.add(fnctn);
 					
@@ -123,7 +124,7 @@ public class GcnoRecordsParser {
 					continue;
 				}
 				
-				else if ((int)tag ==  GCOV_TAG_BLOCKS){
+				else if (tag ==  GCOV_TAG_BLOCKS){
 					blocks = new ArrayList<Block>();
 					for (int i = 0; i < length; i++) {
 						long BlckFlag = stream.readInt()& MasksGenerator.UNSIGNED_INT_MASK;
@@ -164,7 +165,7 @@ public class GcnoRecordsParser {
 							if (a.getSrcBlock() != null ) {
 								 // Exceptional exit from this function, the 
 								 // 	source block must be a call.
-								srcBlk = blocks.get((int)srcBlockIndice);		
+								srcBlk = blocks.get(srcBlockIndice);		
 								srcBlk.setCallSite(true);
 								a.setCallNonReturn(true);
 							} else {
@@ -210,10 +211,10 @@ public class GcnoRecordsParser {
 						}
 					} while (true);
 					
-					fnctn.getFunctionBlocks().get(((int)numBlock))
+					fnctn.getFunctionBlocks().get((numBlock))
 					.setEncoding(lineNos);
 				
-					fnctn.getFunctionBlocks().get(((int)numBlock))
+					fnctn.getFunctionBlocks().get((numBlock))
 					.setNumLine(ix);
 					
 					continue;
