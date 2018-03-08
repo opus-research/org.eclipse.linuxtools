637c5a13455f72d16b619d17ae2198abb98ff052
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:53:06 +0000

    First commited as before_19090_rev1

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/event/types/Definition.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/event/types/Definition.java
@@ -15,10 +15,10 @@ package org.eclipse.linuxtools.ctf.core.event.types;
 import org.eclipse.linuxtools.ctf.core.event.io.BitBuffer;
 
 /**
- * A CTF definiton
+ * A CTF definition
  *
- * A definition is like an object of a declaration class. It fills the
- * declaration with values. <br>
+ * A definition is like an object of a declaration class. It fills the declaration
+ * with values. <br>
  * An example: <br>
  * int i = 0; <br>
  * <b>int</b> is the declaration.<br>
@@ -136,18 +136,9 @@ public abstract class Definition {
      *            The declaration which has an alignment
      * @since 2.2
      */
-    protected static void alignRead(BitBuffer input, IDeclaration declaration) {
-        int mask = (int) declaration.getAlignment() - 1;
-
-        /*
-         * The alignment is a power of 2
-         */
-        int pos = input.position();
-        if ((pos & mask) == 0) {
-            return;
-        }
-        pos = (pos + mask) & ~mask;
-
+    protected static void alignRead(BitBuffer input, IDeclaration declaration){
+        long align = declaration.getAlignment();
+        long pos = input.position() + ((align - (input.position() % align)) % align);
         input.position(pos);
     }
 
