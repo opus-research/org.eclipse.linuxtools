afb41bae3419914ba53f34aba34eaac8f310df22
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 18:34:08 +0000

    First commited as after_6986_rev6

---
+++ lttng/org.eclipse.linuxtools.lttng2.kernel.ui/src/org/eclipse/linuxtools/internal/lttng2/kernel/ui/views/statistics/StateSystemStatisticsView.java
@@ -0,0 +1,19 @@
+package org.eclipse.linuxtools.internal.lttng2.kernel.ui.views.statistics;
+
+import org.eclipse.linuxtools.internal.lttng2.kernel.ui.viewers.statistics.StateSystemStatisticsViewer;
+import org.eclipse.linuxtools.tmf.ui.viewers.statistics.TmfStatisticsViewer;
+import org.eclipse.linuxtools.tmf.ui.views.statistics.TmfStatisticsView;
+
+/**
+ * View that replaces the default statistics view from TMF. It uses as the
+ * global tab the statistics viewer that uses the state system.
+ *
+ * @author Mathieu Denis
+ */
+public class StateSystemStatisticsView extends TmfStatisticsView {
+
+    @Override
+    protected TmfStatisticsViewer getGlobalViewer() {
+        return new StateSystemStatisticsViewer();
+    }
+}
