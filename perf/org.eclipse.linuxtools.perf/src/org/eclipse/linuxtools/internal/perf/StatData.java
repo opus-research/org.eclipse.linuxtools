13695c9f1011a1ff3824ab8d1e88ba6ec78ea64e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:03:39 +0000

    First commited as after_16528_rev1

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/StatData.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/StatData.java
@@ -10,14 +10,19 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.perf;
 
-import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.eclipse.core.filesystem.EFS;
+import org.eclipse.core.filesystem.IFileStore;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IPath;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.linuxtools.internal.perf.handlers.PerfSaveStatsHandler;
+import org.eclipse.linuxtools.profiling.launch.IRemoteFileProxy;
+import org.eclipse.linuxtools.profiling.launch.RemoteProxyManager;
 
 /**
  * This class handles the execution of the perf stat command
@@ -30,7 +35,7 @@ public class StatData extends AbstractDataManipulator {
 	private int runCount;
 	private String [] events;
 
-	public StatData(String title, File workDir, String prog, String [] args, int runCount, String[] events) {
+	public StatData(String title, IPath workDir, String prog, String [] args, int runCount, String[] events) {
 		super(title, workDir);
 		this.prog = prog;
 		this.args = args;
@@ -78,7 +83,8 @@ public class StatData extends AbstractDataManipulator {
 	 * report data files.
 	 */
 	public void updateStatData() {
-
+		URI curStatPathURI = null;
+		URI oldStatPathURI = null;
 		// build file name format
 		StringBuilder stringBuilder = new StringBuilder();
 		stringBuilder.append(PerfPlugin.PERF_COMMAND);
@@ -87,26 +93,32 @@ public class StatData extends AbstractDataManipulator {
 		String statNameFormat = stringBuilder.toString();
 
 		// get current stat file
-		Path workingDir = new Path(getWorkDir().getAbsolutePath());
+		IPath workingDir = getWorkDir();
 		String curStatName = String.format(statNameFormat, ""); //$NON-NLS-1$
 		IPath curStatPath = workingDir.append(curStatName);
-		File curStatFile = new File(curStatPath.toOSString());
+		IRemoteFileProxy proxy = null;
+		try {
+			curStatPathURI = new URI(curStatPath.toPortableString());
+			proxy = RemoteProxyManager.getInstance().getFileProxy(curStatPathURI);
 
-		if (curStatFile.exists()) {
-			// get previous stat file
-			String oldStatName = String.format(statNameFormat, ".old"); //$NON-NLS-1$
-			IPath oldStatPath = workingDir.append(oldStatName);
-			File oldStatFile = oldStatPath.toFile();
-
-			if (oldStatFile.exists()) {
-				oldStatFile.delete();
+			IFileStore curFileStore = proxy.getResource(curStatPathURI.getPath());
+			if (curFileStore.fetchInfo().exists()) {
+				// get previous stat file
+				String oldStatName = String.format(statNameFormat, ".old"); //$NON-NLS-1$
+				IPath oldStatPath = workingDir.append(oldStatName);
+				oldStatPathURI = new URI(oldStatPath.toPortableString());
+				IFileStore oldFileStore = proxy.getResource(oldStatPathURI.getPath());
+				if (oldFileStore.fetchInfo().exists()) {
+					oldFileStore.delete(EFS.NONE, null);
+				}
+				curFileStore.copy(oldFileStore, EFS.NONE, null);
 			}
-
-			curStatFile.renameTo(oldStatFile);
+			PerfSaveStatsHandler saveStats = new PerfSaveStatsHandler();
+			saveStats.saveData(PerfPlugin.PERF_COMMAND);
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		} catch (CoreException e) {
+			e.printStackTrace();
 		}
-
-		PerfSaveStatsHandler saveStats = new PerfSaveStatsHandler();
-		saveStats.saveData(PerfPlugin.PERF_COMMAND);
 	}
-
 }
