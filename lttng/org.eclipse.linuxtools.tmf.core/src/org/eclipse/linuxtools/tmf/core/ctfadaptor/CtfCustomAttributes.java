a2544aae4d3eb1f2cb74f77f9b795122fc61d0ed
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:03:21 +0000

    First commited as after_9699_rev2

---
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/ctfadaptor/CtfCustomAttributes.java
@@ -0,0 +1,24 @@
+/*******************************************************************************
+ * Copyright (c) 2011-2013 Ericsson, Ecole Polytechnique de Montreal and others
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors: Ansgar Radermacher - support for model URI
+ *
+ *******************************************************************************/
+
+package org.eclipse.linuxtools.tmf.core.ctfadaptor;
+
+/**
+ * Set of constants defining common custom attribute names
+ *
+ */
+public class CtfCustomAttributes {
+    /**
+     * Model URI for traces related to EMF models
+     */
+    public final static String modelURI = "model.emf.uri"; //$NON-NLS-1$
+}
