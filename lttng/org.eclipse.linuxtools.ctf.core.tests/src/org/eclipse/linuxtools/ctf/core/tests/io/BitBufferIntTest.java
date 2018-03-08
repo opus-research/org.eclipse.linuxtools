1afa84fd27f3a4da9aeb4be9f2d878ff7329dcbb
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:41:16 +0000

    First commited as after_18786_rev5

--- lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/io/BitBufferIntTest.java
+++ lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/io/BitBufferIntTest.java
@@ -193,6 +193,26 @@ public class BitBufferIntTest {
     }
 
     /**
+     * Run the int getInt(int) method test.
+     */
+    @Test
+    public void testGetLong_pos0() {
+        fixture.position(0);
+        long result = fixture.getLong();
+        assertEquals(0x01020304050607L, result);
+    }
+
+    /**
+     * Run the int getInt(int) method test.
+     */
+    @Test
+    public void testGetLong_pos1() {
+        fixture.position(1);
+        long result = fixture.getLong();
+        assertEquals(0x0102030405060708L, result);
+    }
+
+    /**
      * Run the void putInt(int) method test.
      */
     @Test
