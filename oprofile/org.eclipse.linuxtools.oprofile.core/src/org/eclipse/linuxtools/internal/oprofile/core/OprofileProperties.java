2d73ea9a55721348d6c6fe906d0d2891968ecfc8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:17:16 +0000

    First commited as before_12246_rev1

--- oprofile/org.eclipse.linuxtools.oprofile.core/src/org/eclipse/linuxtools/internal/oprofile/core/OprofileProperties.java
+++ oprofile/org.eclipse.linuxtools.oprofile.core/src/org/eclipse/linuxtools/internal/oprofile/core/OprofileProperties.java
@@ -21,11 +21,6 @@ public class OprofileProperties {
 	private OprofileProperties() {
 	}
 
-	/**
-	 * Return a string representing the plugin name
-	 * @param key
-	 * @return the plugin name
-	 */
 	public static String getString(String key) {
 		try {
 			return RESOURCE_BUNDLE.getString(key);
