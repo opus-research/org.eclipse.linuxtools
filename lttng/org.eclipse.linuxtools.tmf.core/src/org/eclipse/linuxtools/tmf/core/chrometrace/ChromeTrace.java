2134606be0036722881a84084df76e643c61708a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:49:40 +0000

    First commited as after_11366_rev1

---
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/chrometrace/ChromeTrace.java
@@ -0,0 +1,171 @@
+package org.eclipse.linuxtools.tmf.core.chrometrace;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.JsonParseException;
+import org.codehaus.jackson.map.JsonMappingException;
+import org.codehaus.jackson.map.ObjectMapper;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.exceptions.TmfTraceException;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfContext;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfEventParser;
+import org.eclipse.linuxtools.tmf.core.trace.ITmfLocation;
+import org.eclipse.linuxtools.tmf.core.trace.TmfLocation;
+import org.eclipse.linuxtools.tmf.core.trace.TmfTrace;
+
+/**
+ * @author ematkho
+ * @since 2.0
+ */
+public class ChromeTrace extends TmfTrace implements ITmfEventParser{
+
+    private final static String compareTo = "{\"traceEvents\""; //$NON-NLS-1$
+    private ITmfEvent events[];
+
+    private ChromeLocation fLoc;
+
+    @Override
+    public boolean validate(IProject project, String path) {
+        File chromeTrace = new File(path);
+        if (chromeTrace.isFile()) {
+
+            FileReader fr = null;
+            try {
+                fr = new FileReader(chromeTrace);
+                char cbuf[] = new char[compareTo.length()];
+                fr.read(cbuf);
+                String start = new String(cbuf);
+                fr.close();
+                return compareTo.equals(start);
+            } catch (FileNotFoundException e) {
+            } catch (IOException e) {
+            } finally {
+                if (fr != null) {
+                    try {
+                        fr.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public void initTrace(IResource resource, String path, Class<? extends ITmfEvent> type) throws TmfTraceException {
+        super.initTrace(resource, path, type);
+        parse(new File(path));
+    }
+
+
+    @Override
+    protected void processEvent(ITmfEvent event) {
+    }
+
+
+    @SuppressWarnings("nls")
+    private void parse(File file) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode rootNode = mapper.readValue(file, JsonNode.class);
+            final JsonNode jsonNode = rootNode.get("traceEvents");
+            events = new ChromeEvent[jsonNode.size()];
+            for(int i = 0 ; jsonNode.has(i); i++){
+                JsonNode event = jsonNode.get(i);
+                String type = event.get("cat").getValueAsText();
+                long ts = event.get("ts").getLongValue();
+                String pid = Integer.toString(event.get("pid").getIntValue());
+                String tid = Integer.toString(event.get("tid").getIntValue());
+                String name = event.get("name").getTextValue();
+                JsonNode argsNode = event.get("args");
+                List<String> fields = new ArrayList<String>();
+                String ph = event.get("ph").getTextValue();
+                fields.add(pid);
+                fields.add(tid);
+                fields.add(ph);
+                fields.add(name);
+                for( int j = 0; argsNode.has(j);j++){
+                    fields.add(event.get(j).getTextValue());
+                }
+                ChromeEvent c = new ChromeEvent(i, ts, type, pid, tid, fields, this);
+                events[i] = c;
+            }
+        } catch (FileNotFoundException e) {
+        } catch (JsonParseException e) {
+        } catch (JsonMappingException e) {
+        } catch (IOException e) {
+        }
+    }
+
+    @Override
+    public synchronized long getNbEvents() {
+        return events.length;
+    }
+
+    @Override
+    public ITmfLocation getCurrentLocation() {
+        return fLoc;
+    }
+
+    @Override
+    public synchronized ITmfEvent getNext(ITmfContext context) {
+        ChromeContext c = (ChromeContext) context;
+        if( c.getLocation() == null) {
+            c.setLocation(new ChromeLocation(0));
+        }
+        Integer pos = (Integer) c.getLocation().getLocationInfo();
+        context.setLocation(new ChromeLocation(pos+1));
+        context.setRank(pos+1);
+        return events[pos];
+    }
+
+    @Override
+    public double getLocationRatio(ITmfLocation location) {
+        if (events.length > 0) {
+            return ((Integer) fLoc.getLocationInfo()) / events.length;
+        }
+        return 0;
+    }
+
+    @Override
+    public ITmfContext seekEvent(ITmfLocation location) {
+        if( location == null ) {
+            location = new ChromeLocation(0);
+        }
+        Integer index = (Integer)location.getLocationInfo();
+        index = Math.min(events.length-1, index);
+        index = Math.max(0, index);
+        fLoc = new ChromeLocation(index);
+        final ChromeContext chromeContext = new ChromeContext(fLoc, this);
+        chromeContext.setRank(index);
+        return chromeContext;
+    }
+
+    @Override
+    public ITmfContext seekEvent(double ratio) {
+        Integer index = (int) (ratio * events.length);
+        return seekEvent(index);
+    }
+
+    class ChromeLocation extends TmfLocation{
+        public ChromeLocation(Integer location){
+            super(location);
+        }
+    }
+
+    @Override
+    public ITmfEvent parseEvent(ITmfContext context) {
+        Integer locationInfo = (Integer) context.getLocation().getLocationInfo();
+        return events[locationInfo];
+    }
+}
