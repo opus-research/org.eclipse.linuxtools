80eb828ba3c01cce9f20de1db08438088c34dbe3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:34:06 +0000

    First commited as after_15413_rev2

--- oprofile/org.eclipse.linuxtools.oprofile.core/src/org/eclipse/linuxtools/internal/oprofile/core/Oprofile.java
+++ oprofile/org.eclipse.linuxtools.oprofile.core/src/org/eclipse/linuxtools/internal/oprofile/core/Oprofile.java
@@ -58,8 +58,10 @@ public class Oprofile
 	 * loaded. If it is not, it will attempt to load it
 	 * (which will cause the system to prompt the user for
 	 * root access).
+	 *
+	 * @since 2.1
 	 */
-	static private void initializeOprofileModule() {
+	public static void initializeOprofileModule() {
 		// Check if kernel module is loaded, if not, try to load it
 		if (!isKernelModuleLoaded())
 			initializeOprofile();
@@ -67,7 +69,7 @@ public class Oprofile
 		//it still may not have loaded, if not, critical error
 		if (!isKernelModuleLoaded()) {
 			OprofileCorePlugin.showErrorDialog("oprofileInit", null); //$NON-NLS-1$
-//			throw new ExceptionInInitializerError(OprofileProperties.getString("fatal.kernelModuleNotLoaded")); //$NON-NLS-1$
+			//			throw new ExceptionInInitializerError(OprofileProperties.getString("fatal.kernelModuleNotLoaded")); //$NON-NLS-1$
 		}  else {
 			initializeOprofileCore();
 		}
@@ -162,7 +164,8 @@ public class Oprofile
 	}
 
 	/**
-	 * Returns the default location of the oprofile samples directory.
+	 * Returns the default location of the opcontrol samples directory
+	 * or the project directory if the profiler is operf.
 	 * @return the default samples directory
 	 */
 	public static String getDefaultSamplesDirectory() {
@@ -189,7 +192,7 @@ public class Oprofile
 	}
 
 	/**
-	 * Checks the requested counter, event, and unit mask for vailidity.
+	 * Checks the requested counter, event, and unit mask for validity.
 	 * @param ctr	the counter
 	 * @param event	the event name
 	 * @param um	the unit mask
@@ -253,7 +256,7 @@ public class Oprofile
 	public static void updateInfo(){
 		if (!isKernelModuleLoaded()){
 			initializeOprofile();
-			}
+		}
 		if(isKernelModuleLoaded()){
 			info = OpInfo.getInfo();
 		}
@@ -268,6 +271,10 @@ public class Oprofile
 	 */
 	public static class OprofileProject {
 		private static IProject project;
+		public final static String OPERF_BINARY = "operf"; //$NON-NLS-1$
+		public final static String OPCONTROL_BINARY = "opcontrol"; //$NON-NLS-1$
+		private static String binary = OPERF_BINARY;
+
 
 		/**
 		 * Set the project to be profiled
@@ -284,6 +291,25 @@ public class Oprofile
 		public static IProject getProject() {
 			return project;
 		}
+
+		/**
+		 * Set the profiling binary to be used (operf or opcontrol)
+		 * @param binary
+		 * @since 2.1
+		 */
+		public static void setProfilingBinary(String binary) {
+			OprofileProject.binary = binary;
+
+		}
+		/**
+		 * Get the profiling binary (operf or opcontrol)
+		 * @return binary
+		 * @since 2.1
+		 */
+		public static String getProfilingBinary() {
+			return binary;
+		}
+
 	}
 
 }
