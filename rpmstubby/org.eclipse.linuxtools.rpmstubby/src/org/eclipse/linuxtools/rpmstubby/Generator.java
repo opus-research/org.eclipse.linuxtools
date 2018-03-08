cfa2f53f22fd260d913b7bc7592cb910460840f6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:33:12 +0000

    First commited as after_15257_rev2

--- rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/rpmstubby/Generator.java
+++ rpmstubby/org.eclipse.linuxtools.rpmstubby/src/org/eclipse/linuxtools/rpmstubby/Generator.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013 Red Hat Inc. and others.
+ * Copyright (c) 2011, 2013 Red Hat Inc. and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -7,7 +7,7 @@
  *
  * Contributors:
  *     Alexander Kurtakov - initial API and implementation
- *     Neil Guzman        - python, ruby, perl implementation
+ *     Neil Guzman        - python, ruby, perl implementation (B#350065,B#350066)
  *******************************************************************************/
 
 package org.eclipse.linuxtools.rpmstubby;
