f8cf51df5cf16f1d2cf31fde14f99082b0a9778b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 19:02:38 +0000

    First commited as after_8002_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/parsers/custom/CustomTxtTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/parsers/custom/CustomTxtTrace.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2010 Ericsson
- * 
+ *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *   Patrick Tasse - Initial API and implementation
  *******************************************************************************/
@@ -164,15 +164,9 @@ public class CustomTxtTrace extends TmfTrace<CustomTxtEvent> implements ITmfEven
     }
 
     @Override
-    public synchronized CustomTxtEvent parseEvent(final ITmfContext tmfContext) {
-        ITmfContext context = seekEvent(tmfContext.getLocation());
-        return parse(context);
-    }
-
-    @Override
     public synchronized CustomTxtEvent getNext(final ITmfContext context) {
-        final ITmfContext savedContext = context.clone();
-        final CustomTxtEvent event = parse(context);
+        ITmfContext savedContext = new TmfContext(context.getLocation(), context.getRank());
+        final CustomTxtEvent event = parseEvent(context);
         if (event != null) {
             updateAttributes(savedContext, event.getTimestamp());
             context.increaseRank();
@@ -180,7 +174,8 @@ public class CustomTxtTrace extends TmfTrace<CustomTxtEvent> implements ITmfEven
         return event;
     }
 
-    private synchronized CustomTxtEvent parse(final ITmfContext tmfContext) {
+    @Override
+    public synchronized CustomTxtEvent parseEvent(final ITmfContext tmfContext) {
         if (fFile == null) {
             return null;
         }
--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/parsers/custom/CustomXmlTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/parsers/custom/CustomXmlTrace.java
@@ -1,11 +1,11 @@
 /*******************************************************************************
  * Copyright (c) 2010 Ericsson
- * 
+ *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
  * accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  *   Patrick Tasse - Initial API and implementation
  *******************************************************************************/
@@ -169,15 +169,9 @@ public class CustomXmlTrace extends TmfTrace<CustomXmlEvent> implements ITmfEven
     }
 
     @Override
-    public synchronized CustomXmlEvent parseEvent(final ITmfContext tmfContext) {
-        ITmfContext context = seekEvent(tmfContext.getLocation());
-        return parse(context);
-    }
-
-    @Override
     public synchronized CustomXmlEvent getNext(final ITmfContext context) {
-        final ITmfContext savedContext = context.clone();
-        final CustomXmlEvent event = parse(context);
+        ITmfContext savedContext = new TmfContext(context.getLocation(), context.getRank());
+        final CustomXmlEvent event = parseEvent(context);
         if (event != null) {
             updateAttributes(savedContext, event.getTimestamp());
             context.increaseRank();
@@ -185,7 +179,8 @@ public class CustomXmlTrace extends TmfTrace<CustomXmlEvent> implements ITmfEven
         return event;
     }
 
-    private synchronized CustomXmlEvent parse(final ITmfContext tmfContext) {
+    @Override
+    public synchronized CustomXmlEvent parseEvent(final ITmfContext tmfContext) {
         if (fFile == null) {
             return null;
         }
