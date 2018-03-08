2fe2b8a211bfb1518673d568861f41bdd3845a8d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:56:49 +0000

    First commited as before_19181_rev1

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/event/types/Definition.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/event/types/Definition.java
@@ -13,10 +13,9 @@
 package org.eclipse.linuxtools.ctf.core.event.types;
 
 import org.eclipse.linuxtools.ctf.core.event.io.BitBuffer;
-import org.eclipse.linuxtools.ctf.core.trace.CTFReaderException;
 
 /**
- * A CTF definition
+ * A CTF definiton
  *
  * A definition is like an object of a declaration class. It fills the
  * declaration with values. <br>
@@ -124,12 +123,9 @@ public abstract class Definition {
      *
      * @param input
      *            the bitbuffer containing the data to read.
-     * @throws CTFReaderException
-     *             An error occurred reading the data. If the buffer is reading
-     *             beyond its end, this exception will be raised.
      * @since 2.0
      */
-    public abstract void read(BitBuffer input) throws CTFReaderException;
+    public abstract void read(BitBuffer input);
 
     /**
      * Offset the buffer position wrt the current alignment.
@@ -140,16 +136,18 @@ public abstract class Definition {
      *            The declaration which has an alignment
      * @since 2.2
      */
-    protected static void alignRead(BitBuffer input, IDeclaration declaration){
-        long mask = declaration.getAlignment() -1;
+    protected static void alignRead(BitBuffer input, IDeclaration declaration) {
+        int mask = (int) declaration.getAlignment() - 1;
+
         /*
          * The alignment is a power of 2
          */
-        long pos = input.position();
+        int pos = input.position();
         if ((pos & mask) == 0) {
             return;
         }
         pos = (pos + mask) & ~mask;
+
         input.position(pos);
     }
 
