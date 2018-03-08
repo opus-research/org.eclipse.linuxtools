9c3e2a435896d28a123354c1dd0d97124e0a08d0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:55:05 +0000

    First commited as before_13659_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/graphingapi/ui/wizards/graph/SelectGraphWizard.java
+++ systemtap/org.eclipse.linuxtools.systemtap.graphingapi.ui/src/org/eclipse/linuxtools/systemtap/graphingapi/ui/wizards/graph/SelectGraphWizard.java
@@ -21,8 +21,8 @@ import org.eclipse.ui.IWorkbench;
 
 
 
-public class SelectGraphAndSeriesWizard extends Wizard implements INewWizard {
-	public SelectGraphAndSeriesWizard(IDataSet data, GraphData gdata) {
+public class SelectGraphWizard extends Wizard implements INewWizard {
+	public SelectGraphWizard(IDataSet data, GraphData gdata) {
 		model = new GraphModel(data);
 		edit = (gdata != null);
 		if (edit) {
@@ -40,14 +40,17 @@ public class SelectGraphAndSeriesWizard extends Wizard implements INewWizard {
 
 	@Override
 	public void addPages() {
-		setWindowTitle(Localization.getString(!edit ? "SelectGraphAndSeriesWizard.CreateGraph" : "SelectGraphAndSeriesWizard.EditGraph"));  //$NON-NLS-1$//$NON-NLS-2$
-		selectGraphandSeriesPage = new SelectGraphAndSeriesWizardPage();
-		addPage(selectGraphandSeriesPage);
+		setWindowTitle(Localization.getString(!edit ? "SelectGraphWizard.CreateGraph" : "SelectGraphWizard.EditGraph"));  //$NON-NLS-1$//$NON-NLS-2$
+		selectGraphPage = new SelectGraphWizardPage();
+		addPage(selectGraphPage);
+		selectSeriesPage = new SelectSeriesWizardPage();
+		addPage(selectSeriesPage);
 	}
 
 	@Override
 	public boolean canFinish() {
-		if (selectGraphandSeriesPage.isPageComplete())
+		if (this.getContainer().getCurrentPage() == selectSeriesPage &&
+			selectSeriesPage.isPageComplete())
 			return true;
 		return false;
 	}
@@ -71,7 +74,17 @@ public class SelectGraphAndSeriesWizard extends Wizard implements INewWizard {
 		return edit;
 	}
 
-	public SelectGraphAndSeriesWizardPage selectGraphandSeriesPage;
+	@Override
+	public void dispose() {
+		if(null != selectGraphPage)
+			selectGraphPage.dispose();
+		if(null != selectSeriesPage)
+			selectSeriesPage.dispose();
+		super.dispose();
+	}
+
+	public SelectGraphWizardPage selectGraphPage;
+	public SelectSeriesWizardPage selectSeriesPage;
 	public GraphModel model;
 	private boolean edit;
 }
