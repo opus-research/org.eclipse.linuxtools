2b25fa75d1a4a12cec57a45de04f8e3f794c3d68
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:04:46 +0000

    First commited as after_13926_rev1

--- perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/ui/StatComparisonView.java
+++ perf/org.eclipse.linuxtools.perf/src/org/eclipse/linuxtools/internal/perf/ui/StatComparisonView.java
@@ -21,6 +21,8 @@ import org.eclipse.jface.resource.JFaceResources;
 import org.eclipse.linuxtools.internal.perf.IPerfData;
 import org.eclipse.linuxtools.internal.perf.PerfPlugin;
 import org.eclipse.linuxtools.internal.perf.StatComparisonData;
+import org.eclipse.linuxtools.internal.perf.handlers.PerfStatDiffMenuAction;
+import org.eclipse.linuxtools.internal.perf.handlers.PerfStatDiffMenuAction.Type;
 import org.eclipse.linuxtools.internal.perf.model.PMStatEntry;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.StyleRange;
@@ -29,6 +31,8 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
@@ -48,6 +52,7 @@ public class StatComparisonView extends ViewPart {
 			+ PMStatEntry.DECIMAL + ").*"; //$NON-NLS-1$
 
 	private StyledText text;
+	private StatComparisonData diffData;
 	private static int SECONDARY_ID = 0;
 
 	public StatComparisonView() {
@@ -63,14 +68,24 @@ public class StatComparisonView extends ViewPart {
 		IPerfData statsDiff = PerfPlugin.getDefault()
 				.getStatDiffData();
 		if (statsDiff != null) {
-			setStyledText(statsDiff.getPerfData());
-			setContentDescription(statsDiff.getTitle());
+			diffData = (StatComparisonData)statsDiff;
+			updateData(statsDiff, true);
 		}
+
+		fillToolbarActions();
 	}
 
 	@Override
 	public void setFocus() {
 		return;
+	}
+
+	/**
+	 * Get perf data associated with the current view.
+	 * @return IPerfData data associated with this view.
+	 */
+	public IPerfData getDiffData(){
+		return diffData;
 	}
 
 	/**
@@ -80,15 +95,11 @@ public class StatComparisonView extends ViewPart {
 	 * @param input text to display
 	 */
 	private void setStyledText(String input) {
-		text.setText(input);
-		text.setAlignment(SWT.LEFT);
+		setBasicStyledText(input);
 		List<StyleRange> styles = new ArrayList<StyleRange>();
 		int ptr = 0;
 
-		// set default TextConsole font (monospaced).
-		text.setFont(JFaceResources.getFont(JFaceResources.TEXT_FONT));
-
-		String[] lines = input.split("\n");
+		String[] lines = input.split("\n"); //$NON-NLS-1$
 
 		for(String line : lines){
 			if (Pattern.matches(OCCURRENCE, line)) {
@@ -116,8 +127,67 @@ public class StatComparisonView extends ViewPart {
 		text.setStyleRanges(styles.toArray(new StyleRange[0]));
 	}
 
+	private void setBasicStyledText(String input){
+		text.setText(input);
+		text.setAlignment(SWT.LEFT);
+		// set default TextConsole font (monospaced).
+		text.setFont(JFaceResources.getFont(JFaceResources.TEXT_FONT));
+	}
+
 	/**
-	 * Show new view with provided input.
+	 * Create toolbar actions associated with this view.
+	 */
+	private void fillToolbarActions() {
+		// create [Old] [New] [Diff] actions.
+		IActionBars bars = getViewSite().getActionBars();
+		for (Type type : PerfStatDiffMenuAction.Type.values()) {
+			bars.getMenuManager().add(
+					new PerfStatDiffMenuAction(type, getViewSite().getSecondaryId()));
+		}
+	}
+
+	/**
+	 * Update contents of current view, replacing the containing data and text styling.
+	 *
+	 * @param data IPerfData data replacement.
+	 * @param style boolean true if styling is to be applied, false otherwise.
+	 */
+	public void updateData(IPerfData data, boolean style){
+		if(data != null){
+			if (style) {
+				setStyledText(data.getPerfData());
+			} else {
+				setBasicStyledText(data.getPerfData());
+			}
+			setContentDescription(data.getTitle());
+		}
+	}
+
+	/**
+	 * Utility method to get an instance of a {@link StatComparisonView} by
+	 * providing the secondary identifier.
+	 *
+	 * @param sID String secondary identifier.
+	 * @return IViewPart {@link StatComparisonView} associated with the
+	 *         specified secondary identifier.
+	 */
+	public static IViewPart getView(String sID) {
+		IViewPart view = null;
+		try {
+			view = PlatformUI
+					.getWorkbench()
+					.getActiveWorkbenchWindow()
+					.getActivePage()
+					.showView(PerfPlugin.STAT_DIFF_VIEW_ID, sID,
+							IWorkbenchPage.VIEW_ACTIVATE);
+		} catch (PartInitException e) {
+			PerfPlugin.getDefault().openError(e, ""); //$NON-NLS-1$
+		}
+		return view;
+	}
+
+	/**
+	 * Create new view.
 	 */
 	public static void refreshView() {
 		Display.getDefault().syncExec(new Runnable() {
