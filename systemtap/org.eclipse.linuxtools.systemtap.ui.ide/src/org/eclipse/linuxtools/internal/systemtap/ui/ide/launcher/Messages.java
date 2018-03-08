d18ed2944bc78caa9d7d3a596aa597831721c470
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 23:45:14 +0000

    First commited as before_18911_rev7

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/Messages.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/launcher/Messages.java
@@ -57,14 +57,6 @@ public class Messages extends NLS {
 	public static String SystemTapScriptGraphOptionsTab_sampleOutputNoMatch;
 	public static String SystemTapScriptGraphOptionsTab_sampleOutputIsEmpty;
 
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsButton;
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsTooltip;
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsTitle;
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsMessage;
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsErrorTitle;
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsError;
-	public static String SystemTapScriptGraphOptionsTab_generateFromPrintsEmpty;
-
 	public static String SystemTapScriptLaunchConfigurationTab_0;
 	public static String SystemTapScriptLaunchConfigurationTab_1;
 	public static String SystemTapScriptLaunchConfigurationTab_2;
@@ -97,6 +89,7 @@ public class Messages extends NLS {
 	public static String SystemTapScriptLaunchError_graph;
 	public static String SystemTapScriptLaunchError_fileNotFound;
 	public static String SystemTapScriptLaunchError_fileNotStp;
+	public static String SystemTapScriptLaunchError_waitForConsoles;
 
 	static {
 		// initialize resource bundle
