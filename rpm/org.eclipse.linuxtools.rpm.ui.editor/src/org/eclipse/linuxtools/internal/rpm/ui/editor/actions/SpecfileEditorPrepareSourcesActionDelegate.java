2e9c935bf28e6c110ad65a69bf135faa1b6fafb7
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:02:51 +0000

    First commited as after_13828_rev1

---
+++ rpm/org.eclipse.linuxtools.rpm.ui.editor/src/org/eclipse/linuxtools/internal/rpm/ui/editor/actions/SpecfileEditorPrepareSourcesActionDelegate.java
@@ -0,0 +1,17 @@
+package org.eclipse.linuxtools.internal.rpm.ui.editor.actions;
+
+import org.eclipse.core.commands.AbstractHandler;
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+
+public class SpecfileEditorPrepareSourcesActionDelegate extends AbstractHandler {
+
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		// download sources
+
+		// prepare sources
+
+		return null;
+	}
+
+}
