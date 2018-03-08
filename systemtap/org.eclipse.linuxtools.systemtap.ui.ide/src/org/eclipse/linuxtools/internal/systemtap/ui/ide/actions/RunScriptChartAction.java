76582ab56b27fb418e25db372ddca58db3abda9a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:14:57 +0000

    First commited as before_10055_rev2

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/actions/RunScriptChartAction.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/actions/RunScriptChartAction.java
@@ -9,23 +9,20 @@
  *     IBM Corporation - Jeff Briggs, Henry Hughes, Ryan Morse, Anithra P J
  *******************************************************************************/
 
-package org.eclipse.linuxtools.internal.systemtap.ui.graphicalrun.actions;
+package org.eclipse.linuxtools.internal.systemtap.ui.ide.actions;
 
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-import org.eclipse.jface.dialogs.ErrorDialog;
 import org.eclipse.jface.wizard.WizardDialog;
-import org.eclipse.linuxtools.internal.systemtap.ui.graphicalrun.GraphicalRunPlugin;
+import org.eclipse.linuxtools.systemtap.ui.consolelog.actions.ChartStreamDaemon2;
 import org.eclipse.linuxtools.systemtap.ui.consolelog.structures.ScriptConsole;
-import org.eclipse.linuxtools.systemtap.ui.graphicalrun.structures.ChartStreamDaemon2;
 import org.eclipse.linuxtools.systemtap.ui.graphing.GraphingConstants;
 import org.eclipse.linuxtools.systemtap.ui.graphing.GraphingPerspective;
 import org.eclipse.linuxtools.systemtap.ui.graphing.views.GraphSelectorView;
 import org.eclipse.linuxtools.systemtap.ui.graphingapi.nonui.datasets.IDataSet;
 import org.eclipse.linuxtools.systemtap.ui.graphingapi.nonui.datasets.IDataSetParser;
 import org.eclipse.linuxtools.systemtap.ui.graphingapi.ui.wizards.dataset.DataSetWizard;
+import org.eclipse.linuxtools.systemtap.ui.ide.actions.Messages;
 import org.eclipse.linuxtools.systemtap.ui.ide.actions.RunScriptAction;
-import org.eclipse.swt.widgets.Display;
+import org.eclipse.linuxtools.systemtap.ui.structures.ui.ExceptionErrorDialog;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPage;
@@ -41,6 +38,7 @@ public class RunScriptChartAction extends RunScriptAction {
 
 	@Override
 	protected void scriptConsoleInitialized(ScriptConsole console){
+		getChartingOptions();
 		console.getCommand().addInputStreamListener(new ChartStreamDaemon2(console, dataSet, parser));
 		try {
 			IWorkbenchPage p = PlatformUI.getWorkbench().showPerspective(GraphingPerspective.ID, PlatformUI.getWorkbench().getActiveWorkbenchWindow());
@@ -48,12 +46,7 @@ public class RunScriptChartAction extends RunScriptAction {
 			String name = console.getName();
 			((GraphSelectorView)ivp).createScriptSet(name.substring(name.lastIndexOf('/')+1), dataSet);
 		} catch(WorkbenchException we) {
-			IStatus status = new Status(IStatus.ERROR,
-				      GraphicalRunPlugin.PLUGIN_ID, 1, Messages.RunScriptChartAction_couldNotSwitchToGraphicPerspective , we);
-			ErrorDialog.openError(Display.getCurrent().getActiveShell(),
-					Messages.RunScriptChartAction_couldNotSwitchToGraphicPerspective,
-					null,
-					status);
+			ExceptionErrorDialog.openError(Messages.RunScriptChartAction_couldNotSwitchToGraphicPerspective, we);
 		}
 	}
 
@@ -77,12 +70,6 @@ public class RunScriptChartAction extends RunScriptAction {
 			continueRun = false;
 		}
 		wizard.dispose();
-	}
-
-	@Override
-	protected String[] buildStandardScript() {
-		getChartingOptions();
-		return super.buildStandardScript();
 	}
 
 	private IDataSet dataSet = null;
