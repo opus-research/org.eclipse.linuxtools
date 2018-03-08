4bb95d366909e31679bcb7bdbc80bebf9afb4ec5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:11:44 +0000

    First commited as before_12135_rev4

--- lttng/org.eclipse.linuxtools.lttng2.kernel.core/src/org/eclipse/linuxtools/internal/lttng2/kernel/core/stateprovider/CtfKernelStateInput.java
+++ lttng/org.eclipse.linuxtools.lttng2.kernel.core/src/org/eclipse/linuxtools/internal/lttng2/kernel/core/stateprovider/CtfKernelStateInput.java
@@ -44,7 +44,7 @@ public class CtfKernelStateInput extends AbstractStateChangeInput {
      * Version number of this state provider. Please bump this if you modify the
      * contents of the generated state history in some way.
      */
-    private static final int VERSION = 0;
+    private static final int VERSION = 1;
 
     /* Event names HashMap. TODO: This can be discarded once we move to Java 7 */
     private final HashMap<String, Integer> knownEventNames;
@@ -333,6 +333,13 @@ public class CtfKernelStateInput extends AbstractStateChangeInput {
                 /* Set the process' syscall name, to be the same as the parent's */
                 quark = ss.getQuarkRelativeAndAdd(parentTidNode, Attributes.SYSTEM_CALL);
                 value = ss.queryOngoingState(quark);
+                if (value.isNull()) {
+                    /*
+                     * Maybe we were missing info about the parent? At least we
+                     * will set the child right. Let's suppose "sys_clone".
+                     */
+                    value = TmfStateValue.newValueString(LttngStrings.SYS_CLONE);
+                }
                 quark = ss.getQuarkRelativeAndAdd(childTidNode, Attributes.SYSTEM_CALL);
                 ss.modifyAttribute(ts, value, quark);
             }
@@ -404,6 +411,25 @@ public class CtfKernelStateInput extends AbstractStateChangeInput {
                     }
                     ss.modifyAttribute(ts, value, quark);
                 }
+            }
+                break;
+
+            case 12: // "sched_wakeup":
+            case 13: // "sched_wakeup_new":
+            /* Fields (same fields for both types):
+             * string comm, int32 pid, int32 prio, int32 success,
+             * int32 target_cpu */
+            {
+                final int tid = ((Long) content.getField(LttngStrings.TID).getValue()).intValue();
+                final int threadNode = ss.getQuarkRelativeAndAdd(getNodeThreads(), String.valueOf(tid));
+
+                /*
+                 * The process indicated in the event's payload is now ready to
+                 * run. Assign it to the "wait for cpu" state.
+                 */
+                quark = ss.getQuarkRelativeAndAdd(threadNode, Attributes.STATUS);
+                value = TmfStateValue.newValueInt(StateValues.PROCESS_STATUS_WAIT_FOR_CPU);
+                ss.modifyAttribute(ts, value, quark);
             }
                 break;
 
@@ -503,6 +529,8 @@ public class CtfKernelStateInput extends AbstractStateChangeInput {
         map.put(LttngStrings.SCHED_PROCESS_EXIT, 9);
         map.put(LttngStrings.SCHED_PROCESS_FREE, 10);
         map.put(LttngStrings.STATEDUMP_PROCESS_STATE, 11);
+        map.put(LttngStrings.SCHED_WAKEUP, 12);
+        map.put(LttngStrings.SCHED_WAKEUP_NEW, 13);
 
         return map;
     }
