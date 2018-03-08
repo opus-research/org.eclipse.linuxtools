e36b5d6833d559bc2bb3d0420c72b73dcfd35a6d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 20:57:13 +0000

    First commited as before_11550_rev1

--- systemtap/org.eclipse.linuxtools.callgraph/src/org/eclipse/linuxtools/internal/callgraph/graphlisteners/AutoScrollSelectionListener.java
+++ systemtap/org.eclipse.linuxtools.callgraph/src/org/eclipse/linuxtools/internal/callgraph/graphlisteners/AutoScrollSelectionListener.java
@@ -4,7 +4,7 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
  *     Red Hat - initial API and implementation
  *******************************************************************************/
@@ -15,11 +15,12 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 
 public class AutoScrollSelectionListener extends SelectionAdapter{
-	public static final int AUTO_SCROLL_UP = 0;
-	public static final int AUTO_SCROLL_DOWN = 1;
+	public static final int AutoScroll_up = 0;
+	public static final int AutoScroll_down = 1;
+	public static final int AutoScroll_bar = 2;
 	private final int type;
 	private final StapGraph graph;
-
+	
 	public AutoScrollSelectionListener(int type, StapGraph g) {
 		this.type = type;
 		this.graph = g;
@@ -27,12 +28,10 @@ public class AutoScrollSelectionListener extends SelectionAdapter{
 
 	@Override
 	public void widgetSelected(SelectionEvent e) {
-		if (type == AUTO_SCROLL_UP) {
+		if (type == AutoScroll_up)
 			AutoScrollHelper.scrollUp(graph);
-		}
-		if (type == AUTO_SCROLL_DOWN) {
+		if (type == AutoScroll_down)
 			AutoScrollHelper.scrollDown(graph);
-		}
 	}
 
 }
