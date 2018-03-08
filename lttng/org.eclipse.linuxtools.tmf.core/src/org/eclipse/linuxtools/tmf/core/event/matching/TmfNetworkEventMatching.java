fd53d7b6338a37c6f95a70b50933dffe40671e17
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:46:35 +0000

    First commited as before_15907_rev2

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/matching/TmfNetworkEventMatching.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/event/matching/TmfNetworkEventMatching.java
@@ -124,7 +124,7 @@ public class TmfNetworkEventMatching extends TmfEventMatching {
          * just hope developers register NetworkMatchDefinition for Network
          * match types...
          */
-        ITmfNetworkMatchDefinition def = (ITmfNetworkMatchDefinition) getEventDefinition(event.getTrace());
+        TmfNetworkMatchDefinition def = (TmfNetworkMatchDefinition) getEventDefinition(event.getTrace());
         if (def == null) {
             return;
         }
