c35d9b04d414f6c07677fa55398271ff7868a4cc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 22:14:49 +0000

    First commited as before_14295_rev3

--- profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/profiling/launch/IRemoteProxyManager.java
+++ profiling/org.eclipse.linuxtools.profiling.launch/src/org/eclipse/linuxtools/profiling/launch/IRemoteProxyManager.java
@@ -11,7 +11,6 @@
 package org.eclipse.linuxtools.profiling.launch;
 
 import java.net.URI;
-import java.util.Map;
 
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.runtime.CoreException;
@@ -26,22 +25,4 @@ public interface IRemoteProxyManager {
 	public IRemoteCommandLauncher getLauncher(URI uri) throws CoreException;
 	public String getOS(IProject project) throws CoreException;
 	public String getOS(URI uri) throws CoreException;
-	/**
-	 * Method to get system's environment variables.
-	 *
-	 * @param Project
-	 *            IProject
-	 * @return Mapping of environment variables
-	 * @since 2.1
-	*/	
-	public Map<String, String> getEnv(IProject project) throws CoreException;
-        /**
-         * Method to get system's environment variables.
-         *      
-         * @param Resource URI
-         *            URI
-         * @return Mapping of environment variables
-         * @since 2.1
-        */
-	public Map<String, String> getEnv(URI uri) throws CoreException;
 }
