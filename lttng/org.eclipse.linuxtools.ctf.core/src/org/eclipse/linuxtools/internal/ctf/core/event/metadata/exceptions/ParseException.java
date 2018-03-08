626bc7123ece80d00196e20ba004aaf722156101
Matheus Paixao <mhepaixao@gmail.com>
2018-03-08 21:05:41 +0000

    First commited as after_11788_rev4

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/internal/ctf/core/event/metadata/exceptions/ParseException.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/internal/ctf/core/event/metadata/exceptions/ParseException.java
@@ -12,6 +12,7 @@
 
 package org.eclipse.linuxtools.internal.ctf.core.event.metadata.exceptions;
 
+
 /**
  * <b><u>ParseException</u></b>
  */
@@ -19,8 +20,9 @@ public class ParseException extends Exception {
 
     private static final long serialVersionUID = 7901917601459652080L;
 
+
     /**
-     * Enoty constructor
+     * Enpty constructor
      */
     public ParseException() {
         super();
@@ -34,5 +36,15 @@ public class ParseException extends Exception {
     public ParseException(String message) {
         super(message);
     }
+
+    /**
+     * Copy constructor
+     * @param e the exception to throw
+     */
+    public ParseException(Exception e) {
+        super(e);
+    }
+
+
 
 }
