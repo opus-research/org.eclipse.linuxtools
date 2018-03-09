de268e1d08fc4fce8846dcd2425ebb579b5271fd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:31:53 +0000

    First commited as after_19877_rev3

--- rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/Messages.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/Messages.java
@@ -80,6 +80,12 @@ public final class Messages {
 	public static String Createrepo_errorTryingToFindCommand;
 	/****/
 	public static String Createrepo_errorCommandNotFound;
+	/****/
+	public static String Createrepo_errorWrongVersion;
+	/****/
+	public static String Createrepo_errorCancelled;
+	/****/
+	public static String Createrepo_errorPasingVersion;
 
 	// RepoFormEditor
 	/****/
