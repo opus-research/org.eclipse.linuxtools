47843f0da2ba761cc2962c808ed4db87ff1c84cd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:35:36 +0000

    First commited as after_19910_rev7

---
+++ lttng/org.eclipse.linuxtools.tmf.analysis.xml.core/src/org/eclipse/linuxtools/internal/tmf/analysis/xml/core/stateprovider/model/TmfXmlStateChange.java
@@ -0,0 +1,176 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Ecole Polytechnique de Montreal
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   Florian Wininger - Initial API and implementation
+ ******************************************************************************/
+
+package org.eclipse.linuxtools.internal.tmf.analysis.xml.core.stateprovider.model;
+
+import org.eclipse.linuxtools.tmf.analysis.xml.core.stateprovider.TmfXmlStrings;
+import org.eclipse.linuxtools.tmf.analysis.xml.core.stateprovider.XmlStateProvider;
+import org.eclipse.linuxtools.tmf.core.event.ITmfEvent;
+import org.eclipse.linuxtools.tmf.core.exceptions.AttributeNotFoundException;
+import org.eclipse.linuxtools.tmf.core.exceptions.StateValueTypeException;
+import org.eclipse.linuxtools.tmf.core.exceptions.TimeRangeException;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * This Class implement a State Change is the XML state provider
+ *
+ * <pre>
+ *  example 1: Simple state change
+ *  <stateChange>
+ *      <location value="CurrentThread" />
+ *      <constant value="System_call" />
+ *      <stateValue type="null" />
+ *  </stateChange>
+ *
+ *  example 2: Conditional state change
+ *  <stateChange>
+ *     <if>
+ *      <condition>
+ *        <location value="CurrentThread" />
+ *        <constant value="System_call" />
+ *        <stateValue type="null" />
+ *      </condition>
+ *     </if>
+ *    <then>
+ *      <location value="CurrentThread" />
+ *      <constant value="Status" />
+ *      <stateValue int="$PROCESS_STATUS_RUN_USERMODE"/>
+ *    </then>
+ *    <else>
+ *      <location value="CurrentThread" />
+ *      <constant value="Status" />
+ *      <stateValue int="$PROCESS_STATUS_RUN_SYSCALL"/>
+ *    </else>
+ *  </stateChange>
+ * </pre>
+ *
+ * @author Florian Wininger
+ *
+ */
+public class TmfXmlStateChange {
+
+    private final IXmlStateChange fChange;
+    private final XmlStateProvider fProvider;
+
+    /**
+     * Constructor
+     *
+     * @param statechange
+     *            XML node root of this state change
+     * @param provider
+     *            The state provider this state change belongs to
+     */
+    TmfXmlStateChange(Element statechange, XmlStateProvider provider) {
+        fProvider = provider;
+
+        /*
+         * child nodes is either a list of TmfXmlStateAttributes and
+         * TmfXmlStateValues, or an if-then-else series of nodes.
+         */
+        Node ifNode = statechange.getElementsByTagName(TmfXmlStrings.IF).item(0);
+        /* the state change has a condition */
+        if (ifNode != null) {
+            fChange = new XmlConditionalChange(statechange);
+        } else {
+            /* the state change does not have a condition */
+            fChange = new XmlStateValueChange(statechange);
+        }
+    }
+
+    /**
+     * Execute the state change for an event. It validates the condition and
+     * executes the required change.
+     *
+     * @param event
+     *            The event to process
+     * @throws AttributeNotFoundException
+     *             Pass through the exception it received
+     * @throws TimeRangeException
+     *             Pass through the exception it received
+     * @throws StateValueTypeException
+     *             Pass through the exception it received
+     */
+    public void handleEvent(ITmfEvent event) throws AttributeNotFoundException, StateValueTypeException, TimeRangeException {
+        fChange.handleEvent(event);
+    }
+
+    /* Interface for both private classes to handle the event */
+    private interface IXmlStateChange {
+        void handleEvent(ITmfEvent event) throws AttributeNotFoundException, StateValueTypeException, TimeRangeException;
+    }
+
+    /**
+     * Conditional state change with a condition to verify
+     */
+    private class XmlConditionalChange implements IXmlStateChange {
+        private final TmfXmlCondition fCondition;
+        private final TmfXmlStateChange fThenChange;
+        private final TmfXmlStateChange fElseChange;
+
+        public XmlConditionalChange(Element statechange) {
+            /*
+             * The if node exist, it has been verified before calling this
+             */
+            Node ifNode = statechange.getElementsByTagName(TmfXmlStrings.IF).item(0);
+            fCondition = new TmfXmlCondition(ifNode, fProvider);
+
+            Node thenNode = statechange.getElementsByTagName(TmfXmlStrings.THEN).item(0);
+            if (thenNode != null) {
+                fThenChange = new TmfXmlStateChange((Element) thenNode, fProvider);
+            } else {
+                fThenChange = null;
+            }
+            Node elseNode = statechange.getElementsByTagName(TmfXmlStrings.ELSE).item(0);
+            if (elseNode != null) {
+                fElseChange = new TmfXmlStateChange((Element) elseNode, fProvider);
+            } else {
+                fElseChange = null;
+            }
+
+        }
+
+        @Override
+        public void handleEvent(ITmfEvent event) throws AttributeNotFoundException, StateValueTypeException, TimeRangeException {
+            TmfXmlStateChange toExecute = fThenChange;
+            if (!fCondition.testForEvent(event)) {
+                toExecute = fElseChange;
+            }
+
+            if (toExecute == null) {
+                return;
+            }
+            toExecute.handleEvent(event);
+
+        }
+    }
+
+    /**
+     * State change with no condition
+     */
+    private class XmlStateValueChange implements IXmlStateChange {
+        private final TmfXmlStateValue fValue;
+
+        public XmlStateValueChange(Element statechange) {
+            NodeList childNodes = statechange.getChildNodes();
+            fValue = new TmfXmlStateValue(childNodes, fProvider);
+        }
+
+        @Override
+        public void handleEvent(ITmfEvent event) throws AttributeNotFoundException, StateValueTypeException, TimeRangeException {
+            fValue.handleEvent(event);
+        }
+
+    }
+
+}
