1cec1b3becd4efa6a7c824320275f2deee9b65cd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:13:01 +0000

    First commited as before_19458_rev2

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/trace/CTFTrace.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/ctf/core/trace/CTFTrace.java
@@ -382,7 +382,6 @@ public class CTFTrace implements IDefinitionScope {
      * Method majorIsSet is the major version number set?
      *
      * @return boolean is the major set?
-     * @since 3.0
      */
     public boolean majorIsSet() {
         return major != null;
