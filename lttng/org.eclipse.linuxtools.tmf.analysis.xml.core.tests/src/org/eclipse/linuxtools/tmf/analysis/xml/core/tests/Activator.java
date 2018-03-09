0bf5e6ae52df2401dfe6592379b6062acd1d530d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:37:01 +0000

    First commited as before_19914_rev9

--- lttng/org.eclipse.linuxtools.tmf.analysis.xml.core.tests/src/org/eclipse/linuxtools/tmf/analysis/xml/core/tests/Activator.java
+++ lttng/org.eclipse.linuxtools.tmf.analysis.xml.core.tests/src/org/eclipse/linuxtools/tmf/analysis/xml/core/tests/Activator.java
@@ -12,13 +12,13 @@
 
 package org.eclipse.linuxtools.tmf.analysis.xml.core.tests;
 
-import org.eclipse.core.runtime.Plugin;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends Plugin {
+public class Activator extends AbstractUIPlugin {
 
     /**
      * The plug-in ID
