e3ec5a2c98380e40130113d439f5540f4c5052e6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:29:26 +0000

    First commited as before_19815_rev4

--- rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/wizard/CreaterepoNewWizardPageTwo.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/wizard/CreaterepoNewWizardPageTwo.java
@@ -16,7 +16,6 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.jface.wizard.WizardPage;
-import org.eclipse.linuxtools.internal.rpm.createrepo.Activator;
 import org.eclipse.linuxtools.internal.rpm.createrepo.Messages;
 import org.eclipse.linuxtools.rpm.createrepo.ICreaterepoConstants;
 import org.eclipse.swt.SWT;
@@ -27,7 +26,6 @@ import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
-import org.eclipse.ui.PlatformUI;
 
 /**
  * This page allows the user to initialize the .repo file
@@ -86,14 +84,11 @@ public class CreaterepoNewWizardPageTwo extends WizardPage {
 				setPageComplete(isValid());
 			}
 		};
-		repositoryIDTxt = createTextFieldWithLabel(information, Messages.CreaterepoNewWizardPageTwo_labelID,
-				Messages.CreaterepoNewWizardPageTwo_tooltipID);
+		repositoryIDTxt = createTextFieldWithLabel(information, Messages.CreaterepoNewWizardPageTwo_labelID);
 		repositoryIDTxt.addModifyListener(modifyListner);
-		repositoryNameTxt = createTextFieldWithLabel(information, Messages.CreaterepoNewWizardPageTwo_labelName,
-				Messages.CreaterepoNewWizardPageTwo_tooltipName);
+		repositoryNameTxt = createTextFieldWithLabel(information, Messages.CreaterepoNewWizardPageTwo_labelName);
 		repositoryNameTxt.addModifyListener(modifyListner);
-		repositoryBaseURLTxt = createTextFieldWithLabel(information, Messages.CreaterepoNewWizardPageTwo_labelURL,
-				Messages.CreaterepoNewWizardPageTwo_tooltipURL);
+		repositoryBaseURLTxt = createTextFieldWithLabel(information, Messages.CreaterepoNewWizardPageTwo_labelURL);
 		repositoryBaseURLTxt.addModifyListener(modifyListner);
 		setControl(container);
 	}
@@ -105,7 +100,7 @@ public class CreaterepoNewWizardPageTwo extends WizardPage {
 	 * @param labelName The name on the label.
 	 * @return The newly created text field.
 	 */
-	protected Text createTextFieldWithLabel(Composite parent, String labelName, String tooltip) {
+	protected Text createTextFieldWithLabel(Composite parent, String labelName) {
 		GridData layoutData = new GridData();
 		// create the label
 		Label respositoryBaseURLLbl = new Label(parent, SWT.NONE);
@@ -120,7 +115,6 @@ public class CreaterepoNewWizardPageTwo extends WizardPage {
 		layoutData.horizontalAlignment = GridData.FILL;
 		layoutData.verticalAlignment = GridData.CENTER;
 		textField.setLayoutData(layoutData);
-		textField.setToolTipText(tooltip);
 		return textField;
 	}
 
@@ -203,5 +197,4 @@ public class CreaterepoNewWizardPageTwo extends WizardPage {
 		}
 		return repositoryBaseURLTxt.getText().trim();
 	}
-
 }
