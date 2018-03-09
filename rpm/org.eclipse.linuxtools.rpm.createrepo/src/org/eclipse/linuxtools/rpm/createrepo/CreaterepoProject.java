6e5ca0946f10a2fab45f372878e2042544cf6f00
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:12:46 +0000

    First commited as before_19457_rev1

--- rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/rpm/createrepo/CreaterepoProject.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/rpm/createrepo/CreaterepoProject.java
@@ -31,7 +31,6 @@ import org.eclipse.core.runtime.Status;
 import org.eclipse.core.runtime.preferences.IEclipsePreferences;
 import org.eclipse.linuxtools.internal.rpm.createrepo.Activator;
 import org.eclipse.linuxtools.internal.rpm.createrepo.Createrepo;
-import org.eclipse.linuxtools.internal.rpm.createrepo.CreaterepoCommandCreator;
 import org.eclipse.linuxtools.internal.rpm.createrepo.Messages;
 import org.osgi.framework.FrameworkUtil;
 
@@ -152,25 +151,6 @@ public class CreaterepoProject {
 	}
 
 	/**
-	 * Execute the createrepo command with a call to update.
-	 *
-	 * @param os Direct execution stream to this.
-	 * @return The status of the execution.
-	 * @throws CoreException Thrown when failure to execute command.
-	 */
-	public IStatus update(OutputStream os) throws CoreException {
-		if (!getContentFolder().exists()) {
-			createContentFolder();
-		}
-		Createrepo createrepo = new Createrepo();
-		List<String> commands = getCommandArguments();
-		commands.add(ICreaterepoConstants.DASH.concat(CreaterepoPreferenceConstants.PREF_UPDATE));
-		IStatus result = createrepo.execute(os, this, commands);
-		getProject().refreshLocal(IResource.DEPTH_INFINITE, monitor);
-		return result;
-	}
-
-	/**
 	 * Get the project.
 	 *
 	 * @return The project.
@@ -235,10 +215,8 @@ public class CreaterepoProject {
 	 *
 	 * @return The command arguments.
 	 */
-	private List<String> getCommandArguments() {
+	private static List<String> getCommandArguments() {
 		List<String> commands = new ArrayList<String>();
-		CreaterepoCommandCreator creator = new CreaterepoCommandCreator(projectPreferences);
-		commands.addAll(creator.getCommands());
 		return commands;
 	}
 
