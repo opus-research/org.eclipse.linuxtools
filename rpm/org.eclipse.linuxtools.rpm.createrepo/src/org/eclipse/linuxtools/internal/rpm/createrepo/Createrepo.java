e1a975a0cc1d934676a11c879c68b2118469a4b6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:22:44 +0000

    First commited as before_19614_rev1

--- rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/Createrepo.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo/src/org/eclipse/linuxtools/internal/rpm/createrepo/Createrepo.java
@@ -18,12 +18,9 @@ import java.util.List;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
-import org.eclipse.linuxtools.rpm.core.utils.BufferedProcessInputStream;
 import org.eclipse.linuxtools.rpm.core.utils.Utils;
-import org.eclipse.linuxtools.rpm.createrepo.CreaterepoPreferenceConstants;
 import org.eclipse.linuxtools.rpm.createrepo.CreaterepoProject;
 import org.eclipse.linuxtools.rpm.createrepo.ICreaterepoConstants;
-import org.eclipse.osgi.util.NLS;
 import org.osgi.framework.FrameworkUtil;
 
 /**
@@ -31,27 +28,17 @@ import org.osgi.framework.FrameworkUtil;
  */
 public class Createrepo {
 
-	/*
-	 * Default commands that every execution will have.
-	 */
-	private static final String[] DEFAULT_ARGUMENTS = {
-		CreaterepoPreferenceConstants.PREF_VERBOSE, CreaterepoPreferenceConstants.PREF_PROFILE};
-
 	/**
 	 * Holds the command line switches.
 	 */
 	private List<String> commandSwitches;
 
 	/**
-	 * Initialize the command switches to be "createrepo" and
-	 * default command arguments.
+	 * Initialize the command switches to only be "createrepo".
 	 */
 	public Createrepo() {
 		commandSwitches = new ArrayList<String>();
 		commandSwitches.add(ICreaterepoConstants.CREATEREPO_COMMAND);
-		for (String arg : DEFAULT_ARGUMENTS) {
-			commandSwitches.add(ICreaterepoConstants.DASH.concat(arg));
-		}
 	}
 
 	/**
@@ -66,59 +53,15 @@ public class Createrepo {
 	 * @throws CoreException Occurs when error trying to execute the command.
 	 */
 	public IStatus execute(final OutputStream os, CreaterepoProject project, List<String> commands) throws CoreException {
-		IStatus available = checkIfAvailable(os);
-		if (!available.isOK()) {
-			return available;
-		}
 		commandSwitches.addAll(commands);
 		commandSwitches.add(project.getContentFolder().getLocation().toOSString());
-		/* Display what the execution looks like */
-		String commandString = ICreaterepoConstants.EMPTY_STRING;
-		for (String arg : commandSwitches) {
-			commandString = commandString.concat(arg + " "); //$NON-NLS-1$
-		}
-		commandString = commandString.concat("\n"); //$NON-NLS-1$
 		try {
-			os.write(commandString.getBytes());
 			return Utils.runCommand(os, project.getProject(), commandSwitches.toArray(new String[commandSwitches.size()]));
 		} catch (IOException e) {
 			IStatus status = new Status(
 					IStatus.ERROR,
 					FrameworkUtil.getBundle(CreaterepoProject.class).getSymbolicName(),
-					NLS.bind(Messages.Createrepo_errorExecuting, commandString), null);
-			throw new CoreException(status);
-		}
-	}
-
-	/**
-	 * Check if the createrepo command is available in the system.
-	 *
-	 * @param os The outputstream to place the messages.
-	 * @return The status of whether or not createrepo was found.
-	 * @throws CoreException Occurs when createrepo is not found or executing the command failed.
-	 */
-	public static IStatus checkIfAvailable(final OutputStream os) throws CoreException {
-		try {
-			BufferedProcessInputStream bpis = Utils.runCommandToInputStream("which", "createrepo"); //$NON-NLS-1$ //$NON-NLS-2$
-			// error executing "which createrepo", most likely due to it not being found
-			if (bpis.getExitValue() == 1) {
-				return new Status(IStatus.ERROR,
-						FrameworkUtil.getBundle(CreaterepoProject.class).getSymbolicName(),
-						Messages.Createrepo_errorCommandNotFound, null);
-			}
-			return new Status(IStatus.OK, FrameworkUtil.getBundle(CreaterepoProject.class).getSymbolicName(),
-					ICreaterepoConstants.EMPTY_STRING, null);
-		} catch (IOException e) {
-			IStatus status = new Status(
-					IStatus.WARNING,
-					FrameworkUtil.getBundle(CreaterepoProject.class).getSymbolicName(),
-					Messages.Createrepo_errorTryingToFindCommand, null);
-			throw new CoreException(status);
-		} catch (InterruptedException e) {
-			IStatus status = new Status(
-					IStatus.CANCEL,
-					FrameworkUtil.getBundle(CreaterepoProject.class).getSymbolicName(),
-					Messages.Createrepo_jobCancelled, null);
+					Messages.Createrepo_errorExecuting, null);
 			throw new CoreException(status);
 		}
 	}
