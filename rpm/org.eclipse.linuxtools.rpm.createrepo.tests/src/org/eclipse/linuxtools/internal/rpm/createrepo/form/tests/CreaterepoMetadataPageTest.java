5fb10854cf8c0d72be8162ef43f28ae5569aa717
Matheus Paixao <mhepaixao@gmail.com>
2018-03-09 00:28:33 +0000

    First commited as after_19734_rev3

--- rpm/org.eclipse.linuxtools.rpm.createrepo.tests/src/org/eclipse/linuxtools/internal/rpm/createrepo/form/tests/CreaterepoMetadataPageTest.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo.tests/src/org/eclipse/linuxtools/internal/rpm/createrepo/form/tests/CreaterepoMetadataPageTest.java
@@ -60,7 +60,7 @@ public class CreaterepoMetadataPageTest {
 	private SWTBot metadataPageBot;
 
 	/**
-	 * Initialize the test project. Will close the welcome view.
+	 * Initialize the test project.
 	 *
 	 * @throws CoreException
 	 */
