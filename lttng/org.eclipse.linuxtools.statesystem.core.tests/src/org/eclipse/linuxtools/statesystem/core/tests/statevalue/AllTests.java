69a572e99086144ef58d5e948523d28c721c536a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:41:01 +0000

    First commited as after_33716_rev11

--- lttng/org.eclipse.linuxtools.statesystem.core.tests/src/org/eclipse/linuxtools/statesystem/core/tests/statevalue/AllTests.java
+++ lttng/org.eclipse.linuxtools.statesystem.core.tests/src/org/eclipse/linuxtools/statesystem/core/tests/statevalue/AllTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2014 Ericsson
+ * Copyright (c) 2014 École Polytechnique de Montréal
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -7,21 +7,20 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *   Alexandre Montplaisir - Initial API and implementation
+ *   Naser Ezzati - Initial API and implementation
  *******************************************************************************/
 
-package org.eclipse.linuxtools.statesystem.core.tests;
+package org.eclipse.linuxtools.statesystem.core.tests.statevalue;
 
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 /**
- * Master test suite
+ * Test suite for org.eclipse.linuxtools.tmf.core.statesystem
  */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        StateSystemPushPopTest.class,
-        org.eclipse.linuxtools.statesystem.core.tests.backend.AllTests.class
+        StateValueCompareToTest.class
 })
 public class AllTests {
 
--- lttng/org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/AllTmfCoreTests.java
+++ lttng/org.eclipse.linuxtools.tmf.core.tests/src/org/eclipse/linuxtools/tmf/core/tests/AllTmfCoreTests.java
@@ -28,7 +28,6 @@ import org.junit.runners.Suite;
     org.eclipse.linuxtools.tmf.core.tests.event.lookup.AllTests.class,
     org.eclipse.linuxtools.tmf.core.tests.request.AllTests.class,
     org.eclipse.linuxtools.tmf.core.tests.signal.AllTests.class,
-    org.eclipse.linuxtools.tmf.core.tests.statesystem.AllTests.class,
     org.eclipse.linuxtools.tmf.core.tests.statesystem.mipmap.AllTests.class,
     org.eclipse.linuxtools.tmf.core.tests.synchronization.AllTests.class,
     org.eclipse.linuxtools.tmf.core.tests.trace.AllTests.class,
