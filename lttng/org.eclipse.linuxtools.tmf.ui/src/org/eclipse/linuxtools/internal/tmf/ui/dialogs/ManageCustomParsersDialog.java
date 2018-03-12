ebdb39c42913e668570691b7a78bfe69d76eeb1c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:11:22 +0000

    First commited as after_32280_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/dialogs/ManageCustomParsersDialog.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/internal/tmf/ui/dialogs/ManageCustomParsersDialog.java
@@ -24,7 +24,6 @@ import org.eclipse.linuxtools.internal.tmf.ui.parsers.wizards.CustomXmlParserWiz
 import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomTraceDefinition;
 import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomTxtTraceDefinition;
 import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomXmlTraceDefinition;
-import org.eclipse.linuxtools.tmf.core.project.model.TmfTraceType;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
@@ -175,12 +174,15 @@ public class ManageCustomParsersDialog extends Dialog {
             @Override
             public void widgetSelected(SelectionEvent e) {
                 WizardDialog dialog = null;
+                String selection = parserList.getSelection()[0];
+                String category = selection.substring(0, selection.indexOf(':'));
+                String name = selection.substring(selection.indexOf(':') + 1);
                 if (txtButton.getSelection()) {
                     dialog = new WizardDialog(getShell(),
-                            new CustomTxtParserWizard(CustomTxtTraceDefinition.load(TmfTraceType.CUSTOM_TXT_CATEGORY, parserList.getSelection()[0]))); //TODO
+                            new CustomTxtParserWizard(CustomTxtTraceDefinition.load(category, name)));
                 } else if (xmlButton.getSelection()) {
                     dialog = new WizardDialog(getShell(),
-                            new CustomXmlParserWizard(CustomXmlTraceDefinition.load(TmfTraceType.CUSTOM_XML_CATEGORY, parserList.getSelection()[0]))); //TODO
+                            new CustomXmlParserWizard(CustomXmlTraceDefinition.load(category, name)));
                 }
                 if (dialog != null) {
                     dialog.open();
@@ -206,10 +208,13 @@ public class ManageCustomParsersDialog extends Dialog {
                         Messages.ManageCustomParsersDialog_DeleteParserDialogHeader,
                         Messages.ManageCustomParsersDialog_DeleteConfirmation + parserList.getSelection()[0] + "?"); //$NON-NLS-1$
                 if (confirm) {
+                    String selection = parserList.getSelection()[0];
+                    String category = selection.substring(0, selection.indexOf(':'));
+                    String name = selection.substring(selection.indexOf(':') + 1);
                     if (txtButton.getSelection()) {
-                        CustomTxtTraceDefinition.delete(TmfTraceType.CUSTOM_TXT_CATEGORY, parserList.getSelection()[0]); //TODO
+                        CustomTxtTraceDefinition.delete(category, name);
                     } else if (xmlButton.getSelection()) {
-                        CustomXmlTraceDefinition.delete(TmfTraceType.CUSTOM_XML_CATEGORY, parserList.getSelection()[0]); //TODO
+                        CustomXmlTraceDefinition.delete(category, name);
                     }
                     fillParserList();
                 }
@@ -263,11 +268,14 @@ public class ManageCustomParsersDialog extends Dialog {
                 dialog.setFilterExtensions(new String[] { "*.xml", "*" }); //$NON-NLS-1$ //$NON-NLS-2$
                 String path = dialog.open();
                 if (path != null) {
+                    String selection = parserList.getSelection()[0];
+                    String category = selection.substring(0, selection.indexOf(':'));
+                    String name = selection.substring(selection.indexOf(':') + 1);
                     CustomTraceDefinition def = null;
                     if (txtButton.getSelection()) {
-                        def = CustomTxtTraceDefinition.load(TmfTraceType.CUSTOM_TXT_CATEGORY, parserList.getSelection()[0]); //TODO
+                        def = CustomTxtTraceDefinition.load(category, name);
                     } else if (xmlButton.getSelection()) {
-                        def = CustomXmlTraceDefinition.load(TmfTraceType.CUSTOM_XML_CATEGORY, parserList.getSelection()[0]); //TODO
+                        def = CustomXmlTraceDefinition.load(category, name);
                     }
                     if (def != null) {
                         def.save(path);
@@ -291,11 +299,11 @@ public class ManageCustomParsersDialog extends Dialog {
         parserList.removeAll();
         if (txtButton.getSelection()) {
             for (CustomTxtTraceDefinition def : CustomTxtTraceDefinition.loadAll()) {
-                parserList.add(def.definitionName);
+                parserList.add(def.categoryName + ':' + def.definitionName);
             }
         } else if (xmlButton.getSelection()) {
             for (CustomXmlTraceDefinition def : CustomXmlTraceDefinition.loadAll()) {
-                parserList.add(def.definitionName);
+                parserList.add(def.categoryName + ':' + def.definitionName);
             }
         }
         editButton.setEnabled(false);
