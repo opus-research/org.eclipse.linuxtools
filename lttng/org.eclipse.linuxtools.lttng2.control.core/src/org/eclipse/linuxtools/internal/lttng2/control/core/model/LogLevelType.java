757d3c5eb93f0bd7764ce4f689f13a86fbd0fe5e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:41:39 +0000

    First commited as after_30975_rev4

--- lttng/org.eclipse.linuxtools.lttng2.control.core/src/org/eclipse/linuxtools/internal/lttng2/control/core/model/LogLevelType.java
+++ lttng/org.eclipse.linuxtools.lttng2.control.core/src/org/eclipse/linuxtools/internal/lttng2/control/core/model/LogLevelType.java
@@ -11,6 +11,8 @@
  *********************************************************************/
 package org.eclipse.linuxtools.internal.lttng2.control.core.model;
 
+import java.security.InvalidParameterException;
+
 /**
  * <p>
  * Type of log Level enumeration.
@@ -24,13 +26,16 @@ public enum LogLevelType {
     // Enum definition
     // ------------------------------------------------------------------------
     /** range of log levels [0,logLevel] */
-    LOGLEVEL("<="), //$NON-NLS-1$
+    LOGLEVEL("<=", "RANGE"), //$NON-NLS-1$ //$NON-NLS-2$
+
+    /** all log level */
+    LOGLEVEL_ALL(":", "ALL"),  //$NON-NLS-1$//$NON-NLS-2$
 
     /** single log level */
-    LOGLEVEL_ONLY("=="), //$NON-NLS-1$
+    LOGLEVEL_ONLY("==", "SINGLE"), //$NON-NLS-1$ //$NON-NLS-2$
 
     /** no log level */
-    LOGLEVEL_NONE(""); //$NON-NLS-1$
+    LOGLEVEL_NONE("", "UNKNOWN"); //$NON-NLS-1$ //$NON-NLS-2$
 
     // ------------------------------------------------------------------------
     // Constuctors
@@ -40,8 +45,9 @@ public enum LogLevelType {
      * Private constructor
      * @param name the name of state
      */
-    private LogLevelType(String shortName) {
+    private LogLevelType(String shortName, String miName) {
         fShortName = shortName;
+        fMiName = miName;
     }
 
      // ------------------------------------------------------------------------
@@ -51,6 +57,7 @@ public enum LogLevelType {
      * Name of enum.
      */
     private final String fShortName;
+    private final String fMiName;
 
     // ------------------------------------------------------------------------
     // Accessors
@@ -62,4 +69,33 @@ public enum LogLevelType {
         return fShortName;
     }
 
+    /**
+     * @return machine interface name string
+     */
+    public String getMiName() {
+        return fMiName;
+    }
+
+    // ------------------------------------------------------------------------
+    // Utility
+    // ------------------------------------------------------------------------
+    /**
+     * Return the corresponding {@link LogLevelType} to String "name"
+     * @param name String to compare to retrieve the good LogLevelType
+     * @return the corresponding {@link LogLevelType}
+     */
+    public static LogLevelType valueOfString(String name) {
+        if (name == null) {
+            throw new InvalidParameterException();
+        }
+        for (LogLevelType lltype : LogLevelType.values()) {
+            boolean isEqual = lltype.fShortName.equalsIgnoreCase(name) || lltype.fMiName.equalsIgnoreCase(name);
+            if (isEqual) {
+                return lltype;
+            }
+        }
+
+        // No match
+        return LogLevelType.LOGLEVEL_NONE;
+    }
 }
