a1ce82086e0f9e04790cca1be1b7ea0e0d3c4a94
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:47:58 +0000

    First commited as before_31134_rev3

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/internal/ctf/core/event/metadata/IOStructGen.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/internal/ctf/core/event/metadata/IOStructGen.java
@@ -32,6 +32,7 @@ import org.eclipse.linuxtools.ctf.core.event.types.Encoding;
 import org.eclipse.linuxtools.ctf.core.event.types.EnumDeclaration;
 import org.eclipse.linuxtools.ctf.core.event.types.FloatDeclaration;
 import org.eclipse.linuxtools.ctf.core.event.types.IDeclaration;
+import org.eclipse.linuxtools.ctf.core.event.types.IEventHeaderDeclaration;
 import org.eclipse.linuxtools.ctf.core.event.types.IntegerDeclaration;
 import org.eclipse.linuxtools.ctf.core.event.types.StringDeclaration;
 import org.eclipse.linuxtools.ctf.core.event.types.StructDeclaration;
@@ -45,6 +46,8 @@ import org.eclipse.linuxtools.internal.ctf.core.event.metadata.exceptions.ParseE
 import org.eclipse.linuxtools.internal.ctf.core.event.types.ArrayDeclaration;
 import org.eclipse.linuxtools.internal.ctf.core.event.types.SequenceDeclaration;
 import org.eclipse.linuxtools.internal.ctf.core.event.types.StructDeclarationFlattener;
+import org.eclipse.linuxtools.internal.ctf.core.event.types.composite.EventHeaderCompactDeclaration;
+import org.eclipse.linuxtools.internal.ctf.core.event.types.composite.EventHeaderLargeDeclaration;
 
 /**
  * IOStructGen
@@ -55,6 +58,17 @@ public class IOStructGen {
     // Attributes
     // ------------------------------------------------------------------------
 
+    private static final @NonNull String MAP = "map"; //$NON-NLS-1$
+    private static final @NonNull String ENCODING = "encoding"; //$NON-NLS-1$
+    private static final @NonNull String BASE = "base"; //$NON-NLS-1$
+    private static final @NonNull String SIZE = "size"; //$NON-NLS-1$
+    private static final @NonNull String SIGNED = "signed"; //$NON-NLS-1$
+    private static final @NonNull String LINE = "line"; //$NON-NLS-1$
+    private static final @NonNull String FILE = "file"; //$NON-NLS-1$
+    private static final @NonNull String IP = "ip"; //$NON-NLS-1$
+    private static final @NonNull String FUNC = "func"; //$NON-NLS-1$
+    private static final @NonNull String NAME = "name"; //$NON-NLS-1$
+    private static final @NonNull String EMPTY_STRING = ""; //$NON-NLS-1$
     private static final int INTEGER_BASE_16 = 16;
     private static final int INTEGER_BASE_10 = 10;
     private static final int INTEGER_BASE_8 = 8;
@@ -206,7 +220,7 @@ public class IOStructGen {
             parseCallsite(callsite);
         }
 
-        if (streams.size() > 0) {
+        if (!streams.isEmpty()) {
             for (CommonTree stream : streams) {
                 parseStream(stream);
             }
@@ -305,17 +319,17 @@ public class IOStructGen {
              * this is to replace the previous quotes with nothing...
              * effectively deleting them
              */
-            final String nullString = ""; //$NON-NLS-1$
+            final String nullString = EMPTY_STRING;
             left = child.getChild(0).getChild(0).getChild(0).getText();
-            if (left.equals("name")) { //$NON-NLS-1$
+            if (left.equals(NAME)) {
                 name = child.getChild(1).getChild(0).getChild(0).getText().replaceAll(regex, nullString);
-            } else if (left.equals("func")) { //$NON-NLS-1$
+            } else if (left.equals(FUNC)) {
                 funcName = child.getChild(1).getChild(0).getChild(0).getText().replaceAll(regex, nullString);
-            } else if (left.equals("ip")) { //$NON-NLS-1$
+            } else if (left.equals(IP)) {
                 ip = Long.decode(child.getChild(1).getChild(0).getChild(0).getText());
-            } else if (left.equals("file")) { //$NON-NLS-1$
+            } else if (left.equals(FILE)) {
                 fileName = child.getChild(1).getChild(0).getChild(0).getText().replaceAll(regex, nullString);
-            } else if (left.equals("line")) { //$NON-NLS-1$
+            } else if (left.equals(LINE)) {
                 lineNumber = Long.parseLong(child.getChild(1).getChild(0).getChild(0).getText());
             }
         }
@@ -640,11 +654,14 @@ public class IOStructGen {
             IDeclaration eventHeaderDecl = parseTypeSpecifierList(
                     typeSpecifier, null);
 
-            if (!(eventHeaderDecl instanceof StructDeclaration)) {
+            if (eventHeaderDecl instanceof StructDeclaration) {
+                stream.setEventHeader((StructDeclaration) eventHeaderDecl);
+            } else if (eventHeaderDecl instanceof IEventHeaderDeclaration) {
+                stream.setEventHeader((IEventHeaderDeclaration) eventHeaderDecl);
+            } else {
                 throw new ParseException("event.header expects a struct"); //$NON-NLS-1$
             }
 
-            stream.setEventHeader((StructDeclaration) eventHeaderDecl);
         } else if (left.equals(MetadataStrings.EVENT_CONTEXT)) {
             if (stream.isEventContextSet()) {
                 throw new ParseException("event.context already defined"); //$NON-NLS-1$
@@ -783,8 +800,13 @@ public class IOStructGen {
             }
 
             long id = getEventID(rightNode);
-
-            event.setId(id);
+            if (id > Integer.MAX_VALUE) {
+                throw new ParseException("id is greater than int.maxvalue, unsupported. id : " + id); //$NON-NLS-1$
+            }
+            if (id < 0) {
+                throw new ParseException("negative id, unsupported. id : " + id); //$NON-NLS-1$
+            }
+            event.setId((int) id);
         } else if (left.equals(MetadataStrings.STREAM_ID)) {
             if (event.streamIsSet()) {
                 throw new ParseException("stream id already defined"); //$NON-NLS-1$
@@ -1147,7 +1169,7 @@ public class IOStructGen {
          * sequence. For example, int a[3][len] means that we have an array of 3
          * (sequences of length 'len' of (int)).
          */
-        if (lengths.size() > 0) {
+        if (!lengths.isEmpty()) {
             /* We begin at the end */
             Collections.reverse(lengths);
 
@@ -1236,6 +1258,15 @@ public class IOStructGen {
             break;
         case CTFParser.STRUCT:
             declaration = parseStruct(firstChild);
+            StructDeclaration structDeclaration = (StructDeclaration) declaration;
+            IDeclaration idEnumDecl = structDeclaration.getFields().get("id"); //$NON-NLS-1$
+            if (EventHeaderCompactDeclaration.isCompactEventHeader(structDeclaration)) {
+                ByteOrder bo = ((EnumDeclaration) idEnumDecl).getContainerType().getByteOrder();
+                declaration = new EventHeaderCompactDeclaration(bo);
+            } else if (EventHeaderLargeDeclaration.isLargeEventHeader(structDeclaration)) {
+                ByteOrder bo = ((EnumDeclaration) idEnumDecl).getContainerType().getByteOrder();
+                declaration = new EventHeaderLargeDeclaration(bo);
+            }
             break;
         case CTFParser.VARIANT:
             declaration = parseVariant(firstChild);
@@ -1396,7 +1427,7 @@ public class IOStructGen {
         long alignment = 0;
         int base = DEFAULT_INT_BASE;
         @NonNull
-        String clock = ""; //$NON-NLS-1$
+        String clock = EMPTY_STRING;
 
         Encoding encoding = Encoding.NONE;
 
@@ -1418,19 +1449,19 @@ public class IOStructGen {
                 }
                 String left = concatenateUnaryStrings(leftStrings);
 
-                if (left.equals("signed")) { //$NON-NLS-1$
+                if (left.equals(SIGNED)) {
                     signed = getSigned(rightNode);
                 } else if (left.equals(MetadataStrings.BYTE_ORDER)) {
                     byteOrder = getByteOrder(rightNode);
-                } else if (left.equals("size")) { //$NON-NLS-1$
+                } else if (left.equals(SIZE)) {
                     size = getSize(rightNode);
                 } else if (left.equals(MetadataStrings.ALIGN)) {
                     alignment = getAlignment(rightNode);
-                } else if (left.equals("base")) { //$NON-NLS-1$
+                } else if (left.equals(BASE)) {
                     base = getBase(rightNode);
-                } else if (left.equals("encoding")) { //$NON-NLS-1$
+                } else if (left.equals(ENCODING)) {
                     encoding = getEncoding(rightNode);
-                } else if (left.equals("map")) { //$NON-NLS-1$
+                } else if (left.equals(MAP)) {
                     clock = getClock(rightNode);
                 } else {
                     Activator.log(IStatus.WARNING, Messages.IOStructGen_UnknownIntegerAttributeWarning + " " + left); //$NON-NLS-1$
@@ -1460,7 +1491,7 @@ public class IOStructGen {
     @NonNull
     private static String getClock(CommonTree rightNode) {
         String clock = rightNode.getChild(1).getChild(0).getChild(0).getText();
-        return clock == null ? "" : clock; //$NON-NLS-1$
+        return clock == null ? EMPTY_STRING : clock;
     }
 
     private static StringDeclaration parseString(CommonTree string)
@@ -1491,7 +1522,7 @@ public class IOStructGen {
                     }
                     String left = concatenateUnaryStrings(leftStrings);
 
-                    if (left.equals("encoding")) { //$NON-NLS-1$
+                    if (left.equals(ENCODING)) {
                         encoding = getEncoding(rightNode);
                     } else {
                         throw new ParseException("String: unknown attribute " //$NON-NLS-1$
@@ -2693,7 +2724,9 @@ public class IOStructGen {
             }
 
             long intval = parseUnaryInteger(firstChild);
-
+            if (intval > Integer.MAX_VALUE) {
+                throw new ParseException("Event id larger than int.maxvalue, something is amiss"); //$NON-NLS-1$
+            }
             return intval;
         }
         throw new ParseException("invalid value for event id"); //$NON-NLS-1$
