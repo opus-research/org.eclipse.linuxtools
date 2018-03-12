1b344a5f78a87c14b6c067b2e29e32a55942b4e5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:10:16 +0000

    First commited as after_59609_rev5

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerContentProvider.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerContentProvider.java
@@ -71,6 +71,8 @@ public class DockerExplorerContentProvider implements ITreeContentProvider {
 			final DockerImagesCategory imagesCategory = (DockerImagesCategory) parentElement;
 			final IDockerConnection connection = imagesCategory.getConnection();
 			if(connection.isImagesLoaded()) {
+				//System.err.println("Images in DockerImagesCategory: "
+				// + connection.getImages());
 				return connection.getImages().toArray();
 			}
 			loadImages(imagesCategory);
@@ -89,6 +91,11 @@ public class DockerExplorerContentProvider implements ITreeContentProvider {
 			protected IStatus run(final IProgressMonitor monitor) {
 				containersCategory.getConnection().getContainers(true);
 				return Status.OK_STATUS;
+			}
+
+			@Override
+			public boolean belongsTo(Object family) {
+				return family == DockerExplorerView.class;
 			}
 		};
 		loadContainersJob.addJobChangeListener(new JobChangeAdapter() {
@@ -117,6 +124,12 @@ public class DockerExplorerContentProvider implements ITreeContentProvider {
 				imagesCategory.getConnection().getImages(true);
 				return Status.OK_STATUS;
 			}
+
+			@Override
+			public boolean belongsTo(Object family) {
+				return family == DockerExplorerView.class;
+			}
+
 		};
 		loadImagesJob.addJobChangeListener(new JobChangeAdapter() {
 			@Override
