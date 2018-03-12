97e467970725655705665483c2ccd2720c65fa2b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:51:03 +0000

    First commited as before_73588_rev3

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/commands/BuildImageCommandHandler.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/commands/BuildImageCommandHandler.java
@@ -28,17 +28,19 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
 import org.eclipse.linuxtools.docker.core.DockerException;
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
-import org.eclipse.linuxtools.internal.docker.core.DockerConnection;
-import org.eclipse.linuxtools.internal.docker.ui.DockerConnectionWatcher;
+import org.eclipse.linuxtools.internal.docker.ui.jobs.BuildDockerImageJob;
 import org.eclipse.linuxtools.internal.docker.ui.views.DVMessages;
-import org.eclipse.linuxtools.internal.docker.ui.views.ImageBuildProgressHandler;
 import org.eclipse.linuxtools.internal.docker.ui.wizards.ImageBuild;
 import org.eclipse.linuxtools.internal.docker.ui.wizards.WizardMessages;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.IWorkbenchPart;
+import org.eclipse.ui.IWorkbenchWindow;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.handlers.HandlerUtil;
 
@@ -55,7 +57,23 @@ public class BuildImageCommandHandler extends AbstractHandler {
 		final ImageBuild wizard = new ImageBuild();
 		final WizardDialog wizardDialog = new NonModalWizardDialog(HandlerUtil.getActiveShell(event), wizard);
 		wizardDialog.create();
-		connection = DockerConnectionWatcher.getInstance().getConnection();
+		IWorkbenchWindow window = PlatformUI.getWorkbench()
+				.getActiveWorkbenchWindow();
+		IWorkbenchPage activePage = null;
+		IWorkbenchPart activePart = null;
+		if (window != null)
+			activePage = window.getActivePage();
+		if (activePage != null)
+			activePart = activePage.getActivePart();
+		connection = CommandUtils.getCurrentConnection(activePart);
+		// if no current connection, try the first connection in the list of
+		// connections
+		if (connection == null) {
+			IDockerConnection[] connections = DockerConnectionManager
+					.getInstance().getConnections();
+			if (connections.length > 0)
+				connection = connections[0];
+		}
 		if (connection == null || !connection.isActive()) {
 			// if no active connection, issue error message dialog and return
 			Display.getDefault().syncExec(() -> MessageDialog.openError(
@@ -79,6 +97,7 @@ public class BuildImageCommandHandler extends AbstractHandler {
 			@Override
 			protected IStatus run(final IProgressMonitor monitor) {
 				final String id = wizard.getImageName();
+				@SuppressWarnings("unused")
 				final int lines = wizard.getNumberOfLines();
 				final IPath path = wizard.getDirectory();
 
@@ -134,10 +153,9 @@ public class BuildImageCommandHandler extends AbstractHandler {
 				try {
 					monitor.subTask(
 							DVMessages.getString(BUILD_IMAGE_JOB_TITLE));
-					((DockerConnection) connection)
-							.buildImage(path, id,
-									new ImageBuildProgressHandler(connection,
-											id, lines));
+					final Job buildImageJob = new BuildDockerImageJob(
+							connection, path, id, null);
+					buildImageJob.schedule();
 					monitor.worked(1);
 				} catch (final DockerException e) {
 					Display.getDefault().syncExec(() -> MessageDialog.openError(
@@ -151,8 +169,6 @@ public class BuildImageCommandHandler extends AbstractHandler {
 									id),
 							e.getMessage()));
 					// for now
-				} catch (InterruptedException e) {
-					// do nothing
 				} finally {
 					monitor.done();
 				}
