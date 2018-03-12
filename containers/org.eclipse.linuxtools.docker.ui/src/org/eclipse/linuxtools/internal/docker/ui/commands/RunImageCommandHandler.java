58d0c0e34543728e37ab4ea6e698c11bcacaef77
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:13:42 +0000

    First commited as before_61191_rev9

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/commands/RunImageCommandHandler.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/commands/RunImageCommandHandler.java
@@ -143,8 +143,10 @@ public class RunImageCommandHandler extends AbstractHandler {
 					}
 					startContainerMonitor.done();
 					// create a launch configuration from the container
-					LaunchConfigurationUtils.createLaunchConfiguration(image,
-							containerConfig, hostConfig, container.name(),
+					LaunchConfigurationUtils.createLaunchConfiguration(
+image,
+							containerConfig,
+							hostConfig, containerName,
 							removeWhenExits);
 				} catch (final DockerException | InterruptedException e) {
 					Display.getDefault().syncExec(new Runnable() {
