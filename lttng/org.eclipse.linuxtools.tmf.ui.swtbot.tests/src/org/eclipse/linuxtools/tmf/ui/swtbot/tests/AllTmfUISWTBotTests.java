a1845e0c6e2a761b1b7c0ea42a0544ba755fec15
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:27:26 +0000

    First commited as before_30258_rev8

--- lttng/org.eclipse.linuxtools.tmf.ui.swtbot.tests/src/org/eclipse/linuxtools/tmf/ui/swtbot/tests/AllTmfUISWTBotTests.java
+++ lttng/org.eclipse.linuxtools.tmf.ui.swtbot.tests/src/org/eclipse/linuxtools/tmf/ui/swtbot/tests/AllTmfUISWTBotTests.java
@@ -23,7 +23,6 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
         TestCustomTxtWizard.class,
-        TestCustomXmlWizard.class,
         TracingPerspectiveChecker.class
 })
 public class AllTmfUISWTBotTests {
