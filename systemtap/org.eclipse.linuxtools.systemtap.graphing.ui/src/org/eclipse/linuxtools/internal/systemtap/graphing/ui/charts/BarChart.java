edc8ad29b99081f6010f5fc5a9a64746921dc550
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:18:18 +0000

    First commited as before_86529_rev1

--- systemtap/org.eclipse.linuxtools.systemtap.graphing.ui/src/org/eclipse/linuxtools/internal/systemtap/graphing/ui/charts/BarChart.java
+++ systemtap/org.eclipse.linuxtools.systemtap.graphing.ui/src/org/eclipse/linuxtools/internal/systemtap/graphing/ui/charts/BarChart.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2013, 2016 Red Hat Inc.
+ * Copyright (c) 2013 Red Hat Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -29,8 +29,7 @@ public class BarChart extends Chart {
     private IAxis xAxis = null;
 
     private boolean updateSuspended = false;
-    @Override
-	public void suspendUpdate(boolean suspend) {
+    public void suspendUpdate(boolean suspend) {
         if (updateSuspended == suspend) {
             return;
         }
