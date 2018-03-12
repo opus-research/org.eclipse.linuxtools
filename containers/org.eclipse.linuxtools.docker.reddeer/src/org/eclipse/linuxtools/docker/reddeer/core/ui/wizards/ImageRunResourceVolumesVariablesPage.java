9f9b53aa67a05325fe3d7ba0e2616be40f010c81
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:38:36 +0000

    First commited as before_102263_rev1

--- containers/org.eclipse.linuxtools.docker.reddeer/src/org/eclipse/linuxtools/docker/reddeer/core/ui/wizards/ImageRunResourceVolumesVariablesPage.java
+++ containers/org.eclipse.linuxtools.docker.reddeer/src/org/eclipse/linuxtools/docker/reddeer/core/ui/wizards/ImageRunResourceVolumesVariablesPage.java
@@ -18,7 +18,6 @@ import org.jboss.reddeer.core.matcher.WithTextMatcher;
 import org.jboss.reddeer.jface.wizard.WizardPage;
 import org.jboss.reddeer.swt.impl.button.CheckBox;
 import org.jboss.reddeer.swt.impl.button.FinishButton;
-import org.jboss.reddeer.swt.impl.button.NextButton;
 import org.jboss.reddeer.swt.impl.button.OkButton;
 import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.button.RadioButton;
@@ -40,10 +39,6 @@ public class ImageRunResourceVolumesVariablesPage extends WizardPage {
 	public void finish() {
 		new FinishButton().click();
 		new WaitWhile(new JobIsRunning(), TimePeriod.VERY_LONG);
-	}
-	
-	public void next() {
-		new NextButton().click();
 	}
 
 	public void addDataVolumeNoExternalMount(String containerPath) {
