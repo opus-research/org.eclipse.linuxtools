087cc2b4a525b188ce21b6dcb7a0de90800bc362
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:46:36 +0000

    First commited as before_71733_rev6

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerConnection.java
@@ -19,8 +19,6 @@ import java.util.List;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.linuxtools.internal.docker.core.DockerContainerRefreshManager;
 
-import com.spotify.docker.client.DockerCertificateException;
-
 public interface IDockerConnection {
 
 	public void addContainerListener(IDockerContainerListener listener);
@@ -194,19 +192,10 @@ public interface IDockerConnection {
 	void pullImage(String id, DockerAuthConfig authConfig,
 			IDockerProgressHandler handler)
 			throws DockerException, InterruptedException;
-	/**
-	 * @since 2.0
-	 */
-	void pullImage(String id, IRegistryAccount info, IDockerProgressHandler handler) throws DockerException, InterruptedException, DockerCertificateException;
 
 	public List<IDockerImageSearchResult> searchImages(final String term) throws DockerException;
-
+	
 	void pushImage(String name, IDockerProgressHandler handler) throws DockerException, InterruptedException;
-
-	/**
-	 * @since 2.0
-	 */
-	void pushImage(String name, IRegistryAccount info, IDockerProgressHandler handler) throws DockerException, InterruptedException, DockerCertificateException;
 
 	/**
 	 * Adds a tag to an existing image
