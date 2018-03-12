d6c8f22582efb0f7e0c7a90d68d2f29dc66265ac
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:29:35 +0000

    First commited as before_33104_rev2

--- lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/trace/CTFStreamTest.java
+++ lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/trace/CTFStreamTest.java
@@ -18,10 +18,9 @@ import static org.junit.Assume.assumeTrue;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Map;
 import java.util.Set;
 
-import org.eclipse.linuxtools.ctf.core.event.IEventDeclaration;
+import org.eclipse.jdt.annotation.NonNull;
 import org.eclipse.linuxtools.ctf.core.event.types.IDeclaration;
 import org.eclipse.linuxtools.ctf.core.event.types.StructDeclaration;
 import org.eclipse.linuxtools.ctf.core.tests.shared.CtfTestTrace;
@@ -33,6 +32,7 @@ import org.eclipse.linuxtools.internal.ctf.core.event.EventDeclaration;
 import org.eclipse.linuxtools.internal.ctf.core.event.metadata.exceptions.ParseException;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
@@ -51,6 +51,11 @@ public class CTFStreamTest {
 
     private CTFStreamInput fInput;
 
+    @BeforeClass
+    public static void initialize() {
+        assumeTrue(testTrace.exists());
+    }
+
     /**
      * Perform pre-test initialization.
      *
@@ -58,7 +63,6 @@ public class CTFStreamTest {
      */
     @Before
     public void setUp() throws CTFReaderException {
-        assumeTrue(testTrace.exists());
         fixture = new CTFStream(testTrace.getTrace());
         fixture.setEventContext(new StructDeclaration(1L));
         fixture.setPacketContext(new StructDeclaration(1L));
@@ -69,13 +73,14 @@ public class CTFStreamTest {
     }
 
     @After
-    public void tearDown() throws IOException{
+    public void tearDown() throws IOException {
         fInput.close();
     }
 
+    @NonNull
     private static File createFile() {
         File path = new File(testTrace.getPath());
-        return path.listFiles(new FilenameFilter() {
+        final File[] listFiles = path.listFiles(new FilenameFilter() {
             @Override
             public boolean accept(File dir, String name) {
                 if (name.contains("hann")) {
@@ -83,7 +88,11 @@ public class CTFStreamTest {
                 }
                 return false;
             }
-        })[0];
+        });
+        assertNotNull(listFiles);
+        final File returnFile = listFiles[0];
+        assertNotNull(returnFile);
+        return returnFile;
     }
 
     /**
@@ -156,8 +165,7 @@ public class CTFStreamTest {
      */
     @Test
     public void testGetEvents() {
-        Map<Long, IEventDeclaration> result = fixture.getEvents();
-        assertNotNull(result);
+        assertNotNull(fixture.getEventDeclarations());
     }
 
     /**
--- lttng/org.eclipse.linuxtools.lttng2.control.ui.tests/src/org/eclipse/linuxtools/lttng2/control/ui/tests/service/LTTngControlServiceTest.java
+++ lttng/org.eclipse.linuxtools.lttng2.control.ui.tests/src/org/eclipse/linuxtools/lttng2/control/ui/tests/service/LTTngControlServiceTest.java
@@ -41,6 +41,7 @@ import org.eclipse.linuxtools.internal.lttng2.control.core.model.ISessionInfo;
 import org.eclipse.linuxtools.internal.lttng2.control.core.model.ISnapshotInfo;
 import org.eclipse.linuxtools.internal.lttng2.control.core.model.IUstProviderInfo;
 import org.eclipse.linuxtools.internal.lttng2.control.core.model.LogLevelType;
+import org.eclipse.linuxtools.internal.lttng2.control.core.model.TraceChannelOutputType;
 import org.eclipse.linuxtools.internal.lttng2.control.core.model.TraceEnablement;
 import org.eclipse.linuxtools.internal.lttng2.control.core.model.TraceEventType;
 import org.eclipse.linuxtools.internal.lttng2.control.core.model.TraceLogLevel;
@@ -78,7 +79,7 @@ public class LTTngControlServiceTest {
     private static final String SCEN_GET_SESSION_NAMES1 = "GetSessionNames1";
     private static final String SCEN_GET_SESSION_NAME_NOT_EXIST = "GetSessionNameNotExist";
     private static final String SCEN_GET_SESSION_NAME_NOT_EXIST_VERBOSE = "GetSessionNameNotExistVerbose";
-    private static final String SCEN_GET_SESSION_GARBAGE_OUT = "GetSessionGarbageOut";
+    protected static final String SCEN_GET_SESSION_GARBAGE_OUT = "GetSessionGarbageOut";
     private static final String SCEN_GET_SESSION1 = "GetSession1";
     private static final String SCEN_GET_KERNEL_PROVIDER1 = "GetKernelProvider1";
     private static final String SCEN_LIST_WITH_NO_KERNEL1 = "ListWithNoKernel1";
@@ -104,10 +105,12 @@ public class LTTngControlServiceTest {
     private static final String SCEN_CREATE_SESSION_2_1 = "CreateSessionLttng2.1";
     private static final String SCEN_CREATE_SESSION_VERBOSE_2_1 = "CreateSessionLttngVerbose2.1";
     private static final String SCEN_CREATE_SNAPSHOT_SESSION = "CreateSessionSnapshot";
+    private static final String SCEN_CREATE_SNAPSHOT_SESSION_2_5 = "CreateSessionSnapshot2.5";
     private static final String SCEN_CREATE_STREAMED_SNAPSHOT_SESSION = "CreateSessionStreamedSnapshot";
     private static final String SCEN_CREATE_SNAPSHOT_SESSION_ERRORS = "CreateSessionSnapshotErrors";
-    private static final String SCEN_CREATE_LIVE_SESSION = "CreateSessionLive";
+    protected static final String SCEN_CREATE_LIVE_SESSION = "CreateSessionLive";
     private static final String SCEN_CREATE_LIVE_SESSION_ERRORS = "CreateSessionLiveErrors";
+
 
     // ------------------------------------------------------------------------
     // Test data
@@ -115,8 +118,8 @@ public class LTTngControlServiceTest {
 
     private CommandShellFactory fShellFactory;
     private String fTestfile;
-    private LTTngToolsFileShell fShell;
-    private ILttngControlService fService;
+    protected LTTngToolsFileShell fShell;
+    protected ILttngControlService fService;
 
     // ------------------------------------------------------------------------
     // Housekeeping
@@ -132,15 +135,43 @@ public class LTTngControlServiceTest {
     public void setUp() throws Exception {
         fShellFactory = CommandShellFactory.getInstance();
 
-        URL location = FileLocator.find(FrameworkUtil.getBundle(this.getClass()), new Path(DIRECTORY + File.separator + TEST_STREAM), null);
+        URL location = FileLocator.find(FrameworkUtil.getBundle(this.getClass()), new Path(getTestDirectory() + File.separator + getTestStream()), null);
         File testfile = new File(FileLocator.toFileURL(location).toURI());
         fTestfile = testfile.getAbsolutePath();
 
         fShell = fShellFactory.getFileShell();
         fShell.loadScenarioFile(fTestfile);
-        fService = new LTTngControlService(fShell);
+        fService = getControlService();
+        if (fService == null) {
+            throw new Exception("Unable to obtain a valid ControlService");
+        }
 
         ControlPreferences.getInstance().init(Activator.getDefault().getPreferenceStore());
+    }
+
+    /**
+     * @return the string of the test directory to use
+     */
+    protected String getTestDirectory() {
+        return DIRECTORY;
+    }
+
+    /**
+     * @return the LttngCon
+     */
+    protected ILttngControlService getControlService() {
+        return new LTTngControlService(fShell);
+    }
+
+    public LTTngToolsFileShell getfShell() {
+        return fShell;
+    }
+
+    /**
+     * @return
+     */
+    protected String getTestStream() {
+        return TEST_STREAM;
     }
 
     @After
@@ -159,7 +190,7 @@ public class LTTngControlServiceTest {
             fShell.setScenario(SCEN_LTTNG_VERSION);
             ILttngControlService service = LTTngControlServiceFactory.getInstance().getLttngControlService(fShell);
             assertNotNull(service);
-            assertEquals("2.1.0", service.getVersion());
+            assertEquals("2.1.0", service.getVersionString());
         } catch (ExecutionException e) {
             fail("Exeption thrown " + e);
         }
@@ -171,7 +202,7 @@ public class LTTngControlServiceTest {
             fShell.setScenario(SCEN_LTTNG_VERSION_WITH_PROMPT);
             ILttngControlService service = LTTngControlServiceFactory.getInstance().getLttngControlService(fShell);
             assertNotNull(service);
-            assertEquals("2.0.0", service.getVersion());
+            assertEquals("2.0.0", service.getVersionString());
         } catch (ExecutionException e) {
             fail("Exeption thrown " + e);
         }
@@ -306,7 +337,8 @@ public class LTTngControlServiceTest {
             // Verify Kernel's channel0
             assertEquals("channel0", channels[0].getName());
             assertEquals(4, channels[0].getNumberOfSubBuffers());
-            assertEquals("splice()", channels[0].getOutputType());
+            assertEquals("splice()", channels[0].getOutputType().getInName());
+            assertEquals(TraceChannelOutputType.SPLICE, channels[0].getOutputType());
             assertEquals(false, channels[0].isOverwriteMode());
             assertEquals(200, channels[0].getReadTimer());
             assertEquals(TraceEnablement.ENABLED, channels[0].getState());
@@ -330,7 +362,8 @@ public class LTTngControlServiceTest {
             // Verify Kernel's channel1
             assertEquals("channel1", channels[1].getName());
             assertEquals(4, channels[1].getNumberOfSubBuffers());
-            assertEquals("splice()", channels[1].getOutputType());
+            assertEquals("splice()", channels[1].getOutputType().getInName());
+            assertEquals(TraceChannelOutputType.SPLICE, channels[1].getOutputType());
             assertEquals(true, channels[1].isOverwriteMode());
             assertEquals(400, channels[1].getReadTimer());
             assertEquals(TraceEnablement.DISABLED, channels[1].getState());
@@ -349,7 +382,8 @@ public class LTTngControlServiceTest {
             // Verify UST global's mychannel1
             assertEquals("mychannel1", ustChannels[0].getName());
             assertEquals(8, ustChannels[0].getNumberOfSubBuffers());
-            assertEquals("mmap()", ustChannels[0].getOutputType());
+            assertEquals("mmap()", ustChannels[0].getOutputType().getInName());
+            assertEquals(TraceChannelOutputType.MMAP, ustChannels[0].getOutputType());
             assertEquals(true, ustChannels[0].isOverwriteMode());
             assertEquals(100, ustChannels[0].getReadTimer());
             assertEquals(TraceEnablement.DISABLED, ustChannels[0].getState());
@@ -363,7 +397,8 @@ public class LTTngControlServiceTest {
             // Verify UST global's channel0
             assertEquals("channel0", ustChannels[1].getName());
             assertEquals(4, ustChannels[1].getNumberOfSubBuffers());
-            assertEquals("mmap()", ustChannels[1].getOutputType());
+            assertEquals("mmap()", ustChannels[1].getOutputType().getInName());
+            assertEquals(TraceChannelOutputType.MMAP, ustChannels[1].getOutputType());
             assertEquals(false, ustChannels[1].isOverwriteMode());
             assertEquals(200, ustChannels[1].getReadTimer());
             assertEquals(TraceEnablement.ENABLED, ustChannels[1].getState());
@@ -380,7 +415,7 @@ public class LTTngControlServiceTest {
             assertEquals(TraceEnablement.DISABLED, ustEvents[0].getState());
 
             assertEquals("*", ustEvents[1].getName());
-            assertEquals(TraceLogLevel.LEVEL_UNKNOWN, ustEvents[1].getLogLevel());
+            assertEquals(getAllEventTraceLogLevel(), ustEvents[1].getLogLevel());
             assertEquals(TraceEventType.TRACEPOINT, ustEvents[1].getEventType());
             assertEquals(TraceEnablement.ENABLED, ustEvents[1].getState());
 
@@ -397,6 +432,13 @@ public class LTTngControlServiceTest {
         } catch (ExecutionException e) {
             fail(e.toString());
         }
+    }
+
+    /**
+     * @return
+     */
+    protected TraceLogLevel getAllEventTraceLogLevel() {
+        return TraceLogLevel.LEVEL_UNKNOWN;
     }
 
     public void testGetKernelProvider() {
@@ -1270,6 +1312,44 @@ public class LTTngControlServiceTest {
     public void testCreateSnapshotSession() {
         try {
             fShell.setScenario(SCEN_CREATE_SNAPSHOT_SESSION);
+            ISessionInfo params = new SessionInfo("mysession");
+            params.setSnapshot(true);
+            ISessionInfo sessionInfo = fService.createSession(params, new NullProgressMonitor());
+            assertNotNull(sessionInfo);
+            assertEquals("mysession", sessionInfo.getName());
+            assertTrue(sessionInfo.isSnapshotSession());
+            assertEquals("", sessionInfo.getSessionPath());
+            assertTrue(!sessionInfo.isStreamedTrace());
+
+            assertEquals(TraceSessionState.INACTIVE, sessionInfo.getSessionState());
+
+            String[] names = fService.getSessionNames(new NullProgressMonitor());
+            assertEquals(names[0], "mysession");
+
+            ISnapshotInfo snapshotInfo = fService.getSnapshotInfo("mysession", new NullProgressMonitor());
+            assertNotNull(snapshotInfo);
+            assertEquals("snapshot-1", snapshotInfo.getName());
+            assertEquals("/home/user/lttng-traces/mysession-20130913-141651", snapshotInfo.getSnapshotPath());
+            assertEquals(1, snapshotInfo.getId());
+            assertTrue(!snapshotInfo.isStreamedSnapshot());
+
+            // we need to set the snapshotInfo to so that the session path is set correctly
+            sessionInfo.setSnapshotInfo(snapshotInfo);
+            assertEquals("/home/user/lttng-traces/mysession-20130913-141651", sessionInfo.getSessionPath());
+
+            fService.recordSnapshot("mysession", new NullProgressMonitor());
+
+            fService.destroySession("mysession", new NullProgressMonitor());
+
+        } catch (ExecutionException e) {
+            fail(e.toString());
+        }
+    }
+
+    @Test
+    public void testCreateSnapshotSession2_5() {
+        try {
+            fShell.setScenario(SCEN_CREATE_SNAPSHOT_SESSION_2_5);
             ISessionInfo params = new SessionInfo("mysession");
             params.setSnapshot(true);
             ISessionInfo sessionInfo = fService.createSession(params, new NullProgressMonitor());
