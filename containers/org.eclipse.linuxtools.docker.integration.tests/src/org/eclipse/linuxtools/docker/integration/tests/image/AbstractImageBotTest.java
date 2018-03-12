f06997cbf269af447750b8cbf86d8447bea97561
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:31:23 +0000

    First commited as after_97157_rev8

--- containers/org.eclipse.linuxtools.docker.integration.tests/src/org/eclipse/linuxtools/docker/integration/tests/image/AbstractImageBotTest.java
+++ containers/org.eclipse.linuxtools.docker.integration.tests/src/org/eclipse/linuxtools/docker/integration/tests/image/AbstractImageBotTest.java
@@ -35,6 +35,7 @@ import org.jboss.reddeer.eclipse.ui.views.properties.PropertiesView;
 import org.jboss.reddeer.jface.preference.PreferenceDialog;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
 import org.jboss.reddeer.swt.impl.button.OkButton;
+import org.jboss.reddeer.swt.impl.button.PushButton;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
 import org.junit.After;
@@ -128,7 +129,7 @@ public class AbstractImageBotTest extends AbstractDockerBotTest {
 		} catch (SWTLayerException e) {
 			new DefaultShell("Preferences").setFocus();
 		}
-		new OkButton().click();
+		new PushButton("Apply and Close").click();
 	}
 
 	protected void deleteRegister(String serverAddress) {
