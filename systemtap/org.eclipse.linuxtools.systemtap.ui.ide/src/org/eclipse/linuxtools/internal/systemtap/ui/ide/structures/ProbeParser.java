6696b83c942155eb638f6c7bf5e8ed773bfa915a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:48:23 +0000

    First commited as before_31139_rev3

--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/structures/ProbeParser.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/structures/ProbeParser.java
@@ -141,7 +141,7 @@ public final class ProbeParser extends TreeTapsetParser {
             TreeNode groupNode = null;
             while (st.hasNextLine()) {
                 if (monitor.isCanceled()) {
-                    canceled = true;
+                    canceled = false;
                     break;
                 }
                 String tokenString = st.nextLine();
