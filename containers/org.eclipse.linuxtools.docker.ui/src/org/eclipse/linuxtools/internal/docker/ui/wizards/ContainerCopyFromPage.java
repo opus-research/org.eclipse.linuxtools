6aff3522a1151bff8db45c9a8e619a63115a883e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:15:57 +0000

    First commited as before_85927_rev1

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerCopyFromPage.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerCopyFromPage.java
@@ -11,7 +11,6 @@
 package org.eclipse.linuxtools.internal.docker.ui.wizards;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.eclipse.jface.layout.GridDataFactory;
@@ -20,7 +19,6 @@ import org.eclipse.jface.viewers.ICheckStateListener;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.linuxtools.docker.ui.Activator;
-import org.eclipse.linuxtools.internal.docker.core.ContainerFileProxy;
 import org.eclipse.linuxtools.internal.docker.ui.CheckboxTreeAndListGroup;
 import org.eclipse.linuxtools.internal.docker.ui.MinimizedFileSystemElement;
 import org.eclipse.linuxtools.internal.docker.ui.SWTImagesFactory;
@@ -66,10 +64,7 @@ public class ContainerCopyFromPage extends WizardPage {
 	private final static String DESC = "ContainerCopyFrom.desc"; //$NON-NLS-1$
 	private static final String TARGET_LABEL = "ContainerCopyFrom.target.label"; //$NON-NLS-1$
 	private static final String TARGET_TOOLTIP = "ContainerCopyFrom.target.tooltip"; //$NON-NLS-1$
-	private static final String SOURCE_LABEL = "ContainerCopyFrom.source.label"; //$NON-NLS-1$
-	private static final String SOURCE_TOOLTIP = "ContainerCopyFrom.source.tooltip"; //$NON-NLS-1$
 	private static final String NO_TARGET_SPECIFIED = "ContainerCopyFrom.notarget.error"; //$NON-NLS-1$
-	private static final String NO_SOURCE_SPECIFIED = "ContainerCopyFrom.nosource.error"; //$NON-NLS-1$
 	private static final String BROWSE_LABEL = "ContainerCopyFrom.browse.label"; //$NON-NLS-1$
 
 	// the root file representative to populate the viewer with
@@ -77,13 +72,10 @@ public class ContainerCopyFromPage extends WizardPage {
 
 	private IImportStructureProvider structureProvider;
 
-	private boolean isRunning;
-
     // the visual selection widget group
     CheckboxTreeAndListGroup selectionGroup;
 
 	private Text targetText;
-	private Text sourceText;
 	private Button browseButton;
 
 	private String target;
@@ -106,8 +98,7 @@ public class ContainerCopyFromPage extends WizardPage {
 	 *            - name of container
 	 */
 	public ContainerCopyFromPage(FileSystemElement fileSystemElement,
-			IImportStructureProvider structureProvider, String containerName,
-			boolean isRunning) {
+			IImportStructureProvider structureProvider, String containerName) {
 		super(WizardMessages.getString(NAME));
 		setDescription(WizardMessages.getFormattedString(DESC,
 				containerName));
@@ -115,7 +106,6 @@ public class ContainerCopyFromPage extends WizardPage {
 		setImageDescriptor(SWTImagesFactory.DESC_WIZARD);
         root = fileSystemElement;
 		this.structureProvider = structureProvider;
-		this.isRunning = isRunning;
     }
 
 
@@ -129,22 +119,13 @@ public class ContainerCopyFromPage extends WizardPage {
 	/*
 	 * Get an iterator for the selected items to copy
 	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
+	@SuppressWarnings("rawtypes")
 	public Iterator getValueIterator() {
-		if (isRunning) {
-			return selectionGroup.getAllWhiteCheckedItems();
-		} else {
-			// We need to fake a collection of MinimizedFileSystemElement with
-			// one element.
-			ArrayList out = new ArrayList();
-			String sourceName = sourceText.getText();
-			MinimizedFileSystemElement element = new MinimizedFileSystemElement(
-					sourceName, null, false);
-			element.setFileSystemObject(
-					new ContainerFileProxy(sourceName, "", false)); //$NON-NLS-1$
-			out.add(element);
-			return out.iterator();
-		}
+		// TODO: look at also returning all checked table items and
+		// remove files from that directory to shorten the
+		// time taken to copy (i.e. copy the whole directory
+		// at once).
+		return selectionGroup.getAllCheckedListItems();
 	}
 
 	private ModifyListener Listener = e -> validate();
@@ -165,21 +146,8 @@ public class ContainerCopyFromPage extends WizardPage {
 			}
 		}
 
-		if (!error) {
-			if (isRunning) {
-				if (selectionGroup.getCheckedElementCount() == 0) {
-					error = true;
-					setErrorMessage(
-							WizardMessages.getString(NO_SOURCE_SPECIFIED));
-				}
-			} else {
-				if (sourceText.getText().length() == 0) {
-					error = true;
-					setErrorMessage(
-							WizardMessages.getString(NO_SOURCE_SPECIFIED));
-				}
-			}
-		}
+		if (selectionGroup.getCheckedElementCount() == 0)
+			complete = false;
 
 		if (!error) {
 			target = targetText.getText();
@@ -224,55 +192,31 @@ public class ContainerCopyFromPage extends WizardPage {
         input.addChild(root);
         root.setParent(input);
 
-		Composite selectionComposite = null;
+		Composite selectionComposite = new Composite(composite, SWT.NULL);
+		GridLayout selectionLayout = new GridLayout();
+		selectionComposite.setLayout(selectionLayout);
 
-		if (isRunning) {
-			selectionComposite = new Composite(composite, SWT.NULL);
-			GridLayout selectionLayout = new GridLayout();
-			selectionComposite.setLayout(selectionLayout);
+		selectionGroup = new CheckboxTreeAndListGroup(selectionComposite, input,
+				getFolderProvider(), getDynamicFolderProvider(),
+				new WorkbenchLabelProvider(),
+                getFileProvider(), new WorkbenchLabelProvider(), SWT.NONE,
+                SIZING_SELECTION_WIDGET_WIDTH, // since this page has no other significantly-sized
+                SIZING_SELECTION_WIDGET_HEIGHT); // widgets we need to hardcode the combined widget's
+        // size, otherwise it will open too small
 
-			selectionGroup = new CheckboxTreeAndListGroup(selectionComposite,
-					input, getFolderProvider(), getDynamicFolderProvider(),
-					new WorkbenchLabelProvider(), getFileProvider(),
-					new WorkbenchLabelProvider(), SWT.NONE,
-					SIZING_SELECTION_WIDGET_WIDTH, // since this page has no
-													// other significantly-sized
-					SIZING_SELECTION_WIDGET_HEIGHT); // widgets we need to
-														// hardcode the combined
-														// widget's
-			// size, otherwise it will open too small
-			GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).span(3, 1)
-					.grab(true, false).applyTo(selectionComposite);
-		} else {
-			Label sourceLabel = new Label(composite, SWT.NONE);
-			sourceLabel.setText(WizardMessages.getString(SOURCE_LABEL));
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).span(3, 1)
+				.grab(true, false).applyTo(selectionComposite);
 
-			GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER)
-					.span(1, 1).grab(false, false).applyTo(sourceLabel);
-
-			sourceText = new Text(composite, SWT.BORDER | SWT.SINGLE);
-			sourceText.addModifyListener(Listener);
-			sourceText.setToolTipText(WizardMessages.getString(SOURCE_TOOLTIP));
-
-			GridDataFactory.fillDefaults().align(SWT.FILL, SWT.FILL).span(1, 1)
-					.grab(true, false).applyTo(sourceText);
-
-		}
-
-		if (isRunning) {
-			WorkbenchViewerComparator comparator = new WorkbenchViewerComparator();
-			selectionGroup.setTreeComparator(comparator);
-			selectionGroup.setListComparator(comparator);
-			selectionGroup.addCheckStateListener(CheckListener);
-		}
+        WorkbenchViewerComparator comparator = new WorkbenchViewerComparator();
+        selectionGroup.setTreeComparator(comparator);
+        selectionGroup.setListComparator(comparator);
+		selectionGroup.addCheckStateListener(CheckListener);
 
 		setControl(composite);
 		validate();
 		setPageComplete(false);
 
-		if (isRunning) {
-			selectionGroup.aboutToOpen();
-		}
+		selectionGroup.aboutToOpen();
     }
 
 	private SelectionListener onBrowseSelect() {
