a9ffd9c318876519af6356ad89e8dfa3cbe4153e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:07:04 +0000

    First commited as before_58565_rev1

--- rpm/org.eclipse.linuxtools.rpm.createrepo.tests/src/org/eclipse/linuxtools/internal/rpm/createrepo/form/tests/ICreaterepoTestConstants.java
+++ rpm/org.eclipse.linuxtools.rpm.createrepo.tests/src/org/eclipse/linuxtools/internal/rpm/createrepo/form/tests/ICreaterepoTestConstants.java
@@ -57,7 +57,6 @@ public interface ICreaterepoTestConstants {
      */
     String PROJECT_NAME_LABEL = "Project name:"; //$NON-NLS-1$
     String CREATEREPO_CATEGORY = "Createrepo"; //$NON-NLS-1$
-    String CREATEREPO_PROJECT_CATEGORY = "RPM"; //$NON-NLS-1$
     String CREATEREPO_PROJECT_WIZARD = "Createrepo Wizard"; //$NON-NLS-1$
 
     /*
