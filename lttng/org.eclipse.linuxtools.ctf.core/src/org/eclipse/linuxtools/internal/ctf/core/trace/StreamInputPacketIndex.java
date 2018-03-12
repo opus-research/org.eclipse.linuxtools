907334ceabfee031c9ff7fdb478242b57ce718a8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:41:26 +0000

    First commited as before_30939_rev4

--- lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/internal/ctf/core/trace/StreamInputPacketIndex.java
+++ lttng/org.eclipse.linuxtools.ctf.core/src/org/eclipse/linuxtools/internal/ctf/core/trace/StreamInputPacketIndex.java
@@ -14,12 +14,8 @@
 
 package org.eclipse.linuxtools.internal.ctf.core.trace;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Iterator;
-import java.util.List;
 import java.util.ListIterator;
+import java.util.Vector;
 
 import org.eclipse.linuxtools.ctf.core.trace.CTFReaderException;
 
@@ -28,7 +24,7 @@ import org.eclipse.linuxtools.ctf.core.trace.CTFReaderException;
  * <p>
  * TODO Implement me. Please.
  */
-public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry> {
+public class StreamInputPacketIndex {
 
     // ------------------------------------------------------------------------
     // Attributes
@@ -38,16 +34,28 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
      * Entries of the index. They are sorted by increasing begin timestamp.
      * index builder.
      */
-    private final List<StreamInputPacketIndexEntry> fEntries = new ArrayList<>();
-    private transient StreamInputPacketIndexEntry fLastElement;
+    private final Vector<StreamInputPacketIndexEntry> entries = new Vector<>();
 
     // ------------------------------------------------------------------------
     // Getters/Setters/Predicates
     // ------------------------------------------------------------------------
 
-    @Override
+    /**
+     * Gets the entries
+     *
+     * @return the entries
+     */
+    public Vector<StreamInputPacketIndexEntry> getEntries() {
+        return this.entries;
+    }
+
+    /**
+     * Gets an iterator to the entries
+     *
+     * @return an iterator to the entries
+     */
     public ListIterator<StreamInputPacketIndexEntry> listIterator() {
-        return fEntries.listIterator();
+        return this.entries.listIterator();
     }
 
     /**
@@ -57,9 +65,8 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
      *            the position to get
      * @return the iterator
      */
-    @Override
     public ListIterator<StreamInputPacketIndexEntry> listIterator(int n) {
-        return fEntries.listIterator(n);
+        return this.entries.listIterator(n);
     }
 
     // ------------------------------------------------------------------------
@@ -67,59 +74,35 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
     // ------------------------------------------------------------------------
 
     /**
-     * Appends the specified element to the end of this list (optional
-     * operation).
-     *
-     * <p>
-     * Lists that support this operation may place limitations on what elements
-     * may be added to this list. In particular, some lists will refuse to add
-     * null elements, and others will impose restrictions on the type of
-     * elements that may be added. List classes should clearly specify in their
-     * documentation any restrictions on what elements may be added.
+     * Adds an entry to the index.
      *
      * @param entry
-     *            element to be appended to this list
-     * @return <tt>true</tt> (as specified by {@link Collection#add})
-     * @throws UnsupportedOperationException
-     *             if the <tt>add</tt> operation is not supported by this list
-     * @throws ClassCastException
-     *             if the class of the specified element prevents it from being
-     *             added to this list
-     * @throws NullPointerException
-     *             if the specified element is null and this list does not
-     *             permit null elements
-     * @throws IllegalArgumentException
-     *             if some property of this element prevents it from being added
-     *             to this list
+     *            The entry to add
      * @throws CTFReaderException
      *             If there was a problem reading the entry
      */
-    public boolean addEntry(StreamInputPacketIndexEntry entry)
+    public void addEntry(StreamInputPacketIndexEntry entry)
             throws CTFReaderException {
+        assert (entry.getContentSizeBits() != 0);
 
         /* Validate consistent entry. */
         if (entry.getTimestampBegin() > entry.getTimestampEnd()) {
             throw new CTFReaderException("Packet begin timestamp is after end timestamp"); //$NON-NLS-1$
         }
 
-        /*
-         * Validate entries are inserted in monotonic increasing timestamp
-         * order.
-         */
-        if (!fEntries.isEmpty()) {
-            if (entry.getTimestampBegin() < fLastElement.getTimestampBegin()) {
+        /* Validate entries are inserted in monotonic increasing timestamp order. */
+        if (!this.entries.isEmpty()) {
+            if (entry.getTimestampBegin() < this.entries.lastElement()
+                    .getTimestampBegin()) {
                 throw new CTFReaderException("Packets begin timestamp decreasing"); //$NON-NLS-1$
             }
         }
-        fEntries.add(entry);
-        fLastElement = fEntries.get(fEntries.size() - 1);
-        return true;
+        this.entries.add(entry);
     }
 
     /**
-     * Returns the first PacketIndexEntry that could include the timestamp, that
-     * is the last packet with a begin timestamp smaller than the given
-     * timestamp.
+     * Returns the first PacketIndexEntry that could include the timestamp,
+     * that is the last packet with a begin timestamp smaller than the given timestamp.
      *
      * @param timestamp
      *            The timestamp to look for.
@@ -130,7 +113,7 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
         /*
          * Start with min and max covering all the elements.
          */
-        int max = fEntries.size() - 1;
+        int max = this.entries.size() - 1;
         int min = 0;
 
         int guessI;
@@ -139,8 +122,8 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
         /*
          * If the index is empty, return the iterator at the very beginning.
          */
-        if (isEmpty()) {
-            return fEntries.listIterator();
+        if (this.getEntries().isEmpty()) {
+            return this.getEntries().listIterator();
         }
 
         if (timestamp < 0) {
@@ -153,7 +136,7 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
              * Guess in the middle of min and max.
              */
             guessI = min + ((max - min) / 2);
-            guessEntry = fEntries.get(guessI);
+            guessEntry = this.entries.get(guessI);
 
             /*
              * If we reached the point where we focus on a single packet, our
@@ -165,147 +148,19 @@ public class StreamInputPacketIndex implements List<StreamInputPacketIndexEntry>
 
             if (timestamp <= guessEntry.getTimestampEnd()) {
                 /*
-                 * If the timestamp is lower or equal to the end of the guess
-                 * packet, then the guess packet becomes the new inclusive max.
+                 * If the timestamp is lower or equal to the end of the guess packet,
+                 * then the guess packet becomes the new inclusive max.
                  */
                 max = guessI;
             } else {
                 /*
-                 * If the timestamp is greater than the end of the guess packet,
-                 * then the new inclusive min is the packet after the guess
-                 * packet.
+                 * If the timestamp is greater than the end of the guess packet, then
+                 * the new inclusive min is the packet after the guess packet.
                  */
                 min = guessI + 1;
             }
         }
 
-        return fEntries.listIterator(guessI);
-    }
-
-    /**
-     * Get the last element of the index
-     *
-     * @return the last element in the index
-     */
-    public StreamInputPacketIndexEntry lastElement() {
-        return fLastElement;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return fEntries.isEmpty();
-    }
-
-    @Override
-    public StreamInputPacketIndexEntry get(int index) {
-        return fEntries.get(index);
-    }
-
-    /**
-     * This will work in log(n) time since the data structure contains elements
-     * in a non-repeating increasing manner.
-     */
-    @Override
-    public int indexOf(Object o) {
-        int indexOf = -1;
-        if (o instanceof StreamInputPacketIndexEntry) {
-            StreamInputPacketIndexEntry streamInputPacketIndexEntry = (StreamInputPacketIndexEntry) o;
-            indexOf = Collections.binarySearch(fEntries, streamInputPacketIndexEntry);
-        }
-        return (indexOf < 0) ? -1 : indexOf;
-    }
-
-    @Override
-    public int size() {
-        return fEntries.size();
-    }
-
-    @Override
-    public boolean contains(Object o) {
-        return fEntries.contains(o);
-    }
-
-    @Override
-    public Iterator<StreamInputPacketIndexEntry> iterator() {
-        return fEntries.iterator();
-    }
-
-    @Override
-    public boolean containsAll(Collection<?> c) {
-        return fEntries.containsAll(c);
-    }
-
-    @Override
-    public Object[] toArray() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public <T> T[] toArray(T[] a) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean remove(Object o) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(Collection<? extends StreamInputPacketIndexEntry> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean addAll(int index, Collection<? extends StreamInputPacketIndexEntry> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean removeAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean retainAll(Collection<?> c) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void clear() {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public StreamInputPacketIndexEntry set(int index, StreamInputPacketIndexEntry element) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public void add(int index, StreamInputPacketIndexEntry element) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public StreamInputPacketIndexEntry remove(int index) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public int lastIndexOf(Object o) {
-        return fEntries.lastIndexOf(o);
-    }
-
-    @Override
-    public List<StreamInputPacketIndexEntry> subList(int fromIndex, int toIndex) {
-        throw new UnsupportedOperationException();
-    }
-
-    @Override
-    public boolean add(StreamInputPacketIndexEntry e) {
-        try {
-            return addEntry(e);
-        } catch (CTFReaderException e1) {
-        }
-        return false;
+        return this.entries.listIterator(guessI);
     }
 }
