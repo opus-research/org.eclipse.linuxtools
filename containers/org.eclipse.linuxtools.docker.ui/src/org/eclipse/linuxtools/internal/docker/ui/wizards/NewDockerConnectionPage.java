35de881396e24accfb15844ee7bcf9d5b3368f2e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:08:54 +0000

    First commited as before_59283_rev5

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/NewDockerConnectionPage.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/NewDockerConnectionPage.java
@@ -18,8 +18,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.TimeUnit;
@@ -51,12 +49,14 @@ import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
 import org.eclipse.linuxtools.docker.core.DockerException;
 import org.eclipse.linuxtools.docker.core.EnumDockerConnectionSettings;
+import org.eclipse.linuxtools.docker.core.IDockerConnectionSettings;
 import org.eclipse.linuxtools.docker.ui.Activator;
 import org.eclipse.linuxtools.internal.docker.core.DockerConnection;
 import org.eclipse.linuxtools.internal.docker.core.DockerConnection.Builder;
 import org.eclipse.linuxtools.internal.docker.core.DockerMachine;
+import org.eclipse.linuxtools.internal.docker.core.TCPConnectionSettings;
+import org.eclipse.linuxtools.internal.docker.core.UnixSocketConnectionSettings;
 import org.eclipse.linuxtools.internal.docker.ui.SWTImagesFactory;
-import org.eclipse.linuxtools.internal.docker.ui.preferences.PreferenceConstants;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
@@ -72,7 +72,6 @@ import org.eclipse.swt.widgets.Group;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.dialogs.ListDialog;
-import org.eclipse.ui.dialogs.PreferencesUtil;
 
 /**
  * {@link WizardPage} to input the settings to connect to a Docker
@@ -81,7 +80,6 @@ import org.eclipse.ui.dialogs.PreferencesUtil;
  */
 public class NewDockerConnectionPage extends WizardPage {
 
-	private static final String DOCKER_MACHINE_PREFERENCE_PAGE_ID = "org.eclipse.linuxtools.docker.ui.preferences.DockerMachinePreferencePage";
 	private final DataBindingContext dbc;
 	private final NewDockerConnectionPageModel model;
 
@@ -142,17 +140,7 @@ public class NewDockerConnectionPage extends WizardPage {
 		connectionNameText.setToolTipText(WizardMessages
 				.getString("NewDockerConnectionPage.nameTooltip")); //$NON-NLS-1$
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER)
-				.grab(true, false).span(1, 1).applyTo(connectionNameText);
-
-		// the 'Search' button
-		final Button searchButton = new Button(container, SWT.NONE);
-		searchButton.setText(WizardMessages
-				.getString("NewDockerConnectionPage.searchButton")); //$NON-NLS-1$
-		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).span(1, 1)
-				.align(SWT.BEGINNING, SWT.CENTER).grab(false, false)
-				.applyTo(searchButton);
-		searchButton.addSelectionListener(onSearchButtonSelection());
-
+				.grab(true, false).span(2, 1).applyTo(connectionNameText);
 
 		// custom settings checkbox
 		final Button customConnectionSettingsButton = new Button(container,
@@ -241,12 +229,22 @@ public class NewDockerConnectionPage extends WizardPage {
 		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER)
 				.applyTo(tcpCertPathBrowseButton);
 
+		// the 'Search' button
+		final Button searchButton = new Button(container, SWT.NONE);
+		searchButton.setText(WizardMessages
+				.getString("NewDockerConnectionPage.searchButton")); //$NON-NLS-1$
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER)
+				.span(COLUMNS - 1, 1).align(SWT.BEGINNING, SWT.CENTER)
+				.applyTo(searchButton);
+		searchButton.addSelectionListener(onSearchButtonSelection());
+
 		// the 'test connection' button
 		final Button testConnectionButton = new Button(container, SWT.NONE);
 		testConnectionButton.setText(WizardMessages
 				.getString("NewDockerConnectionPage.testConnection")); //$NON-NLS-1$
-		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER).span(3, 1)
-				.align(SWT.END, SWT.CENTER).applyTo(testConnectionButton);
+		GridDataFactory.fillDefaults().align(SWT.FILL, SWT.CENTER)
+				.span(1, 1).align(SWT.END, SWT.CENTER)
+				.applyTo(testConnectionButton);
 		testConnectionButton
 				.addSelectionListener(onTestConnectionButtonSelection());
 
@@ -405,16 +403,42 @@ public class NewDockerConnectionPage extends WizardPage {
 							monitor.beginTask(WizardMessages.getString(
 									"NewDockerConnectionPage.retrieveTask"), //$NON-NLS-1$
 									1);
-							final DockerConnection.Defaults defaults = new DockerConnection.Defaults();
-							model.setTcpCertPath(defaults.getTcpCertPath());
-							model.setTcpTLSVerify(defaults.getTcpTlsVerify());
-							model.setTcpHost(defaults.getTcpHost());
-							model.setUnixSocketPath(
-									defaults.getUnixSocketPath());
-							model.setBindingMode(defaults.getBindingMode());
-							model.setCustomSettings(
-									!defaults.isSettingsResolved());
-							model.setConnectionName(defaults.getName());
+							final List<IDockerConnectionSettings> defaults = DockerConnectionManager
+									.getInstance().findConnectionSettings();
+							if (!defaults.isEmpty()) {
+								final IDockerConnectionSettings defaultConnectionSettings = defaults
+										.get(0);
+								model.setCustomSettings(
+										!defaultConnectionSettings
+												.isSettingsResolved());
+								model.setConnectionName(
+										defaultConnectionSettings.getName());
+								switch (defaultConnectionSettings.getType()) {
+								case TCP_CONNECTION:
+									final TCPConnectionSettings tcpConnectionSettings = (TCPConnectionSettings) defaultConnectionSettings;
+									model.setTcpConnectionBindingMode(true);
+									model.setTcpCertPath(tcpConnectionSettings
+											.getPathToCertificates());
+									model.setTcpTLSVerify(tcpConnectionSettings
+											.isTlsVerify());
+									model.setTcpHost(
+											tcpConnectionSettings.getHost());
+									break;
+								case UNIX_SOCKET_CONNECTION:
+									model.setUnixSocketBindingMode(true);
+									final UnixSocketConnectionSettings unixSocketConnectionSettings = (UnixSocketConnectionSettings) defaultConnectionSettings;
+									model.setUnixSocketPath(
+											unixSocketConnectionSettings
+													.getPath());
+									break;
+								}
+							} else {
+								// fall-back to custom settings, suggesting a
+								// Unix Socket connection to the user.
+								model.setCustomSettings(true);
+								model.setUnixSocketBindingMode(true);
+							}
+
 							monitor.done();
 						}
 					});
@@ -600,53 +624,12 @@ public class NewDockerConnectionPage extends WizardPage {
 
 	private SelectionListener onSearchButtonSelection() {
 		return new SelectionAdapter() {
-			
-			private String getVMDriverInstallDir() {
-				return Activator.getDefault()
-						.getPreferenceStore().getString(
-								PreferenceConstants.VM_DRIVER_INSTALLATION_DIRECTORY);
-			}
-
-			private String getDockerMachineInstallDir() {
-				return Activator.getDefault()
-						.getPreferenceStore()
-						.getString(PreferenceConstants.DOCKER_MACHINE_INSTALLATION_DIRECTORY);
-			}
-			
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				if (getDockerMachineInstallDir().equals("")
-						|| getVMDriverInstallDir().equals("")) {
-					final boolean confirm = MessageDialog.openConfirm(
-							getShell(),
-									WizardMessages.getString(
-											"DockerMachineSupport.title"), //$NON-NLS-1$
-							WizardMessages
-									.getString("DockerMachineSupport.message")); //$NON-NLS-1$
-					if (confirm) {
-						PreferencesUtil.createPreferenceDialogOn(getShell(),
-								DOCKER_MACHINE_PREFERENCE_PAGE_ID,
-								new String[] {
-										DOCKER_MACHINE_PREFERENCE_PAGE_ID },
-								null).open();
-						// after user input data, check again
-						if (getDockerMachineInstallDir().equals("")
-								|| getVMDriverInstallDir().equals("")) {
-							return;
-						}
-					} else {
-						return;
-					}
-				}
-				final String dockerMachineInstallDir = getDockerMachineInstallDir();
-				final String vmDriverInstallDir = getVMDriverInstallDir();
-				
-				final String[] dmNames = DockerMachine
-						.getNames(dockerMachineInstallDir);
-				final List<String> activeNames = new ArrayList<>();
+				String[] dmNames = DockerMachine.getNames();
+				List<String> activeNames = new ArrayList<>();
 				for (String name : dmNames) {
-					if (DockerMachine.getHost(name, dockerMachineInstallDir,
-							vmDriverInstallDir) != null) {
+					if (DockerMachine.getHost(name) != null) {
 						activeNames.add(name);
 					}
 				}
@@ -661,10 +644,8 @@ public class NewDockerConnectionPage extends WizardPage {
 					connPrompt.setInput(activeNames.toArray(new String[0]));
 					if (connPrompt.open() == 0 && connPrompt.getResult().length > 0) {
 						String name = ((String) connPrompt.getResult()[0]);
-						String host = DockerMachine.getHost(name,
-								dockerMachineInstallDir, vmDriverInstallDir);
-						String certPath = DockerMachine.getCertPath(name,
-								dockerMachineInstallDir, vmDriverInstallDir);
+						String host = DockerMachine.getHost(name);
+						String certPath = DockerMachine.getCertPath(name);
 						model.setBindingMode(EnumDockerConnectionSettings.TCP_CONNECTION);
 						model.setConnectionName(name);
 						model.setUnixSocketPath(null);
@@ -678,29 +659,18 @@ public class NewDockerConnectionPage extends WizardPage {
 						}
 					}
 				} else {
-					if (dmNames.length == 1) {
-						MessageDialog.openInformation(getShell(),
-								WizardMessages.getString(
-										"NewDockerConnectionPage.searchDialog.discovery.title"), //$NON-NLS-1$
-								WizardMessages.getFormattedString(
-										"NewDockerConnectionPage.searchDialog.discovery.innactive.single", //$NON-NLS-1$
-										dmNames[0]));
-					} else if (dmNames.length > 1) {
-						final StringBuffer connections = new StringBuffer();
-						for (Iterator<String> iterator = Arrays.asList(dmNames)
-								.iterator(); iterator.hasNext();) {
-							final String dmName = iterator.next();
-							connections.append(dmName);
-							if (iterator.hasNext()) {
-								connections.append(", "); //$NON-NLS-1$
-							}
+					if (dmNames.length > 0) {
+						StringBuffer connections = new StringBuffer();
+						for (String conn : dmNames) {
+							connections.append(", "); //$NON-NLS-1$
+							connections.append(conn);
 						}
 						MessageDialog.openInformation(getShell(),
 								WizardMessages.getString(
 										"NewDockerConnectionPage.searchDialog.discovery.title"), //$NON-NLS-1$
 								WizardMessages.getFormattedString(
-										"NewDockerConnectionPage.searchDialog.discovery.innactive.multiple", //$NON-NLS-1$
-										connections.toString()));
+										"NewDockerConnectionPage.searchDialog.discovery.innactive", //$NON-NLS-1$
+										connections.substring(2)));
 					} else {
 						MessageDialog.openInformation(getShell(),
 								WizardMessages.getString(
@@ -711,8 +681,6 @@ public class NewDockerConnectionPage extends WizardPage {
 				}
 			}
 		};
-
-			
 	}
 
 	/**
