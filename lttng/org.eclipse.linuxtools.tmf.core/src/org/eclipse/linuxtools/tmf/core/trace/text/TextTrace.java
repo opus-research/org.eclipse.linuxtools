c41e14b74c7e678edba70e7d9d5253c1a6a2da87
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:47:28 +0000

    First commited as before_34755_rev1

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/text/TextTrace.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/trace/text/TextTrace.java
@@ -292,10 +292,7 @@ public abstract class TextTrace<T extends TextTraceEvent> extends TmfTrace imple
     protected abstract Pattern getFirstLinePattern();
 
     /**
-     * Parses the first line data and returns a new event. When constructing the
-     * event, the concrete trace should use the trace's timestamp transform to
-     * create the timestamp, by either transforming the parsed time value
-     * directly or by using the method {@link #createTimestamp(long)}.
+     * Parses the first line data and returns a new event.
      *
      * @param matcher
      *            The matcher
