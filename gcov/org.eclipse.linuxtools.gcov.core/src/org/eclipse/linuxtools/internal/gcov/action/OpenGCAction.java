c4d2e3f0e03c264356c7d9544d63f937af17a6dd
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:06:17 +0000

    First commited as after_80242_rev1

--- gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/action/OpenGCAction.java
+++ gcov/org.eclipse.linuxtools.gcov.core/src/org/eclipse/linuxtools/internal/gcov/action/OpenGCAction.java
@@ -124,10 +124,15 @@ public class OpenGCAction implements IEditorLauncher {
     }
 
     private void displayCoverage(IPath file, String binaryPath, File gcda, boolean isCompleteCoverageResultWanted) {
+        System.out.println(ResourcesPlugin.getWorkspace());
+        System.out.println(ResourcesPlugin.getWorkspace().getRoot());
+        System.out.println(ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(file));
+        System.out.println(ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(file).getProject());
         IProject project = ResourcesPlugin.getWorkspace().getRoot().getFileForLocation(file).getProject();
         GcovAnnotationModelTracker.getInstance().addProject(project, new Path(binaryPath));
         GcovAnnotationModelTracker.getInstance().annotateAllCEditors();
 
+System.out.println("here 1");
         if (isCompleteCoverageResultWanted) {
             CovView.displayCovResults(binaryPath, gcda.getAbsolutePath());
         } else {
