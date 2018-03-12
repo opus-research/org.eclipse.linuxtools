bcc03308ca51b15b739d7c5bab730a6bfb75ccf3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:29:17 +0000

    First commited as before_94675_rev9

--- oprofile/org.eclipse.linuxtools.oprofile.launch/src/org/eclipse/linuxtools/internal/oprofile/launch/launching/OprofileManualLaunchConfigurationDelegate.java
+++ oprofile/org.eclipse.linuxtools.oprofile.launch/src/org/eclipse/linuxtools/internal/oprofile/launch/launching/OprofileManualLaunchConfigurationDelegate.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009, 2017 Red Hat, Inc. and others
+ * Copyright (c) 2009 Red Hat, Inc. and others
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,10 +10,16 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.oprofile.launch.launching;
 
+import java.lang.reflect.InvocationTargetException;
+
 import org.eclipse.debug.core.ILaunch;
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
+import org.eclipse.linuxtools.internal.oprofile.core.OpcontrolException;
 import org.eclipse.linuxtools.internal.oprofile.core.Oprofile;
+import org.eclipse.linuxtools.internal.oprofile.core.OprofileCorePlugin;
 import org.eclipse.linuxtools.internal.oprofile.core.daemon.OprofileDaemonEvent;
 import org.eclipse.linuxtools.internal.oprofile.launch.OprofileLaunchMessages;
 import org.eclipse.linuxtools.internal.oprofile.launch.configuration.LaunchOptions;
@@ -42,27 +48,54 @@ public class OprofileManualLaunchConfigurationDelegate extends AbstractOprofileL
     }
 
     @Override
-	protected void postExec(LaunchOptions options, OprofileDaemonEvent[] daemonEvents, Process process) {
-		Display.getDefault().syncExec(() -> {
-			// TODO: have a initialization dialog to do reset and setupDaemon?
-			// using a progress dialog, can't abort the launch if there's an exception..
-			if (!oprofileStatus()) {
-				return;
-			}
+    protected void postExec(LaunchOptions options, OprofileDaemonEvent[] daemonEvents, Process process) {
+        final LaunchOptions fOptions = options;
+        final OprofileDaemonEvent[] fDaemonEvents = daemonEvents;
+        Display.getDefault().syncExec(() -> {
+		    //TODO: have a initialization dialog to do reset and setupDaemon?
+		    // using a progress dialog, can't abort the launch if there's an exception..
+		    try {
+		        if (!oprofileStatus()) {
+		            return;
+		        }
+		        oprofileReset();
+		        oprofileSetupDaemon(fOptions.getOprofileDaemonOptions(), fDaemonEvents);
+		    } catch (OpcontrolException oe) {
+		        OprofileCorePlugin.showErrorDialog("opcontrolProvider", oe); //$NON-NLS-1$
+		        return; // dont open the dialog
+		    }
 
-			// manual oprofile control dialog
-			final OprofiledControlDialog dlg = new OprofiledControlDialog();
+		    //manual oprofile control dialog
+		    final OprofiledControlDialog dlg = new OprofiledControlDialog();
 
-			// This was earlier in an if (!fLaunch.isTerminated()) {} block. From a
-			// usability perspective I think it's better to show the oprofile control
-			// dialog regardless, since it might not show up at all if the launch
-			// has already be terminated. Anyhow, I think it's better this way.
-			// --Severin, 2011-02-11
-			dlg.setBlockOnOpen(false);
-			dlg.open();
+		    // This was earlier in an if (!fLaunch.isTerminated()) {} block. From a
+		    // usability perspective I think it's better to show the oprofile control
+		    // dialog regardless, since it might not show up at all if the launch
+		    // has already be terminated. Anyhow, I think it's better this way.
+		    // --Severin, 2011-02-11
+		    dlg.setBlockOnOpen(false);
+		    dlg.open();
 
+		    //progress dialog for ensuring the daemon is shut down
+		    IRunnableWithProgress refreshRunner = monitor -> {
+			    monitor.beginTask(OprofileLaunchMessages.getString("oprofiledcontroldialog.post.stopdaemon"), 1); //$NON-NLS-1$
+			    try {
+			        oprofileShutdown();
+			    } catch (OpcontrolException e) {
+			    }
+			    monitor.worked(1);
+			    monitor.done();
+			};
+		    ProgressMonitorDialog dialog = new ProgressMonitorDialog(null);
+		    try {
+		        dialog.run(true, false, refreshRunner);
+		    } catch (InvocationTargetException e1) {
+		        e1.printStackTrace();
+		    } catch (InterruptedException e2) {
+		        // cannot be thrown when cancelable is false
+		    }
 		});
-	}
+    }
 
     /**
      * A custom dialog box to control the oprofile daemon.
@@ -96,28 +129,40 @@ public class OprofileManualLaunchConfigurationDelegate extends AbstractOprofileL
             startDaemonButton.setText(OprofileLaunchMessages.getString("oprofiledcontroldialog.buttons.startdaemon")); //$NON-NLS-1$
             startDaemonButton.addSelectionListener(new SelectionAdapter() {
                 @Override
-				public void widgetSelected(SelectionEvent e) {
-					startDaemonButton.setEnabled(false);
-					stopDaemonButton.setEnabled(true);
-					refreshViewButton.setEnabled(true);
-					resetSessionButton.setEnabled(true);
-					saveSessionButton.setEnabled(true);
-					addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.startdaemon")); //$NON-NLS-1$
-				}
-			});
+                public void widgetSelected(SelectionEvent e) {
+                    try {
+                        oprofileStartCollection();
+                        startDaemonButton.setEnabled(false);
+                        stopDaemonButton.setEnabled(true);
+                        refreshViewButton.setEnabled(true);
+                        resetSessionButton.setEnabled(true);
+                        saveSessionButton.setEnabled(true);
+                    } catch (OpcontrolException oe) {
+                        //disable buttons, notify user of error
+                        disableAllButtons();
+                        OprofileCorePlugin.showErrorDialog("opcontrolProvider", oe); //$NON-NLS-1$
+                    }
+                    addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.startdaemon")); //$NON-NLS-1$
+                }});
 
             stopDaemonButton = new Button(area, SWT.PUSH);
             stopDaemonButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
             stopDaemonButton.setText(OprofileLaunchMessages.getString("oprofiledcontroldialog.buttons.stopdaemon")); //$NON-NLS-1$
             stopDaemonButton.setEnabled(false);        //disabled at start
-			stopDaemonButton.addSelectionListener(new SelectionAdapter() {
-				@Override
-				public void widgetSelected(SelectionEvent e) {
-					startDaemonButton.setEnabled(true);
-					stopDaemonButton.setEnabled(false);
-					addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.stopdaemon")); //$NON-NLS-1$
-				}
-			});
+            stopDaemonButton.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    try {
+                        oprofileShutdown();
+                        startDaemonButton.setEnabled(true);
+                        stopDaemonButton.setEnabled(false);
+                    } catch (OpcontrolException oe) {
+                        //disable buttons, notify user of error
+                        disableAllButtons();
+                        OprofileCorePlugin.showErrorDialog("opcontrolProvider", oe); //$NON-NLS-1$
+                    }
+                    addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.stopdaemon")); //$NON-NLS-1$
+                }});
 
             saveSessionButton = new Button(area, SWT.PUSH);
             saveSessionButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
@@ -135,26 +180,36 @@ public class OprofileManualLaunchConfigurationDelegate extends AbstractOprofileL
             resetSessionButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
             resetSessionButton.setText(OprofileLaunchMessages.getString("oprofiledcontroldialog.buttons.resetsession")); //$NON-NLS-1$
             resetSessionButton.setEnabled(false);        //disabled at start
-			resetSessionButton.addSelectionListener(new SelectionAdapter() {
-				@Override
-				public void widgetSelected(SelectionEvent e) {
-					refreshOprofileView(); // without refresh can lead to inconsistencies for save session
-					addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.reset")); //$NON-NLS-1$
-				}
-			});
+            resetSessionButton.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    try {
+                        oprofileReset();
+                    } catch (OpcontrolException oe) {
+                        //disable buttons, notify user of error
+                        disableAllButtons();
+                        OprofileCorePlugin.showErrorDialog("opcontrolProvider", oe); //$NON-NLS-1$
+                    }
+                    refreshOprofileView();    //without refresh can lead to inconsistencies for save session
+                    addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.reset")); //$NON-NLS-1$
+                }});
 
             refreshViewButton = new Button(area, SWT.PUSH);
             refreshViewButton.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false));
             refreshViewButton.setText(OprofileLaunchMessages.getString("oprofiledcontroldialog.buttons.refreshview")); //$NON-NLS-1$
             refreshViewButton.setEnabled(false);        //disabled at start
-			refreshViewButton.addSelectionListener(new SelectionAdapter() {
-				@Override
-				public void widgetSelected(SelectionEvent e) {
-					addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.dumpsamples")); //$NON-NLS-1$
-					refreshOprofileView();
-					addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.refreshed")); //$NON-NLS-1$
-				}
-			});
+            refreshViewButton.addSelectionListener(new SelectionAdapter() {
+                @Override
+                public void widgetSelected(SelectionEvent e) {
+                    addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.dumpsamples")); //$NON-NLS-1$
+                    try {
+                        oprofileDumpSamples();
+                    } catch (OpcontrolException oe) {
+                        //no error in this case; the user might refresh when the daemon isnt running
+                    }
+                    refreshOprofileView();
+                    addToFeedbackList(OprofileLaunchMessages.getString("oprofiledcontroldialog.feedback.refreshed")); //$NON-NLS-1$
+                }});
 
 
             List feedback = new List(area, SWT.READ_ONLY | SWT.MULTI | SWT.BORDER | SWT.V_SCROLL);
@@ -163,6 +218,15 @@ public class OprofileManualLaunchConfigurationDelegate extends AbstractOprofileL
             feedbackList = feedback;
 
             return area;
+        }
+
+        //helper function
+        private void disableAllButtons() {
+            startDaemonButton.setEnabled(false);
+            stopDaemonButton.setEnabled(false);
+            refreshViewButton.setEnabled(false);
+            resetSessionButton.setEnabled(false);
+            saveSessionButton.setEnabled(false);
         }
 
         //a little hack to get the list to auto scroll to the newly added item
--- oprofile/org.eclipse.linuxtools.oprofile.ui/src/org/eclipse/linuxtools/internal/oprofile/ui/view/OprofileViewSaveDefaultSessionAction.java
+++ oprofile/org.eclipse.linuxtools.oprofile.ui/src/org/eclipse/linuxtools/internal/oprofile/ui/view/OprofileViewSaveDefaultSessionAction.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2017 Red Hat, Inc.
+ * Copyright (c) 2004, 2009 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -11,9 +11,6 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.oprofile.ui.view;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.text.MessageFormat;
 
 import org.eclipse.core.filesystem.IFileStore;
@@ -23,7 +20,9 @@ import org.eclipse.jface.dialogs.IInputValidator;
 import org.eclipse.jface.dialogs.InputDialog;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.window.Window;
+import org.eclipse.linuxtools.internal.oprofile.core.OpcontrolException;
 import org.eclipse.linuxtools.internal.oprofile.core.Oprofile;
+import org.eclipse.linuxtools.internal.oprofile.core.OprofileCorePlugin;
 import org.eclipse.linuxtools.internal.oprofile.core.opxml.sessions.SessionManager;
 import org.eclipse.linuxtools.internal.oprofile.ui.OprofileUiMessages;
 import org.eclipse.linuxtools.internal.oprofile.ui.OprofileUiPlugin;
@@ -50,8 +49,8 @@ public class OprofileViewSaveDefaultSessionAction extends Action {
     @Override
     public void run() {
         boolean defaultSessionExists = false;
-        UiModelRoot modelRoot = UiModelRoot.getDefault();
         String defaultSessionName = null;
+        UiModelRoot modelRoot = UiModelRoot.getDefault();
         IUiModelElement[] modelEvents = null;
 
         if (modelRoot.hasChildren()) {
@@ -84,22 +83,41 @@ public class OprofileViewSaveDefaultSessionAction extends Action {
                         new SaveSessionValidator());
 
                 int result = dialog.open();
-				if (result == Window.OK) {
-					saveSession(dialog.getValue());
-
-					// remove the default session
-                    for (int i = 0; i < modelEvents.length; i++) {
-                        deleteSession(defaultSessionName, modelEvents[i].getLabelText());
+                if (result == Window.OK) {
+                    try {
+                        OprofileCorePlugin.getDefault().getOpcontrolProvider()
+                                .saveSession(dialog.getValue());
+                        // remove the default session
+                        for (int i = 0; i < modelEvents.length; i++) {
+                            OprofileCorePlugin
+                                    .getDefault()
+                                    .getOpcontrolProvider()
+                                    .deleteSession(defaultSessionName,
+                                            modelEvents[i].getLabelText());
+                        }
+                        // clear out collected data by this session
+                        // if opcontol is used
+                        if (Oprofile.OprofileProject.OPCONTROL_BINARY
+                                .equals(Oprofile.OprofileProject
+                                        .getProfilingBinary())) {
+                            OprofileCorePlugin.getDefault()
+                                    .getOpcontrolProvider().reset();
+                        } else if (Oprofile.OprofileProject.OPERF_BINARY.equals(Oprofile.OprofileProject.getProfilingBinary())){
+                            // remove oprofile_data so current event no longer
+                            // be there
+                            OprofileViewDeleteSessionAction
+                                    .deleteOperfDataFolder(Oprofile.OprofileProject
+                                            .getProject()
+                                            .getFolder(
+                                                    Oprofile.OprofileProject.OPERF_DATA));
+                        }
+                        OprofileUiPlugin.getDefault().getOprofileView()
+                                .refreshView();
+                    } catch (OpcontrolException oe) {
+                        OprofileCorePlugin.showErrorDialog(
+                                "opcontrolProvider", oe); //$NON-NLS-1$
                     }
-
-					if (Oprofile.OprofileProject.OPERF_BINARY.equals(Oprofile.OprofileProject.getProfilingBinary())) {
-						// remove oprofile_data so current event no longer
-						// be there
-						OprofileViewDeleteSessionAction.deleteOperfDataFolder(
-								Oprofile.OprofileProject.getProject().getFolder(Oprofile.OprofileProject.OPERF_DATA));
-					}
-					OprofileUiPlugin.getDefault().getOprofileView().refreshView();
-				}
+                }
             } else {
                 MessageDialog
                         .openError(
@@ -111,35 +129,6 @@ public class OprofileViewSaveDefaultSessionAction extends Action {
             }
         }
     }
-
-    public void saveSession(String name) {
-		SessionManager sessMan;
-		try {
-			sessMan = new SessionManager(SessionManager.SESSION_LOCATION);
-			for (String event : sessMan.getSessionEvents(SessionManager.CURRENT)) {
-				sessMan.addSession(name, event);
-				String oldFile = SessionManager.OPXML_PREFIX + SessionManager.MODEL_DATA + event
-						+ SessionManager.CURRENT;
-				String newFile = SessionManager.OPXML_PREFIX + SessionManager.MODEL_DATA + event + name;
-				Process p = Runtime.getRuntime().exec("cp " + oldFile + " " + newFile); //$NON-NLS-1$ //$NON-NLS-2$
-				p.waitFor();
-			}
-			sessMan.write();
-		} catch (FileNotFoundException e) {
-			// intentionally blank
-			// during a save, the session file will exist
-		} catch (IOException | InterruptedException e) {
-			e.printStackTrace();
-		}
-	}
-
-    public void deleteSession(String sessionName, String eventName) {
-		File file = new File(SessionManager.OPXML_PREFIX + SessionManager.MODEL_DATA + eventName + sessionName);
-		file.delete();
-		SessionManager sessMan = new SessionManager(SessionManager.SESSION_LOCATION);
-		sessMan.removeSession(sessionName, eventName);
-		sessMan.write();
-	}
 
     // Original author: Keith Seitz <keiths@redhat.com>
     private class SaveSessionValidator implements IInputValidator {
