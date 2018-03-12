ec3b2eb3eec49b1f9b1024a268f2dae105577b4d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:37:54 +0000

    First commited as before_30743_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/virtualtable/ColumnData.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/widgets/virtualtable/ColumnData.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2010, 2014 Ericsson
+ * Copyright (c) 2010, 2012 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -8,65 +8,39 @@
  *
  * Contributors:
  *   Matthew Khouzam - Extracted from TmfEventsView
- *   Alexandre Montplaisir - Added a tooltip config element
  ******************************************************************************/
 
 package org.eclipse.linuxtools.tmf.ui.widgets.virtualtable;
-
-import org.eclipse.jdt.annotation.Nullable;
-
 /**
- * The configuration element for a {@link TmfVirtualTable} column.
- *
+ * ColumnData
  * @author Matthew Khouzam
+ * @version 1.0
+ *
  */
 public class ColumnData {
-
-    /** The title of the column */
+    /**
+     * The title of the column
+     */
     public final String header;
-
-    /** The width of the column in pixels */
-    public final int width;
-
-    /** The alignment of the column */
-    public final int alignment;
-
-    /** The header's tooltip. 'null' for no special tooltip
-     * @since 3.1 */
-    public final @Nullable String tooltip;
+    /**
+     * the width of the column in pixels
+     */
+    public final int    width;
+    /**
+     * the alignment of the column
+     */
+    public final int    alignment;
 
     /**
-     * Constructor with no tooltip
-     *
-     * @param header
-     *            Column header (title)
-     * @param width
-     *            Column width
-     * @param alignment
-     *            Text alignment for this column
+     * Constructor
+     * @param h header (title)
+     * @param w width
+     * @param a alignment
      */
-    public ColumnData(String header, int width, int alignment) {
-        this(header, width, alignment, null);
-    }
-
-    /**
-     * Constructor with a header tooltip
-     *
-     * @param header
-     *            Column header (title)
-     * @param width
-     *            Column width
-     * @param alignment
-     *            Text alignment for this column
-     * @param tooltip
-     *            The tooltip for this column header, 'null' for no tooltip
-     * @since 3.1
-     */
-    public ColumnData(String header, int width, int alignment, @Nullable String tooltip) {
-        this.header = header;
-        this.width = width;
-        this.alignment = alignment;
-        this.tooltip = tooltip;
+    public ColumnData(String h, int w, int a) {
+        header = h;
+        width = w;
+        alignment = a;
     }
 
 }
