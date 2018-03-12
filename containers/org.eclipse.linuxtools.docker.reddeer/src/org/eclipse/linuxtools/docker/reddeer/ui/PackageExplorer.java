a00ed9d22cd07b87b8ab6a04c9aecc8beb5bebad
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:31:32 +0000

    First commited as after_97157_rev11

---
+++ containers/org.eclipse.linuxtools.docker.reddeer/src/org/eclipse/linuxtools/docker/reddeer/ui/PackageExplorer.java
@@ -0,0 +1,20 @@
+package org.eclipse.linuxtools.docker.reddeer.ui;
+
+import org.jboss.reddeer.eclipse.jdt.ui.AbstractExplorer;
+
+/**
+ * Represents Package Explorer in Eclipse
+ * 
+ * @author Vlado Pakan
+ *
+ */
+public class PackageExplorer extends AbstractExplorer {
+    
+	/**
+	 * Constructs the view with Package Explorer.
+	 */
+	public PackageExplorer() {
+		super("Package Explorer");
+	}
+
+}
