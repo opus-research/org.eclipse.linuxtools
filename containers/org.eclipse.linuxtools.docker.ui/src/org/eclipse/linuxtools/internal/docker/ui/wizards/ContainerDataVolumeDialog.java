db0ccae3763963548f620920e061c81a08e2cf37
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:55:20 +0000

    First commited as before_74631_rev2

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerDataVolumeDialog.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerDataVolumeDialog.java
@@ -396,11 +396,10 @@ public class ContainerDataVolumeDialog extends Dialog {
 			final Label errorMessageIcon, final Label errorMessageLabel) {
 
 		return event -> {
-			// skip if dialog has been closed
-			if (Display.getCurrent() == null || getShell().isDisposed()) {
+			final IStatus status = validateInput();
+			if (Display.getCurrent() == null) {
 				return;
 			}
-			final IStatus status = validateInput();
 			Display.getCurrent().syncExec(() -> {
 				if (status.isOK()) {
 					errorMessageIcon.setVisible(false);
