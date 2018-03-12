9467022ae903ee415f5174ef31a14d7c51df7572
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:50:28 +0000

    First commited as before_31239_rev5

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/viewers/events/TmfEventsTable.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/viewers/events/TmfEventsTable.java
@@ -408,13 +408,7 @@ public class TmfEventsTable extends TmfComponent implements IGotoMarker, IColorS
         }
 
         // Create the UI columns in the table
-        for (TmfEventTableColumn col : fColumns) {
-            TableColumn column = fTable.newTableColumn(SWT.LEFT);
-            column.setText(col.getHeaderName());
-            column.setToolTipText(col.getHeaderTooltip());
-            column.setData(Key.FIELD_ID, col.getFilterFieldId());
-            column.pack();
-        }
+        fTable.createColumns(fColumns);
 
         // Set the frozen row for header row
         fTable.setFrozenRowCount(1);
