7c09e2cf458ec8d9dbdcd6fc7ec9059200774cd5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:45:31 +0000

    First commited as before_31063_rev3

--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/filter/model/TmfFilterCompareNode.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/filter/model/TmfFilterCompareNode.java
@@ -51,8 +51,8 @@ public class TmfFilterCompareNode extends TmfFilterTreeNode {
     private int fResult;
     private Type fType = Type.NUM;
     private String fValue;
-    private Number fValueNumber;
-    private TmfTimestamp fValueTimestamp;
+    private transient Number fValueNumber;
+    private transient TmfTimestamp fValueTimestamp;
 
     /**
      * @param parent the parent node
@@ -230,8 +230,6 @@ public class TmfFilterCompareNode extends TmfFilterTreeNode {
         result = prime * result + fResult;
         result = prime * result + ((fType == null) ? 0 : fType.hashCode());
         result = prime * result + ((fValue == null) ? 0 : fValue.hashCode());
-        result = prime * result + ((fValueNumber == null) ? 0 : fValueNumber.hashCode());
-        result = prime * result + ((fValueTimestamp == null) ? 0 : fValueTimestamp.hashCode());
         return result;
     }
 
@@ -268,20 +266,6 @@ public class TmfFilterCompareNode extends TmfFilterTreeNode {
                 return false;
             }
         } else if (!fValue.equals(other.fValue)) {
-            return false;
-        }
-        if (fValueNumber == null) {
-            if (other.fValueNumber != null) {
-                return false;
-            }
-        } else if (!fValueNumber.equals(other.fValueNumber)) {
-            return false;
-        }
-        if (fValueTimestamp == null) {
-            if (other.fValueTimestamp != null) {
-                return false;
-            }
-        } else if (!fValueTimestamp.equals(other.fValueTimestamp)) {
             return false;
         }
         return true;
--- lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/filter/model/TmfFilterContainsNode.java
+++ lttng/org.eclipse.linuxtools.tmf.core/src/org/eclipse/linuxtools/tmf/core/filter/model/TmfFilterContainsNode.java
@@ -35,7 +35,7 @@ public class TmfFilterContainsNode extends TmfFilterTreeNode {
     private boolean fNot = false;
     private String fField;
     private String fValue;
-    private String fValueUpperCase;
+    private transient String fValueUpperCase;
     private boolean fIgnoreCase = false;
 
     /**
@@ -148,7 +148,6 @@ public class TmfFilterContainsNode extends TmfFilterTreeNode {
         result = prime * result + (fIgnoreCase ? 1231 : 1237);
         result = prime * result + (fNot ? 1231 : 1237);
         result = prime * result + ((fValue == null) ? 0 : fValue.hashCode());
-        result = prime * result + ((fValueUpperCase == null) ? 0 : fValueUpperCase.hashCode());
         return result;
     }
 
@@ -182,13 +181,6 @@ public class TmfFilterContainsNode extends TmfFilterTreeNode {
                 return false;
             }
         } else if (!fValue.equals(other.fValue)) {
-            return false;
-        }
-        if (fValueUpperCase == null) {
-            if (other.fValueUpperCase != null) {
-                return false;
-            }
-        } else if (!fValueUpperCase.equals(other.fValueUpperCase)) {
             return false;
         }
         return true;
