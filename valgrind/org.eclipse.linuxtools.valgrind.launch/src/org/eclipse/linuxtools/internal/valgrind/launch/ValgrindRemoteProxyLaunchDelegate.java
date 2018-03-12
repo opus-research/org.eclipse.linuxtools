742d804881663baca580571f4b336897433fda41
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:39:57 +0000

    First commited as after_30824_rev1

--- valgrind/org.eclipse.linuxtools.valgrind.launch/src/org/eclipse/linuxtools/internal/valgrind/launch/ValgrindRemoteProxyLaunchDelegate.java
+++ valgrind/org.eclipse.linuxtools.valgrind.launch/src/org/eclipse/linuxtools/internal/valgrind/launch/ValgrindRemoteProxyLaunchDelegate.java
@@ -225,7 +225,9 @@ public class ValgrindRemoteProxyLaunchDelegate extends ValgrindLaunchConfigurati
             int state = p.waitFor();
 
             if (state != IRemoteCommandLauncher.OK) {
-                abort("valgrind launcher exited with status " + state + ". See IRemoteCommandLauncher for details.\n",
+                abort(Messages.getString("ValgrindLaunchConfigurationDelegate.Launch_exited_status") + " " //$NON-NLS-1$ //$NON-NLS-2$
+                        + state + ". " + NLS.bind(Messages.getString("ValgrindRemoteProxyLaunchDelegate.see_reference"), "IRemoteCommandLauncher") //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                        + "\n",  //$NON-NLS-1$
                         null, ICDTLaunchConfigurationConstants.ERR_INTERNAL_ERROR);
             }
 
@@ -245,8 +247,8 @@ public class ValgrindRemoteProxyLaunchDelegate extends ValgrindLaunchConfigurati
                     valgrindErrSB.append(line);
                 }
 
-                abort("Standard ouput: " + valgrindOutSB.toString() +
-                        "\nStandard error: " + valgrindErrSB.toString(),
+                abort("Standard ouput: " + valgrindOutSB.toString() + //$NON-NLS-1$
+                        "\nStandard error: " + valgrindErrSB.toString(), //$NON-NLS-1$
                         null, ICDTLaunchConfigurationConstants.ERR_INTERNAL_ERROR);
             }
 
