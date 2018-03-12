bdf6e28bd742e5f78722d5c8d75540834db4fab6
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:01:04 +0000

    First commited as before_79094_rev3

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/RunConsole.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/RunConsole.java
@@ -17,7 +17,6 @@ import java.io.OutputStream;
 
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
 import org.eclipse.linuxtools.docker.core.IDockerContainer;
-import org.eclipse.linuxtools.docker.core.IDockerContainerState;
 import org.eclipse.linuxtools.internal.docker.core.DockerConnection;
 import org.eclipse.linuxtools.internal.docker.ui.views.DVMessages;
 import org.eclipse.ui.console.ConsolePlugin;
@@ -165,14 +164,12 @@ public class RunConsole extends IOConsole {
 			try {
 				DockerConnection conn = (DockerConnection) connection;
 				if (conn.getContainerInfo(containerId).config().openStdin()) {
-					IDockerContainerState state = conn
-							.getContainerInfo(containerId).state();
-					do {
-						if (!state.running())
-							Thread.sleep(1000);
-					} while (!state.running() && state.finishDate() == null);
+					while (!conn.getContainerInfo(containerId).state()
+							.running()) {
+						Thread.sleep(1000);
+					}
+					conn.attachCommand(containerId, null, null);
 				}
-				conn.attachCommand(containerId, null, null);
 			} catch (Exception e) {
 			}
 		});
