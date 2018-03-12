8d1e4126d5b90ecc9bc1df54d2da92a8c4be6b28
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:57:51 +0000

    First commited as before_31563_rev1

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/filter/FilterViewer.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/views/filter/FilterViewer.java
@@ -83,6 +83,9 @@ import org.eclipse.swt.widgets.TreeItem;
 
 class FilterViewer extends Composite {
 
+    private static final String CUSTOM_TXT_CATEGORY = "Custom Text"; //$NON-NLS-1$
+    private static final String CUSTOM_XML_CATEGORY = "Custom XML"; //$NON-NLS-1$
+
     private TreeViewer fViewer;
 
     private Composite fComposite;
@@ -444,7 +447,7 @@ class FilterViewer extends Composite {
             }
             for (CustomTxtTraceDefinition def : CustomTxtTraceDefinition.loadAll()) {
                 if (def.outputs.size() > 0) {
-                    fieldsList.add("[" + TmfTraceType.CUSTOM_TXT_CATEGORY + //$NON-NLS-1$
+                    fieldsList.add("[" + CUSTOM_TXT_CATEGORY + //$NON-NLS-1$
                             " : " + def.definitionName + "]"); //$NON-NLS-1$ //$NON-NLS-2$
                     for (OutputColumn output : def.outputs) {
                         fieldsList.add(output.name);
@@ -454,7 +457,7 @@ class FilterViewer extends Composite {
             }
             for (CustomXmlTraceDefinition def : CustomXmlTraceDefinition.loadAll()) {
                 if (def.outputs.size() > 0) {
-                    fieldsList.add("[" + TmfTraceType.CUSTOM_XML_CATEGORY + //$NON-NLS-1$
+                    fieldsList.add("[" + CUSTOM_XML_CATEGORY + //$NON-NLS-1$
                             " : " + def.definitionName + "]"); //$NON-NLS-1$ //$NON-NLS-2$
                     for (OutputColumn output : def.outputs) {
                         fieldsList.add(output.name);
@@ -594,11 +597,11 @@ class FilterViewer extends Composite {
                 eventsTypeMap.put(text, ce);
             }
             for (CustomTxtTraceDefinition def : CustomTxtTraceDefinition.loadAll()) {
-                String text = TmfTraceType.CUSTOM_TXT_CATEGORY + " : " + def.definitionName; //$NON-NLS-1$
+                String text = CUSTOM_TXT_CATEGORY + " : " + def.definitionName; //$NON-NLS-1$
                 eventsTypeMap.put(text, def);
             }
             for (CustomXmlTraceDefinition def : CustomXmlTraceDefinition.loadAll()) {
-                String text = TmfTraceType.CUSTOM_XML_CATEGORY + " : " + def.definitionName; //$NON-NLS-1$
+                String text = CUSTOM_XML_CATEGORY + " : " + def.definitionName; //$NON-NLS-1$
                 eventsTypeMap.put(text, def);
             }
             return eventsTypeMap;
