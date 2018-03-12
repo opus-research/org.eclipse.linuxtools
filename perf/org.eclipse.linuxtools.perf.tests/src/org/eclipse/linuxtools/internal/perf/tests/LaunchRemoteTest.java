f87461cefdf833f9856403685ed5e345564fb2e8
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:52:27 +0000

    First commited as before_49726_rev1

--- perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/LaunchRemoteTest.java
+++ perf/org.eclipse.linuxtools.perf.tests/src/org/eclipse/linuxtools/internal/perf/tests/LaunchRemoteTest.java
@@ -30,6 +30,7 @@ import org.eclipse.linuxtools.profiling.tests.AbstractRemoteTest;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.osgi.framework.FrameworkUtil;
 
 public class LaunchRemoteTest extends AbstractRemoteTest {
 
@@ -39,12 +40,17 @@ public class LaunchRemoteTest extends AbstractRemoteTest {
     private ILaunchConfigurationWorkingCopy wc;
     private IProject project;
 
+    private final String CONNECTION_NAME = "localhost"; //$NON-NLS-1$
     private final String CONNECTION_DIR = "/tmp/eclipse-perf-ext_project_test/"; //$NON-NLS-1$
+    private final String EXTERNAL_PROJECT_PATH = "remotetools://"+ CONNECTION_NAME + CONNECTION_DIR; //$NON-NLS-1$
+    private final String PROJECT_NAME = "fibTest"; //$NON-NLS-1$
+    private final String SOURCE_FILE = "fib.cpp"; //$NON-NLS-1$
 
     @Before
     public void setUp() throws Exception {
         if ((!(AbstractRemoteTest.USERNAME.isEmpty()))) {
-            project = null;
+            project = createRemoteExternalProjectAndBuild(FrameworkUtil.getBundle(this.getClass()),
+                    PROJECT_NAME, EXTERNAL_PROJECT_PATH, SOURCE_FILE);
 
             config = createConfiguration(project);
             delegate = new PerfLaunchConfigDelegate();
