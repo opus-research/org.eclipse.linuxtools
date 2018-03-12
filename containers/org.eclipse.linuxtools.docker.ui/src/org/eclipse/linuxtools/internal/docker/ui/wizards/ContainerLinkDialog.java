bb054df846cd27bb25fa59c25702919c3e34e7d2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:54:39 +0000

    First commited as before_49932_rev1

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerLinkDialog.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerLinkDialog.java
@@ -175,19 +175,18 @@ public class ContainerLinkDialog extends Dialog {
 								ContainerLinkDialogModel.CONTAINER_ALIAS)
 						.observe(model));
 		containerNameObservable.addValueChangeListener(
-				onContainerLinkSettingsChanged(errorMessageLabel));
+onContainerLinkSettingsChanged());
 		containerAliasObservable.addValueChangeListener(
-				onContainerLinkSettingsChanged(errorMessageLabel));
+onContainerLinkSettingsChanged());
 		return container;
 	}
 
-	private IValueChangeListener onContainerLinkSettingsChanged(
-			final Label errorMessageLabel) {
+	private IValueChangeListener onContainerLinkSettingsChanged() {
 		return new IValueChangeListener() {
 
 			@Override
 			public void handleValueChange(ValueChangeEvent event) {
-				validateInput(errorMessageLabel);
+				validateInput();
 			}
 		};
 	}
@@ -214,7 +213,7 @@ public class ContainerLinkDialog extends Dialog {
 			@Override
 			public IContentProposal[] getProposals(final String contents,
 					final int position) {
-				final List<IContentProposal> proposals = new ArrayList<IContentProposal>();
+				final List<IContentProposal> proposals = new ArrayList<>();
 				for (String containerName : containerSelectionCombo
 						.getItems()) {
 					if (containerName.contains(contents)) {
@@ -227,7 +226,7 @@ public class ContainerLinkDialog extends Dialog {
 		};
 	}
 
-	private void validateInput(final Label errorMessageLabel) {
+	private void validateInput() {
 		final String selectedContainerName = model.getContainerName();
 		final Object[] containerNames = model.getContainerNames().toArray();
 		final String containerAlias = model.getContainerAlias();
