e8a843ca021a78c9a31bd959993257cdf3c6ab99
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:12:57 +0000

    First commited as before_61113_rev2

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
@@ -15,7 +15,6 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.net.SocketTimeoutException;
 import java.net.URI;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
@@ -514,15 +513,7 @@ public class DockerConnection implements IDockerConnection, Closeable {
 				Activator.logErrorMessage(e.getMessage());
 				throw new InterruptedException();
 			} catch (Exception e) {
-				/*
-				 * Temporary workaround for BZ #477485
-				 * Remove when docker-client logs() uses noTimeoutClient.
-				 */
-				if (e.getCause() instanceof SocketTimeoutException) {
-					execute();
-				} else {
-					Activator.logErrorMessage(e.getMessage());
-				}
+				Activator.logErrorMessage(e.getMessage());
 			} finally {
 				follow = false;
 				copyClient.close(); // we are done with copyClient..dispose
@@ -980,8 +971,7 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			final ContainerCreation creation = client
 					.createContainer(builder.build(),
 					containerName);
-
-			final String id = creation != null ? creation.id() : null;
+			final String id = creation.id();
 			// force a refresh of the current containers to include the new one
 			listContainers();
 			return id;
