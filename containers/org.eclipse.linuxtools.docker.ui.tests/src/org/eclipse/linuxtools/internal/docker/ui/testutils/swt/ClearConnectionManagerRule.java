db937d2d4f6532ad1288a30627b1c55ecd98f66c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:30:49 +0000

    First commited as before_64683_rev1

--- containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/testutils/swt/ClearConnectionManagerRule.java
+++ containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/testutils/swt/ClearConnectionManagerRule.java
@@ -15,6 +15,9 @@ import java.util.stream.Stream;
 
 import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
 import org.eclipse.linuxtools.internal.docker.core.DefaultDockerConnectionSettingsFinder;
+import org.eclipse.linuxtools.internal.docker.ui.views.DockerExplorerView;
+import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
+import org.eclipse.swtbot.eclipse.finder.widgets.SWTBotView;
 import org.junit.rules.ExternalResource;
 
 /**
@@ -24,10 +27,16 @@ public class ClearConnectionManagerRule extends ExternalResource {
 
 	@Override
 	protected void after() {
-		Stream.of(DockerConnectionManager.getInstance().getConnections())
-				.forEach(c -> DockerConnectionManager.getInstance().removeConnection(c));
-		DockerConnectionManagerUtils.configureConnectionManager();
+		final SWTWorkbenchBot bot = new SWTWorkbenchBot();
+		final SWTBotView dockerExplorerViewBot = bot.viewById("org.eclipse.linuxtools.docker.ui.dockerExplorerView");
+		final DockerExplorerView dockerExplorerView = (DockerExplorerView) (dockerExplorerViewBot.getViewReference()
+				.getView(true));
+		SWTUtils.syncExec(() -> {
+			Stream.of(DockerConnectionManager.getInstance().getConnections())
+					.forEach(c -> DockerConnectionManager.getInstance().removeConnection(c));
+			dockerExplorerView.getCommonViewer().refresh(true);
+			dockerExplorerView.showConnectionsOrExplanations();
+		});
 		DockerConnectionManager.getInstance().setConnectionSettingsFinder(new DefaultDockerConnectionSettingsFinder());
 	}
-
 }
