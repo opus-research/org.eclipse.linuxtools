53ff3d80490ef77d229d6f74eb68e00ac1ab3592
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:25:09 +0000

    First commited as before_32920_rev1

--- profiling/org.eclipse.linuxtools.profiling.ui/src/org/eclipse/linuxtools/profiling/ui/TitleAreaDialogWithRadioButtons.java
+++ profiling/org.eclipse.linuxtools.profiling.ui/src/org/eclipse/linuxtools/profiling/ui/TitleAreaDialogWithRadioButtons.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Red Hat Inc.
+ *    Red Hat Inc. :  Initial Implementation
  *******************************************************************************/
 package org.eclipse.linuxtools.profiling.ui;
 
