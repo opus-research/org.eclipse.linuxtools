de485f8302f8c937e90b0056513ca7e4db620eba
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:52:02 +0000

    First commited as before_31392_rev5

--- lttng/org.eclipse.linuxtools.lttng2.control.ui/src/org/eclipse/linuxtools/internal/lttng2/control/ui/views/messages/Messages.java
+++ lttng/org.eclipse.linuxtools.lttng2.control.ui/src/org/eclipse/linuxtools/internal/lttng2/control/ui/views/messages/Messages.java
@@ -45,7 +45,6 @@ public final class Messages extends NLS {
     public static String TraceControl_UnexpectedCommandOutputFormat;
     public static String TraceControl_UnexpectedNameError;
     public static String TraceControl_UnexpectedPathError;
-    public static String TraceControl_UnexpectedNumberOfElementError;
 
     public static String TraceControl_UnsupportedVersionError;
     public static String TraceControl_GettingVersionError;
