8725aff67022773394acc711d9403cc8b5ee6794
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:40:44 +0000

    First commited as before_33716_rev5

--- lttng/org.eclipse.linuxtools.statesystem.core/src/org/eclipse/linuxtools/statesystem/core/statevalue/LongStateValue.java
+++ lttng/org.eclipse.linuxtools.statesystem.core/src/org/eclipse/linuxtools/statesystem/core/statevalue/LongStateValue.java
@@ -64,14 +64,4 @@ final class LongStateValue extends TmfStateValue {
     public long unboxLong() {
         return value;
     }
-
-    @Override
-    public int compareTo(ITmfStateValue object) {
-        if (!(object instanceof LongStateValue)) {
-            return -1;
-        }
-        LongStateValue other = (LongStateValue) object;
-        return Long.compare(this.value, other.value);
-    }
-
 }
