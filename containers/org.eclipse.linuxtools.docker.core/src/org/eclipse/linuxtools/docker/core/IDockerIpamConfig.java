506237e66347be7cbddb4a73af4f22a6b46d90a3
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:47:54 +0000

    First commited as before_72654_rev1

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerIpamConfig.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerIpamConfig.java
@@ -10,57 +10,16 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.docker.core;
 
-/**
- * IP Address Management Configuration
- * 
- * Used to create a docker client IPAM
- * 
- * @author jjohnstn
- *
- */
 public interface IDockerIpamConfig {
-	/**
-	 * Get subnet
-	 * 
-	 * @return String containing subnet
-	 */
-	String subnet();
+	public String subnet();
 
-	/**
-	 * Set subnet
-	 * 
-	 * @param subnet
-	 *            subnet string
-	 */
-	void subnet(final String subnet);
+	public void subnet(final String subnet);
 
-	/**
-	 * Get ip range
-	 * 
-	 * @return String containing ip range
-	 */
-	String ipRange();
+	public String ipRange();
 
-	/**
-	 * Set ip range
-	 * 
-	 * @param ipRange
-	 *            ip range string
-	 */
-	void ipRange(final String ipRange);
+	public void ipRange(final String ipRange);
 
-	/**
-	 * Get gateway
-	 * 
-	 * @return String containing gateway
-	 */
-	String gateway();
+	public String gateway();
 
-	/**
-	 * Set gateway
-	 * 
-	 * @param gateway
-	 *            string containing gateway
-	 */
-	void gateway(final String gateway);
+	public void gateway(final String gateway);
 }
--- systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/structures/tparsers/ProbeParser.java
+++ systemtap/org.eclipse.linuxtools.systemtap.ui.ide/src/org/eclipse/linuxtools/internal/systemtap/ui/ide/structures/tparsers/ProbeParser.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2006, 2016 IBM Corporation and others.
+ * Copyright (c) 2006,2012 IBM Corporation.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -382,14 +382,14 @@ public final class ProbeParser extends TreeTapsetParser {
             }
         }
         // Run stap on each discovered probe to obtain their variable information.
-        for (Map.Entry<String, ArrayList<String>> entry : fileToItemMap.entrySet()) {
-            for (String newitem : entry.getValue()) {
+        for (String file : fileToItemMap.keySet()) {
+            for (String newitem : fileToItemMap.get(file)) {
                 if (canceled || monitor.isCanceled()) {
                     canceled = true;
                     break;
                 }
                 addSingleProbeAlias(runStap(new String[]{"-L"}, newitem, false), //$NON-NLS-1$
-                        aliases, null, null, entry.getKey());
+                        aliases, null, null, file);
             }
         }
 
