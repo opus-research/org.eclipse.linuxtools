1073c7b0dfcf768ea8baadd0195532c4618514ad
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:50:30 +0000

    First commited as before_49479_rev1

--- profiling/org.eclipse.linuxtools.remote.proxy.tests/resources/localTestProject/main.c
+++ profiling/org.eclipse.linuxtools.remote.proxy.tests/resources/localTestProject/main.c
@@ -1,14 +1,9 @@
-/*******************************************************************************
- * Copyright (c) 2015 IBM Corporation.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+/*
+ * main.c
  *
- * Contributors:
- *     Wainer dos Santos Moschetta (IBM Corporation) - initial implementation
- *******************************************************************************/
-
+ *  Created on: May 27, 2015
+ *      Author: wainersm
+ */
 #include <stdio.h>
 int main() {
 	printf("Hello local test project\n");
