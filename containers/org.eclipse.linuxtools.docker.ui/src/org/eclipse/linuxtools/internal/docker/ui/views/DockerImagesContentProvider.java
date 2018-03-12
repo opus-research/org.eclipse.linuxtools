437e72cd82ef635b5297981fc19f2eddc5d207ce
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:53:29 +0000

    First commited as before_74113_rev1

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerImagesContentProvider.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerImagesContentProvider.java
@@ -11,8 +11,6 @@
 
 package org.eclipse.linuxtools.internal.docker.ui.views;
 
-import java.util.List;
-
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
@@ -21,7 +19,6 @@ import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.viewers.Viewer;
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
-import org.eclipse.linuxtools.docker.core.IDockerImage;
 import org.eclipse.linuxtools.internal.docker.core.DockerConnection;
 import org.eclipse.swt.widgets.Display;
 
@@ -49,11 +46,7 @@ public class DockerImagesContentProvider implements ITreeContentProvider{
 		if(inputElement instanceof IDockerConnection) {
 			final IDockerConnection connection = (IDockerConnection)inputElement;
 			if (connection.isImagesLoaded()) {
-				final List<IDockerImage> images = connection.getImages();
-				if(images == null) {
-					return EMPTY;
-				}
-				return images.toArray();
+				return connection.getImages().toArray();
 			}
 			loadImages(connection);
 			return EMPTY;
