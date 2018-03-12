2e0ef83cd0fec6e15735102ef9057f333f399d29
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:47:04 +0000

    First commited as before_49107_rev8

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerImagesView.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerImagesView.java
@@ -489,8 +489,7 @@ public class DockerImagesView extends ViewPart implements IDockerImageListener,
 		}
 		int index = 0;
 		for (int i = 0; i < connections.length; ++i) {
-			if (connections[i].getUri() != null
-					&& connections[i].getUri().equals(currUri))
+			if (connections[i].getUri().equals(currUri))
 				index = i;
 		}
 		if (type == IDockerConnectionManagerListener.RENAME_EVENT) {
