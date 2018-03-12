234f39aa9f7ac49c0dee30561d57362664d9dd9a
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:40:31 +0000

    First commited as after_30889_rev1

--- lttng/org.eclipse.linuxtools.btf.core/src/org/eclipse/linuxtools/btf/core/trace/BtfTrace.java
+++ lttng/org.eclipse.linuxtools.btf.core/src/org/eclipse/linuxtools/btf/core/trace/BtfTrace.java
@@ -60,6 +60,7 @@ import com.google.common.collect.ImmutableMap;
  */
 public class BtfTrace extends TmfTrace implements ITmfEventParser, ITmfPersistentlyIndexable, ITmfTraceProperties, AutoCloseable {
 
+    private static final char CR = '\n';
     private static final String VERSION = "#version"; //$NON-NLS-1$
     private static final String CREATOR = "#creator"; //$NON-NLS-1$
     private static final String CREATIONDATE = "#creationDate"; //$NON-NLS-1$
@@ -93,17 +94,17 @@ public class BtfTrace extends TmfTrace implements ITmfEventParser, ITmfPersisten
     private final Map<BtfEventType, String> fEntityTypeTable = new HashMap<>();
     private final Map<Integer, BtfEventType> fEntityTypes = new TreeMap<>();
 
-    private String fVersion;
-    private String fCreator;
-    private String fCreationDate;
-    private String fInputFile;
+    private transient String fVersion;
+    private transient String fCreator;
+    private transient String fCreationDate;
+    private transient String fInputFile;
     // default unit is ns
-    private BtfTimstampFormat fTsFormat = BtfTimstampFormat.NS;
+    private transient BtfTimstampFormat fTsFormat = BtfTimstampFormat.NS;
 
     private File fFile;
-    private RandomAccessFile fFileInput;
-    private long fDataOffset;
-    private long fTsOffset = 0;
+    private transient RandomAccessFile fFileInput;
+    private transient long fDataOffset;
+    private transient long fTsOffset = 0;
 
     /**
      * Default constructor
@@ -304,7 +305,7 @@ public class BtfTrace extends TmfTrace implements ITmfEventParser, ITmfPersisten
             long pos = Math.round(ratio * fFile.length()) - fDataOffset;
             while (pos > 0) {
                 fFileInput.seek(pos - 1);
-                if (fFileInput.read() == '\n') {
+                if (fFileInput.read() == CR) {
                     break;
                 }
                 pos--;
