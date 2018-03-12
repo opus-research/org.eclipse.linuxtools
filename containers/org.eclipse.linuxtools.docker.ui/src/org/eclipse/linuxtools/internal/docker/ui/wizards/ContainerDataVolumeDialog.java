1d01e202091cfbee928ddd3ba500e52ecadf997c
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:55:14 +0000

    First commited as before_74626_rev2

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerDataVolumeDialog.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/wizards/ContainerDataVolumeDialog.java
@@ -396,10 +396,11 @@ public class ContainerDataVolumeDialog extends Dialog {
 			final Label errorMessageIcon, final Label errorMessageLabel) {
 
 		return event -> {
-			final IStatus status = validateInput();
-			if (Display.getCurrent() == null) {
+			// skip if dialog has been closed
+			if (Display.getCurrent() == null || getShell().isDisposed()) {
 				return;
 			}
+			final IStatus status = validateInput();
 			Display.getCurrent().syncExec(() -> {
 				if (status.isOK()) {
 					errorMessageIcon.setVisible(false);
@@ -464,11 +465,7 @@ public class ContainerDataVolumeDialog extends Dialog {
 	}
 
 	private void setOkButtonEnabled(final boolean enabled) {
-		final Button okButton = getButton(IDialogConstants.OK_ID);
-		// skip if 'OK' button does not exist yet.
-		if (okButton != null) {
-			okButton.setEnabled(enabled);
-		}
+		getButton(IDialogConstants.OK_ID).setEnabled(enabled);
 	}
 
 }
