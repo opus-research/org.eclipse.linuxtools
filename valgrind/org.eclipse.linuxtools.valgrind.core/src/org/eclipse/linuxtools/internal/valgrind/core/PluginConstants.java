70566d520a11afbf05a194a490d60ca70de53523
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:23:00 +0000

    First commited as before_62843_rev4

--- valgrind/org.eclipse.linuxtools.valgrind.core/src/org/eclipse/linuxtools/internal/valgrind/core/PluginConstants.java
+++ valgrind/org.eclipse.linuxtools.valgrind.core/src/org/eclipse/linuxtools/internal/valgrind/core/PluginConstants.java
@@ -1,23 +1,16 @@
 /*******************************************************************************
- * Copyright (c) 2011 IBM Corporation
+ * Copyright (c) 2009 Red Hat, Inc.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Rafael M Teixeira <rafaelmt@linux.vnet.ibm.com> - initial API and implementation
+ *    Elliott Baron <ebaron@redhat.com> - initial API and implementation
  *******************************************************************************/
-
 package org.eclipse.linuxtools.internal.valgrind.core;
 
-import org.eclipse.debug.core.ILaunch;
-import org.eclipse.linuxtools.valgrind.core.IValgrindMessage;
-
-public class ValgrindInfo extends AbstractValgrindMessage {
-
-    public ValgrindInfo(IValgrindMessage parent, String text, ILaunch launch) {
-        super(parent, text, launch);
-    }
-
+public interface PluginConstants {
+    // IDs for important plugins
+    String CORE_PLUGIN_ID = "org.eclipse.linuxtools.valgrind.core"; //$NON-NLS-1$
 }
