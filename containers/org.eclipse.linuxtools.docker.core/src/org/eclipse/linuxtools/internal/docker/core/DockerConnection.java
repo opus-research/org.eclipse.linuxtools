cb82cf4787b421151830491786350d98e510c5d0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:43:00 +0000

    First commited as after_47979_rev1

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
@@ -25,7 +25,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.ByteBuffer;
 import java.nio.channels.WritableByteChannel;
@@ -330,26 +329,19 @@ public class DockerConnection implements IDockerConnection {
 		}
 
 		public Builder unixSocket(String unixSocketPath) {
-			try {
-				if (unixSocketPath != null
-						&& new URI(unixSocketPath).getScheme() == null) {
-					unixSocketPath = "unix://" + unixSocketPath;
-				}
-			} catch (URISyntaxException e) {
+			if (unixSocketPath != null && !unixSocketPath.startsWith("unix://")) {
+				unixSocketPath = "unix://" + unixSocketPath;
 			}
 			this.unixSocketPath = unixSocketPath;
 			return this;
 		}
 
 		public Builder tcpHost(String tcpHost) {
-			try {
-				if (tcpHost != null) {
-					if (new URI(tcpHost).getScheme() == null) {
-						tcpHost = "tcp://" + tcpHost;
-					}
-					this.tcpHost = tcpHost.replace("tcp://", "http://");
+			if (tcpHost != null) {
+				if (!tcpHost.startsWith("tcp://")) { //$NON-NLS-1$
+					tcpHost = "tcp://" + tcpHost;
 				}
-			} catch (URISyntaxException e) {
+				this.tcpHost = tcpHost.replace("tcp://", "http://");
 			}
 			return this;
 		}
