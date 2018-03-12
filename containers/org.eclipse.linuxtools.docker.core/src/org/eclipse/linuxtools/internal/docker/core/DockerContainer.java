c8e836cc5a9c8be070d9efcd0a2a8753750ad35e
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:16:49 +0000

    First commited as before_62082_rev2

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerContainer.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerContainer.java
@@ -18,7 +18,6 @@ import org.eclipse.linuxtools.docker.core.IDockerContainer;
 import org.eclipse.linuxtools.docker.core.IDockerContainerInfo;
 import org.eclipse.linuxtools.docker.core.IDockerPortMapping;
 
-import com.spotify.docker.client.DockerClient;
 import com.spotify.docker.client.messages.Container;
 
 public class DockerContainer implements IDockerContainer {
@@ -35,17 +34,8 @@ public class DockerContainer implements IDockerContainer {
 	private Long sizeRootFs;
 	private IDockerContainerInfo containerInfo;
 
-	/**
-	 * Constructor.
-	 * 
-	 * @param connection
-	 *            the Docker connection
-	 * @param container
-	 *            the underlying {@link Container} data returned by the
-	 *            {@link DockerClient}
-	 */
 	public DockerContainer(final IDockerConnection connection,
-			final Container container) {
+			Container container) {
 		this.parent = connection;
 		this.id = container.id();
 		this.image = container.image();
@@ -70,26 +60,6 @@ public class DockerContainer implements IDockerContainer {
 			ports.add(portMapping);
 		}
 		// TODO: include volumes
-	}
-
-	/**
-	 * Constructor.
-	 * 
-	 * @param connection
-	 *            the Docker connection
-	 * @param container
-	 *            the underlying {@link Container} data returned by the
-	 *            {@link DockerClient}
-	 * @param containerInfo
-	 *            the {@link IDockerContainerInfo} that was previously retrieved
-	 *            for this {@link IDockerContainer}, assuming it did not change
-	 *            in the mean time.
-	 */
-	public DockerContainer(final IDockerConnection connection,
-			final Container container,
-			final IDockerContainerInfo containerInfo) {
-		this(connection, container);
-		this.containerInfo = containerInfo;
 	}
 
 	@Override
