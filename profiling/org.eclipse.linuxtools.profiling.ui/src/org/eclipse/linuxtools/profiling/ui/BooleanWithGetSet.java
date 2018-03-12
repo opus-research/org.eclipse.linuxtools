310a50b3555d7d050f171a1d9264fb476efdd8ca
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:32:46 +0000

    First commited as before_33206_rev1

--- profiling/org.eclipse.linuxtools.profiling.ui/src/org/eclipse/linuxtools/profiling/ui/BooleanWithGetSet.java
+++ profiling/org.eclipse.linuxtools.profiling.ui/src/org/eclipse/linuxtools/profiling/ui/BooleanWithGetSet.java
@@ -6,7 +6,7 @@
  * http://www.eclipse.org/legal/epl-v10.html
  *
  * Contributors:
- *    Red Hat Inc.
+ *    lufimtse :  Leo Ufimtsev lufimtse@redhat.com
  *******************************************************************************/
 
 package org.eclipse.linuxtools.profiling.ui;
@@ -19,7 +19,7 @@ package org.eclipse.linuxtools.profiling.ui;
  * upon a way to set/get a boolean (it is used to transfer a boolean between synced threads). <br>
  * This class was created for this purpose.
  * </p>
- * @since 3.1
+ * @since 3.1.0
  */
 public class BooleanWithGetSet {
     private boolean val;
