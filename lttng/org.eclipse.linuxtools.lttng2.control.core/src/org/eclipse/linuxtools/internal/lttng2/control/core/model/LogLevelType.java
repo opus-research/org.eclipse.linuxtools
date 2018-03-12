28ef462f922f86f31facd6d1de18d8fd970b0760
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:41:37 +0000

    First commited as before_30975_rev4

--- lttng/org.eclipse.linuxtools.lttng2.control.core/src/org/eclipse/linuxtools/internal/lttng2/control/core/model/LogLevelType.java
+++ lttng/org.eclipse.linuxtools.lttng2.control.core/src/org/eclipse/linuxtools/internal/lttng2/control/core/model/LogLevelType.java
@@ -24,16 +24,13 @@ public enum LogLevelType {
     // Enum definition
     // ------------------------------------------------------------------------
     /** range of log levels [0,logLevel] */
-    LOGLEVEL("<=", "RANGE"), //$NON-NLS-1$ //$NON-NLS-2$
-
-    /** all log level */
-    LOGLEVEL_ALL(":", "ALL"),  //$NON-NLS-1$//$NON-NLS-2$
+    LOGLEVEL("<="), //$NON-NLS-1$
 
     /** single log level */
-    LOGLEVEL_ONLY("==", "SINGLE"), //$NON-NLS-1$ //$NON-NLS-2$
+    LOGLEVEL_ONLY("=="), //$NON-NLS-1$
 
     /** no log level */
-    LOGLEVEL_NONE("", "UNKNOWN"); //$NON-NLS-1$ //$NON-NLS-2$
+    LOGLEVEL_NONE(""); //$NON-NLS-1$
 
     // ------------------------------------------------------------------------
     // Constuctors
@@ -43,9 +40,8 @@ public enum LogLevelType {
      * Private constructor
      * @param name the name of state
      */
-    private LogLevelType(String shortName, String miName) {
+    private LogLevelType(String shortName) {
         fShortName = shortName;
-        fMiName = miName;
     }
 
      // ------------------------------------------------------------------------
@@ -55,7 +51,6 @@ public enum LogLevelType {
      * Name of enum.
      */
     private final String fShortName;
-    private final String fMiName;
 
     // ------------------------------------------------------------------------
     // Accessors
@@ -67,34 +62,4 @@ public enum LogLevelType {
         return fShortName;
     }
 
-    /**
-     * @return machine interface name string
-     */
-    public String getMiName() {
-        return fMiName;
-    }
-
-    // ------------------------------------------------------------------------
-    // Utility
-    // ------------------------------------------------------------------------
-    /**
-     * Return the corresponding {@link LogLevelType} to String "name"
-     * @param name String to compare to retrieve the good LogLevelType
-     * @return the corresponding {@link LogLevelType}
-     */
-    public static LogLevelType valueOfString(String name) {
-        if (name == null) {
-            throw new NullPointerException();
-        }
-        for (LogLevelType lltype : LogLevelType.values()) {
-            boolean isEqual = lltype.fShortName.equalsIgnoreCase(name) || lltype.fMiName.equalsIgnoreCase(name);
-            if (isEqual) {
-                return lltype;
-            }
-        }
-
-        // No match
-        // Should this throw and exception or log it to the activator ?
-        return LogLevelType.LOGLEVEL_NONE;
-    }
 }
--- lttng/org.eclipse.linuxtools.lttng2.control.ui/src/org/eclipse/linuxtools/internal/lttng2/control/ui/views/service/LttngVersion.java
+++ lttng/org.eclipse.linuxtools.lttng2.control.ui/src/org/eclipse/linuxtools/internal/lttng2/control/ui/views/service/LttngVersion.java
@@ -1,5 +1,5 @@
 /**********************************************************************
- * Copyright (c) 2013 Ericsson
+ * Copyright (c) 2013, 2014 Ericsson
  *
  * All rights reserved. This program and the accompanying materials are
  * made available under the terms of the Eclipse Public License v1.0 which
@@ -8,14 +8,16 @@
  *
  * Contributors:
  *   Bernd Hufmann - Initial API and implementation
+ *   Jonathan Rajotte - Machine interface support and new information
  **********************************************************************/
+
 package org.eclipse.linuxtools.internal.lttng2.control.ui.views.service;
 
 import org.osgi.framework.Version;
 
 /**
- * A version implementation with a special compareTo implementation
- * to bypass problems of older implementation of org.osgi.framework.Version.
+ * A version implementation with a special compareTo implementation to bypass
+ * problems of older implementation of org.osgi.framework.Version.
  *
  * @author Bernd Hufmann
  */
@@ -32,7 +34,7 @@ public class LttngVersion extends Version {
      * Constructor
      *
      * @param version
-     *      The version string
+     *            The version string
      */
     public LttngVersion(String version) {
         super(version);
@@ -45,15 +47,24 @@ public class LttngVersion extends Version {
     }
 
     /**
-     * @param major major version number
-     * @param minor minor version number
-     * @param micro micro version number
-     * @param license licence text of LTTng
-     * @param commit current git commit information about LTTng
-     * @param name name of the version
-     * @param description description of the version
-     * @param url url to website
-     * @param fullVersion complete string representation of the version
+     * @param major
+     *            major version number
+     * @param minor
+     *            minor version number
+     * @param micro
+     *            micro version number
+     * @param license
+     *            licence text of LTTng
+     * @param commit
+     *            current git commit information about LTTng
+     * @param name
+     *            name of the version
+     * @param description
+     *            description of the version
+     * @param url
+     *            url to website
+     * @param fullVersion
+     *            complete string representation of the version
      */
     public LttngVersion(int major, int minor, int micro, String license, String commit, String name, String description, String url, String fullVersion) {
         super(major, minor, micro);
@@ -66,11 +77,12 @@ public class LttngVersion extends Version {
     }
 
     /**
-     * Special compareTo method to fix problem of older implementations of org.osgi.framework.Version
-     * where {@code Version.compareTo} takes an {@code Object} instead a {@code Version} as argument.
+     * Special compareTo method to fix problem of older implementations of
+     * org.osgi.framework.Version where {@code Version.compareTo} takes an
+     * {@code Object} instead a {@code Version} as argument.
      *
      * @param other
-     *      - Other version to compare
+     *            - Other version to compare
      * @return a negative integer, zero, or a positive integer if this version
      *         is less than, equal to, or greater than the specified
      *         {@code LttngVersion} object.
