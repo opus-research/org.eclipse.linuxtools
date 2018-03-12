be03b74a60a733b140244fa6fd9a8458452ad392
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:31:29 +0000

    First commited as before_97157_rev10

--- containers/org.eclipse.linuxtools.docker.integration.tests/src/org/eclipse/linuxtools/docker/integration/tests/image/AbstractImageBotTest.java
+++ containers/org.eclipse.linuxtools.docker.integration.tests/src/org/eclipse/linuxtools/docker/integration/tests/image/AbstractImageBotTest.java
@@ -34,7 +34,7 @@ import org.jboss.reddeer.eclipse.ui.console.ConsoleView;
 import org.jboss.reddeer.eclipse.ui.views.properties.PropertiesView;
 import org.jboss.reddeer.jface.preference.PreferenceDialog;
 import org.jboss.reddeer.swt.exception.SWTLayerException;
-import org.jboss.reddeer.swt.impl.button.PushButton;
+import org.jboss.reddeer.swt.impl.button.OkButton;
 import org.jboss.reddeer.swt.impl.shell.DefaultShell;
 import org.jboss.reddeer.workbench.ui.dialogs.WorkbenchPreferenceDialog;
 import org.junit.After;
@@ -128,7 +128,7 @@ public class AbstractImageBotTest extends AbstractDockerBotTest {
 		} catch (SWTLayerException e) {
 			new DefaultShell("Preferences").setFocus();
 		}
-		new PushButton("Apply and Close").click();
+		new OkButton().click();
 	}
 
 	protected void deleteRegister(String serverAddress) {
@@ -138,7 +138,7 @@ public class AbstractImageBotTest extends AbstractDockerBotTest {
 		dialog.select(page);
 		page.removeRegistry(serverAddress);
 		new WaitWhile(new JobIsRunning());
-		new PushButton("Apply and Close").click();
+		new OkButton().click();
 	}
 
 	protected void deleteRegisterIfExists(String serverAddress) {
