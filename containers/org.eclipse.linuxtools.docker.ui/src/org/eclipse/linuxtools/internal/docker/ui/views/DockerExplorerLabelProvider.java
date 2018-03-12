2c4919562356cafc70839bf40df10ae155d8b551
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:51:38 +0000

    First commited as before_73747_rev6

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerLabelProvider.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerLabelProvider.java
@@ -66,7 +66,7 @@ public class DockerExplorerLabelProvider implements IStyledLabelProvider, ILabel
 	@Override
 	public Image getImage(final Object element) {
 		if(element instanceof IDockerConnection) {
-			if (((IDockerConnection) element).isOpen()) {
+			if (((IDockerConnection) element).isActive()) {
 				return SWTImagesFactory.DESC_REPOSITORY_MIDDLE.createImage();
 			} else {
 				return SWTImagesFactory.DESC_REPOSITORY_MIDDLED.createImage();
