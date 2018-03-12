3cc556ed4321c54239a72706c24b755fa58a83e0
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 13:40:35 +0000

    First commited as before_30903_rev1

--- lttng/org.eclipse.linuxtools.btf.core/src/org/eclipse/linuxtools/btf/core/trace/BtfTrace.java
+++ lttng/org.eclipse.linuxtools.btf.core/src/org/eclipse/linuxtools/btf/core/trace/BtfTrace.java
@@ -93,17 +93,17 @@ public class BtfTrace extends TmfTrace implements ITmfEventParser, ITmfPersisten
     private final Map<BtfEventType, String> fEntityTypeTable = new HashMap<>();
     private final Map<Integer, BtfEventType> fEntityTypes = new TreeMap<>();
 
-    private transient String fVersion;
-    private transient String fCreator;
-    private transient String fCreationDate;
-    private transient String fInputFile;
+    private String fVersion;
+    private String fCreator;
+    private String fCreationDate;
+    private String fInputFile;
     // default unit is ns
-    private transient BtfTimstampFormat fTsFormat = BtfTimstampFormat.NS;
+    private BtfTimstampFormat fTsFormat = BtfTimstampFormat.NS;
 
     private File fFile;
-    private transient RandomAccessFile fFileInput;
-    private transient long fDataOffset;
-    private transient long fTsOffset = 0;
+    private RandomAccessFile fFileInput;
+    private long fDataOffset;
+    private long fTsOffset = 0;
 
     /**
      * Default constructor
@@ -304,7 +304,7 @@ public class BtfTrace extends TmfTrace implements ITmfEventParser, ITmfPersisten
             long pos = Math.round(ratio * fFile.length()) - fDataOffset;
             while (pos > 0) {
                 fFileInput.seek(pos - 1);
-                if(fFileInput.readLine().isEmpty()){
+                if (fFileInput.read() == '\n') {
                     break;
                 }
                 pos--;
