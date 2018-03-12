578691d7dd0d77899d285f3afdf335b901e202e4
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:44:58 +0000

    First commited as before_112487_rev2

--- gprof/org.eclipse.linuxtools.gprof/src/org/eclipse/linuxtools/internal/gprof/parser/GmonDecoder.java
+++ gprof/org.eclipse.linuxtools.gprof/src/org/eclipse/linuxtools/internal/gprof/parser/GmonDecoder.java
@@ -62,7 +62,7 @@ public class GmonDecoder {
     private final IProject project;
 
     // for dump
-	private boolean shouldDump = false;
+    private boolean shouldDump = false;
 
     /**
      * Constructor
@@ -86,7 +86,7 @@ public class GmonDecoder {
         this.project = project;
         program.getBinaryParser().getFormat();
         String cpu = program.getCPU();
-		if (Platform.ARCH_X86_64.equals(cpu) || "aarch64".equals(cpu) || "ppc64".equals(cpu)) { //$NON-NLS-1$
+        if (Platform.ARCH_X86_64.equals(cpu) || "ppc64".equals(cpu)) { //$NON-NLS-1$
             histo = new HistogramDecoder64(this);
             callGraph = new CallGraphDecoder64(this);
             _32_bit_platform = false;
@@ -190,7 +190,6 @@ public class GmonDecoder {
             switch (tag) {
             case VPF_GMON_RECORD_TYPE_HISTOGRAM:
                 histo.decodeHeader(stream);
-				histo.printHistHeader(System.out);
                 histo.decodeHistRecord(stream);
                 break;
             case VPF_GMON_RECORD_TYPE_CALLGRAPH:
