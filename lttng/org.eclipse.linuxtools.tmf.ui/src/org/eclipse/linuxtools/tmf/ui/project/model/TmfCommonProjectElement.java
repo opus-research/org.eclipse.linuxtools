7832a69d434212a565b33107d2285b4fe5ff455b
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:11:16 +0000

    First commited as before_32279_rev8

--- lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/model/TmfCommonProjectElement.java
+++ lttng/org.eclipse.linuxtools.tmf.ui/src/org/eclipse/linuxtools/tmf/ui/project/model/TmfCommonProjectElement.java
@@ -36,6 +36,10 @@ import org.eclipse.linuxtools.internal.tmf.ui.Activator;
 import org.eclipse.linuxtools.tmf.core.TmfCommonConstants;
 import org.eclipse.linuxtools.tmf.core.analysis.IAnalysisModuleHelper;
 import org.eclipse.linuxtools.tmf.core.analysis.TmfAnalysisManager;
+import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomTxtTrace;
+import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomTxtTraceDefinition;
+import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomXmlTrace;
+import org.eclipse.linuxtools.tmf.core.parsers.custom.CustomXmlTraceDefinition;
 import org.eclipse.linuxtools.tmf.core.project.model.TmfTraceType;
 import org.eclipse.linuxtools.tmf.core.project.model.TraceTypeHelper;
 import org.eclipse.linuxtools.tmf.core.signal.TmfSignalHandler;
@@ -110,7 +114,22 @@ public abstract class TmfCommonProjectElement extends TmfProjectModelElement {
 
         Class<? extends ITmfTrace> traceClass = null;
 
-        if (helper != null) {
+        if (helper == null && getTraceType() != null) {
+            if (fTraceTypeId.startsWith(CustomTxtTrace.class.getCanonicalName())) {
+                for (CustomTxtTraceDefinition def : CustomTxtTraceDefinition.loadAll()) {
+                    if (fTraceTypeId.equals(CustomTxtTrace.class.getCanonicalName() + ":" + def.definitionName)) { //$NON-NLS-1$
+                        traceClass = CustomTxtTrace.class;
+                    }
+                }
+            }
+            if (fTraceTypeId.startsWith(CustomXmlTrace.class.getCanonicalName())) {
+                for (CustomXmlTraceDefinition def : CustomXmlTraceDefinition.loadAll()) {
+                    if (fTraceTypeId.equals(CustomXmlTrace.class.getCanonicalName() + ":" + def.definitionName)) { //$NON-NLS-1$
+                        traceClass = CustomTxtTrace.class;
+                    }
+                }
+            }
+        } else if (helper != null) {
             traceClass = helper.getTraceClass();
         }
 
@@ -161,12 +180,12 @@ public abstract class TmfCommonProjectElement extends TmfProjectModelElement {
     }
 
     /**
-     * Refreshes the trace type field by reading the trace type persistent
-     * property of the resource.
+     * Refreshes the trace type filed by reading the trace type persistent
+     * property of the resource referenece.
      */
     public void refreshTraceType() {
         try {
-            fTraceTypeId = TmfTraceType.getTraceTypeId(getResource());
+            fTraceTypeId = getResource().getPersistentProperty(TmfCommonConstants.TRACETYPE);
         } catch (CoreException e) {
             Activator.getDefault().logError(NLS.bind(Messages.TmfCommonProjectElement_ErrorRefreshingProperty, getName()), e);
         }
@@ -352,10 +371,9 @@ public abstract class TmfCommonProjectElement extends TmfProjectModelElement {
             if (trace instanceof IFolder) {
                 IFolder folderTrace = (IFolder) trace;
                 for (IResource member : folderTrace.members()) {
-                    String traceTypeId = TmfTraceType.getTraceTypeId(member);
-                    if (TmfTrace.class.getCanonicalName().equals(traceTypeId)) {
+                    if (TmfTrace.class.getCanonicalName().equals(member.getPersistentProperty(TmfCommonConstants.TRACETYPE))) {
                         member.delete(true, null);
-                    } else if (TmfExperiment.class.getCanonicalName().equals(traceTypeId)) {
+                    } else if (TmfExperiment.class.getCanonicalName().equals(member.getPersistentProperty(TmfCommonConstants.TRACETYPE))) {
                         member.delete(true, null);
                     }
                 }
