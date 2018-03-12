04bbff96a6a105538464e0bb0e2c5a448702bee2
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:55:04 +0000

    First commited as before_74574_rev1

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerContainersView.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerContainersView.java
@@ -316,8 +316,6 @@ public class DockerContainersView extends ViewPart implements
 		this.viewer.setComparator(comparator);
 		// apply search filter
 		this.viewer.addFilter(getContainersFilter());
-		// default to first active connection or currently selected connection
-		// in Explorer View
 		setConnection(CommandUtils.getCurrentConnection(null));
 		this.viewer.addSelectionChangedListener(onContainerSelection());
 		// get the current selection in the tableviewer
@@ -454,7 +452,7 @@ public class DockerContainersView extends ViewPart implements
 			return;
 		}
 		final Object firstSegment = treeSelection.getPaths()[0].getFirstSegment();
-		if(firstSegment instanceof IDockerConnection) {
+		if (firstSegment instanceof IDockerConnection) {
 			final IDockerConnection connection = (IDockerConnection) firstSegment;
 			setConnection(connection);
 		}
