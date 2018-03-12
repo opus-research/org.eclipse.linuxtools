534c33876e438b0a5d8083dd5ed0803c733e2528
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:59:30 +0000

    First commited as before_53968_rev2

--- changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/internal/changelog/core/ChangeLogPreferencesPage.java
+++ changelog/org.eclipse.linuxtools.changelog.core/src/org/eclipse/linuxtools/internal/changelog/core/ChangeLogPreferencesPage.java
@@ -16,7 +16,6 @@ package org.eclipse.linuxtools.internal.changelog.core;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionPoint;
 import org.eclipse.core.runtime.Platform;
-import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
@@ -29,7 +28,6 @@ import org.eclipse.swt.widgets.List;
 import org.eclipse.swt.widgets.Text;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
-import org.osgi.service.prefs.BackingStoreException;
 
 /**
  * @author pmuldoon (Phil Muldoon)
@@ -229,14 +227,11 @@ public class ChangeLogPreferencesPage extends PreferencePage implements
 
     }
 
+    @SuppressWarnings("deprecation")
     @Override
     public boolean performOk() {
         storeValues();
-        try {
-			InstanceScope.INSTANCE.getNode(ChangelogPlugin.PLUGIN_ID).flush();
-		} catch (BackingStoreException e) {
-			// ignore saving exception
-		}
+        ChangelogPlugin.getDefault().savePluginPreferences();
         return true;
     }
 
