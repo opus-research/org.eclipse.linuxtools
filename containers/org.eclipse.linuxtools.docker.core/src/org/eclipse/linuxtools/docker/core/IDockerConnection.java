3f85e82fdefb2f6c510bacb390e6c1299ede0767
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 16:58:03 +0000

    First commited as before_52990_rev3

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerConnection.java
@@ -174,20 +174,11 @@ public interface IDockerConnection {
 	String buildImage(IPath path, String name, IDockerProgressHandler handler)
 			throws DockerException, InterruptedException;
 
-	@Deprecated
-	String createContainer(IDockerContainerConfig c) throws DockerException,
-			InterruptedException;
-
-	@Deprecated
-	String createContainer(final IDockerContainerConfig c,
-			final String containerName) throws DockerException,
-			InterruptedException;
-
-	String createContainer(IDockerContainerConfig c, IDockerHostConfig hc)
+	String createContainer(IDockerContainerConfig c)
 			throws DockerException, InterruptedException;
 
 	public String createContainer(final IDockerContainerConfig config,
-			final IDockerHostConfig hc, final String containerName)
+			final String containerName)
 					throws DockerException, InterruptedException;
 
 	void stopContainer(String id) throws DockerException, InterruptedException;
@@ -205,7 +196,12 @@ public interface IDockerConnection {
 	void startContainer(String id, OutputStream stream)
 			throws DockerException, InterruptedException;
 
-	void startContainer(String id, String loggingId, OutputStream stream)
+	void startContainer(String id, IDockerHostConfig config,
+			OutputStream stream)
+			throws DockerException, InterruptedException;
+
+	void startContainer(String id, String loggingId, IDockerHostConfig config,
+			OutputStream stream)
 			throws DockerException, InterruptedException;
 
 	void commitContainer(String id, String repo, String tag, String comment,
