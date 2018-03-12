2cb1203c45c6f5079f17c5d2f4ad96888e545418
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:49:48 +0000

    First commited as before_73256_rev4

--- containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/testutils/swt/CloseWizardRule.java
+++ containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/testutils/swt/CloseWizardRule.java
@@ -12,13 +12,8 @@
 package org.eclipse.linuxtools.internal.docker.ui.testutils.swt;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
-import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
-import org.eclipse.linuxtools.internal.docker.core.DefaultDockerConnectionSettingsFinder;
 import org.eclipse.swtbot.eclipse.finder.SWTWorkbenchBot;
 import org.eclipse.swtbot.swt.finder.exceptions.WidgetNotFoundException;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotButton;
-import org.eclipse.swtbot.swt.finder.widgets.SWTBotShell;
-import org.eclipse.ui.PlatformUI;
 import org.junit.rules.ExternalResource;
 
 /**
@@ -30,24 +25,11 @@ public class CloseWizardRule extends ExternalResource {
 	protected void after() {
 		final SWTWorkbenchBot bot = new SWTWorkbenchBot();
 		try {
-			while (isInDialog(bot) && getCancelButton(bot) != null) {
-				getCancelButton(bot).click();
+			while (bot.button(IDialogConstants.CANCEL_LABEL) != null) {
+				bot.button(IDialogConstants.CANCEL_LABEL).click();
 			}
-			DockerConnectionManager.getInstance()
-					.setConnectionSettingsFinder(new DefaultDockerConnectionSettingsFinder());
 		} catch (WidgetNotFoundException e) {
 			// ignoring
 		}
-	}
-
-	private static boolean isInDialog(final SWTWorkbenchBot bot) {
-		final SWTBotShell activeShell = bot.activeShell();
-		final String text = SWTUtils.syncExec(() -> PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell().getText());
-		final String shellText = activeShell.getText();
-		return text != null && !text.equals(shellText);
-	}
-
-	private static SWTBotButton getCancelButton(final SWTWorkbenchBot bot) {
-		return bot.button(IDialogConstants.CANCEL_LABEL);
 	}
 }
--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerView.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerView.java
@@ -282,9 +282,6 @@ public class DockerExplorerView extends CommonNavigator implements
 				getCommonViewer()
 						.setSelection(new StructuredSelection(connection));
 				break;
-			case IDockerConnectionManagerListener.RENAME_EVENT:
-				getCommonViewer().refresh(connection);
-				break;
 			case IDockerConnectionManagerListener.REMOVE_EVENT:
 				unregisterListeners(connection);
 				getCommonViewer().refresh();
