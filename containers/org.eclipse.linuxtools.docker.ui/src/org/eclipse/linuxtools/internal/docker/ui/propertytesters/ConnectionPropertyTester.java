1d1a010d94b3e6f27cdbffd4395ba57add1e2047
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:06:51 +0000

    First commited as after_80315_rev2

--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/propertytesters/ConnectionPropertyTester.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/propertytesters/ConnectionPropertyTester.java
@@ -10,10 +10,13 @@
  *******************************************************************************/
 package org.eclipse.linuxtools.internal.docker.ui.propertytesters;
 
+import java.util.List;
+
 import org.eclipse.core.expressions.PropertyTester;
-import org.eclipse.linuxtools.docker.core.DockerConnectionManager;
+import org.eclipse.linuxtools.docker.core.EnumDockerConnectionState;
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
 import org.eclipse.linuxtools.internal.docker.ui.DockerConnectionWatcher;
+import org.eclipse.linuxtools.internal.docker.ui.views.DockerExplorerContentProvider;
 import org.eclipse.ui.PlatformUI;
 
 public class ConnectionPropertyTester extends PropertyTester {
@@ -21,8 +24,8 @@ public class ConnectionPropertyTester extends PropertyTester {
 	/** Property name to check if an active {@link IDockerConnection} exists. */
 	public static final String EXISTS_ACTIVE_CONNECTION = "existsActiveConnection"; //$NON-NLS-1$
 
-	/** Property name to check if any {@link IDockerConnection} exists. */
-	public static final String EXISTS_ANY_CONNECTION = "existsAnyConnection"; //$NON-NLS-1$
+	/** Property name to check if the selected (parent) {@link IDockerConnection} is open. */
+	public static final String CONNECTION_ESTABLISHED = "connectionIsEstablished"; //$NON-NLS-1$
 
 	@Override
 	public boolean test(Object receiver, String property, Object[] args, Object expectedValue) {
@@ -33,9 +36,29 @@ public class ConnectionPropertyTester extends PropertyTester {
 		switch (property) {
 		case EXISTS_ACTIVE_CONNECTION:
 			return expectedValue.equals(DockerConnectionWatcher.getInstance().getConnection() != null);
-		case EXISTS_ANY_CONNECTION:
-			return expectedValue.equals(
-					DockerConnectionManager.getInstance().hasConnections());
+		case CONNECTION_ESTABLISHED:
+			return isConnectionEstablished(receiver, property, args,
+					expectedValue);
+		}
+		return false;
+	}
+
+	private boolean isConnectionEstablished(Object receiver, String property,
+			Object[] args, Object expectedValue) {
+		if (receiver instanceof List && !((List<?>) receiver).isEmpty()) {
+			return isConnectionEstablished(((List<?>) receiver).get(0),
+					property, args,
+					expectedValue);
+		}
+		if (receiver instanceof DockerExplorerContentProvider.DockerImagesCategory) {
+			return isConnectionEstablished(
+					((DockerExplorerContentProvider.DockerImagesCategory) receiver)
+							.getConnection(),
+					property, args, expectedValue);
+		}
+		if (receiver instanceof IDockerConnection) {
+			return expectedValue.equals(((IDockerConnection) receiver)
+					.getState() == EnumDockerConnectionState.ESTABLISHED);
 		}
 		return false;
 	}
