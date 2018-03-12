368d68768ec95fa08b864bb2a7b39973eaeabdb5
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 18:07:13 +0000

    First commited as before_80529_rev1

--- containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/wizards/NewDockerConnectionSWTBotTest.java
+++ containers/org.eclipse.linuxtools.docker.ui.tests/src/org/eclipse/linuxtools/internal/docker/ui/wizards/NewDockerConnectionSWTBotTest.java
@@ -16,7 +16,6 @@ import java.io.IOException;
 
 import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.wizard.Wizard;
-import org.eclipse.linuxtools.docker.core.EnumDockerConnectionState;
 import org.eclipse.linuxtools.docker.core.IDockerConnection;
 import org.eclipse.linuxtools.internal.docker.core.DockerConnection;
 import org.eclipse.linuxtools.internal.docker.core.SystemUtils;
@@ -92,16 +91,6 @@ public class NewDockerConnectionSWTBotTest {
 		this.addConnectionButton = dockerExplorerViewBot.toolbarButton("&Add Connection");
 	}
 
-	@Before
-	public void clearClipboards() {
-		// Clear all clipboards
-		Display.getDefault().syncExec(() -> {
-			Clipboard clip = new Clipboard(Display.getCurrent());
-			clip.clearContents(DND.CLIPBOARD);
-			clip.clearContents(DND.SELECTION_CLIPBOARD);
-		});
-	}
-
 	private IDockerConnection configureUnixSocketConnection(final String connectionName, final String pathToSocket) {
 		final DockerClient client = MockDockerClientFactory.build();
 		final DockerConnection dockerConnection = MockDockerConnectionFactory.from(connectionName, client)
@@ -113,7 +102,7 @@ public class NewDockerConnectionSWTBotTest {
 	private IDockerConnection configureTCPConnection(final String connectionName, final String host) {
 		final DockerClient client = MockDockerClientFactory.build();
 		final DockerConnection dockerConnection = MockDockerConnectionFactory.from(connectionName, client)
-				.withTCPConnectionSettings(host, null, EnumDockerConnectionState.ESTABLISHED);
+				.withTCPConnectionSettings(host, null);
 		DockerConnectionManagerUtils.configureConnectionManager(dockerConnection);
 		return dockerConnection;
 	}
@@ -214,7 +203,8 @@ public class NewDockerConnectionSWTBotTest {
 	public void shouldNotAllowNewConnectionWithDifferentNameAndSameUnixSocketSettings() throws IOException {
 		// given
 		final String dockerSocketTmpPath = File.createTempFile("docker", ".sock").getAbsolutePath();
-		MockDockerConnectionSettingsFinder.validUnixSocketConnectionAvailable("Mock", "unix://" + dockerSocketTmpPath);
+		MockDockerConnectionSettingsFinder.validUnixSocketConnectionAvailable("Mock",
+				"unix://" + dockerSocketTmpPath);
 		// add an existing connection based on the settings above
 		configureUnixSocketConnection("Mock", dockerSocketTmpPath);
 		// when open wizard
@@ -248,7 +238,8 @@ public class NewDockerConnectionSWTBotTest {
 		// given
 		final String dockerSocketTmpPath = File.createTempFile("docker", ".sock").getAbsolutePath();
 		configureUnixSocketConnection("Bar", dockerSocketTmpPath);
-		MockDockerConnectionSettingsFinder.validUnixSocketConnectionAvailable("Mock", "unix://" + dockerSocketTmpPath);
+		MockDockerConnectionSettingsFinder.validUnixSocketConnectionAvailable("Mock",
+				"unix://" + dockerSocketTmpPath);
 		final String otherDockerSocketTmpPath = File.createTempFile("docker", ".sock").getAbsolutePath();
 		// when open wizard
 		addConnectionButton.click();
@@ -295,6 +286,13 @@ public class NewDockerConnectionSWTBotTest {
 	}
 
 	private void verifyPopulateConnectionWithClipboard(final int clipboardType) {
+		// Clear all clipboards
+		Display.getDefault().syncExec(() -> {
+			Clipboard clip = new Clipboard(Display.getCurrent());
+			clip.clearContents(DND.CLIPBOARD);
+			clip.clearContents(DND.SELECTION_CLIPBOARD);
+		});
+
 		// given
 		final String[] connectionData = new String[] {
 				"DOCKER_HOST=https://1.2.3.4:1234 DOCKER_CERT_PATH=/path/to/certs DOCKER_TLS_VERIFY=1" };
--- containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerView.java
+++ containers/org.eclipse.linuxtools.docker.ui/src/org/eclipse/linuxtools/internal/docker/ui/views/DockerExplorerView.java
@@ -106,6 +106,7 @@ public class DockerExplorerView extends CommonNavigator implements
 	@Override
 	public String getContributorId() {
 		return "org.eclipse.linuxtools.docker.ui.propertiesViewContributor"; //$NON-NLS-1$
+		// return getSite().getId();
 	}
 	
 	@SuppressWarnings("unchecked")
