51cd0e26dc09d986b7170ca75fbc4bdaa5ca877d
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:28:53 +0000

    First commited as after_64358_rev2

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/docker/core/IDockerConnection.java
@@ -65,7 +65,7 @@ public interface IDockerConnection {
 	/**
 	 * @return the {@link IDockerImageInfo} for the {@link IDockerImage}
 	 *         identified by the given {@code id} or <code>null</code> if none
-	 *         was found.
+	 *         was found or if the underlying client was not initialized
 	 * @param id
 	 *            the {@link IDockerImage} id
 	 */
@@ -157,8 +157,11 @@ public interface IDockerConnection {
 	public void close();
 
 	/**
-	 * @return the {@link IDockerConnectionInfo} associated with this {@link IDockerConnection}
-	 * @throws DockerException if info retrieval failed
+	 * @return the {@link IDockerConnectionInfo} associated with this
+	 *         {@link IDockerConnection} or <code>null</code> if the underlying
+	 *         client was not initialized.
+	 * @throws DockerException
+	 *             if info retrieval failed
 	 */
 	public IDockerConnectionInfo getInfo() throws DockerException;
 
