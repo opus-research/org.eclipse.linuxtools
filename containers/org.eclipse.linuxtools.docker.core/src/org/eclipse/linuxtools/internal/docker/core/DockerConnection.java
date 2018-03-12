1fdac6dedda58291c18d6acb273f2c2843576026
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:38:22 +0000

    First commited as before_68176_rev3

--- containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
+++ containers/org.eclipse.linuxtools.docker.core/src/org/eclipse/linuxtools/internal/docker/core/DockerConnection.java
@@ -757,6 +757,13 @@ public class DockerConnection implements IDockerConnection, Closeable {
 				this.imagesLoaded = true;
 			}
 		}
+		// avoid returning a 'null' list.
+		if (this.images == null) {
+			this.images = Collections.emptyList();
+		}
+		if (latestImages == null) {
+			latestImages = Collections.emptyList();
+		}
 		return latestImages;
 	}
 
@@ -935,8 +942,21 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			final DockerProgressHandler d = new DockerProgressHandler(handler);
 			final java.nio.file.Path p = FileSystems.getDefault()
 					.getPath(path.makeAbsolute().toOSString());
-			return getClientCopy().build(p, d,
+			/*
+			 * Workaround error message thrown to stderr due to
+			 * lack of Guava 18.0. Remove this when we begin
+			 * using Guava 18.0.
+			 */
+			PrintStream oldErr = System.err;
+			System.setErr(new PrintStream(new OutputStream() {
+				@Override
+				public void write(int b) {
+				}
+			}));
+			String res = getClientCopy().build(p, d,
 					BuildParam.create("forcerm", "true")); //$NON-NLS-1$ //$NON-NLS-2$
+			System.setErr(oldErr);
+			return res;
 		} catch (com.spotify.docker.client.DockerRequestException e) {
 			throw new DockerException(e.message());
 		} catch (com.spotify.docker.client.DockerException | IOException e) {
@@ -953,8 +973,21 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			DockerProgressHandler d = new DockerProgressHandler(handler);
 			java.nio.file.Path p = FileSystems.getDefault().getPath(
 					path.makeAbsolute().toOSString());
-			return getClientCopy().build(p, name, d,
+			/*
+			 * Workaround error message thrown to stderr due to
+			 * lack of Guava 18.0. Remove this when we begin
+			 * using Guava 18.0.
+			 */
+			PrintStream oldErr = System.err;
+			System.setErr(new PrintStream(new OutputStream() {
+				@Override
+				public void write(int b) {
+				}
+			}));
+			String res = getClientCopy().build(p, name, d,
 					BuildParam.create("forcerm", "true")); //$NON-NLS-1$ $NON-NLS-2$
+			System.setErr(oldErr);
+			return res;
 		} catch (com.spotify.docker.client.DockerRequestException e) {
 			throw new DockerException(e.message());
 		} catch (com.spotify.docker.client.DockerException | IOException e) {
@@ -989,7 +1022,21 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			final DockerProgressHandler d = new DockerProgressHandler(handler);
 			final java.nio.file.Path p = FileSystems.getDefault()
 					.getPath(path.makeAbsolute().toOSString());
-			return getClientCopy().build(p, name, d, getBuildParameters(buildOptions));
+			/*
+			 * Workaround error message thrown to stderr due to
+			 * lack of Guava 18.0. Remove this when we begin
+			 * using Guava 18.0.
+			 */
+			PrintStream oldErr = System.err;
+			System.setErr(new PrintStream(new OutputStream() {
+				@Override
+				public void write(int b) {
+				}
+			}));
+			String res = getClientCopy().build(p, name, d,
+					getBuildParameters(buildOptions));
+			System.setErr(oldErr);
+			return res;
 		} catch (com.spotify.docker.client.DockerRequestException e) {
 			throw new DockerException(e.message());
 		} catch (com.spotify.docker.client.DockerException | IOException e) {
@@ -1392,6 +1439,7 @@ public class DockerConnection implements IDockerConnection, Closeable {
 			client.commitContainer(id, repo, tag, info.config(), comment,
 					author);
 			// update images list
+			// FIXME: are we refreshing the list of images twice ?
 			listImages();
 			getImages(true);
 		} catch (com.spotify.docker.client.DockerRequestException e) {
