bbea44181ead52feb6bd7f7597c360f573881836
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:53:24 +0000

    First commited as before_36587_rev2

--- lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/ctftestsuite/CtfTestSuiteTests.java
+++ lttng/org.eclipse.linuxtools.ctf.core.tests/src/org/eclipse/linuxtools/ctf/core/tests/ctftestsuite/CtfTestSuiteTests.java
@@ -86,9 +86,7 @@ public class CtfTestSuiteTests {
         addDirsFrom(dirs, BASE_PATH + "stress/metadata/fail", false);
         addDirsFrom(dirs, BASE_PATH + "stress/metadata/pass", true);
         addDirsFrom(dirs, BASE_PATH + "stress/stream/fail", false);
-
-        // TODO Update for the new directory layout
-        //addDirsFrom(dirs, BASE_PATH + "stress/stream/pass", true);
+        addDirsFrom(dirs, BASE_PATH + "stress/stream/pass", true);
 
         return dirs;
     }
