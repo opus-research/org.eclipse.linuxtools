913a3f62fd6705e5f2d709c7b1bd4ad3d0101ffc
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 17:14:32 +0000

    First commited as before_61345_rev1

--- vagrant/org.eclipse.linuxtools.vagrant.core/src/org/eclipse/linuxtools/internal/vagrant/core/VagrantBox.java
+++ vagrant/org.eclipse.linuxtools.vagrant.core/src/org/eclipse/linuxtools/internal/vagrant/core/VagrantBox.java
@@ -42,9 +42,8 @@ public class VagrantBox implements IVagrantBox {
 
 	@Override
 	public String toString() {
-		return "Name: " + this.name //$NON-NLS-1$
-				+ "Provider : " + this.provider //$NON-NLS-1$
-				+ "Version : " + this.version; //$NON-NLS-1$
+		return "Name: " + this.name + "Provider : " + this.provider
+				+ "Version : " + this.version;
 	}
 
 	@Override
