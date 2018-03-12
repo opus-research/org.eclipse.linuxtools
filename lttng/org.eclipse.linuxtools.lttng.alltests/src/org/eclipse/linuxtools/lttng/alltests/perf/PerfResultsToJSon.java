67b44c516c26ef54c883676a692603c461855433
Matheus Paixao <mhepaixao@gmail.com>
2018-03-12 14:30:22 +0000

    First commited as before_33133_rev4

--- lttng/org.eclipse.linuxtools.lttng.alltests/src/org/eclipse/linuxtools/lttng/alltests/perf/PerfResultsToJSon.java
+++ lttng/org.eclipse.linuxtools.lttng.alltests/src/org/eclipse/linuxtools/lttng/alltests/perf/PerfResultsToJSon.java
@@ -10,7 +10,7 @@
  *   Marc-Andre Laperle - Initial API and implementation
  *******************************************************************************/
 
-package org.eclipse.linuxtools.lttng.alltests;
+package org.eclipse.linuxtools.lttng.alltests.perf;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -23,6 +23,7 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.eclipse.linuxtools.lttng.alltests.Activator;
 import org.eclipse.test.internal.performance.PerformanceTestPlugin;
 import org.eclipse.test.internal.performance.data.Dim;
 import org.eclipse.test.internal.performance.db.DB;
@@ -263,8 +264,7 @@ public class PerfResultsToJSon {
         Variations configVariations = PerformanceTestPlugin.getVariations();
         JSONObject osJvmVariants = createOsJvm();
 
-        @SuppressWarnings("rawtypes")
-        Iterator keysIt = osJvmVariants.keys();
+        Iterator<?> keysIt = osJvmVariants.keys();
         while (keysIt.hasNext()) {
             JSONArray overviewSummarySeries = new JSONArray();
 
